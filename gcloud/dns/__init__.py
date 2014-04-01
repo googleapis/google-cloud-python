650df076b0dc2fac8ee943ccfe866bc9567da110
Kevin Leyow <kleyow@gmail.com>
2014-03-31 21:31:27 -0500

    Initialized gcloud dns.

---
+++ gcloud/dns/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1'
+
+# TODO: Allow specific scopes and authorization levels.
+SCOPE = ('https://www.googleapis.com/auth/cloud-platform',
+         'https://www.googleapis.com/auth/ndev.clouddns.readonly',
+         'https://www.googleapis.com/auth/ndev.clouddns.readwrite')
+"""The scope required for authenticating as a Cloud DNS consumer."""
+
+
+def get_connection(project_name, client_email, private_key_path):
+  from gcloud.credentials import Credentials
+  from gcloud.dns.connection import Connection
+
+  credentials = Credentials.get_for_service_account(
+      client_email, private_key_path, scope=SCOPE)
+  return Connection(project_name=project_name, credentials=credentials)
---
+++ gcloud/dns/resource_record_sets.py
@@ -0,0 +1,6 @@
+class ResourceRecordSets(object):
+
+  def __init__(self, connection=None, kind=None, name=None, rrdatas=None,
+               ttl=None, type=None):
+
+    self.connection = connection
