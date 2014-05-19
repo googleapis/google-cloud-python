4dd8f6785d74abc8e5760033d35cfb7550e7f6b5
Ali Afshar <afshar@google.com>
2014-05-19 14:12:32 -0700

    Don't assume that httplib2 responses contain any preset attributes, since they are looked up dynamically from a response headers dict. Fixes #93

--- gcloud/storage/test_connection.py
+++ gcloud/storage/test_connection.py
@@ -1,10 +1,19 @@
 import unittest2
 
 from gcloud.storage.connection import Connection
-
+from gcloud.storage.exceptions import NotFoundError
 
 class TestConnection(unittest2.TestCase):
 
   def test_init(self):
     connection = Connection('project-name')
     self.assertEqual('project-name', connection.project)
+
+
+class TestExceptions(unittest2.TestCase):
+
+  def test_not_found_always_prints(self):
+    e = NotFoundError({}, None)
+    self.assertEqual('', str(e))
+
+
