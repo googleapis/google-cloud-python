14aacdb0f07253de8ef9943a49f2bae507cd9f6c
Danny Hermes <daniel.j.hermes@gmail.com>
2014-09-21 21:43:14 -0700

    Remove TODO from storage.__init__.

--- gcloud/storage/__init__.py
+++ gcloud/storage/__init__.py
@@ -31,7 +31,6 @@ The main concepts with this API are:
 
 __version__ = '0.1'
 
-# TODO: Allow specific scopes and authorization levels.
 SCOPE = ('https://www.googleapis.com/auth/devstorage.full_control',
          'https://www.googleapis.com/auth/devstorage.read_only',
          'https://www.googleapis.com/auth/devstorage.read_write')
