14195eedf42a9e472bc710927a767219b6056fd0
Danny Hermes <daniel.j.hermes@gmail.com>
2014-10-01 13:25:59 -0700

    Updated datastore.entity by hand until fully passed pep8.

--- gcloud/datastore/entity.py
+++ gcloud/datastore/entity.py
@@ -23,46 +23,46 @@ from gcloud.datastore.key import Key
 
 class Entity(dict):
     """
-  :type dataset: :class:`gcloud.datastore.dataset.Dataset`
-  :param dataset: The dataset in which this entity belongs.
+    :type dataset: :class:`gcloud.datastore.dataset.Dataset`
+    :param dataset: The dataset in which this entity belongs.
 
-  :type kind: string
-  :param kind: The kind of entity this is, akin to a table name in a
-               relational database.
+    :type kind: string
+    :param kind: The kind of entity this is, akin to a table name in a
+                 relational database.
 
-  Entities are mutable and act like a subclass of a dictionary.
-  This means you could take an existing entity and change the key
-  to duplicate the object.
+    Entities are mutable and act like a subclass of a dictionary.
+    This means you could take an existing entity and change the key
+    to duplicate the object.
 
-  This can be used on its own, however it is likely easier to use
-  the shortcut methods provided by :class:`gcloud.datastore.dataset.Dataset`
-  such as:
+    This can be used on its own, however it is likely easier to use
+    the shortcut methods provided by :class:`gcloud.datastore.dataset.Dataset`
+    such as:
 
-  - :func:`gcloud.datastore.dataset.Dataset.entity` to create a new entity.
+    - :func:`gcloud.datastore.dataset.Dataset.entity` to create a new entity.
 
-    >>> dataset.entity('MyEntityKind')
-    <Entity[{'kind': 'MyEntityKind'}] {}>
+      >>> dataset.entity('MyEntityKind')
+      <Entity[{'kind': 'MyEntityKind'}] {}>
 
-  - :func:`gcloud.datastore.dataset.Dataset.get_entity`
-    to retrieve an existing entity.
+    - :func:`gcloud.datastore.dataset.Dataset.get_entity`
+      to retrieve an existing entity.
 
-    >>> dataset.get_entity(key)
-    <Entity[{'kind': 'EntityKind', id: 1234}] {'property': 'value'}>
+      >>> dataset.get_entity(key)
+      <Entity[{'kind': 'EntityKind', id: 1234}] {'property': 'value'}>
 
-  You can the set values on the entity
-  just like you would on any other dictionary.
+    You can the set values on the entity
+    just like you would on any other dictionary.
 
-  >>> entity['age'] = 20
-  >>> entity['name'] = 'JJ'
-  >>> entity
-  <Entity[{'kind': 'EntityKind', id: 1234}] {'age': 20, 'name': 'JJ'}>
+    >>> entity['age'] = 20
+    >>> entity['name'] = 'JJ'
+    >>> entity
+    <Entity[{'kind': 'EntityKind', id: 1234}] {'age': 20, 'name': 'JJ'}>
 
-  And you can cast an entity to a regular Python dictionary
-  with the `dict` builtin:
+    And you can cast an entity to a regular Python dictionary
+    with the `dict` builtin:
 
-  >>> dict(entity)
-  {'age': 20, 'name': 'JJ'}
-  """
+    >>> dict(entity)
+    {'age': 20, 'name': 'JJ'}
+    """
 
     def __init__(self, dataset=None, kind=None):
         if dataset and kind:
@@ -73,28 +73,32 @@ class Entity(dict):
     def dataset(self):
         """Get the :class:`gcloud.datastore.dataset.Dataset` in which this entity belongs.
 
-    .. note::
-      This is based on the :class:`gcloud.datastore.key.Key` set on the entity.
-      That means that if you have no key set, the dataset might be `None`.
-      It also means that if you change the key on the entity, this will refer
-      to that key's dataset.
-    """
+        :rtype: :class:`gcloud.datastore.dataset.Dataset`
+        :returns: The Dataset containing the entity if there is a key,
+                  else None.
+
+        .. note::
+          This is based on the :class:`gcloud.datastore.key.Key` set on the
+          entity. That means that if you have no key set, the dataset might
+          be `None`. It also means that if you change the key on the entity,
+          this will refer to that key's dataset.
+        """
         if self.key():
             return self.key().dataset()
 
     def key(self, key=None):
         """Get or set the :class:`gcloud.datastore.key.Key` on the current entity.
 
-    :type key: :class:`glcouddatastore.key.Key`
-    :param key: The key you want to set on the entity.
+        :type key: :class:`glcouddatastore.key.Key`
+        :param key: The key you want to set on the entity.
 
-    :returns: Either the current key or the :class:`Entity`.
+        :returns: Either the current key or the :class:`Entity`.
 
-    >>> entity.key(my_other_key)  # This returns the original entity.
-    <Entity[{'kind': 'OtherKeyKind', 'id': 1234}] {'property': 'value'}>
-    >>> entity.key()  # This returns the key.
-    <Key[{'kind': 'OtherKeyKind', 'id': 1234}]>
-    """
+        >>> entity.key(my_other_key)  # This returns the original entity.
+        <Entity[{'kind': 'OtherKeyKind', 'id': 1234}] {'property': 'value'}>
+        >>> entity.key()  # This returns the key.
+        <Key[{'kind': 'OtherKeyKind', 'id': 1234}]>
+        """
 
         if key:
             self._key = key
@@ -105,14 +109,14 @@ class Entity(dict):
     def kind(self):
         """Get the kind of the current entity.
 
-    .. note::
-      This relies entirely on
-      the :class:`gcloud.datastore.key.Key`
-      set on the entity.
-      That means that we're not storing the kind of the entity at all,
-      just the properties and a pointer to a Key
-      which knows its Kind.
-    """
+        .. note::
+          This relies entirely on
+          the :class:`gcloud.datastore.key.Key`
+          set on the entity.
+          That means that we're not storing the kind of the entity at all,
+          just the properties and a pointer to a Key
+          which knows its Kind.
+        """
 
         if self.key():
             return self.key().kind()
@@ -121,12 +125,12 @@ class Entity(dict):
     def from_key(cls, key):
         """Factory method for creating an entity based on the :class:`gcloud.datastore.key.Key`.
 
-    :type key: :class:`gcloud.datastore.key.Key`
-    :param key: The key for the entity.
+        :type key: :class:`gcloud.datastore.key.Key`
+        :param key: The key for the entity.
 
-    :returns: The :class:`Entity` derived from the
-              :class:`gcloud.datastore.key.Key`.
-    """
+        :returns: The :class:`Entity` derived from the
+                  :class:`gcloud.datastore.key.Key`.
+        """
 
         return cls().key(key)
 
@@ -134,14 +138,15 @@ class Entity(dict):
     def from_protobuf(cls, pb, dataset=None):
         """Factory method for creating an entity based on a protobuf.
 
-    The protobuf should be one returned from the Cloud Datastore Protobuf API.
+        The protobuf should be one returned from the Cloud Datastore
+        Protobuf API.
 
-    :type key: :class:`gcloud.datastore.datastore_v1_pb2.Entity`
-    :param key: The Protobuf representing the entity.
+        :type key: :class:`gcloud.datastore.datastore_v1_pb2.Entity`
+        :param key: The Protobuf representing the entity.
 
-    :returns: The :class:`Entity` derived from the
-              :class:`gcloud.datastore.datastore_v1_pb2.Entity`.
-    """
+        :returns: The :class:`Entity` derived from the
+                  :class:`gcloud.datastore.datastore_v1_pb2.Entity`.
+        """
 
         # This is here to avoid circular imports.
         from gcloud.datastore import helpers
@@ -158,15 +163,15 @@ class Entity(dict):
     def reload(self):
         """Reloads the contents of this entity from the datastore.
 
-    This method takes the :class:`gcloud.datastore.key.Key`, loads all
-    properties from the Cloud Datastore, and sets the updated properties on
-    the current object.
+        This method takes the :class:`gcloud.datastore.key.Key`, loads all
+        properties from the Cloud Datastore, and sets the updated properties on
+        the current object.
 
-    .. warning::
-      This will override any existing properties if a different value exists
-      remotely, however it will *not* override any properties that exist
-      only locally.
-    """
+        .. warning::
+          This will override any existing properties if a different value
+          exists remotely, however it will *not* override any properties that
+          exist only locally.
+        """
 
         # Note that you must have a valid key, otherwise this makes no sense.
         entity = self.dataset().get_entity(self.key().to_protobuf())
@@ -178,10 +183,12 @@ class Entity(dict):
     def save(self):
         """Save the entity in the Cloud Datastore.
 
-    :rtype: :class:`gcloud.datastore.entity.Entity`
-    :returns: The entity with a possibly updated Key.
-    """
-        key_pb = self.dataset().connection().save_entity(dataset_id=self.dataset().id(), key_pb=self.key().to_protobuf(), properties=dict(self))
+        :rtype: :class:`gcloud.datastore.entity.Entity`
+        :returns: The entity with a possibly updated Key.
+        """
+        key_pb = self.dataset().connection().save_entity(
+            dataset_id=self.dataset().id(),
+            key_pb=self.key().to_protobuf(), properties=dict(self))
 
         # If we are in a transaction and the current entity needs an
         # automatically assigned ID, tell the transaction where to put that.
@@ -200,16 +207,18 @@ class Entity(dict):
     def delete(self):
         """Delete the entity in the Cloud Datastore.
 
-    .. note::
-      This is based entirely off of the :class:`gcloud.datastore.key.Key` set
-      on the entity. Whatever is stored remotely using the key on the entity
-      will be deleted.
-    """
-        self.dataset().connection().delete_entity(dataset_id=self.dataset().id(), key_pb=self.key().to_protobuf())
+        .. note::
+          This is based entirely off of the :class:`gcloud.datastore.key.Key`
+          set on the entity. Whatever is stored remotely using the key on the
+          entity will be deleted.
+        """
+        self.dataset().connection().delete_entity(
+            dataset_id=self.dataset().id(), key_pb=self.key().to_protobuf())
 
     def __repr__(self):  # pragma NO COVER
         # An entity should have a key all the time (even if it's partial).
         if self.key():
-            return '<Entity%s %s>' % (self.key().path(), super(Entity, self).__repr__())
+            return '<Entity%s %s>' % (self.key().path(),
+                                      super(Entity, self).__repr__())
         else:
             return '<Entity %s>' % (super(Entity, self).__repr__())
