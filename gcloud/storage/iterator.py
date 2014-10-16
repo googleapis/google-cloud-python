9583fbe870106bd744f910d35f536c6b265877ab
Tres Seaver <tseaver@palladion.com>
2014-10-16 14:19:27 -0400

    Don't suppress coverage for gcloud.storage.iterator.Iterator.get_items_from_response.

--- gcloud/storage/iterator.py
+++ gcloud/storage/iterator.py
@@ -112,7 +112,7 @@ class Iterator(object):
         self.page_number = 0
         self.next_page_token = None
 
-    def get_items_from_response(self, response):  # pragma NO COVER
+    def get_items_from_response(self, response):
         """Factory method called while iterating. This should be overriden.
 
         This method should be overridden by a subclass.
