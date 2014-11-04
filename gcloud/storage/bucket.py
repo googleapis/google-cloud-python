02dcded9189796cceaf46ffd53cdf57ee1f00d2c
Tres Seaver <tseaver@palladion.com>
2014-11-04 15:01:03 -0500

    Add support for the Bucket's 'logging' field.
    See: https://cloud.google.com/storage/docs/accesslogs
    
    Addresses 'logging' part of #314.

--- gcloud/storage/bucket.py
+++ gcloud/storage/bucket.py
@@ -24,6 +24,7 @@ class Bucket(_MetadataMixin):
     CUSTOM_METADATA_FIELDS = {
         'acl': 'get_acl',
         'defaultObjectAcl': 'get_default_object_acl',
+        'logging': 'get_logging',
     }
     """Mapping of field name -> accessor for fields w/ custom accessors."""
 
@@ -440,6 +441,45 @@ class Bucket(_MetadataMixin):
             for key in self:
                 key.get_acl().all().grant_read()
                 key.save_acl()
+
+    def get_logging(self):
+        """Return info about access logging for this bucket.
+
+        See: https://cloud.google.com/storage/docs/accesslogs#status
+
+        :rtype: dict or None
+        :returns: a dict w/ keys, ``bucket_name`` and ``object_prefix``
+                  (if logging is enabled), or None (if not).
+        """
+        if not self.has_metadata('logging'):
+            self.reload_metadata()
+        info = self.metadata.get('logging')
+        if info is not None:
+            info = info.copy()
+            info['bucket_name'] = info.pop('logBucket')
+            info['object_prefix'] = info.pop('logObjectPrefix', '')
+        return info
+
+    def enable_logging(self, bucket_name, object_prefix=''):
+        """Enable access logging for this bucket.
+
+        See: https://cloud.google.com/storage/docs/accesslogs#delivery
+
+        :type bucket_name: string
+        :param bucket_name: name of bucket in which to store access logs
+
+        :type object_prefix: string
+        :param object_prefix: prefix for access log filenames
+        """
+        info = {'logBucket': bucket_name, 'logObjectPrefix': object_prefix}
+        self.patch_metadata({'logging': info})
+
+    def disable_logging(self):
+        """Disable access logging for this bucket.
+
+        See: https://cloud.google.com/storage/docs/accesslogs#disabling
+        """
+        self.patch_metadata({'logging': None})
 
 
 class BucketIterator(Iterator):
