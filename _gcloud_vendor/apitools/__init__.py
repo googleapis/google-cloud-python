de2122c081269a75c4502a4aceb52818dadcb39a
Tres Seaver <tseaver@palladion.com>
2014-11-25 11:48:45 -0500

    Issue #334, plan B:  'vendor-in' apitools subset.
    - '__init__.py' files are stubs, w/o namespace support.  Deleted package-level
      'from <foo> import *' convenience imports.
    
    - Non-'__init__.py' files pulled from:
      https://github.com/craigcitro/apitools/commit/e5a5c36e24926310712d20b93b4cdd02424a81f5
    
    - Absolute imports converted to explicit relative.

---
+++ _gcloud_vendor/apitools/__init__.py
@@ -0,0 +1,5 @@
+"""Stub, *non*-namespace package initializer.
+
+We do not do the 'namespace' dance here, as we will not be imported at
+all if ``apitools`` can be imported directly.
+"""
---
+++ _gcloud_vendor/apitools/base/__init__.py
@@ -0,0 +1,2 @@
+"""Stub, package initializer."""
+
