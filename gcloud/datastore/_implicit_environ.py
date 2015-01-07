cea9371177dcb04f0869b6a26b58ca86b5c7ccb7
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-07 09:45:32 -0800

    Removing unused _implicit_environ._DatastoreBase class.

--- gcloud/datastore/_implicit_environ.py
+++ gcloud/datastore/_implicit_environ.py
@@ -2,9 +2,6 @@
 
 Acts as a mutable namespace to allow the datastore package to
 imply the current dataset and connection from the enviroment.
-
-Also provides a base class for classes in the `datastore` package
-which could utilize the implicit enviroment.
 """
 
 
@@ -16,15 +13,3 @@ DATASET = None
 
 CONNECTION = None
 """Module global to allow persistent implied connection from enviroment."""
-
-
-class _DatastoreBase(object):
-    """Base for all classes in the datastore package.
-
-    Uses the implicit DATASET object as a default dataset attached
-    to the instances being created. Stores the dataset passed in
-    on the protected (i.e. non-public) attribute `_dataset`.
-    """
-
-    def __init__(self, dataset=None):
-        self._dataset = dataset or DATASET
