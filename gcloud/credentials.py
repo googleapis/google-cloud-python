104f5727a18f813ffcf4d24640a5dab0cb7c1949
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-13 23:08:52 -0800

    Using bSphinx ulleted list in credentials docstring.

--- gcloud/credentials.py
+++ gcloud/credentials.py
@@ -26,10 +26,12 @@ def get_credentials():
       which uses this method under the hood.
 
     Checks environment in order of precedence:
-    - Google App Engine (production and testing)
-    - Env. var. GOOGLE_APPLICATION_CREDENTIALS pointing to stored credentials.
-    - Stored "well known" file associated with ``gcloud`` command line tool.
-    - Google Compute Engine production environment.
+
+    * Google App Engine (production and testing)
+    * Environment variable GOOGLE_APPLICATION_CREDENTIALS pointing to
+      a file with stored credentials information.
+    * Stored "well known" file associated with ``gcloud`` command line tool.
+    * Google Compute Engine production environment.
 
     The file referred to in GOOGLE_APPLICATION_CREDENTIALS is expected to
     contain information about credentials that are ready to use. This means
