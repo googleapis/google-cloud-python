c3286a2a94f42f63ae908f7b347f14edbd07303a
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-27 14:19:37 -0800

    Adding implicit dataset ID support for Compute Engine.
    Fixes #475.

--- gcloud/datastore/_implicit_environ.py
+++ gcloud/datastore/_implicit_environ.py
@@ -18,6 +18,9 @@ Acts as a mutable namespace to allow the datastore package to
 imply the current dataset ID and connection from the enviroment.
 """
 
+import httplib2
+import socket
+
 try:
     from google.appengine.api import app_identity
 except ImportError:
@@ -42,3 +45,36 @@ def app_engine_id():
         return None
 
     return app_identity.get_application_id()
+
+
+def compute_engine_id():
+    """Gets the Compute Engine project ID if it can be inferred.
+
+    Uses 169.254.169.254 for the metadata server to avoid request
+    latency from DNS lookup.
+
+    See https://cloud.google.com/compute/docs/metadata#metadataserver
+    for information about this IP address. (This IP is also used for
+    Amazon EC2 instances, so the metadata flavor is crucial.)
+
+    See https://github.com/google/oauth2client/issues/93 for context about
+    DNS latency.
+
+    :rtype: string or ``NoneType``
+    :returns: Compute Engine project ID if the metadata service is available,
+              else ``None``.
+    """
+    http = httplib2.Http(timeout=0.1)
+    uri = 'http://169.254.169.254/computeMetadata/v1/project/project-id'
+    headers = {'Metadata-Flavor': 'Google'}
+
+    response = content = None
+    try:
+        response, content = http.request(uri, method='GET', headers=headers)
+    except socket.timeout:
+        pass
+
+    if response is None or response['status'] != '200':
+        return None
+
+    return content
