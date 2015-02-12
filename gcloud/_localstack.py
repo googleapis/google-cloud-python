83ed43763efb0f4af5234e6aac01af8c58f17911
Tres Seaver <tseaver@palladion.com>
2015-02-12 16:27:36 -0500

    Add module docstring.

--- gcloud/_localstack.py
+++ gcloud/_localstack.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Thread-local resource stack.
+
+Not an API.
+"""
 
 try:
     from threading import local as Local
