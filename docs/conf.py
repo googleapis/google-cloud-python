7deb85b575bf27ea518d7bcad953b698334741bc
Tres Seaver <tseaver@palladion.com>
2015-03-09 16:30:48 -0400

    Sketch 'gcloud.pubsub' API.

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
 ]
