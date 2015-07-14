e0676355fa796d1b77a60639630530e97e8518fd
Danny Hermes <daniel.j.hermes@gmail.com>
2015-07-14 13:17:30 -0700

    Upgrading pubsub to v1.
    Fixes #957.

--- gcloud/pubsub/connection.py
+++ gcloud/pubsub/connection.py
@@ -28,7 +28,7 @@ class Connection(base_connection.JSONConnection):
     API_BASE_URL = 'https://pubsub.googleapis.com'
     """The base of the API call URL."""
 
-    API_VERSION = 'v1beta2'
+    API_VERSION = 'v1'
     """The version of the API, used in building the API call's URL."""
 
     API_URL_TEMPLATE = '{api_base_url}/{api_version}{path}'
