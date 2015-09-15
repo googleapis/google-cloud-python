bf3318866717dda358458ab59ea07db281a1fb57
Tres Seaver <tseaver@palladion.com>
2015-09-15 13:56:42 -0400

    Normalize speling.
    Addresses:
    https://github.com/GoogleCloudPlatform/gcloud-python/pull/1139#discussion_r39535543
    https://github.com/GoogleCloudPlatform/gcloud-python/pull/1139#discussion_r39535570

--- gcloud/bigquery/connection.py
+++ gcloud/bigquery/connection.py
@@ -18,7 +18,7 @@ from gcloud import connection as base_connection
 
 
 class Connection(base_connection.JSONConnection):
-    """A connection to Google Cloud Bigquery via the JSON REST API."""
+    """A connection to Google Cloud BigQuery via the JSON REST API."""
 
     API_BASE_URL = 'https://www.googleapis.com'
     """The base of the API call URL."""
