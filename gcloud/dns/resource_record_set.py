faee1ece3b80f9abeb53f54a7cbe4b9c6a673b4d
Tres Seaver <tseaver@palladion.com>
2015-09-21 13:42:36 -0400

    Docstring typos.

--- gcloud/dns/resource_record_set.py
+++ gcloud/dns/resource_record_set.py
@@ -48,16 +48,15 @@ class ResourceRecordSet(object):
 
     @classmethod
     def from_api_repr(cls, resource, zone):
-        """Factory:  construct a zone given its API representation
+        """Factory:  construct a record set given its API representation
 
         :type resource: dict
-        :param resource: zone resource representation returned from the API
-
+        :param resource: record sets representation returned from the API
 
         :type zone: :class:`gcloud.dns.zone.ManagedZone`
         :param zone: A zone which holds one or more record sets.
 
-        :rtype: :class:`gcloud.dns.zone.ResourceRecordSets`
+        :rtype: :class:`gcloud.dns.zone.ResourceRecordSet`
         :returns: RRS parsed from ``resource``.
         """
         name = resource['name']
