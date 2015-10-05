f4d3fb6b1ba40449e1a567551338caf98b5f230d
Tres Seaver <tseaver@palladion.com>
2015-10-05 14:31:05 -0400

    Fix copy-pasta:  'dns' -> 'search'.

--- gcloud/search/client.py
+++ gcloud/search/client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""gcloud dns client for interacting with API."""
+"""gcloud search client for interacting with API."""
 
 
 from gcloud.client import JSONClient
