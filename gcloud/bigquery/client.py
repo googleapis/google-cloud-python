13d627f04602cc7db22322111db77972f30cb3b2
Tres Seaver <tseaver@palladion.com>
2015-10-05 14:59:05 -0400

    Correct branding for BQ.
    Addresses:
    https://github.com/GoogleCloudPlatform/gcloud-python/pull/1172#discussion_r41181578

--- gcloud/bigquery/client.py
+++ gcloud/bigquery/client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Client for interacting with the Google Cloud BigQuery API."""
+"""Client for interacting with the Google BigQuery API."""
 
 
 from gcloud.client import JSONClient
