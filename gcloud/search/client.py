8d86dbf35719e0625f8748c97e97c519271790ce
Tres Seaver <tseaver@palladion.com>
2015-10-06 00:23:12 -0400

    Sync module docstring with #1172.
    Addresses:
    https://github.com/GoogleCloudPlatform/gcloud-python/pull/1164#discussion_r41217883

--- gcloud/search/client.py
+++ gcloud/search/client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""gcloud search client for interacting with API."""
+"""Client for interacting with the Google Cloud search API."""
 
 
 from gcloud.client import JSONClient
