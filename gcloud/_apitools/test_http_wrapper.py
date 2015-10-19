14e7dd5c71ca23e9d5e693d3a8a449d6352551df
Tres Seaver <tseaver@palladion.com>
2015-10-19 14:28:32 -0400

    Coverage for 'gcloud._apitools.http_wrapper.Request'.

--- gcloud/_apitools/test_http_wrapper.py
+++ gcloud/_apitools/test_http_wrapper.py
@@ -72,6 +72,46 @@ class Test__Httplib2Debuglevel(unittest2.TestCase):
         self.assertEqual(skip_me.debuglevel, 0)
 
 
+class Test_Request(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud._apitools.http_wrapper import Request
+        return Request
+
+    def _makeOne(self, *args, **kw):
+        return self._getTargetClass()(*args, **kw)
+
+    def test_ctor_defaults(self):
+        request = self._makeOne()
+        self.assertEqual(request.url, '')
+        self.assertEqual(request.http_method, 'GET')
+        self.assertEqual(request.headers, {'content-length': '0'})
+        self.assertEqual(request.body, '')
+        self.assertEqual(request.loggable_body, None)
+
+    def test_loggable_body_setter_w_body_None(self):
+        from gcloud._apitools.exceptions import RequestError
+        request = self._makeOne(body=None)
+        with self.assertRaises(RequestError):
+            request.loggable_body = 'abc'
+
+    def test_body_setter_w_None(self):
+        request = self._makeOne()
+        request.loggable_body = 'abc'
+        request.body = None
+        self.assertEqual(request.headers, {})
+        self.assertEqual(request.body, None)
+        self.assertEqual(request.loggable_body, 'abc')
+
+    def test_body_setter_w_non_string(self):
+        request = self._makeOne()
+        request.loggable_body = 'abc'
+        request.body = body = _Dummy(length=123)
+        self.assertEqual(request.headers, {'content-length': '123'})
+        self.assertTrue(request.body is body)
+        self.assertEqual(request.loggable_body, '<media body>')
+
+
 class _Dummy(object):
     def __init__(self, **kw):
         self.__dict__.update(kw)
