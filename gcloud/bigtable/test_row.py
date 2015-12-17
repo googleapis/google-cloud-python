05ed5b740a1c58da4f5c91c8e777f0cbb93b635d
Danny Hermes <daniel.j.hermes@gmail.com>
2015-12-17 11:00:21 -0800

    Adding Bigtable row filters for integer offset/limits.

--- gcloud/bigtable/test_row.py
+++ gcloud/bigtable/test_row.py
@@ -156,3 +156,95 @@ class TestValueRegexFilter(unittest2.TestCase):
         pb_val = row_filter.to_pb()
         expected_pb = data_pb2.RowFilter(value_regex_filter=regex)
         self.assertEqual(pb_val, expected_pb)
+
+
+class Test_CellCountFilter(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.bigtable.row import _CellCountFilter
+        return _CellCountFilter
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTargetClass()(*args, **kwargs)
+
+    def test_constructor(self):
+        num_cells = object()
+        row_filter = self._makeOne(num_cells)
+        self.assertTrue(row_filter.num_cells is num_cells)
+
+    def test___eq__type_differ(self):
+        num_cells = object()
+        row_filter1 = self._makeOne(num_cells=num_cells)
+        row_filter2 = object()
+        self.assertNotEqual(row_filter1, row_filter2)
+
+    def test___eq__same_value(self):
+        num_cells = object()
+        row_filter1 = self._makeOne(num_cells=num_cells)
+        row_filter2 = self._makeOne(num_cells=num_cells)
+        self.assertEqual(row_filter1, row_filter2)
+
+    def test___ne__same_value(self):
+        num_cells = object()
+        row_filter1 = self._makeOne(num_cells=num_cells)
+        row_filter2 = self._makeOne(num_cells=num_cells)
+        comparison_val = (row_filter1 != row_filter2)
+        self.assertFalse(comparison_val)
+
+
+class TestCellsRowOffsetFilter(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.bigtable.row import CellsRowOffsetFilter
+        return CellsRowOffsetFilter
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTargetClass()(*args, **kwargs)
+
+    def test_to_pb(self):
+        from gcloud.bigtable._generated import bigtable_data_pb2 as data_pb2
+
+        num_cells = 76
+        row_filter = self._makeOne(num_cells)
+        pb_val = row_filter.to_pb()
+        expected_pb = data_pb2.RowFilter(cells_per_row_offset_filter=num_cells)
+        self.assertEqual(pb_val, expected_pb)
+
+
+class TestCellsRowLimitFilter(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.bigtable.row import CellsRowLimitFilter
+        return CellsRowLimitFilter
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTargetClass()(*args, **kwargs)
+
+    def test_to_pb(self):
+        from gcloud.bigtable._generated import bigtable_data_pb2 as data_pb2
+
+        num_cells = 189
+        row_filter = self._makeOne(num_cells)
+        pb_val = row_filter.to_pb()
+        expected_pb = data_pb2.RowFilter(cells_per_row_limit_filter=num_cells)
+        self.assertEqual(pb_val, expected_pb)
+
+
+class TestCellsColumnLimitFilter(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.bigtable.row import CellsColumnLimitFilter
+        return CellsColumnLimitFilter
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTargetClass()(*args, **kwargs)
+
+    def test_to_pb(self):
+        from gcloud.bigtable._generated import bigtable_data_pb2 as data_pb2
+
+        num_cells = 10
+        row_filter = self._makeOne(num_cells)
+        pb_val = row_filter.to_pb()
+        expected_pb = data_pb2.RowFilter(
+            cells_per_column_limit_filter=num_cells)
+        self.assertEqual(pb_val, expected_pb)
