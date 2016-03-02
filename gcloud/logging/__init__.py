a96ff79c7957611009a21eb2a61f6c17e72b3b2b
Tres Seaver <tseaver@palladion.com>
2016-03-02 17:53:20 -0500

    Clean up copy-pasta:
    - Copyright year -> 2016.
    - Pubsub -> Logging.
    - Other formatting issues from @dhermes' review.

--- gcloud/logging/__init__.py
+++ gcloud/logging/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All rights reserved.
+# Copyright 2016 Google Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Google Cloud Logging API wrapper.
-"""
+"""Google Cloud Logging API wrapper."""
 
 from gcloud.logging.client import Client
 from gcloud.logging.connection import Connection
