44f95583b062cea5a90d56f2557e441562760b9a
Tres Seaver <tseaver@palladion.com>
2016-03-03 11:24:48 -0500

    Fix docs coverage for logging.

--- scripts/verify_included_modules.py
+++ scripts/verify_included_modules.py
@@ -36,6 +36,7 @@ IGNORED_MODULES = frozenset([
     'gcloud.datastore.__init__',
     'gcloud.dns.__init__',
     'gcloud.iterator',
+    'gcloud.logging.__init__',
     'gcloud.pubsub.__init__',
     'gcloud.resource_manager.__init__',
     'gcloud.search.__init__',
