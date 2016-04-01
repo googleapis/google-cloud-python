e91d92e6ec5db4b58c251c2d50b86f3d35839390
Danny Hermes <daniel.j.hermes@gmail.com>
2016-04-01 11:09:28 -0700

    Upgrading version to 0.12.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ GRPC_EXTRAS = ['grpcio >= 0.13.0']
 
 setup(
     name='gcloud',
-    version='0.11.0',
+    version='0.12.0',
     description='API Client library for Google Cloud',
     author='Google Cloud Platform',
     author_email='jjg+gcloud-python@google.com',
