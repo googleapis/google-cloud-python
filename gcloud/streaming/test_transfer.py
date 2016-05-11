ceb1c07a9237d6919f1591e13cbfcdee822eb904
Tres Seaver <tseaver@palladion.com>
2016-05-11 14:26:50 -0400

    Remove inline disabling of 'no-name-in-module'
    On 'master', it is now disabled globally.

--- gcloud/streaming/test_transfer.py
+++ gcloud/streaming/test_transfer.py
@@ -1787,9 +1787,7 @@ class Test_Upload(unittest2.TestCase):
 def _email_chunk_parser():
     import six
     if six.PY3:  # pragma: NO COVER  Python3
-        # pylint: disable=no-name-in-module
         from email.parser import BytesParser
-        # pylint: enable=no-name-in-module
         parser = BytesParser()
         return parser.parsebytes
     else:
