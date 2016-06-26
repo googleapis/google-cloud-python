ebf27b58ce063e5ad3b3c029e99a1799d3bf13d6
Tres Seaver <tseaver@palladion.com>
2016-06-25 20:40:23 -0400

    Add 'ReadRowsResponseV2' wrapper.
    Processes the 'ReadRowsResponse.CellChunk' state  machine.
    
    Add tests based on JSON acceptance tests:
    https://github.com/GoogleCloudPlatform/cloud-bigtable-client/blob/master/bigtable-client-core/src/test/resources/com/google/cloud/bigtable/grpc/scanner/v2/read-rows-acceptance-test.json.

--- gcloud/bigtable/row_data.py
+++ gcloud/bigtable/row_data.py
@@ -321,3 +321,255 @@ class PartialRowsData(object):
                 self.consume_next()
             except StopIteration:
                 break
+
+
+class ReadRowsResponseError(RuntimeError):
+    """Exception raised to to invalid chunk / response data from back-end."""
+
+
+def _raise_if(predicate):
+    """Helper for validation methods."""
+    if predicate:
+        raise ReadRowsResponseError()
+
+
+class PartialCellV2(object):
+    """Data for a not-yet-complete cell."""
+
+    def __init__(self, row_key, family_name, qualifier, timestamp_micros,
+                 labels=(), value=b''):
+        self.row_key = row_key
+        self.family_name = family_name
+        self.qualifier = qualifier
+        self.timestamp_micros = timestamp_micros
+        self.labels = labels
+        self.value = value
+
+    def append_value(self, value):
+        """Append bytes from a new chunk to value.
+
+        :type value: bytes
+        :param value: bytes to append
+        """
+        self.value += value
+
+
+class PartialRowsDataV2(object):
+    """Handle state involved in consuming a ``ReadRows`` streaming response.
+
+    :type response_iterator:
+        :class:`grpc.framework.alpha._reexport._CancellableIterator` returning
+        :class:`gcloud.bigtable._generated_v2.bigtable_pb2.ReadRowsResponse`
+    :param response_iterator:
+        A streaming iterator returned from a ``ReadRows`` request.
+    """
+    # State names
+    START = "Start"
+    NEW_ROW = "New row"
+    ROW_IN_PROGRESS = "Row in progress"
+    CELL_IN_PROGRESS = "Cell in progress"
+
+    def __init__(self, response_iterator):
+        self._response_iterator = response_iterator
+        # Fully-processed rows, keyed by `row_key`
+        self._rows = {}
+        # Counter for responses pulled from iterator
+        self._counter = 0
+        # Maybe cached from previous response
+        self._last_scanned_row_key = None
+        # In-progress row, unset until first response, after commit/reset
+        self._row = None
+        # Last complete row, unset until first commit
+        self._previous_row = None
+        # In-progress cell, unset until first response, after completion
+        self._cell = None
+        # Last complete cell, unset until first completion, after new row
+        self._previous_cell = None
+
+    @property
+    def state(self):
+        """Name of state machine state."""
+        if self._last_scanned_row_key is None:
+            return self.START
+        if self._row is None:
+            assert self._cell is None
+            assert self._previous_cell is None
+            return self.NEW_ROW
+        if self._cell is not None:
+            return self.CELL_IN_PROGRESS
+        if self._previous_cell is not None:
+            return self.ROW_IN_PROGRESS
+        return self.NEW_ROW  # row added, no chunk yet processed
+
+    @property
+    def rows(self):
+        """Property returning all rows accumulated from the stream.
+
+        :rtype: dict
+        :returns: Dictionary of :class:`PartialRowData`.
+        """
+        _raise_if(self.state not in (self.NEW_ROW,))
+        # NOTE: To avoid duplicating large objects, this is just the
+        #       mutable private data.
+        return self._rows
+
+    @staticmethod
+    def _validate_chunk_status(chunk):
+        """Helper for :meth:`_validate_chunk_row_in_progress`, etc."""
+        # No reseet with other keys
+        if chunk.reset_row:
+            _raise_if(chunk.row_key)
+            _raise_if(chunk.HasField('family_name'))
+            _raise_if(chunk.HasField('qualifier'))
+            _raise_if(chunk.timestamp_micros)
+            _raise_if(chunk.labels)
+            _raise_if(chunk.value_size)
+            _raise_if(chunk.value)
+        # No commit with value size
+        _raise_if(chunk.commit_row and chunk.value_size > 0)
+        # No negative value_size (inferred as a general constraint).
+        _raise_if(chunk.value_size < 0)
+
+    def _validate_chunk_new_row(self, chunk):
+        """Helper for :meth:`_validate_chunk`."""
+        assert self.state == self.NEW_ROW
+        _raise_if(chunk.reset_row)
+        _raise_if(not chunk.row_key)
+        _raise_if(not chunk.family_name)
+        _raise_if(not chunk.qualifier)
+        # This constraint is not enforced in the Go example.
+        _raise_if(chunk.value_size > 0 and chunk.commit_row is not False)
+        # This constraint is from the Go example, not the spec.
+        _raise_if(self._previous_row is not None and
+                  chunk.row_key <= self._previous_row.row_key)
+
+    def _same_as_previous(self, chunk):
+        """Helper for :meth:`_validate_chunk_row_in_progress`"""
+        previous = self._previous_cell
+        return (chunk.row_key == previous.row_key and
+                chunk.family_name == previous.family_name and
+                chunk.qualifier == previous.qualifier and
+                chunk.labels == previous.labels)
+
+    def _validate_chunk_row_in_progress(self, chunk):
+        """Helper for :meth:`_validate_chunk`"""
+        assert self.state == self.ROW_IN_PROGRESS
+        self._validate_chunk_status(chunk)
+        if not chunk.HasField('commit_row') and not chunk.reset_row:
+            _raise_if(not chunk.timestamp_micros or not chunk.value)
+        _raise_if(chunk.row_key and
+                  chunk.row_key != self._row.row_key)
+        _raise_if(chunk.HasField('family_name') and
+                  not chunk.HasField('qualifier'))
+        previous = self._previous_cell
+        _raise_if(self._same_as_previous(chunk) and
+                  chunk.timestamp_micros <= previous.timestamp_micros)
+
+    def _validate_chunk_cell_in_progress(self, chunk):
+        """Helper for :meth:`_validate_chunk`"""
+        assert self.state == self.CELL_IN_PROGRESS
+        self._validate_chunk_status(chunk)
+        self._copy_from_current(chunk)
+
+    def _validate_chunk(self, chunk):
+        """Helper for :meth:`consume_next`."""
+        if self.state == self.NEW_ROW:
+            self._validate_chunk_new_row(chunk)
+        if self.state == self.ROW_IN_PROGRESS:
+            self._validate_chunk_row_in_progress(chunk)
+        if self.state == self.CELL_IN_PROGRESS:
+            self._validate_chunk_cell_in_progress(chunk)
+
+    def _save_current_cell(self):
+        """Helper for :meth:`consume_next`."""
+        row, cell = self._row, self._cell
+        family = row._cells.setdefault(cell.family_name, {})
+        qualified = family.setdefault(cell.qualifier, [])
+        complete = Cell.from_pb(self._cell)
+        qualified.append(complete)
+        self._cell, self._previous_cell = None, cell
+
+    def _copy_from_current(self, chunk):
+        """Helper for :meth:`consume_next`."""
+        current = self._cell
+        if current is not None:
+            if not chunk.row_key:
+                chunk.row_key = current.row_key
+            if not chunk.HasField('family_name'):
+                chunk.family_name.value = current.family_name
+            if not chunk.HasField('qualifier'):
+                chunk.qualifier.value = current.qualifier
+            if not chunk.timestamp_micros:
+                chunk.timestamp_micros = current.timestamp_micros
+            if not chunk.labels:
+                chunk.labels.extend(current.labels)
+
+    def _copy_from_previous(self, cell):
+        """Helper for :meth:`consume_next`."""
+        previous = self._previous_cell
+        if previous is not None:
+            if not cell.row_key:
+                cell.row_key = previous.row_key
+            if not cell.family_name:
+                cell.family_name = previous.family_name
+            if not cell.qualifier:
+                cell.qualifier = previous.qualifier
+
+    def _save_current_row(self):
+        """Helper for :meth:`consume_next`."""
+        if self._cell:
+            self._save_current_cell()
+        self._rows[self._row.row_key] = self._row
+        self._row, self._previous_row = None, self._row
+        self._previous_cell = None
+
+    def consume_next(self):
+        """Consume the next ``ReadRowsResponse`` from the stream.
+
+        Parse the response and its chunks into a new/existing row in
+        :attr:`_rows`
+        """
+        response = self._response_iterator.next()
+        self._counter += 1
+
+        if self._last_scanned_row_key is None:  # first response
+            if response.last_scanned_row_key:
+                raise ReadRowsResponseError()
+
+        self._last_scanned_row_key = response.last_scanned_row_key
+
+        row = self._row
+        cell = self._cell
+
+        for chunk in response.chunks:
+
+            self._validate_chunk(chunk)
+
+            if chunk.reset_row:
+                row = self._row = None
+                cell = self._cell = self._previous_cell = None
+                continue
+
+            if row is None:
+                row = self._row = PartialRowData(chunk.row_key)
+
+            if cell is None:
+                cell = self._cell = PartialCellV2(
+                    chunk.row_key,
+                    chunk.family_name.value,
+                    chunk.qualifier.value,
+                    chunk.timestamp_micros,
+                    chunk.labels,
+                    chunk.value)
+                self._copy_from_previous(cell)
+            else:
+                cell.append_value(chunk.value)
+
+            if chunk.commit_row:
+                self._save_current_row()
+                row = cell = None
+                continue
+
+            if chunk.value_size == 0:
+                self._save_current_cell()
+                cell = None
