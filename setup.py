1ce8ca0f5b50eb77fc721548ffb39c8ac26f8fd1
Tres Seaver <tseaver@palladion.com>
2016-08-03 16:33:29 -0400

    Drop support for Python 2.6.
    Closes #1607.

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
