1b4cc437208246493d49095179798c29113a33b5
Danny Hermes <daniel.j.hermes@gmail.com>
2016-08-22 15:45:06 -0700

    Adding Natural Language Entity for responses.

---
+++ gcloud/language/test_entity.py
@@ -0,0 +1,39 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+
+class TestEntity(unittest.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.language.entity import Entity
+        return Entity
+
+    def _makeOne(self, *args, **kw):
+        return self._getTargetClass()(*args, **kw)
+
+    def test_constructor_defaults(self):
+        name = 'Italian'
+        entity_type = 'LOCATION'
+        metadata = {'wikipedia_url': 'http://en.wikipedia.org/wiki/Italy'}
+        salience = 0.19960518
+        mentions = ['Italian']
+        entity = self._makeOne(name, entity_type, metadata,
+                               salience, mentions)
+        self.assertEqual(entity.name, name)
+        self.assertEqual(entity.entity_type, entity_type)
+        self.assertEqual(entity.metadata, metadata)
+        self.assertEqual(entity.salience, salience)
+        self.assertEqual(entity.mentions, mentions)
