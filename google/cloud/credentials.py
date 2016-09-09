4dfcc40527aabde4a6f346b11c378161cdefa4a6
Danny Hermes <daniel.j.hermes@gmail.com>
2016-09-09 15:23:34 -0700

    Adding new Travis key.
    PR #2288 broke the Travis build since the key stored (from 2014)
    didn't have the project ID as a value.

--- google/cloud/credentials.py
+++ google/cloud/credentials.py
@@ -62,10 +62,15 @@ def get_credentials():
 
       {
           'type': 'service_account',
-          'client_id': '...',
-          'client_email': '...',
+          'project_id': '...',
           'private_key_id': '...',
-          'private_key': '...'
+          'private_key': '...',
+          'client_email': '...',
+          'client_id': '...',
+          'auth_uri': '...',
+          'token_uri': '...',
+          'auth_provider_x509_cert_url': '...',
+          'client_x509_cert_url': '...'
       }
 
     The second of these is simply a JSON key downloaded from the Google APIs
