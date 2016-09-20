212f1917ebbe674234b31f29209cb0d3c372a546
Thomas Schultz <daspecster@gmail.com>
2016-09-20 09:34:55 -0400

    Remove orphaned _fixtures.py. Fixes #2356

--- unit_tests/vision/test_face.py
+++ unit_tests/vision/test_face.py
@@ -21,7 +21,7 @@ class TestFace(unittest.TestCase):
         return Face
 
     def setUp(self):
-        from google.cloud.vision._fixtures import FACE_DETECTION_RESPONSE
+        from unit_tests.vision._fixtures import FACE_DETECTION_RESPONSE
         self.FACE_ANNOTATIONS = FACE_DETECTION_RESPONSE['responses'][0]
         self.face_class = self._getTargetClass()
         self.face = self.face_class.from_api_repr(
