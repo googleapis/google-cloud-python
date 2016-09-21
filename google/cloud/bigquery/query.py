c2f84abb2bd2d1f92c01957095811f4ff91a0054
Tres Seaver <tseaver@palladion.com>
2016-09-21 13:51:40 -0400

    Ensure that 'QueryResults.from_query_job' copies 'job.name'.
    Closes #2368.

--- google/cloud/bigquery/query.py
+++ google/cloud/bigquery/query.py
@@ -77,6 +77,8 @@ class QueryResults(object):
         """
         instance = cls(job.query, job._client, job.udf_resources)
         instance._job = job
+        job_ref = instance._properties.setdefault('jobReference', {})
+        job_ref['jobId'] = job.name
         if job.default_dataset is not None:
             instance.default_dataset = job.default_dataset
         if job.use_query_cache is not None:
