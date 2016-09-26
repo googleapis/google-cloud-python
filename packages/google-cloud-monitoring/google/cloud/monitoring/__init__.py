2e6287a82fbb970532e419229ca374f4c1fe60f2
Danny Hermes <daniel.j.hermes@gmail.com>
2016-09-26 16:18:53 -0700

    Moving all monitoring files into subdirectory.
    Done via:
    
    $ mkdir -p monitoring/google/cloud
    $ cp google/__init__.py monitoring/google/__init__.py
    $ git add monitoring/google/__init__.py
    $ cp google/cloud/__init__.py monitoring/google/cloud/__init__.py
    $ git add monitoring/google/cloud/__init__.py
    $ git mv google/cloud/monitoring monitoring/google/cloud/monitoring
    $ git mv unit_tests/monitoring monitoring/unit_tests

---
+++ packages/google-cloud-monitoring/google/cloud/monitoring/__init__.py
@@ -0,0 +1,47 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Stackdriver Monitoring API wrapper."""
+
+from google.cloud.monitoring.client import Client
+from google.cloud.monitoring.connection import Connection
+from google.cloud.monitoring.group import Group
+from google.cloud.monitoring.label import LabelDescriptor
+from google.cloud.monitoring.label import LabelValueType
+from google.cloud.monitoring.metric import Metric
+from google.cloud.monitoring.metric import MetricDescriptor
+from google.cloud.monitoring.metric import MetricKind
+from google.cloud.monitoring.metric import ValueType
+from google.cloud.monitoring.query import Aligner
+from google.cloud.monitoring.query import Query
+from google.cloud.monitoring.query import Reducer
+from google.cloud.monitoring.resource import Resource
+from google.cloud.monitoring.resource import ResourceDescriptor
+from google.cloud.monitoring.timeseries import Point
+from google.cloud.monitoring.timeseries import TimeSeries
+
+__all__ = (
+    'Client',
+    'Connection',
+    'Group',
+    'LabelDescriptor', 'LabelValueType',
+    'Metric', 'MetricDescriptor', 'MetricKind', 'ValueType',
+    'Aligner', 'Query', 'Reducer',
+    'Resource', 'ResourceDescriptor',
+    'Point', 'TimeSeries',
+    'SCOPE',
+)
+
+
+SCOPE = Connection.SCOPE
