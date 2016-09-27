7df15c7f0b0ee5e6d13fa5c50c9d165db2713f48
Danny Hermes <daniel.j.hermes@gmail.com>
2016-09-27 09:10:26 -0700

    Changing all "Cloud BigQuery" mentions to "BigQuery".

--- packages/google-cloud-bigquery/google/cloud/bigquery/__init__.py
+++ packages/google-cloud-bigquery/google/cloud/bigquery/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Google Cloud BigQuery API wrapper.
+"""Google BigQuery API wrapper.
 
 The main concepts with this API are:
 
