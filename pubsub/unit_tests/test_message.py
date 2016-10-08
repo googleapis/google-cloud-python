da181177f2d1d815e24aa6eb721088ff666293e0
Thomas Schultz <daspecster@gmail.com>
2016-10-08 17:02:13 -0400

    Fix pubsub errors when receiving unencoded message. #2513.

--- pubsub/unit_tests/test_message.py
+++ pubsub/unit_tests/test_message.py
@@ -89,6 +89,19 @@ class TestMessage(unittest.TestCase):
         self.assertEqual(message.attributes, {})
         self.assertIsNone(message.service_timestamp)
 
+    def test_from_api_repr_bad_b64_data(self):
+        DATA = b'wefwefw'
+        BAD_B64_DATA = b'd2Vmd2Vmdw='
+        MESSAGE_ID = '12345'
+        TIMESTAMP = '2016-03-18-19:38:22.001393427Z'
+        api_repr = {
+            'data': BAD_B64_DATA,
+            'messageId': MESSAGE_ID,
+            'publishTimestamp': TIMESTAMP,
+        }
+        message = self._getTargetClass().from_api_repr(api_repr)
+        self.assertEqual(message.data, DATA)
+
     def test_from_api_repr_no_attributes(self):
         from base64 import b64encode as b64
         DATA = b'DEADBEEF'
