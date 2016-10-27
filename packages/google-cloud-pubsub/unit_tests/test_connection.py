387d33dbe84871b142f9ad37d5eef9bb19746e6d
Danny Hermes <daniel.j.hermes@gmail.com>
2016-10-27 12:55:13 -0700

    Updating Client.list_subscriptions() unit tests.
    These changes are needed to cover the change to iterators.

--- packages/google-cloud-pubsub/unit_tests/test_connection.py
+++ packages/google-cloud-pubsub/unit_tests/test_connection.py
@@ -423,20 +423,35 @@ class Test_SubscriberAPI(_Base):
         self.assertIs(api._client, client)
 
     def test_list_subscriptions_no_paging(self):
+        from google.cloud.pubsub.client import Client
+        from google.cloud.pubsub.subscription import Subscription
+        from google.cloud.pubsub.topic import Topic
+
         SUB_INFO = {'name': self.SUB_PATH, 'topic': self.TOPIC_PATH}
         RETURNED = {'subscriptions': [SUB_INFO]}
         connection = _Connection(RETURNED)
-        client = _Client(connection, self.PROJECT)
+        creds = _Credentials()
+        client = Client(project=self.PROJECT, credentials=creds)
+        client.connection = connection
         api = self._makeOne(client)
 
-        subscriptions, next_token = api.list_subscriptions(self.PROJECT)
+        iterator = api.list_subscriptions(self.PROJECT)
+        subscriptions = list(iterator)
+        next_token = iterator.next_page_token
 
+        # Check the token returned.
+        self.assertIsNone(next_token)
+        # Check the subscription object returned.
         self.assertEqual(len(subscriptions), 1)
         subscription = subscriptions[0]
-        self.assertIsInstance(subscription, dict)
-        self.assertEqual(subscription['name'], self.SUB_PATH)
-        self.assertEqual(subscription['topic'], self.TOPIC_PATH)
-        self.assertIsNone(next_token)
+        self.assertIsInstance(subscription, Subscription)
+        self.assertEqual(subscription.name, self.SUB_NAME)
+        self.assertIsInstance(subscription.topic, Topic)
+        self.assertEqual(subscription.topic.name, self.TOPIC_NAME)
+        self.assertIs(subscription._client, client)
+        self.assertEqual(subscription._project, self.PROJECT)
+        self.assertIsNone(subscription.ack_deadline)
+        self.assertIsNone(subscription.push_endpoint)
 
         self.assertEqual(connection._called_with['method'], 'GET')
         path = '/%s' % (self.LIST_SUBSCRIPTIONS_PATH,)
@@ -444,6 +459,11 @@ class Test_SubscriberAPI(_Base):
         self.assertEqual(connection._called_with['query_params'], {})
 
     def test_list_subscriptions_with_paging(self):
+        import six
+        from google.cloud.pubsub.client import Client
+        from google.cloud.pubsub.subscription import Subscription
+        from google.cloud.pubsub.topic import Topic
+
         TOKEN1 = 'TOKEN1'
         TOKEN2 = 'TOKEN2'
         SIZE = 1
@@ -453,18 +473,30 @@ class Test_SubscriberAPI(_Base):
             'nextPageToken': 'TOKEN2',
         }
         connection = _Connection(RETURNED)
-        client = _Client(connection, self.PROJECT)
+        creds = _Credentials()
+        client = Client(project=self.PROJECT, credentials=creds)
+        client.connection = connection
         api = self._makeOne(client)
 
-        subscriptions, next_token = api.list_subscriptions(
+        iterator = api.list_subscriptions(
             self.PROJECT, page_token=TOKEN1, page_size=SIZE)
+        page = six.next(iterator.pages)
+        subscriptions = list(page)
+        next_token = iterator.next_page_token
 
+        # Check the token returned.
+        self.assertEqual(next_token, TOKEN2)
+        # Check the subscription object returned.
         self.assertEqual(len(subscriptions), 1)
         subscription = subscriptions[0]
-        self.assertIsInstance(subscription, dict)
-        self.assertEqual(subscription['name'], self.SUB_PATH)
-        self.assertEqual(subscription['topic'], self.TOPIC_PATH)
-        self.assertEqual(next_token, TOKEN2)
+        self.assertIsInstance(subscription, Subscription)
+        self.assertEqual(subscription.name, self.SUB_NAME)
+        self.assertIsInstance(subscription.topic, Topic)
+        self.assertEqual(subscription.topic.name, self.TOPIC_NAME)
+        self.assertIs(subscription._client, client)
+        self.assertEqual(subscription._project, self.PROJECT)
+        self.assertIsNone(subscription.ack_deadline)
+        self.assertIsNone(subscription.push_endpoint)
 
         self.assertEqual(connection._called_with['method'], 'GET')
         path = '/%s' % (self.LIST_SUBSCRIPTIONS_PATH,)
@@ -478,7 +510,9 @@ class Test_SubscriberAPI(_Base):
         client = _Client(connection, self.PROJECT)
         api = self._makeOne(client)
 
-        subscriptions, next_token = api.list_subscriptions(self.PROJECT)
+        iterator = api.list_subscriptions(self.PROJECT)
+        subscriptions = list(iterator)
+        next_token = iterator.next_page_token
 
         self.assertEqual(len(subscriptions), 0)
         self.assertIsNone(next_token)
@@ -843,3 +877,10 @@ class _Client(object):
     def __init__(self, connection, project):
         self.connection = connection
         self.project = project
+
+
+class _Credentials(object):
+
+    @staticmethod
+    def create_scoped_required():
+        return False
