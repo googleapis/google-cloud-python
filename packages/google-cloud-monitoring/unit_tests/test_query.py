09098a6b65faa06d81afda3de67cb25570ae8766
Danny Hermes <daniel.j.hermes@gmail.com>
2016-11-07 19:22:59 -0800

    Updating connection -> _connection attribute in some packages.
    In particular: dns, language, logging, monitoring.
    
    Transitively, error reporting has also been covered (by logging).

--- packages/google-cloud-monitoring/unit_tests/test_query.py
+++ packages/google-cloud-monitoring/unit_tests/test_query.py
@@ -14,6 +14,7 @@
 
 import unittest
 
+
 PROJECT = 'my-project'
 
 METRIC_TYPE = 'compute.googleapis.com/instance/uptime'
@@ -646,4 +647,4 @@ class _Client(object):
 
     def __init__(self, project, connection):
         self.project = project
-        self.connection = connection
+        self._connection = connection
