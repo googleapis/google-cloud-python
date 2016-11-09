4dc223e105eb9bfeb42ec1d7d9aa665993e0c23a
Danny Hermes <daniel.j.hermes@gmail.com>
2016-11-08 20:20:59 -0800

    Renaming _getTargetClass to _get_target_class.
    Done via:
    
    $ git grep -l 'def _getTargetClass(self)' | \
    > xargs sed -i s/'def _getTargetClass(self)'/'@staticmethod\n    def _get_target_class()'/g

--- packages/google-cloud-language/unit_tests/test_entity.py
+++ packages/google-cloud-language/unit_tests/test_entity.py
@@ -17,7 +17,8 @@ import unittest
 
 class TestEntity(unittest.TestCase):
 
-    def _getTargetClass(self):
+    @staticmethod
+    def _get_target_class():
         from google.cloud.language.entity import Entity
         return Entity
 
