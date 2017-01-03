166daf581c91baf0bf1679581dd6e969a342f787
Thomas Schultz <daspecster@gmail.com>
2017-01-03 12:10:59 -0500

    Add gRPC conversion helper functions.

--- packages/google-cloud-vision/google/cloud/vision/_gax.py
+++ packages/google-cloud-vision/google/cloud/vision/_gax.py
@@ -14,12 +14,55 @@
 
 """GAX Client for interacting with the Google Cloud Vision API."""
 
+from google.cloud.gapic.vision.v1 import image_annotator_client
+from google.cloud.grpc.vision.v1 import image_annotator_pb2
+
+from google.cloud._helpers import _to_bytes
+
 
 class _GAPICVisionAPI(object):
     """Vision API for interacting with the gRPC version of Vision.
 
-    :type client: :class:`~google.cloud.core.client.Client`
+    :type client: :class:`~google.cloud.vision.client.Client`
     :param client: Instance of ``Client`` object.
     """
     def __init__(self, client=None):
-        raise NotImplementedError
+        self._client = client
+        self._api = image_annotator_client.ImageAnnotatorClient()
+
+
+def _to_gapic_feature(feature):
+    """Helper function to convert a ``Feature`` to a gRPC ``Feature``.
+
+    :type feature: :class:`~google.cloud.vision.feature.Feature`
+    :param feature: Local ``Feature`` class to be converted to gRPC ``Feature``
+                    instance.
+
+    :rtype: :class:`~google.cloud.grpc.vision.v1.image_annotator_pb2.Feature`
+    :returns: gRPC ``Feature`` converted from
+              :class:`~google.cloud.vision.feature.Feature`.
+    """
+    return image_annotator_pb2.Feature(
+        type=getattr(image_annotator_pb2.Feature, feature.feature_type),
+        max_results=feature.max_results)
+
+
+def _to_gapic_image(image):
+    """Helper function to convert an ``Image`` to a gRPC ``Image``.
+
+    :type image: :class:`~google.cloud.vision.image.Image`
+    :param image: Local ``Image`` class to be converted to gRPC ``Image``.
+
+    :rtype: :class:`~google.cloud.grpc.vision.v1.image_annotator_pb2.Image`
+    :returns: gRPC ``Image`` converted from
+              :class:`~google.cloud.vision.image.Image`.
+    """
+    if image.content is not None:
+        return image_annotator_pb2.Image(content=_to_bytes(image.content))
+    if image.source is not None:
+        return image_annotator_pb2.Image(
+            source=image_annotator_pb2.ImageSource(
+                gcs_image_uri=image.source
+            ),
+        )
+    raise ValueError('No image content or source found.')
