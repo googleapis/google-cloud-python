b09a1e82e096ca3922b860549ab87623ca9c1830
Danny Hermes <daniel.j.hermes@gmail.com>
2017-01-26 15:15:44 -0800

    Moving _http and _credentials default values from base connection to client.
    Still leaving proxy properties on Connection to point to the http
    and credentials objects owned by the client.

--- core/unit_tests/test__http.py
+++ core/unit_tests/test__http.py
@@ -28,61 +28,27 @@ class TestConnection(unittest.TestCase):
     def _make_one(self, *args, **kw):
         return self._get_target_class()(*args, **kw)
 
-    def test_ctor_defaults(self):
-        conn = self._make_one()
-        self.assertIsNone(conn.credentials)
+    def test_constructor(self):
+        client = object()
+        conn = self._make_one(client)
+        self.assertIs(conn._client, client)
 
-    def test_ctor_explicit(self):
-        import google.auth.credentials
+    def test_credentials_property(self):
+        client = mock.Mock(spec=['_credentials'])
+        conn = self._make_one(client)
+        self.assertIs(conn.credentials, client._credentials)
 
-        credentials = mock.Mock(spec=google.auth.credentials.Scoped)
-
-        conn = self._make_one(credentials)
-
-        credentials.with_scopes.assert_called_once_with(conn.SCOPE)
-        self.assertIs(conn.credentials, credentials.with_scopes.return_value)
-        self.assertIsNone(conn._http)
-
-    def test_ctor_explicit_http(self):
-        http = object()
-        conn = self._make_one(http=http)
-        self.assertIsNone(conn.credentials)
-        self.assertIs(conn.http, http)
-
-    def test_ctor_credentials_wo_create_scoped(self):
-        credentials = object()
-        conn = self._make_one(credentials)
-        self.assertIs(conn.credentials, credentials)
-        self.assertIsNone(conn._http)
-
-    def test_http_w_existing(self):
-        conn = self._make_one()
-        conn._http = http = object()
-        self.assertIs(conn.http, http)
-
-    def test_http_wo_creds(self):
-        import httplib2
-
-        conn = self._make_one()
-        self.assertIsInstance(conn.http, httplib2.Http)
-
-    def test_http_w_creds(self):
-        import google.auth.credentials
-        import google_auth_httplib2
-
-        credentials = mock.Mock(spec=google.auth.credentials.Credentials)
-
-        conn = self._make_one(credentials)
-
-        self.assertIsInstance(conn.http, google_auth_httplib2.AuthorizedHttp)
-        self.assertIs(conn.http.credentials, credentials)
+    def test_http_property(self):
+        client = mock.Mock(spec=['_http'])
+        conn = self._make_one(client)
+        self.assertIs(conn.http, client._http)
 
     def test_user_agent_format(self):
         from pkg_resources import get_distribution
 
         expected_ua = 'gcloud-python/{0}'.format(
             get_distribution('google-cloud-core').version)
-        conn = self._make_one()
+        conn = self._make_one(object())
         self.assertEqual(conn.USER_AGENT, expected_ua)
 
 
@@ -97,7 +63,7 @@ class TestJSONConnection(unittest.TestCase):
     def _make_one(self, *args, **kw):
         return self._get_target_class()(*args, **kw)
 
-    def _makeMockOne(self, *args, **kw):
+    def _make_mock_one(self, *args, **kw):
         class MockConnection(self._get_target_class()):
             API_URL_TEMPLATE = '{api_base_url}/mock/{api_version}{path}'
             API_BASE_URL = 'http://mock'
@@ -110,38 +76,14 @@ class TestJSONConnection(unittest.TestCase):
         self.assertIsNone(klass.API_BASE_URL)
         self.assertIsNone(klass.API_VERSION)
 
-    def test_ctor_defaults(self):
-        conn = self._make_one()
-        self.assertIsNone(conn.credentials)
-
-    def test_ctor_explicit(self):
-        conn = self._make_one(mock.sentinel.credentials)
-        self.assertIs(conn.credentials, mock.sentinel.credentials)
-
-    def test_http_w_existing(self):
-        conn = self._make_one()
-        conn._http = http = object()
-        self.assertIs(conn.http, http)
-
-    def test_http_wo_creds(self):
-        import httplib2
-
-        conn = self._make_one()
-        self.assertIsInstance(conn.http, httplib2.Http)
-
-    def test_http_w_creds(self):
-        import google.auth.credentials
-        import google_auth_httplib2
-
-        credentials = mock.Mock(spec=google.auth.credentials.Credentials)
-
-        conn = self._make_one(credentials)
-
-        self.assertIsInstance(conn.http, google_auth_httplib2.AuthorizedHttp)
-        self.assertIs(conn.http.credentials, credentials)
+    def test_constructor(self):
+        client = object()
+        conn = self._make_one(client)
+        self.assertIs(conn._client, client)
 
     def test_build_api_url_no_extra_query_params(self):
-        conn = self._makeMockOne()
+        client = object()
+        conn = self._make_mock_one(client)
         # Intended to emulate self.mock_template
         URI = '/'.join([
             conn.API_BASE_URL,
@@ -155,7 +97,8 @@ class TestJSONConnection(unittest.TestCase):
         from six.moves.urllib.parse import parse_qsl
         from six.moves.urllib.parse import urlsplit
 
-        conn = self._makeMockOne()
+        client = object()
+        conn = self._make_mock_one(client)
         uri = conn.build_api_url('/foo', {'bar': 'baz'})
 
         scheme, netloc, path, qs, _ = urlsplit(uri)
@@ -172,12 +115,13 @@ class TestJSONConnection(unittest.TestCase):
         self.assertEqual(parms['bar'], 'baz')
 
     def test__make_request_no_data_no_content_type_no_headers(self):
-        conn = self._make_one()
-        URI = 'http://example.com/test'
-        http = conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'text/plain'},
             b'',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_one(client)
+        URI = 'http://example.com/test'
         headers, content = conn._make_request('GET', URI)
         self.assertEqual(headers['status'], '200')
         self.assertEqual(headers['content-type'], 'text/plain')
@@ -193,12 +137,13 @@ class TestJSONConnection(unittest.TestCase):
         self.assertEqual(http._called_with['headers'], expected_headers)
 
     def test__make_request_w_data_no_extra_headers(self):
-        conn = self._make_one()
-        URI = 'http://example.com/test'
-        http = conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'text/plain'},
             b'',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_one(client)
+        URI = 'http://example.com/test'
         conn._make_request('GET', URI, {}, 'application/json')
         self.assertEqual(http._called_with['method'], 'GET')
         self.assertEqual(http._called_with['uri'], URI)
@@ -212,12 +157,13 @@ class TestJSONConnection(unittest.TestCase):
         self.assertEqual(http._called_with['headers'], expected_headers)
 
     def test__make_request_w_extra_headers(self):
-        conn = self._make_one()
-        URI = 'http://example.com/test'
-        http = conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'text/plain'},
             b'',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_one(client)
+        URI = 'http://example.com/test'
         conn._make_request('GET', URI, headers={'X-Foo': 'foo'})
         self.assertEqual(http._called_with['method'], 'GET')
         self.assertEqual(http._called_with['uri'], URI)
@@ -231,18 +177,19 @@ class TestJSONConnection(unittest.TestCase):
         self.assertEqual(http._called_with['headers'], expected_headers)
 
     def test_api_request_defaults(self):
+        http = _Http(
+            {'status': '200', 'content-type': 'application/json'},
+            b'{}',
+        )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         PATH = '/path/required'
-        conn = self._makeMockOne()
         # Intended to emulate self.mock_template
         URI = '/'.join([
             conn.API_BASE_URL,
             'mock',
             '%s%s' % (conn.API_VERSION, PATH),
         ])
-        http = conn._http = _Http(
-            {'status': '200', 'content-type': 'application/json'},
-            b'{}',
-        )
         self.assertEqual(conn.api_request('GET', PATH), {})
         self.assertEqual(http._called_with['method'], 'GET')
         self.assertEqual(http._called_with['uri'], URI)
@@ -255,20 +202,22 @@ class TestJSONConnection(unittest.TestCase):
         self.assertEqual(http._called_with['headers'], expected_headers)
 
     def test_api_request_w_non_json_response(self):
-        conn = self._makeMockOne()
-        conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'text/plain'},
             b'CONTENT',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
 
         self.assertRaises(TypeError, conn.api_request, 'GET', '/')
 
     def test_api_request_wo_json_expected(self):
-        conn = self._makeMockOne()
-        conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'text/plain'},
             b'CONTENT',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         self.assertEqual(conn.api_request('GET', '/', expect_json=False),
                          b'CONTENT')
 
@@ -276,11 +225,12 @@ class TestJSONConnection(unittest.TestCase):
         from six.moves.urllib.parse import parse_qsl
         from six.moves.urllib.parse import urlsplit
 
-        conn = self._makeMockOne()
-        http = conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'application/json'},
             b'{}',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         self.assertEqual(conn.api_request('GET', '/', {'foo': 'bar'}), {})
         self.assertEqual(http._called_with['method'], 'GET')
         uri = http._called_with['uri']
@@ -307,11 +257,12 @@ class TestJSONConnection(unittest.TestCase):
     def test_api_request_w_headers(self):
         from six.moves.urllib.parse import urlsplit
 
-        conn = self._makeMockOne()
-        http = conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'application/json'},
             b'{}',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         self.assertEqual(
             conn.api_request('GET', '/', headers={'X-Foo': 'bar'}), {})
         self.assertEqual(http._called_with['method'], 'GET')
@@ -341,7 +292,12 @@ class TestJSONConnection(unittest.TestCase):
 
         DATA = {'foo': 'bar'}
         DATAJ = json.dumps(DATA)
-        conn = self._makeMockOne()
+        http = _Http(
+            {'status': '200', 'content-type': 'application/json'},
+            b'{}',
+        )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         # Intended to emulate self.mock_template
         URI = '/'.join([
             conn.API_BASE_URL,
@@ -349,10 +305,6 @@ class TestJSONConnection(unittest.TestCase):
             conn.API_VERSION,
             '',
         ])
-        http = conn._http = _Http(
-            {'status': '200', 'content-type': 'application/json'},
-            b'{}',
-        )
         self.assertEqual(conn.api_request('POST', '/', data=DATA), {})
         self.assertEqual(http._called_with['method'], 'POST')
         self.assertEqual(http._called_with['uri'], URI)
@@ -368,29 +320,33 @@ class TestJSONConnection(unittest.TestCase):
     def test_api_request_w_404(self):
         from google.cloud.exceptions import NotFound
 
-        conn = self._makeMockOne()
-        conn._http = _Http(
+        http = _Http(
             {'status': '404', 'content-type': 'text/plain'},
             b'{}'
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         self.assertRaises(NotFound, conn.api_request, 'GET', '/')
 
     def test_api_request_w_500(self):
         from google.cloud.exceptions import InternalServerError
 
-        conn = self._makeMockOne()
-        conn._http = _Http(
+        http = _Http(
             {'status': '500', 'content-type': 'text/plain'},
             b'{}',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
         self.assertRaises(InternalServerError, conn.api_request, 'GET', '/')
 
     def test_api_request_non_binary_response(self):
-        conn = self._makeMockOne()
-        http = conn._http = _Http(
+        http = _Http(
             {'status': '200', 'content-type': 'application/json'},
             u'{}',
         )
+        client = mock.Mock(_http=http, spec=['_http'])
+        conn = self._make_mock_one(client)
+
         result = conn.api_request('GET', '/')
         # Intended to emulate self.mock_template
         URI = '/'.join([
