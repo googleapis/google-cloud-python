e371a204b1022abc8f031294d2aface30e6088b1
Danny Hermes <daniel.j.hermes@gmail.com>
2017-01-26 16:52:18 -0800

    Changing logging Connection to only accept client.

--- packages/google-cloud-logging/unit_tests/test__http.py
+++ packages/google-cloud-logging/unit_tests/test__http.py
@@ -38,9 +38,9 @@ class TestConnection(unittest.TestCase):
         return self._get_target_class()(*args, **kw)
 
     def test_default_url(self):
-        creds = _make_credentials()
-        conn = self._make_one(creds)
-        self.assertEqual(conn.credentials, creds)
+        client = object()
+        conn = self._make_one(client)
+        self.assertIs(conn._client, client)
 
 
 class Test_LoggingAPI(unittest.TestCase):
