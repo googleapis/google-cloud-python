af26b616d4ccb4437b80b24d2f7f3e06dc341ce1
Danny Hermes <daniel.j.hermes@gmail.com>
2017-01-26 16:26:39 -0800

    Changing runtimeconfig Connection to only accept client.

--- packages/google-cloud-runtimeconfig/unit_tests/test_connection.py
+++ packages/google-cloud-runtimeconfig/unit_tests/test_connection.py
@@ -27,6 +27,6 @@ class TestConnection(unittest.TestCase):
         return self._get_target_class()(*args, **kw)
 
     def test_default_url(self):
-        creds = object()
-        conn = self._make_one(creds)
-        self.assertEqual(conn.credentials, creds)
+        client = object()
+        conn = self._make_one(client)
+        self.assertIs(conn._client, client)
