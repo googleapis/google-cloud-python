a87686787271bcf5ae276d212fe8c715dd0572ff
Jeff Reback <jeff@reback.net>
2017-02-08 08:31:10 -0500

    initial version from pandasv 0.19.0-419-gfe246cc

--- packages/pandas-gbq/pandas_gbq/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = version = '0.3.0.post'
-
-from .data import (get_components_yahoo, get_data_famafrench, get_data_google, get_data_yahoo, get_data_enigma,  # noqa
-        get_data_yahoo_actions, get_quote_google, get_quote_yahoo, DataReader, Options)  # noqa
