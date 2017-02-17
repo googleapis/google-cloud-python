883433397748ee43641a3597696322d3182df9bf
Danny Hermes <daniel.j.hermes@gmail.com>
2017-02-16 16:10:52 -0800

    Renaming all remaining connection.py modules as _http.py.

--- packages/google-cloud-translate/unit_tests/test__http.py
+++ packages/google-cloud-translate/unit_tests/test__http.py
@@ -19,7 +19,7 @@ class TestConnection(unittest.TestCase):
 
     @staticmethod
     def _get_target_class():
-        from google.cloud.translate.connection import Connection
+        from google.cloud.translate._http import Connection
 
         return Connection
 
