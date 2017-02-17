cdceb21a179a1e0200e0f5fc0d604b8279a99362
Danny Hermes <daniel.j.hermes@gmail.com>
2017-02-16 16:41:57 -0800

    Adding ability to send version info header on HTTP requests.
    Added an "extra headers" feature to enable this. I am not a fan
    of changing `Connection()` so haphazardly, but I hope to
    completely re-factor / destory `Connection()` in the near-term
    so I am less worried.
    
    This only adds the storage and datastore header info, for the
    purposes of a simple review. Once we agree on the approach,
    I can add support in the other subpackages.

--- packages/google-cloud-storage/unit_tests/test__http.py
+++ packages/google-cloud-storage/unit_tests/test__http.py
@@ -14,6 +14,8 @@
 
 import unittest
 
+import mock
+
 
 class TestConnection(unittest.TestCase):
 
@@ -25,6 +27,36 @@ class TestConnection(unittest.TestCase):
 
     def _make_one(self, *args, **kw):
         return self._get_target_class()(*args, **kw)
+
+    def test_extra_headers(self):
+        from google.cloud import _http as base_http
+        from google.cloud.storage import _http as MUT
+
+        http = mock.Mock(spec=['request'])
+        response = mock.Mock(status=200, spec=['status'])
+        data = b'brent-spiner'
+        http.request.return_value = response, data
+        client = mock.Mock(_http=http, spec=['_http'])
+
+        conn = self._make_one(client)
+        req_data = 'hey-yoooouuuuu-guuuuuyyssss'
+        result = conn.api_request(
+            'GET', '/rainbow', data=req_data, expect_json=False)
+        self.assertEqual(result, data)
+
+        expected_headers = {
+            'Content-Length': str(len(req_data)),
+            'Accept-Encoding': 'gzip',
+            base_http.CLIENT_INFO_HEADER: MUT._CLIENT_INFO,
+            'User-Agent': conn.USER_AGENT,
+        }
+        expected_uri = conn.build_api_url('/rainbow')
+        http.request.assert_called_once_with(
+            body=req_data,
+            headers=expected_headers,
+            method='GET',
+            uri=expected_uri,
+        )
 
     def test_build_api_url_no_extra_query_params(self):
         conn = self._make_one(object())
