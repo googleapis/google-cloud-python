46731d110d96167b6a79e98e5da20a4e84d4ac48
Jeff Reback <jeff@reback.net>
2017-02-22 16:06:43 -0500

    update versioneer

--- packages/pandas-gbq/pandas_gbq/__init__.py
+++ packages/pandas-gbq/pandas_gbq/__init__.py
@@ -1,7 +1,11 @@
 from .gbq import to_gbq, read_gbq  # noqa
 
-# use the closest tagged version if possible
 from ._version import get_versions
-v = get_versions()
-__version__ = v.get('closest-tag', v['version'])
-del get_versions, v
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
