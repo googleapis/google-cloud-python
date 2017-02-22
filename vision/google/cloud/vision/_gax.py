73b3b492d4d3e19e53fcb638e02a8c355aa168eb
Luke Sneeringer <lukesneeringer@google.com>
2017-02-22 14:55:59 -0800

    Remove errant **kwargs documentation.

--- vision/google/cloud/vision/_gax.py
+++ vision/google/cloud/vision/_gax.py
@@ -26,9 +26,6 @@ class _GAPICVisionAPI(object):
 
     :type client: :class:`~google.cloud.vision.client.Client`
     :param client: Instance of ``Client`` object.
-
-    :type kwargs: dict
-    :param kwargs: Additional keyword arguments are sent to the GAPIC client.
     """
     def __init__(self, client=None):
         self._client = client
