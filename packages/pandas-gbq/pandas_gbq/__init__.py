7b14f3e8222c86c651c0654a466c98e97ec9d313
Jeff Reback <jeff@reback.net>
2017-02-22 16:12:20 -0500

    don't use a prefix in setup.cfg

--- packages/pandas-gbq/pandas_gbq/__init__.py
+++ packages/pandas-gbq/pandas_gbq/__init__.py
@@ -2,10 +2,6 @@ from .gbq import to_gbq, read_gbq  # noqa
 
 from ._version import get_versions
 versions = get_versions()
-__version__ = versions['version']
+__version__ = versions.get('closest-tag', versions['version'])
 __git_revision__ = versions['full-revisionid']
 del get_versions, versions
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
--- packages/pandas-gbq/pandas_gbq/_version.py
+++ packages/pandas-gbq/pandas_gbq/_version.py
@@ -41,8 +41,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "v"
-    cfg.parentdir_prefix = "pandas_gbq-"
+    cfg.tag_prefix = ""
+    cfg.parentdir_prefix = "pandas_gbq/_version.py"
     cfg.versionfile_source = "pandas_gbq/_version.py"
     cfg.verbose = False
     return cfg
