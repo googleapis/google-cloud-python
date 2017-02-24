52e1bd4b5944be6df5c367052575346c16e7dccf
Luke Sneeringer <luke@sneeringer.com>
2017-02-24 10:54:27 -0800

    Send back the complete API responses. (#3059)
    * Return full API responses for Natural Language.
    
    * Return full API responses from NL.
    
    * Do the hyphenated copyright thing for edited files.
    
    * Update usage doc.
    
    * Updates based on @dhermes feedback.
    
    * Update system tests.
    
    * Added system tests and Entity Response tests.
    
    Still need explicit SyntaxResponse and SentimentResponse tests.
    
    * Remove explcit dict.get('foo', None)
    
    * Fix some of the pylint errors.
    
    * Finish fixing pylint errors.
    
    * It is 2017.
    
    * Add SentimentResponse tests.
    
    * Unit tests for SyntaxResponse.
    
    * Missed a dict.get('foo', None) case.
    
    * Use assertIsNone
    
    * Remove wikipedia_url as an attribute.
    
    * PEP 257 compliance.
    
    * Add Sentiment isInstance check.
    
    * Add API responses documentation.
    
    * Adding sentences to docs.
    
    * Fix typo.

--- language/setup.py
+++ language/setup.py
@@ -55,7 +55,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-language',
-    version='0.22.2',
+    version='0.23.0',
     description='Python Client for Google Cloud Natural Language',
     long_description=README,
     namespace_packages=[
--- language/unit_tests/test_entity.py
+++ language/unit_tests/test_entity.py
@@ -30,17 +30,17 @@ class TestEntity(unittest.TestCase):
         name = 'Italian'
         entity_type = 'LOCATION'
         wiki_url = 'http://en.wikipedia.org/wiki/Italy'
-        metadata = {'wikipedia_url': wiki_url}
-        base_metadata = {'foo': 'bar'}
-        metadata.update(base_metadata)
+        metadata = {
+            'foo': 'bar',
+            'wikipedia_url': wiki_url,
+        }
         salience = 0.19960518
         mentions = ['Italian']
         entity = self._make_one(name, entity_type, metadata,
                                 salience, mentions)
         self.assertEqual(entity.name, name)
         self.assertEqual(entity.entity_type, entity_type)
-        self.assertEqual(entity.wikipedia_url, wiki_url)
-        self.assertEqual(entity.metadata, base_metadata)
+        self.assertEqual(entity.metadata, metadata)
         self.assertEqual(entity.salience, salience)
         self.assertEqual(entity.mentions, mentions)
 
@@ -68,6 +68,5 @@ class TestEntity(unittest.TestCase):
         self.assertEqual(entity.name, name)
         self.assertEqual(entity.entity_type, entity_type)
         self.assertEqual(entity.salience, salience)
-        self.assertEqual(entity.wikipedia_url, wiki_url)
-        self.assertEqual(entity.metadata, {})
+        self.assertEqual(entity.metadata, {'wikipedia_url': wiki_url})
         self.assertEqual(entity.mentions, [mention1, mention2, mention3])
