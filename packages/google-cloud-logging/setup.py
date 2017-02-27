7b420da11d85c47e8f7b1e79e68225a676035cac
Luke Sneeringer <luke@sneeringer.com>
2017-02-27 15:27:22 -0800

    Bump logging dependencies to 0.91.0 (#3079)

--- packages/google-cloud-logging/setup.py
+++ packages/google-cloud-logging/setup.py
@@ -52,12 +52,12 @@ SETUP_BASE = {
 REQUIREMENTS = [
     'google-cloud-core >= 0.23.1, < 0.24dev',
     'grpcio >= 1.0.2, < 2.0dev',
-    'gapic-google-cloud-logging-v2 >= 0.90.1, < 0.91dev',
+    'gapic-google-cloud-logging-v2 >= 0.91.0, < 0.92dev',
 ]
 
 setup(
     name='google-cloud-logging',
-    version='0.23.0',
+    version='0.23.1',
     description='Python Client for Stackdriver Logging',
     long_description=README,
     namespace_packages=[
