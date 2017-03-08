f77c78119c312b056c684a3317aecab33e80a5d8
daspecster <daspecster@gmail.com>
2017-03-08 11:47:53 -0500

    Release Error Reporting 0.23.1.

--- error_reporting/setup.py
+++ error_reporting/setup.py
@@ -57,7 +57,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-error-reporting',
-    version='0.23.0',
+    version='0.23.1',
     description='Python Client for Stackdriver Error Reporting',
     long_description=README,
     namespace_packages=[
