a286ce3abe05afbb49d1a436b397fb6ea0b055ce
Luke Sneeringer <lukesneeringer@google.com>
2017-03-15 09:48:29 -0700

    Nox fix.

--- nox.py
+++ nox.py
@@ -25,7 +25,7 @@ def docs(session):
     session.interpreter = 'python3.6'
 
     # Install Sphinx and also all of the google-cloud-* packages.
-    session.chdir(os.path.dirname(__file__))
+    session.chdir(os.path.realpath(os.path.dirname(__file__)))
     session.install('sphinx', 'sphinx_rtd_theme')
     session.install(
         'core/', 'bigquery/', 'bigtable/', 'datastore/', 'error_reporting/',
