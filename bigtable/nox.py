8bc40a6f9207ceec73f02aa8b3d98a8ab7b9f161
Luke Sneeringer <lukesneeringer@google.com>
2017-03-15 11:21:22 -0700

    Fix nox files; remove parallelism again.

--- bigtable/nox.py
+++ bigtable/nox.py
@@ -26,7 +26,7 @@ def unit_tests(session, python_version):
     session.interpreter = 'python%s' % python_version
 
     # Install all test dependencies, then install this package in-place.
-    session.chdir(os.path.dirname(__file__))
+    session.chdir(os.path.realpath(os.path.dirname(__file__)))
     session.install('mock', 'pytest', 'pytest-cov', '../core/')
     session.install('-e', '.')
 
@@ -52,7 +52,7 @@ def system_tests(session, python_version):
 
     # Install all test dependencies, then install this package into the
     # virutalenv's dist-packages.
-    session.chdir(os.path.dirname(__file__))
+    session.chdir(os.path.realpath(os.path.dirname(__file__)))
     session.install('mock', 'pytest',
                     '../core/', '../test_utils/')
     session.install('.')
@@ -69,7 +69,7 @@ def lint(session):
     serious code quality issues.
     """
     session.interpreter = 'python3.6'
-    session.chdir(os.path.dirname(__file__))
+    session.chdir(os.path.realpath(os.path.dirname(__file__)))
     session.install('flake8')
     session.install('.')
     session.run('flake8', 'google/cloud/bigtable')
@@ -83,7 +83,7 @@ def cover(session):
     test runs (not system test runs), and then erases coverage data.
     """
     session.interpreter = 'python3.6'
-    session.chdir(os.path.dirname(__file__))
+    session.chdir(os.path.realpath(os.path.dirname(__file__)))
     session.install('coverage', 'pytest-cov')
     session.run('coverage', 'report', '--show-missing', '--fail-under=100')
     session.run('coverage', 'erase')
