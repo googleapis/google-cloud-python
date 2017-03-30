d388a1864a188220b4ca62627721dd992030cdad
Luke Sneeringer <luke@sneeringer.com>
2017-03-30 07:51:10 -0700

    Make clients explicitly unpickleable. (#3230)

--- packages/google-cloud-core/tests/unit/test_client.py
+++ packages/google-cloud-core/tests/unit/test_client.py
@@ -47,6 +47,16 @@ class TestClient(unittest.TestCase):
     def _make_one(self, *args, **kw):
         return self._get_target_class()(*args, **kw)
 
+    def test_unpickleable(self):
+        import pickle
+
+        CREDENTIALS = _make_credentials()
+        HTTP = object()
+
+        client_obj = self._make_one(credentials=CREDENTIALS, http=HTTP)
+        with self.assertRaises(pickle.PicklingError):
+            pickle.dumps(client_obj)
+
     def test_ctor_defaults(self):
         from google.cloud._testing import _Monkey
         from google.cloud import client
