fe90514c3bd95883ee3bd7f429497074688813e7
Danny Hermes <daniel.j.hermes@gmail.com>
2017-04-21 12:08:20 -0700

    Adding support for Twine upload on CircleCI.

---
+++ packages/google-resumable-media/scripts/twine_upload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ev
+
+# If this is not a CircleCI tag, no-op.
+if [[ -z "$CIRCLE_TAG" ]]; then
+  echo "This is not a release tag. Doing nothing."
+  exit 0
+fi
+
+# Move into the package, build the distribution and upload.
+python3 setup.py sdist bdist_wheel
+
+pip3 install twine
+twine upload dist/*
