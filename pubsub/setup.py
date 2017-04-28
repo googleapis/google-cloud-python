c43b496c4abbbea7565c7147d97c6a8ee8275f1d
Danny Hermes <daniel.j.hermes@gmail.com>
2017-04-28 11:15:27 -0700

    Cut releases of core, error reporting, pubsub, spanner and storage. (#3340)
    Also updating the umbrella/uber package along the way.

--- pubsub/setup.py
+++ pubsub/setup.py
@@ -51,14 +51,14 @@ SETUP_BASE = {
 
 
 REQUIREMENTS = [
-    'google-cloud-core >= 0.24.0, < 0.25dev',
+    'google-cloud-core >= 0.24.1, < 0.25dev',
     'grpcio >= 1.0.2, < 2.0dev',
     'gapic-google-cloud-pubsub-v1 >= 0.15.0, < 0.16dev',
 ]
 
 setup(
     name='google-cloud-pubsub',
-    version='0.24.0',
+    version='0.25.0',
     description='Python Client for Google Cloud Pub/Sub',
     long_description=README,
     namespace_packages=[
