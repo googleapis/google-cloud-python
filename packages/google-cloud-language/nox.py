3e12350df4949772f489f9b7763c9baa726b7a5f
Jon Wayne Parrott <jonwayne@google.com>
2017-06-23 15:08:10 -0700

    Re-enable pylint in info-only mode for all packages (#3519)

--- packages/google-cloud-language/nox.py
+++ packages/google-cloud-language/nox.py
@@ -66,15 +66,22 @@ def system_tests(session, python_version):
 
 @nox.session
 def lint(session):
-    """Run flake8.
+    """Run linters.
 
-    Returns a failure if flake8 finds linting errors or sufficiently
+    Returns a failure if the linters find linting errors or sufficiently
     serious code quality issues.
     """
     session.interpreter = 'python3.6'
-    session.install('flake8', *LOCAL_DEPS)
+    session.install('flake8', 'pylint', 'gcp-devrel-py-tools', *LOCAL_DEPS)
     session.install('.')
     session.run('flake8', 'google/cloud/language')
+    session.run(
+        'gcp-devrel-py-tools', 'run-pylint',
+        '--config', 'pylint.config.py',
+        '--library-filesets', 'google',
+        '--test-filesets', 'tests',
+        # Temporarily allow this to fail.
+        success_codes=range(0, 100))
 
 
 @nox.session
