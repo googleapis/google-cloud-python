49c477e39eb9d0d952ae0c76bd744edc3e2b7000
Tres Seaver <tseaver@palladion.com>
2017-06-26 16:47:37 -0400

    Prep monitoring-0.25.0 release.

--- monitoring/setup.py
+++ monitoring/setup.py
@@ -51,12 +51,12 @@ SETUP_BASE = {
 
 
 REQUIREMENTS = [
-    'google-cloud-core >= 0.24.0, < 0.25dev',
+    'google-cloud-core >= 0.25.0, < 0.26dev',
 ]
 
 setup(
     name='google-cloud-monitoring',
-    version='0.24.0',
+    version='0.25.0',
     description='Python Client for Stackdriver Monitoring',
     long_description=README,
     namespace_packages=[
