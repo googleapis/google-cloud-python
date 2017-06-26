bf78c5d1c5cd80dd2f8886ba67632e3195e0b417
Tres Seaver <tseaver@palladion.com>
2017-06-26 17:28:58 -0400

    Prep core-0.25.0 release. (#3526)

--- packages/google-cloud-core/setup.py
+++ packages/google-cloud-core/setup.py
@@ -61,7 +61,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-core',
-    version='0.24.1',
+    version='0.25.0',
     description='API Client library for Google Cloud: Core Helpers',
     long_description=README,
     namespace_packages=[
