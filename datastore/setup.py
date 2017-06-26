25b987c6f3e264a97a60d51be65abad5bf4eb61d
Tres Seaver <tseaver@palladion.com>
2017-06-26 18:31:36 -0400

    Prep datastore-1.1.0 release. (#3527)

--- datastore/setup.py
+++ datastore/setup.py
@@ -51,14 +51,14 @@ SETUP_BASE = {
 
 
 REQUIREMENTS = [
-    'google-cloud-core >= 0.24.0, < 0.25dev',
+    'google-cloud-core >= 0.25.0, < 0.26dev',
     'google-gax>=0.15.7, <0.16dev',
     'gapic-google-cloud-datastore-v1 >= 0.15.0, < 0.16dev',
 ]
 
 setup(
     name='google-cloud-datastore',
-    version='1.0.0',
+    version='1.1.0',
     description='Python Client for Google Cloud Datastore',
     long_description=README,
     namespace_packages=[
