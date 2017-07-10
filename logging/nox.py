f345bb358dbbc0d747ab2c475a4fd4aa6696cf54
Craig Silverstein <csilvers@gmail.com>
2017-07-10 09:12:24 -0700

    Add support for logging the trace-id in webapp2 apps. (#3593)

--- logging/nox.py
+++ logging/nox.py
@@ -36,7 +36,7 @@ def unit_tests(session, python_version):
     # Install all test dependencies, then install this package in-place.
     session.install(
         'mock', 'pytest', 'pytest-cov',
-        'flask', 'django', *LOCAL_DEPS)
+        'flask', 'webapp2', 'webob', 'django', *LOCAL_DEPS)
     session.install('-e', '.')
 
     # Run py.test against the unit tests.
