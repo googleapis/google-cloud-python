6d26cf98c7420b9a46ee4453f5fd2b63ea851899
Jon Wayne Parrott <jonwayne@google.com>
2017-07-18 12:34:25 -0700

    Remove references to google.cloud.operation from spanner

--- spanner/google/cloud/spanner/database.py
+++ spanner/google/cloud/spanner/database.py
@@ -27,7 +27,6 @@ import six
 # pylint: disable=ungrouped-imports
 from google.cloud.exceptions import Conflict
 from google.cloud.exceptions import NotFound
-from google.cloud.operation import register_type
 from google.cloud.spanner import __version__
 from google.cloud.spanner._helpers import _options_with_prefix
 from google.cloud.spanner.batch import Batch
@@ -43,10 +42,6 @@ _DATABASE_NAME_RE = re.compile(
     r'instances/(?P<instance_id>[a-z][-a-z0-9]*)/'
     r'databases/(?P<database_id>[a-z][a-z0-9_\-]*[a-z0-9])$'
     )
-
-register_type(admin_v1_pb2.Database)
-register_type(admin_v1_pb2.CreateDatabaseMetadata)
-register_type(admin_v1_pb2.UpdateDatabaseDdlMetadata)
 
 
 class Database(object):
@@ -205,7 +200,6 @@ class Database(object):
                 ))
             raise
 
-        future.caller_metadata = {'request_type': 'CreateDatabase'}
         return future
 
     def exists(self):
@@ -252,7 +246,7 @@ class Database(object):
         See
         https://cloud.google.com/spanner/reference/rpc/google.spanner.admin.database.v1#google.spanner.admin.database.v1.DatabaseAdmin.UpdateDatabase
 
-        :rtype: :class:`google.cloud.operation.Operation`
+        :rtype: :class:`google.cloud.future.operation.Operation`
         :returns: an operation instance
         """
         client = self._instance._client
@@ -267,7 +261,6 @@ class Database(object):
                 raise NotFound(self.name)
             raise
 
-        future.caller_metadata = {'request_type': 'UpdateDatabaseDdl'}
         return future
 
     def drop(self):
