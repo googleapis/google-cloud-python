a9a140431ab5eddb99de036bafbf8fb0136926ed
Jon Wayne Parrott <jonwayne@google.com>
2017-07-27 11:21:30 -0700

    Remove httplib2, replace with Requests (#3674)
    * Core: remove httplib2, replace with Requests
    
    Additionally remove make_exception in favor of from_http_status and from_http_response.
    
    * Datastore: replace httplib2 with Requests
    
    * DNS: replace httplib2 with Requests
    
    * Error Reporting: replace httplib2 with requests
    
    * Language: replace httplib2 with Requests
    
    * Logging: replace httplib2 with requests
    
    * Monitoring: replace httplib2 with Requests
    
    * Pubsub: replace httplib2 with Requests
    
    * Resource Manager: replace httplib2 with Requests
    
    * Runtimeconfig: replace httplib2 with Requests
    
    * Speech: replace httplib2 with Requests
    
    * Storage: replace httplib2 with Requests
    
    * BigQuery: replace httplib2 with Requests
    
    * Translate: replace httplib2 with Requests
    
    * Vision: replace httplib2 with Requests

--- packages/google-cloud-language/google/cloud/language/client.py
+++ packages/google-cloud-language/google/cloud/language/client.py
@@ -33,10 +33,10 @@ class Client(client_module.Client):
                         passed), falls back to the default inferred from the
                         environment.
 
-    :type _http: :class:`~httplib2.Http`
+    :type _http: :class:`~requests.Session`
     :param _http: (Optional) HTTP object to make requests. Can be any object
                   that defines ``request()`` with the same interface as
-                  :meth:`~httplib2.Http.request`. If not passed, an
+                  :meth:`requests.Session.request`. If not passed, an
                   ``_http`` object is created that is bound to the
                   ``credentials`` for the current object.
                   This parameter should be considered private, and could
