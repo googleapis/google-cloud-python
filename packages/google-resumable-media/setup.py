ec78391646ab48f8bfe627216fd8f45e9c96eeb3
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-01 11:43:58 -0700

    Updating version to 0.2.2.

--- packages/google-resumable-media/setup.py
+++ packages/google-resumable-media/setup.py
@@ -29,7 +29,7 @@ REQUIREMENTS = [
 
 setuptools.setup(
     name='google-resumable-media',
-    version='0.2.1',
+    version='0.2.2',
     description='Utilities for Google Media Downloads and Resumable Uploads',
     author='Google Cloud Platform',
     author_email='googleapis-publisher@google.com',
