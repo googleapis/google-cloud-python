e9971fa7b71cb480e0c528edd6eb45ee8fbddbe6
Luke Sneeringer <lukesneeringer@google.com>
2017-08-04 09:37:31 -0700

    Fix import order.

--- core/google/cloud/obselete.py
+++ core/google/cloud/obselete.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pkg_resources
 import warnings
+
+import pkg_resources
 
 
 def complain(distribution_name):
