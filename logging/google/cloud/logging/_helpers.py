87af48a4b7f2498d3d3b292c3f5f02cd883d768c
Angela Li <yanhuil@google.com>
2017-08-23 15:08:22 -0700

    Logging: Use metadata server to detect GKE environment (#3856)

--- logging/google/cloud/logging/_helpers.py
+++ logging/google/cloud/logging/_helpers.py
@@ -14,10 +14,16 @@
 
 """Common logging helpers."""
 
+import requests
 
 from google.cloud.logging.entries import ProtobufEntry
 from google.cloud.logging.entries import StructEntry
 from google.cloud.logging.entries import TextEntry
+
+METADATA_URL = 'http://metadata/computeMetadata/v1/'
+METADATA_HEADERS = {
+    'Metadata-Flavor': 'Google'
+}
 
 
 def entry_from_resource(resource, client, loggers):
@@ -46,3 +52,32 @@ def entry_from_resource(resource, client, loggers):
         return ProtobufEntry.from_api_repr(resource, client, loggers)
 
     raise ValueError('Cannot parse log entry resource.')
+
+
+def retrieve_metadata_server(metadata_key):
+    """Retrieve the metadata key in the metadata server.
+
+    See: https://cloud.google.com/compute/docs/storing-retrieving-metadata
+
+    :type metadata_key: str
+    :param metadata_key: Key of the metadata which will form the url. You can
+                         also supply query parameters after the metadata key.
+                         e.g. "tags?alt=json"
+
+    :rtype: str
+    :returns: The value of the metadata key returned by the metadata server.
+    """
+    url = METADATA_URL + metadata_key
+
+    try:
+        response = requests.get(url, headers=METADATA_HEADERS)
+
+        if response.status_code == requests.codes.ok:
+            return response.text
+
+    except requests.exceptions.RequestException:
+        # Ignore the exception, connection failed means the attribute does not
+        # exist in the metadata server.
+        pass
+
+    return None
