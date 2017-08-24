882e66e8924e64c1caf307fc8f9387b0fae4d68c
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-24 14:55:56 -0700

    Upgrading Spanner to 0.27.0 after google-cloud-core update. (#3873)

--- packages/google-cloud-spanner/setup.py
+++ packages/google-cloud-spanner/setup.py
@@ -60,7 +60,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-spanner',
-    version='0.26.0',
+    version='0.27.0',
     description='Python Client for Cloud Spanner',
     long_description=README,
     namespace_packages=[
