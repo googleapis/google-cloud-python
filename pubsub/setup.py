23ef4d694d61824a6f0d491f24c27ffe76b96db3
Luke Sneeringer <lukesneeringer@google.com>
2017-08-24 15:41:09 -0700

    Update Pub/Sub version number to 0.28.2

--- pubsub/setup.py
+++ pubsub/setup.py
@@ -61,7 +61,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-pubsub',
-    version='0.28.1',
+    version='0.28.2',
     description='Python Client for Google Cloud Pub/Sub',
     long_description=README,
     namespace_packages=[
