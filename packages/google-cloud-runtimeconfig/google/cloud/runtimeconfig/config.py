ba7b9397f0f316a31871740e46f7472e44d4d136
Luke Sneeringer <luke@sneeringer.com>
2017-09-01 13:22:56 -0700

    Typo correction. (#3910)

--- packages/google-cloud-runtimeconfig/google/cloud/runtimeconfig/config.py
+++ packages/google-cloud-runtimeconfig/google/cloud/runtimeconfig/config.py
@@ -186,8 +186,8 @@ class Config(object):
 
           >>> from google.cloud import runtimeconfig
           >>> client = runtimeconfig.Client()
-          >>> config = client.get_config('my-config')
-          >>> print(config.get_varialbe('variable-name'))
+          >>> config = client.config('my-config')
+          >>> print(config.get_variable('variable-name'))
           <Variable: my-config, variable-name>
           >>> print(config.get_variable('does-not-exist'))
           None
