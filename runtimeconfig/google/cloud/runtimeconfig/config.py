da74ebc660ff64578f3608c3b85ccc9099f560d8
Tres Seaver <tseaver@palladion.com>
2017-09-01 16:12:46 -0400

    Fix 'client.get_config', too
    Closes #3894.

--- runtimeconfig/google/cloud/runtimeconfig/config.py
+++ runtimeconfig/google/cloud/runtimeconfig/config.py
@@ -186,7 +186,7 @@ class Config(object):
 
           >>> from google.cloud import runtimeconfig
           >>> client = runtimeconfig.Client()
-          >>> config = client.get_config('my-config')
+          >>> config = client.config('my-config')
           >>> print(config.get_variable('variable-name'))
           <Variable: my-config, variable-name>
           >>> print(config.get_variable('does-not-exist'))
