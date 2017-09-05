f18a0b71ce0823ae9723c38a5ef85be2a35ea09e
Luke Sneeringer <luke@sneeringer.com>
2017-09-05 13:38:34 -0700

    Use Sphinx 1.6.3 on local and RTD. (#3916)

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ import pkg_resources
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.6.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
