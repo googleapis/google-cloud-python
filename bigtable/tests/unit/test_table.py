454dd9c5bf5a8ceb816fc1c637242086d27cf5bd
Cal Peyser <cpeyser@google.com>
2017-09-08 14:25:32 -0400

    Adding RPC retries to Bigtable.

--- bigtable/tests/unit/test_table.py
+++ bigtable/tests/unit/test_table.py
@@ -493,7 +493,8 @@ class TestTable(unittest.TestCase):
         from google.cloud._testing import _Monkey
         from tests.unit._testing import _FakeStub
         from google.cloud.bigtable.row_data import PartialRowsData
-        from google.cloud.bigtable import table as MUT
+        from google.cloud.bigtable import retry as MUT
+        from google.cloud.bigtable.retry import ReadRowsIterator
 
         client = _Client()
         instance = _Instance(self.INSTANCE_NAME, client=client)
@@ -513,20 +514,18 @@ class TestTable(unittest.TestCase):
         # Patch the stub used by the API method.
         client._data_stub = stub = _FakeStub(response_iterator)
 
-        # Create expected_result.
-        expected_result = PartialRowsData(response_iterator)
-
-        # Perform the method and check the result.
         start_key = b'start-key'
         end_key = b'end-key'
         filter_obj = object()
         limit = 22
         with _Monkey(MUT, _create_row_request=mock_create_row_request):
+            # Perform the method and check the result.
             result = table.read_rows(
                 start_key=start_key, end_key=end_key, filter_=filter_obj,
                 limit=limit)
 
-        self.assertEqual(result, expected_result)
+        self.assertIsInstance(result._response_iterator, ReadRowsIterator)
+        self.assertEqual(result._response_iterator.client, client)
         self.assertEqual(stub.method_calls, [(
             'ReadRows',
             (request_pb,),
@@ -534,12 +533,169 @@ class TestTable(unittest.TestCase):
         )])
         created_kwargs = {
             'start_key': start_key,
+            'end_inclusive': False,
             'end_key': end_key,
             'filter_': filter_obj,
             'limit': limit,
-            'end_inclusive': False,
+            'start_key_closed': True,
         }
         self.assertEqual(mock_created, [(table.name, created_kwargs)])
+
+    def test_read_rows_one_chunk(self):
+        from google.cloud._testing import _Monkey
+        from tests.unit._testing import _FakeStub
+        from google.cloud.bigtable import retry as MUT
+        from google.cloud.bigtable.retry import ReadRowsIterator
+        from google.cloud.bigtable.row_data import Cell
+        from google.cloud.bigtable.row_data import PartialRowsData
+
+        client = _Client()
+        instance = _Instance(self.INSTANCE_NAME, client=client)
+        table = self._make_one(self.TABLE_ID, instance)
+
+        # Create request_pb
+        request_pb = object()  # Returned by our mock.
+        mock_created = []
+
+        def mock_create_row_request(table_name, **kwargs):
+            mock_created.append((table_name, kwargs))
+            return request_pb
+
+        # Create response_iterator
+        chunk = _ReadRowsResponseCellChunkPB(
+            row_key=self.ROW_KEY,
+            family_name=self.FAMILY_NAME,
+            qualifier=self.QUALIFIER,
+            timestamp_micros=self.TIMESTAMP_MICROS,
+            value=self.VALUE,
+            commit_row=True,
+        )
+        response_pb = _ReadRowsResponsePB(chunks=[chunk])
+        response_iterator = iter([response_pb])
+
+        # Patch the stub used by the API method.
+        client._data_stub = stub = _FakeStub(response_iterator)
+
+        start_key = b'start-key'
+        end_key = b'end-key'
+        filter_obj = object()
+        limit = 22
+        with _Monkey(MUT, _create_row_request=mock_create_row_request):
+            # Perform the method and check the result.
+            result = table.read_rows(
+                start_key=start_key, end_key=end_key, filter_=filter_obj,
+                limit=limit)
+            result.consume_all()
+
+    def test_read_rows_retry_timeout(self):
+        from google.cloud._testing import _Monkey
+        from tests.unit._testing import _CustomFakeStub
+        from google.cloud.bigtable.row_data import PartialRowsData
+        from google.cloud.bigtable import retry as MUT
+        from google.cloud.bigtable.retry import ReadRowsIterator
+        from google.gax import BackoffSettings
+        from google.gax.errors import RetryError
+        from grpc import StatusCode, RpcError
+        import time
+
+        client = _Client()
+        instance = _Instance(self.INSTANCE_NAME, client=client)
+        table = self._make_one(self.TABLE_ID, instance)
+
+        # Create request_pb
+        request_pb = object()  # Returned by our mock.
+        mock_created = []
+
+        def mock_create_row_request(table_name, **kwargs):
+            mock_created.append((table_name, kwargs))
+            return request_pb
+
+        # Create a slow response iterator to cause a timeout
+        class MockTimeoutError(RpcError):
+            def code(self):
+                return StatusCode.DEADLINE_EXCEEDED
+
+        def _wait_then_raise():
+            time.sleep(0.1)
+            raise MockTimeoutError()
+
+        # Patch the stub used by the API method.  The stub should create a new
+        # slow_iterator every time its queried.
+        def make_slow_iterator():
+            return (_wait_then_raise() for i in range(10))
+        client._data_stub = stub = _CustomFakeStub(make_slow_iterator)
+
+        # Set to timeout before RPC completes
+        test_backoff_settings = BackoffSettings(
+            initial_retry_delay_millis=10,
+            retry_delay_multiplier=0.3,
+            max_retry_delay_millis=30000,
+            initial_rpc_timeout_millis=1000,
+            rpc_timeout_multiplier=1.0,
+            max_rpc_timeout_millis=25 * 60 * 1000,
+            total_timeout_millis=1000
+        )
+
+        start_key = b'start-key'
+        end_key = b'end-key'
+        filter_obj = object()
+        limit = 22
+        with _Monkey(MUT, _create_row_request=mock_create_row_request):
+            # Verify that a RetryError is thrown on read.
+            result = table.read_rows(
+                start_key=start_key, end_key=end_key, filter_=filter_obj,
+                limit=limit, backoff_settings=test_backoff_settings)
+            with self.assertRaises(RetryError):
+                result.consume_next()
+
+    def test_read_rows_non_idempotent_error_throws(self):
+        from google.cloud._testing import _Monkey
+        from tests.unit._testing import _CustomFakeStub
+        from google.cloud.bigtable.row_data import PartialRowsData
+        from google.cloud.bigtable import retry as MUT
+        from google.cloud.bigtable.retry import ReadRowsIterator
+        from google.gax import BackoffSettings
+        from google.gax.errors import RetryError
+        from grpc import StatusCode, RpcError
+        import time
+
+        client = _Client()
+        instance = _Instance(self.INSTANCE_NAME, client=client)
+        table = self._make_one(self.TABLE_ID, instance)
+
+        # Create request_pb
+        request_pb = object()  # Returned by our mock.
+        mock_created = []
+
+        def mock_create_row_request(table_name, **kwargs):
+            mock_created.append((table_name, kwargs))
+            return request_pb
+
+        # Create response iterator that raises a non-idempotent exception
+        class MockNonIdempotentError(RpcError):
+            def code(self):
+                return StatusCode.RESOURCE_EXHAUSTED
+
+        def _raise():
+            raise MockNonIdempotentError()
+
+        # Patch the stub used by the API method.  The stub should create a new
+        # slow_iterator every time its queried.
+        def make_raising_iterator():
+            return (_raise() for i in range(10))
+        client._data_stub = stub = _CustomFakeStub(make_raising_iterator)
+
+        start_key = b'start-key'
+        end_key = b'end-key'
+        filter_obj = object()
+        limit = 22
+        with _Monkey(MUT, _create_row_request=mock_create_row_request):
+            # Verify that a RetryError is thrown on read.
+            result = table.read_rows(
+                start_key=start_key, end_key=end_key, filter_=filter_obj,
+                limit=limit)
+            with self.assertRaises(MockNonIdempotentError):
+                result.consume_next()
 
     def test_sample_row_keys(self):
         from tests.unit._testing import _FakeStub
@@ -573,12 +729,14 @@ class TestTable(unittest.TestCase):
 class Test__create_row_request(unittest.TestCase):
 
     def _call_fut(self, table_name, row_key=None, start_key=None, end_key=None,
-                  filter_=None, limit=None, end_inclusive=False):
-        from google.cloud.bigtable.table import _create_row_request
+                  filter_=None, limit=None, start_key_closed=True,
+                  end_inclusive=False):
+        from google.cloud.bigtable.retry import _create_row_request
 
         return _create_row_request(
             table_name, row_key=row_key, start_key=start_key, end_key=end_key,
-            filter_=filter_, limit=limit, end_inclusive=end_inclusive)
+            start_key_closed=start_key_closed, filter_=filter_,
+            limit=limit, end_inclusive=end_inclusive)
 
     def test_table_name_only(self):
         table_name = 'table_name'
@@ -601,12 +759,21 @@ class Test__create_row_request(unittest.TestCase):
         expected_result.rows.row_keys.append(row_key)
         self.assertEqual(result, expected_result)
 
-    def test_row_range_start_key(self):
+    def test_row_range_start_key_closed(self):
         table_name = 'table_name'
         start_key = b'start_key'
         result = self._call_fut(table_name, start_key=start_key)
         expected_result = _ReadRowsRequestPB(table_name=table_name)
         expected_result.rows.row_ranges.add(start_key_closed=start_key)
+        self.assertEqual(result, expected_result)
+
+    def test_row_range_start_key_open(self):
+        table_name = 'table_name'
+        start_key = b'start_key'
+        result = self._call_fut(table_name, start_key=start_key,
+            start_key_closed=False)
+        expected_result = _ReadRowsRequestPB(table_name=table_name)
+        expected_result.rows.row_ranges.add(start_key_open=start_key)
         self.assertEqual(result, expected_result)
 
     def test_row_range_end_key(self):
