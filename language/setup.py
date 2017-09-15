e49c5813f6ccee56e7974c6953a738933079f098
Luke Sneeringer <luke@sneeringer.com>
2017-09-15 11:03:03 -0700

    Natural Language 0.29.0 (#3972)

--- language/setup.py
+++ language/setup.py
@@ -61,7 +61,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name='google-cloud-language',
-    version='0.28.0',
+    version='0.29.0',
     description='Python Client for Google Cloud Natural Language',
     long_description=README,
     namespace_packages=[
