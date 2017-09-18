ae832232fd2f68b2bd8fc96becee9c31e1ce7011
Jonathan Amsterdam <jba@google.com>
2017-09-18 18:06:15 -0400

    fix lint agian

--- bigquery/tests/unit/test_dataset.py
+++ bigquery/tests/unit/test_dataset.py
@@ -700,9 +700,5 @@ class _Connection(object):
 
         self._requested.append(kw)
 
-        try:
-            response, self._responses = self._responses[0], self._responses[1:]
-        except IndexError:
-            raise NotFound('miss')
-        else:
-            return response
+        response, self._responses = self._responses[0], self._responses[1:]
+        return response
