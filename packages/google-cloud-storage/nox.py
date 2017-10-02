c0b80712244575ad5f568b23f3b672e48e222bc6
Tres Seaver <tseaver@palladion.com>
2017-10-02 16:05:03 -0400

    Pass '*posargs' to py.test when running system tests. (#4099)

--- packages/google-cloud-storage/nox.py
+++ packages/google-cloud-storage/nox.py
@@ -74,7 +74,7 @@ def system_tests(session, python_version):
     session.install('.')
 
     # Run py.test against the system tests.
-    session.run('py.test', '--quiet', 'tests/system.py')
+    session.run('py.test', '--quiet', 'tests/system.py', *session.posargs)
 
 
 @nox.session
