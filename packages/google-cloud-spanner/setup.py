60f2cf6ea9e6c453267f496677fbf26cffacdcd8
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-02 10:15:57 -0700

    Avoiding `grpcio==1.6.0` in deps. (#4096)
    This is due to `google-gax` doing the same, which has broken
    RTD builds:
    
    https://readthedocs.org/projects/google-cloud-python/builds/6063446/
    
    The motivation for avoiding `grpcio==1.6.0` is:
    
    https://github.com/grpc/grpc/issues/12455

--- packages/google-cloud-spanner/setup.py
+++ packages/google-cloud-spanner/setup.py
@@ -54,7 +54,7 @@ REQUIREMENTS = [
     'google-auth >= 1.1.0',
     'google-cloud-core >= 0.27.0, < 0.28dev',
     'google-gax>=0.15.15, <0.16dev',
-    'googleapis-common-protos[grpc]>=1.5.2, <2.0dev',
+    'googleapis-common-protos[grpc]>=1.5.3, <2.0dev',
     'grpc-google-iam-v1>=0.11.4, <0.12dev',
     'requests>=2.18.4, <3.0dev',
 ]
