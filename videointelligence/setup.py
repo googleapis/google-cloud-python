bf11538e77fdcb7e16713d58bd78b2ad69d35133
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-03 16:39:57 -0700

    Adding `googleapis-common-protos` back to `videointelligence`.

--- videointelligence/setup.py
+++ videointelligence/setup.py
@@ -38,6 +38,7 @@ setup(
     ],
     packages=find_packages(exclude=('tests*',)),
     install_requires=(
+        'googleapis-common-protos >= 1.5.3, < 2.0dev',
         'google-gax >= 0.15.14, < 0.16dev',
         'grpcio >= 1.2.0, < 1.6dev',
         'six >= 1.10.0',
