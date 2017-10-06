76fae2eddd2174699f71c4fbba23416079ecb92a
Jon Wayne Parrott <jonwayne@google.com>
2017-10-06 13:53:05 -0700

    Add google.api.core.operations_v1 (#4081)

--- packages/google-cloud-core/google/api/core/operation.py
+++ packages/google-cloud-core/google/api/core/operation.py
@@ -187,7 +187,7 @@ def _cancel_http(api_request, operation_name):
 
 
 def from_http_json(operation, api_request, result_type, **kwargs):
-    """Create an operation future from using a HTTP/JSON client.
+    """Create an operation future using a HTTP/JSON client.
 
     This interacts with the long-running operations `service`_ (specific
     to a given API) vis `HTTP/JSON`_.
@@ -243,7 +243,7 @@ def _cancel_grpc(operations_stub, operation_name):
 
 
 def from_grpc(operation, operations_stub, result_type, **kwargs):
-    """Create an operation future from using a gRPC client.
+    """Create an operation future using a gRPC client.
 
     This interacts with the long-running operations `service`_ (specific
     to a given API) via gRPC.
@@ -266,4 +266,31 @@ def from_grpc(operation, operations_stub, result_type, **kwargs):
         _refresh_grpc, operations_stub, operation.name)
     cancel = functools.partial(
         _cancel_grpc, operations_stub, operation.name)
+    return Operation(operation, refresh, cancel, result_type, **kwargs)
+
+
+def from_gapic(operation, operations_client, result_type, **kwargs):
+    """Create an operation future from a gapic client.
+
+    This interacts with the long-running operations `service`_ (specific
+    to a given API) via a gapic client.
+
+    .. _service: https://github.com/googleapis/googleapis/blob/\
+                 050400df0fdb16f63b63e9dee53819044bffc857/\
+                 google/longrunning/operations.proto#L38
+
+    Args:
+        operation (google.longrunning.operations_pb2.Operation): The operation.
+        operations_client (google.api.core.operations_v1.OperationsClient):
+            The operations client.
+        result_type (type): The protobuf result type.
+        kwargs: Keyword args passed into the :class:`Operation` constructor.
+
+    Returns:
+        Operation: The operation future to track the given operation.
+    """
+    refresh = functools.partial(
+        operations_client.get_operation, operation.name)
+    cancel = functools.partial(
+        operations_client.cancel_operation, operation.name)
     return Operation(operation, refresh, cancel, result_type, **kwargs)
