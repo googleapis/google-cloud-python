863d293f16f7df1962a4a367b8bf913daefe02fe
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-13 15:33:01 -0700

    Rolling back the dependency update for trace.
    This is so that an "off-master" release can be done
    without being gated on a `google-cloud-core` release.

--- trace/setup.py
+++ trace/setup.py
@@ -9,7 +9,8 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'google-gax>=0.15.7, <0.16dev',
-    'google-cloud-core[grpc] >= 0.27.1, < 0.28dev',
+    'googleapis-common-protos[grpc]>=1.5.2, <2.0dev',
+    'google-cloud-core >= 0.27.0, < 0.28dev',
 ]
 
 setup(
