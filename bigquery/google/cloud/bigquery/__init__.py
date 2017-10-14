c70d6e42b313a293b61cb92002b0320712d88176
Jonathan Amsterdam <jba@users.noreply.github.com>
2017-10-14 07:10:15 -0400

    bigquery: classes to support external table config (#4182)
    * bigquery: classes to support external table config
    
    These classes will be used to configure external table
    definitions for queries (transient external tables)
    as well as external data configurations for tables (permanent
    external tables).
    
    This PR just establishes the classes. Subsequent PRs will
    wire them into query jobs and tables.

--- bigquery/google/cloud/bigquery/__init__.py
+++ bigquery/google/cloud/bigquery/__init__.py
@@ -43,6 +43,12 @@ from google.cloud.bigquery.query import UDFResource
 from google.cloud.bigquery.schema import SchemaField
 from google.cloud.bigquery.table import Table
 from google.cloud.bigquery.table import TableReference
+from google.cloud.bigquery.external_config import ExternalConfig
+from google.cloud.bigquery.external_config import BigtableOptions
+from google.cloud.bigquery.external_config import BigtableColumnFamily
+from google.cloud.bigquery.external_config import BigtableColumn
+from google.cloud.bigquery.external_config import CSVOptions
+from google.cloud.bigquery.external_config import GoogleSheetsOptions
 
 __all__ = [
     '__version__',
@@ -63,4 +69,10 @@ __all__ = [
     'TableReference',
     'UDFResource',
     'DEFAULT_RETRY',
+    'ExternalConfig',
+    'BigtableOptions',
+    'BigtableColumnFamily',
+    'BigtableColumn',
+    'CSVOptions',
+    'GoogleSheetsOptions',
 ]
