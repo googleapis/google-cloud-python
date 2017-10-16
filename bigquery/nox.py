397cf6216de6f1111b6baa2ceaaa51086cf8908d
Tim Swast <swast@google.com>
2017-10-16 10:37:37 -0700

    BigQuery: Use in-place development install for system and snippet tests.
    This will allow the use of the `-r` parameter for nox to work properly.

--- bigquery/nox.py
+++ bigquery/nox.py
@@ -76,7 +76,7 @@ def system_tests(session, python_version):
         os.path.join('..', 'storage'),
         os.path.join('..', 'test_utils'),
     )
-    session.install('.')
+    session.install('-e', '.')
 
     # Run py.test against the system tests.
     session.run(
@@ -109,7 +109,7 @@ def snippets_tests(session, python_version):
         os.path.join('..', 'storage'),
         os.path.join('..', 'test_utils'),
     )
-    session.install('.')
+    session.install('-e', '.')
 
     # Run py.test against the system tests.
     session.run(
