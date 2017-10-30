df14d234ce9fc2a4cdb8be0e0792fe5b59d35f79
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-30 15:45:44 -0700

    Adding **explicit** dependency on `google-api-core` to BQ.

--- bigquery/setup.py
+++ bigquery/setup.py
@@ -52,6 +52,7 @@ SETUP_BASE = {
 
 REQUIREMENTS = [
     'google-cloud-core >= 0.28.0, < 0.29dev',
+    'google-api-core >= 0.1.1, < 0.2.0dev',
     'google-auth >= 1.0.0',
     'google-resumable-media >= 0.2.1',
     'requests >= 2.18.0',
