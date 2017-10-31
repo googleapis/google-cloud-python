b7e5b912e3a0b0516e9b0f62e583dfca9b6fdaf3
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-31 12:54:01 -0700

    Making release for most packages.
    Every package except those that have already been released
    (`google-cloud-core`, `google-api-core`, `google-cloud-bigquery`):
    
    - `google-cloud`
    - `google-cloud-bigtable`
    - `google-cloud-datastore`
    - `google-cloud-dns`
    - `google-cloud-error-reporting`
    - `google-cloud-firestore`
    - `google-cloud-language`
    - `google-cloud-logging`
    - `google-cloud-monitoring`
    - `google-cloud-resource-manager`
    - `google-cloud-runtimeconfig`
    - `google-cloud-spanner`
    - `google-cloud-speech`
    - `google-cloud-storage`
    - `google-cloud-trace`
    - `google-cloud-translate`
    - `google-cloud-videointelligence`
    - `google-cloud-vision`

--- datastore/setup.py
+++ datastore/setup.py
@@ -52,13 +52,14 @@ SETUP_BASE = {
 
 REQUIREMENTS = [
     'google-cloud-core >= 0.28.0, < 0.29dev',
-    'google-gax>=0.15.7, <0.16dev',
+    'google-api-core >= 0.1.1, < 0.2.0dev',
+    'google-gax >= 0.15.7, < 0.16dev',
     'gapic-google-cloud-datastore-v1 >= 0.15.0, < 0.16dev',
 ]
 
 setup(
     name='google-cloud-datastore',
-    version='1.3.0',
+    version='1.4.0',
     description='Python Client for Google Cloud Datastore',
     long_description=README,
     namespace_packages=[
--- videointelligence/setup.py
+++ videointelligence/setup.py
@@ -29,7 +29,7 @@ setup(
     author='Google Cloud Platform',
     author_email='googleapis-publisher@google.com',
     name='google-cloud-videointelligence',
-    version='0.27.2',
+    version='0.28.0',
     description='Python Client for Google Cloud Video Intelligence',
     long_description=readme,
     namespace_packages=[
