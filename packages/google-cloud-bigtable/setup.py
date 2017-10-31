37ddaf212295798098d522ff27cb29fbbd40d968
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-31 15:43:51 -0700

    Marking all remaining versions as "dev". (#4299)
    This is to make it clear the code is between releases. Any code
    that relies on a **new** feature (e.g. of `google-api-core`) will
    then be able to **explicitly** make this clear by using the
    lower bound of the `devN` version.
    
    Fixes #4208.
    
    See: https://snarky.ca/how-i-manage-package-version-numbers/

--- packages/google-cloud-bigtable/setup.py
+++ packages/google-cloud-bigtable/setup.py
@@ -58,7 +58,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-bigtable',
-    version='0.28.0',
+    version='0.28.1.dev1',
     description='Python Client for Google Cloud Bigtable',
     long_description=README,
     namespace_packages=[
