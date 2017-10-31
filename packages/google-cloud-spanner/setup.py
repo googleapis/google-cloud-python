ec83cb7a1dbe6cda0ec4c450cef4abcc2bb622d7
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-31 14:28:55 -0700

    Making release for most packages. (#4296)
    * Making release for most packages.
    
    Every package except those that have already been released
    (`google-cloud-core`, `google-api-core`, `google-cloud-bigquery`):
    
    - `google-cloud`
    - `google-cloud-bigtable`
    - `google-cloud-datastore`
    - `google-cloud-dns`
    - `google-cloud-error-reporting`
    - `google-cloud-firestore`
    - `google-cloud-language`
    - `google-cloud-logging`
    - `google-cloud-monitoring`
    - `google-cloud-resource-manager`
    - `google-cloud-runtimeconfig`
    - `google-cloud-spanner`
    - `google-cloud-speech`
    - `google-cloud-storage`
    - `google-cloud-trace`
    - `google-cloud-translate`
    - `google-cloud-videointelligence`
    - `google-cloud-vision`
    
    * Adding changelog files for each package.

--- packages/google-cloud-spanner/setup.py
+++ packages/google-cloud-spanner/setup.py
@@ -53,14 +53,15 @@ SETUP_BASE = {
 REQUIREMENTS = [
     'google-auth >= 1.1.0',
     'google-cloud-core[grpc] >= 0.28.0, < 0.29dev',
-    'google-gax>=0.15.15, <0.16dev',
-    'grpc-google-iam-v1>=0.11.4, <0.12dev',
-    'requests>=2.18.4, <3.0dev',
+    'google-api-core >= 0.1.1, < 0.2.0dev',
+    'google-gax >= 0.15.15, < 0.16dev',
+    'grpc-google-iam-v1 >= 0.11.4, < 0.12dev',
+    'requests >= 2.18.4, < 3.0dev',
 ]
 
 setup(
     name='google-cloud-spanner',
-    version='0.28.0',
+    version='0.29.0',
     description='Python Client for Cloud Spanner',
     long_description=README,
     namespace_packages=[
