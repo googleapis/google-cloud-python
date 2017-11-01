ad1c2ab90cc8e6f4ead173ceb434f67571a2775c
Tres Seaver <tseaver@palladion.com>
2017-11-01 13:07:22 -0400

    Add 'Client.create_anonymous_client' factory method.

--- storage/setup.py
+++ storage/setup.py
@@ -53,7 +53,7 @@ SETUP_BASE = {
 REQUIREMENTS = [
     'google-cloud-core >= 0.28.0, < 0.29dev',
     'google-api-core >= 0.1.1, < 0.2.0dev',
-    'google-auth >= 1.0.0',
+    'google-auth >= 1.2.0',
     'google-resumable-media >= 0.3.1',
     'requests >= 2.18.0',
 ]
