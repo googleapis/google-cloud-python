ec9f36f52de0a63a9092e3b647ea7127c7889976
Danny Hermes <daniel.j.hermes@gmail.com>
2017-11-01 21:47:55 -0700

    Making a `nox -s default` session for all packages. (#4324)
    * Making a `nox -s default` session for all packages.
    
    * Using "default" `nox` session on AppVeyor.
    
    This was 32-bit or 64-bit Python can be used, depending
    on which is the active `python` / the active `nox.exe`.

--- packages/google-cloud-speech/nox.py
+++ packages/google-cloud-speech/nox.py
@@ -26,16 +26,14 @@ LOCAL_DEPS = (
 
 
 @nox.session
-@nox.parametrize('py', ['2.7', '3.4', '3.5', '3.6'])
-def unit(session, py):
-    """Run the unit test suite."""
+def default(session):
+    """Default unit test session.
 
-    # Run unit tests against all supported versions of Python.
-    session.interpreter = 'python{}'.format(py)
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'unit-' + py
-
+    This is intended to be run **without** an interpreter set, so
+    that the current ``python`` (on the ``PATH``) or the version of
+    Python corresponding to the ``nox`` binary the ``PATH`` can
+    run the tests.
+    """
     # Install all test dependencies, then install this package in-place.
     session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
     session.install('-e', '.')
@@ -52,6 +50,20 @@ def unit(session, py):
         '--cov-fail-under=0',
         os.path.join('tests', 'unit'),
     )
+
+
+@nox.session
+@nox.parametrize('py', ['2.7', '3.4', '3.5', '3.6'])
+def unit(session, py):
+    """Run the unit test suite."""
+
+    # Run unit tests against all supported versions of Python.
+    session.interpreter = 'python{}'.format(py)
+
+    # Set the virtualenv dirname.
+    session.virtualenv_dirname = 'unit-' + py
+
+    default(session)
 
 
 @nox.session
