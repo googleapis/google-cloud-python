88f6409b2b0345c7b40af127bcf1fc2fa9c711d1
Jon Wayne Parrott <jonwayne@google.com>
2017-11-09 10:13:17 -0800

    Add client_info to api_core.gapic_v1 __all__, tweak docstring (#4367)
    * Add client_info to api_core.gapic_v1 __all__, tweak docstring
    
    * Fix lint
    
    * fix missing word

--- api_core/google/api_core/gapic_v1/__init__.py
+++ api_core/google/api_core/gapic_v1/__init__.py
@@ -12,11 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from google.api_core.gapic_v1 import client_info
 from google.api_core.gapic_v1 import config
 from google.api_core.gapic_v1 import method
 from google.api_core.gapic_v1 import routing_header
 
 __all__ = [
+    'client_info',
     'config',
     'method',
     'routing_header',
