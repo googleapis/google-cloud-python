98302b6e2e3dc225d7c59735d0f969f341160cf1
chemelnucfin <alan.c.wu@gmail.com>
2017-11-08 21:51:38 -0800

    Allow specifying read consistency (#4343)
    * Closes #4340 - Specify Read Consistency
    
    * review changes
    
    * merge conflicts; correct imports

--- packages/google-cloud-datastore/tests/unit/test_client.py
+++ packages/google-cloud-datastore/tests/unit/test_client.py
@@ -1033,41 +1033,6 @@ class TestClient(unittest.TestCase):
                 client, project=self.PROJECT, namespace=namespace2, kind=kind)
 
 
-class Test__get_read_options(unittest.TestCase):
-
-    def _call_fut(self, eventual, transaction_id):
-        from google.cloud.datastore.client import _get_read_options
-
-        return _get_read_options(eventual, transaction_id)
-
-    def test_eventual_w_transaction(self):
-        with self.assertRaises(ValueError):
-            self._call_fut(True, b'123')
-
-    def test_eventual_wo_transaction(self):
-        from google.cloud.datastore_v1.proto import datastore_pb2
-
-        read_options = self._call_fut(True, None)
-        expected = datastore_pb2.ReadOptions(
-            read_consistency=datastore_pb2.ReadOptions.EVENTUAL)
-        self.assertEqual(read_options, expected)
-
-    def test_default_w_transaction(self):
-        from google.cloud.datastore_v1.proto import datastore_pb2
-
-        txn_id = b'123abc-easy-as'
-        read_options = self._call_fut(False, txn_id)
-        expected = datastore_pb2.ReadOptions(transaction=txn_id)
-        self.assertEqual(read_options, expected)
-
-    def test_default_wo_transaction(self):
-        from google.cloud.datastore_v1.proto import datastore_pb2
-
-        read_options = self._call_fut(False, None)
-        expected = datastore_pb2.ReadOptions()
-        self.assertEqual(read_options, expected)
-
-
 class _NoCommitBatch(object):
 
     def __init__(self, client):
