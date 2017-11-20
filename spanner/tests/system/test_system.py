0ff24b711efda31c0b5e89ba760abaa7a7e2fbfd
Alan Wu <alan.c.wu@gmail.com>
2017-11-20 04:56:06 -0800

    Spanner: Query NaN

--- spanner/tests/system/test_system.py
+++ spanner/tests/system/test_system.py
@@ -1165,6 +1165,15 @@ class TestSessionAPI(unittest.TestCase, _TestData):
             expected=[(107,)],
         )
 
+        # Find NaN
+        self._check_sql_results(
+            snapshot,
+            sql='SELECT eye_d FROM all_types WHERE IS_NAN(approx_value)',
+            params={'nan': float('nan')},
+            param_types={'nan': Type(code=FLOAT64)},
+            expected=[(1207,), (2000,)],
+        )
+
         rows = list(snapshot.execute_sql(
             'SELECT'
             ' [CAST("-inf" AS FLOAT64),'
