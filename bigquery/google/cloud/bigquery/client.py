f91a252f1077b9aa43b572e921b17c2b1cee20da
Alix Hamilton <alixh@google.com>
2017-11-21 14:14:45 -0800

    BigQuery: removes Client.query_rows() (#4429)

--- bigquery/google/cloud/bigquery/client.py
+++ bigquery/google/cloud/bigquery/client.py
@@ -17,7 +17,6 @@
 from __future__ import absolute_import
 
 import collections
-import concurrent.futures
 import functools
 import os
 import uuid
@@ -29,8 +28,6 @@ from google.resumable_media.requests import MultipartUpload
 from google.resumable_media.requests import ResumableUpload
 
 from google.api_core import page_iterator
-from google.api_core.exceptions import GoogleAPICallError
-from google.api_core.exceptions import NotFound
 from google.cloud import exceptions
 from google.cloud.client import ClientWithProject
 
@@ -1144,67 +1141,6 @@ class Client(ClientWithProject):
 
         return errors
 
-    def query_rows(
-            self, query, job_config=None, job_id=None, job_id_prefix=None,
-            timeout=None, retry=DEFAULT_RETRY):
-        """Start a query job and wait for the results.
-
-        See
-        https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs#configuration.query
-
-        :type query: str
-        :param query:
-            SQL query to be executed. Defaults to the standard SQL dialect.
-            Use the ``job_config`` parameter to change dialects.
-
-        :type job_config: :class:`google.cloud.bigquery.job.QueryJobConfig`
-        :param job_config: (Optional) Extra configuration options for the job.
-
-        :type job_id: str
-        :param job_id: (Optional) ID to use for the query job.
-
-        :type job_id_prefix: str or ``NoneType``
-        :param job_id_prefix: (Optional) the user-provided prefix for a
-                              randomly generated job ID. This parameter will be
-                              ignored if a ``job_id`` is also given.
-
-        :type timeout: float
-        :param timeout:
-            (Optional) How long (in seconds) to wait for job to complete
-            before raising a :class:`concurrent.futures.TimeoutError`.
-
-        :rtype: :class:`~google.api_core.page_iterator.Iterator`
-        :returns:
-            Iterator of row data :class:`~google.cloud.bigquery.table.Row`-s.
-            During each page, the iterator will have the ``total_rows``
-            attribute set, which counts the total number of rows **in the
-            result set** (this is distinct from the total number of rows in
-            the current page: ``iterator.page.num_items``).
-
-        :raises:
-            :class:`~google.api_core.exceptions.GoogleAPICallError` if the
-            job failed or :class:`concurrent.futures.TimeoutError` if the job
-            did not complete in the given timeout.
-
-            When an exception happens, the query job will be cancelled on a
-            best-effort basis.
-        """
-        job_id = _make_job_id(job_id, job_id_prefix)
-
-        try:
-            job = self.query(
-                query, job_config=job_config, job_id=job_id, retry=retry)
-            rows_iterator = job.result(timeout=timeout)
-        except (GoogleAPICallError, concurrent.futures.TimeoutError):
-            try:
-                self.cancel_job(job_id)
-            except NotFound:
-                # It's OK if couldn't cancel because job never got created.
-                pass
-            raise
-
-        return rows_iterator
-
     def list_rows(self, table, selected_fields=None, max_results=None,
                   page_token=None, start_index=None, retry=DEFAULT_RETRY):
         """List the rows of the table.
@@ -1303,12 +1239,12 @@ class Client(ClientWithProject):
         """
         config = QueryJobConfig()
         config.use_legacy_sql = True  # required for '$' syntax
-        rows = self.query_rows(
+        query_job = self.query(
             'SELECT partition_id from [%s:%s.%s$__PARTITIONS_SUMMARY__]' %
             (table.project, table.dataset_id, table.table_id),
             job_config=config,
             retry=retry)
-        return [row[0] for row in rows]
+        return [row[0] for row in query_job]
 
 
 # pylint: disable=unused-argument
