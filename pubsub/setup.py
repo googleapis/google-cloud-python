f236dd3b63929e84bf90a429b9b504a02d3b8f82
Danny Hermes <daniel.j.hermes@gmail.com>
2017-12-11 09:19:48 -0800

    Pub / sub release 0.29.4.

--- pubsub/setup.py
+++ pubsub/setup.py
@@ -59,7 +59,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-pubsub',
-    version='0.29.3',
+    version='0.29.4',
     description='Python Client for Google Cloud Pub/Sub',
     long_description=README,
     namespace_packages=[
