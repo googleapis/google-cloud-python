86f2fff35df0791a1b8ae0fc310af19004ad9bd0
Luke Sneeringer <lukesneeringer@google.com>
2017-12-13 07:11:54 -0800

    Updates to address @jonparrott feedback.

--- container/setup.py
+++ container/setup.py
@@ -1,3 +1,17 @@
+# Copyright 2017, Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """A setup module for the GAPIC Google Container Engine API library.
 
 See:
@@ -36,13 +50,14 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
     ],
     description='GAPIC library for the Google Container Engine API',
+    zip_safe=False,
     include_package_data=True,
     long_description=long_description,
     install_requires=install_requires,
     license='Apache 2.0',
     packages=find_packages(exclude=('tests*',)),
     namespace_packages=['google', 'google.cloud'],
-    url='https://github.com/googleapis/googleapis')
+    url='https://github.com/GoogleCloudPlatform/google-cloud-python',
+)
