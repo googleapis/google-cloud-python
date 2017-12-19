355df9fa7fb90095c726ec14790937fd5387b60c
chemelnucfin <alan.c.wu@gmail.com>
2017-12-19 10:08:12 -0800

    Spanner: minor typo (#4625)
    just merging.

--- packages/google-cloud-spanner/google/cloud/spanner_v1/session.py
+++ packages/google-cloud-spanner/google/cloud/spanner_v1/session.py
@@ -182,7 +182,7 @@ class Session(object):
                       table's primary key
 
         :type limit: int
-        :param limit: (Optional) maxiumn number of rows to return
+        :param limit: (Optional) maximum number of rows to return
 
         :rtype: :class:`~google.cloud.spanner_v1.streamed.StreamedResultSet`
         :returns: a result set instance which can be used to consume rows.
