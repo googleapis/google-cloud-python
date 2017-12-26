7b9dec1f2c4893315cc143f729a96706910614dc
Aneep Tandel <aneep@objectsbydesign.com>
2017-12-26 17:58:16 +0530

    Fix retry error for python 2

--- api_core/google/api_core/general_helpers.py
+++ api_core/google/api_core/general_helpers.py
@@ -33,6 +33,7 @@ def wraps(wrapped):
 
 
 def my_decorator(f):
+    """A functools.wraps helper that handles partial objects on Python 2."""
     @functools.wraps(f)
     def wrapper(*args, **kwds):
         return f(*args, **kwds)
