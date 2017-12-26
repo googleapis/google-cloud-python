be2f2acb3c79af9984cbf08bce3ae66f01ecd3fa
Aneep Tandel <aneep@objectsbydesign.com>
2017-12-26 17:17:58 +0530

    Fix retry error for python 2

--- api_core/google/api_core/general_helpers.py
+++ api_core/google/api_core/general_helpers.py
@@ -36,6 +36,5 @@ def wraps(wrapped):
 def my_decorator(f):
     @wraps(f)
     def wrapper(*args, **kwds):
-        print 'Calling decorated function'
         return f(*args, **kwds)
     return wrapper
