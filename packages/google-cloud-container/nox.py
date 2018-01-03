d33c8a9aac7af7302aa0a4681ff5e837666a4182
chemelnucfin <alan.c.wu@gmail.com>
2018-01-02 22:04:03 -0800

    nox unittest updates (#4646)

--- packages/google-cloud-container/nox.py
+++ packages/google-cloud-container/nox.py
@@ -13,29 +13,58 @@
 # limitations under the License.
 
 from __future__ import absolute_import
+
 import os
 
 import nox
 
 
+LOCAL_DEPS = (
+    os.path.join('..', 'api_core'),
+)
+
+
 @nox.session
-@nox.parametrize('python_version', ['2.7', '3.4', '3.5', '3.6'])
-def unit_tests(session, python_version):
-    """Run the unit test suite."""
+def default(session):
+    """Default unit test session.
 
-    session.interpreter = 'python{}'.format(python_version)
-
-    session.virtualenv_dirname = 'unit-' + python_version
-
-    session.install('pytest')
+    This is intended to be run **without** an interpreter set, so
+    that the current ``python`` (on the ``PATH``) or the version of
+    Python corresponding to the ``nox`` binary on the ``PATH`` can
+    run the tests.
+    """
+    # Install all test dependencies, then install this package in-place.
+    session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
     session.install('-e', '.')
 
-    session.run('py.test', '--quiet', os.path.join('tests', 'unit'))
+    # Run py.test against the unit tests.
+    session.run(
+        'py.test',
+        '--quiet',
+        '--cov=google.cloud.container',
+        '--cov=google.cloud.container_v1',        
+        '--cov=tests.unit',
+        '--cov-append',
+        '--cov-config=.coveragerc',
+        '--cov-report=',
+        os.path.join('tests', 'unit'),
+        *session.posargs
+    )
 
 
 @nox.session
-@nox.parametrize('python_version', ['2.7', '3.6'])
-def system_tests(session, python_version):
+@nox.parametrize('py', ['2.7', '3.4', '3.5', '3.6'])
+def unit(session, py):
+    """Run the unit test suite."""
+
+    session.interpreter = 'python{}'.format(py)
+    session.virtualenv_dirname = 'unit-' + py
+    default(session)
+
+
+@nox.session
+@nox.parametrize('py', ['2.7', '3.6'])
+def system(session, py):
     """Run the system test suite."""
 
     # Sanity check: Only run system tests if the environment variable is set.
@@ -43,10 +72,10 @@ def system_tests(session, python_version):
         session.skip('Credentials must be set via environment variable.')
 
     # Run the system tests against latest Python 2 and Python 3 only.
-    session.interpreter = 'python{}'.format(python_version)
+    session.interpreter = 'python{}'.format(py)
 
     # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'sys-' + python_version
+    session.virtualenv_dirname = 'sys-' + py
 
     # Install all test dependencies, then install this package into the
     # virtualenv's dist-packages.
