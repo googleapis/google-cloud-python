8979ee0c405f979f6aa65fcbf7964a6c460023d3
Jon Wayne Parrott <jonwayne@google.com>
2018-01-05 08:53:41 -0800

    api_core: Add grpc.Channel stub to grpc_helpers

--- api_core/google/api_core/grpc_helpers.py
+++ api_core/google/api_core/grpc_helpers.py
@@ -14,6 +14,8 @@
 
 """Helpers for :mod:`grpc`."""
 
+import collections
+
 import grpc
 import six
 
@@ -136,3 +138,178 @@ def create_channel(target, credentials=None, scopes=None, **kwargs):
 
     return google.auth.transport.grpc.secure_authorized_channel(
         credentials, request, target, **kwargs)
+
+
+_ChannelCall = collections.namedtuple(
+    '_ChannelCall', ['request', 'timeout', 'metadata', 'credentials'])
+
+
+class _CallableStub(object):
+    """Stub for the grpc callable interface."""
+
+    def __init__(self, method, channel):
+        self._method = method
+        self._channel = channel
+        self.response = None
+        """Union[protobuf.Message, Callable[protobuf.Message], exception]:
+        The response to give when invoking this callable. If this is a
+        callable, it will be invoked with the request protobuf. If it's an
+        exception, the exception will be raised when this is invoked.
+        """
+        self.responses = None
+        """Iterator[protobuf.Message]: A iterator of responses. If specified,
+        self.response will be populated on each invocation by calling
+        ``next(self.responses)``."""
+        self.requests = []
+        """List[protobuf.Message]: All requests sent to this callable."""
+        self.calls = []
+        """List[Tuple]: All invocations of this callable. Each tuple is the
+        request, timeout, metadata, and credentials."""
+
+    def __call__(self, request, timeout=None, metadata=None, credentials=None):
+        self._channel._requests.append((self._method, request))
+        self.requests.append(request)
+        self.calls.append(
+            _ChannelCall(request, timeout, metadata, credentials))
+
+        if self.responses:
+            self.response = next(self.responses)
+
+        if callable(self.response):
+            return self.response(request)
+
+        if isinstance(self.response, Exception):
+            raise self.response
+
+        if self.response is not None:
+            return self.response
+
+        raise ValueError(
+            'Method stub for "{}" has no responses.'.format(self._method))
+
+
+def _simplify_method_name(method):
+    """Simplifies a gRPC method name.
+
+    When gRPC invokes the channel to create a callable, it gives a full
+    method name like "/google.pubsub.v1.Publisher/CreateTopic". This
+    returns just the name of the method, in this case "CreateTopic".
+
+    Args:
+        method (str): The name of the method.
+
+    Returns:
+        str: The simplified name of the method.
+    """
+    return method.rsplit('/', 1).pop()
+
+
+class ChannelStub(grpc.Channel):
+    """A testing stub for the grpc.Channel interface.
+
+    This can be used to test any client that eventually uses a gRPC channel
+    to communicate. By passing in a channel stub, you can configure which
+    responses are returned and track which requests are made.
+
+    For example:
+
+    .. code-block:: python
+
+        channel_stub = grpc_helpers.ChannelStub()
+        channel_stub.GetFoo.response = foo_pb2.Foo(name='bar')
+
+        client = FooClient(channel_stub)
+
+        foo = client.get_foo(labels=['baz'])
+
+        assert foo.name == 'bar'
+        assert channel_stub.GetFoo.requests[0].labels = ['baz']
+
+    Each method on the stub can be accessed and configured on the channel.
+    Here's some examples of various configurations:
+
+    .. code-block:: python
+
+        # Return a basic response:
+
+        channel_stub.GetFoo.response = foo_pb2.Foo(name='bar')
+        assert client.get_foo().name == 'bar'
+
+        # Raise an exception:
+        channel_stub.GetFoo.response = NotFound('...')
+
+        with pytest.raises(NotFound):
+            client.get_foo()
+
+        # Use a sequence of responses:
+        channel_stub.GetFoo.responses = iter([
+            foo_pb2.Foo(name='bar'),
+            foo_pb2.Foo(name='baz')])
+
+        assert client.get_foo().name == 'bar'
+        assert client.get_foo().name == 'baz'
+
+        # Use a callable
+
+        def on_get_foo(request):
+            return foo_pb2.Foo(name='bar' + request.id)
+
+        channel_stub.GetFoo.response = on_get_foo
+
+        assert client.get_foo(id='123').name == 'bar123'
+    """
+
+    def __init__(self, responses=[]):
+        self._requests = []
+        self._method_stubs = {}
+
+    @property
+    def requests(self):
+        """Sequence[Tuple[str, protobuf.Message]]: Returns a list of all
+        requests made on this channel in order. The tuple is of method name,
+        request proto."""
+        return self._requests
+
+    def _stub_for_method(self, method):
+        method = _simplify_method_name(method)
+        if method not in self._method_stubs:
+            self._method_stubs[method] = _CallableStub(method, self)
+
+        return self._method_stubs[method]
+
+    def __getattr__(self, key):
+        # Ideally this would only return stubs for known methods, however,
+        # grpc doesn't actually create these methods until they're needed.
+        return self._stub_for_method(key)
+
+    def unary_unary(
+            self, method,
+            request_serializer=None, response_deserializer=None):
+        """grpc.Channel.unary_unary implementation."""
+        return self._stub_for_method(method)
+
+    def unary_stream(
+            self, method,
+            request_serializer=None, response_deserializer=None):
+        """grpc.Channel.unary_stream implementation."""
+        return self._stub_for_method(method)
+
+    def stream_unary(
+            self, method,
+            request_serializer=None, response_deserializer=None):
+        """grpc.Channel.stream_unary implementation."""
+        return self._stub_for_method(method)
+
+    def stream_stream(
+            self, method,
+            request_serializer=None, response_deserializer=None):
+        """grpc.Channel.stream_stream implementation."""
+        return self._stub_for_method(method)
+
+    def subscribe(self, callback, try_to_connect=False):
+        """grpc.Channel.subscribe implementation."""
+        return
+
+    def unsubscribe(self, callback):
+        """grpc.Channel.unsubscribe implementation."""
+        return
