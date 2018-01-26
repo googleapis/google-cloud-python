727f19b4d8e38ce79528f82fba99855aa466e60b
Jon Wayne Parrott <jonwayne@google.com>
2018-01-26 13:10:53 -0800

    Fix bug in autogen: invalid import paths in proto files

--- trace/google/cloud/trace_v2/proto/tracing_pb2.py
+++ trace/google/cloud/trace_v2/proto/tracing_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
-from google.devtools.cloudtrace_v2.proto import trace_pb2 as google_dot_devtools_dot_cloudtrace__v2_dot_proto_dot_trace__pb2
+from google.cloud.trace_v2.proto import trace_pb2 as google_dot_devtools_dot_cloudtrace__v2_dot_proto_dot_trace__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
 
