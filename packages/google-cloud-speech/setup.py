47081aa5931505e7debcaa8a3eead46671d492ce
Luke Sneeringer <luke@sneeringer.com>
2018-02-09 14:29:00 -0800

    Bump Speech version to 0.32.0 (#4865)
    Also add documentation for v1p1beta1.

--- packages/google-cloud-speech/setup.py
+++ packages/google-cloud-speech/setup.py
@@ -60,7 +60,7 @@ REQUIREMENTS = [
 
 setup(
     name='google-cloud-speech',
-    version='0.31.1',
+    version='0.32.0',
     description='Python Client for Google Cloud Speech',
     long_description=README,
     namespace_packages=[
