1ce5dab7126ec86eb9a834d215c5034e29f1e8ce
Jon Wayne Parrott <jonwayne@google.com>
2018-02-12 15:54:10 -0800

    Remove gax usage from BigTable (#4873)

--- bigtable/setup.py
+++ bigtable/setup.py
@@ -52,8 +52,7 @@ SETUP_BASE = {
 
 REQUIREMENTS = [
     'google-cloud-core >= 0.28.0, < 0.29dev',
-    'google-api-core >= 0.1.1, < 0.2.0dev',
-    'google-gax >= 0.15.7, < 0.16dev',
+    'google-api-core[grpc] >= 0.1.1, < 0.2.0dev',
 ]
 
 setup(
