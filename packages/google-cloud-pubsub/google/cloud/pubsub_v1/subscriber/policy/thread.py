f827fb3075a00c93b91274728875fbe2b5749378
chemelnucfin <alan.c.wu@gmail.com>
2018-02-21 13:31:35 -0800

    Add support for streaming pull receipts. (#4878)
    * Closes (#4325) - PubSub: Implement StreamingPullRequest modify acknowledgement as message receipt
    
    * Review Changes
    
    * Minor fixups
    
    * Minor fixups

--- packages/google-cloud-pubsub/google/cloud/pubsub_v1/subscriber/policy/thread.py
+++ packages/google-cloud-pubsub/google/cloud/pubsub_v1/subscriber/policy/thread.py
@@ -329,8 +329,20 @@ class Policy(base.BasePolicy):
     def on_response(self, response):
         """Process all received Pub/Sub messages.
 
-        For each message, schedule a callback with the executor.
+        For each message, send a modified acknowledgement request to the
+        server. This prevents expiration of the message due to buffering by
+        gRPC or proxy/firewall. This makes the server and client expiration
+        timer closer to each other thus preventing the message being
+        redelivered multiple times.
+
+        After the messages have all had their ack deadline updated, execute
+        the callback for each message using the executor.
         """
+        items = [
+            base.ModAckRequest(message.ack_id, self.histogram.percentile(99))
+            for message in response.received_messages
+        ]
+        self.modify_ack_deadline(items)
         for msg in response.received_messages:
             _LOGGER.debug(
                 'Using %s to process message with ack_id %s.',
