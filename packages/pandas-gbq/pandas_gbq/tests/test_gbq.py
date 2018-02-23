829bebe4eedae31df86a0267b338220a27f7a0cc
Maximilian Roos <5635139+maxim-lian@users.noreply.github.com>
2018-02-23 06:07:41 -0500

    fix test_parse test (#126)

--- packages/pandas-gbq/pandas_gbq/tests/test_gbq.py
+++ packages/pandas-gbq/pandas_gbq/tests/test_gbq.py
@@ -365,12 +365,16 @@ class GBQUnitTests(object):
             gbq.read_gbq('SELECT 1')
 
     def test_that_parse_data_works_properly(self):
+
+        from google.cloud.bigquery.table import Row
         test_schema = {'fields': [
-            {'mode': 'NULLABLE', 'name': 'valid_string', 'type': 'STRING'}]}
-        test_page = [{'f': [{'v': 'PI'}]}]
+            {'mode': 'NULLABLE', 'name': 'column_x', 'type': 'STRING'}]}
+        field_to_index = {'column_x': 0}
+        values = ('row_value',)
+        test_page = [Row(values, field_to_index)]
 
         test_output = gbq._parse_data(test_schema, test_page)
-        correct_output = DataFrame({'valid_string': ['PI']})
+        correct_output = DataFrame({'column_x': ['row_value']})
         tm.assert_frame_equal(test_output, correct_output)
 
     def test_read_gbq_with_invalid_private_key_json_should_fail(self):
