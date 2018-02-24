03fd77afb21763a106ab938a46388d813682e7d4
Jon Wayne Parrott <jonwayne@google.com>
2018-02-23 16:34:37 -0800

    Re-enable lint for tests, remove usage of pylint (#4921)

--- packages/google-cloud-language/nox.py
+++ packages/google-cloud-language/nox.py
@@ -99,16 +99,9 @@ def lint(session):
     serious code quality issues.
     """
     session.interpreter = 'python3.6'
-    session.install('flake8', 'pylint', 'gcp-devrel-py-tools', *LOCAL_DEPS)
+    session.install('flake8')
     session.install('.')
-    session.run('flake8', 'google/cloud/language')
-    session.run(
-        'gcp-devrel-py-tools', 'run-pylint',
-        '--config', 'pylint.config.py',
-        '--library-filesets', 'google',
-        '--test-filesets', 'tests',
-        # Temporarily allow this to fail.
-        success_codes=range(0, 100))
+    session.run('flake8', 'google', 'tests')
 
 
 @nox.session
