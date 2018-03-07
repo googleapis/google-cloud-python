db26463ab3eec01d877820df255525438e402ca5
chemelnucfin <alan.c.wu@gmail.com>
2018-03-07 09:39:40 -0800

    dlp nox default style (#4997)

--- packages/google-cloud-dlp/nox.py
+++ packages/google-cloud-dlp/nox.py
@@ -18,32 +18,59 @@ import os
 import nox
 
 
+LOCAL_DEPS = (
+    os.path.join('..', 'api_core'),
+    os.path.join('..', 'core'),
+)
 @nox.session
-@nox.parametrize('python_version', ['2.7', '3.4', '3.5', '3.6'])
-def unit_tests(session, python_version):
-    """Run the unit test suite."""
+def default(session):
+    """Run the unit test suite.
 
-    session.interpreter = 'python{}'.format(python_version)
-
-    session.virtualenv_dirname = 'unit-' + python_version
-
-    session.install('pytest')
+    This is intended to be run **without** an interpreter set, so
+    that the current ``python`` (on the ``PATH``) or the version of
+    Python corresponding to the ``nox`` binary the ``PATH`` can
+    run the tests.
+    """
+    session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
     session.install('-e', '.')
 
-    session.run('py.test', '--quiet', os.path.join('tests', 'unit'))
+    session.run(
+        'py.test',
+        '--quiet',
+        '--cov=google.cloud.dlp_v2beta1',
+        '--cov-append',
+        '--cov-config=.coveragerc',
+        '--cov-report=',
+        '--cov-fail-under=97',
+        os.path.join('tests', 'unit'),
+        *session.posargs
+    )
+
+@nox.session
+@nox.parametrize('py', ['2.7', '3.4', '3.5', '3.6'])
+def unit(session, py):
+    """Run the unit test suite."""
+
+    # Run unit tests against all supported versions of Python.
+    session.interpreter = 'python{}'.format(py)
+
+    # Set the virtualenv dirname.
+    session.virtualenv_dirname = 'unit-' + py
+
+    default(session)
 
 
 @nox.session
-@nox.parametrize('python_version', ['2.7', '3.6'])
-def system_tests(session, python_version):
+@nox.parametrize('py', ['2.7', '3.6'])
+def system(session, py):
     """Run the system test suite."""
 
     if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
         session.skip('Credentials must be set via environment variable.')
 
-    session.interpreter = 'python{}'.format(python_version)
+    session.interpreter = 'python{}'.format(py)
 
-    session.virtualenv_dirname = 'sys-' + python_version
+    session.virtualenv_dirname = 'sys-' + py
 
     session.install('pytest')
     session.install('-e', '.')
@@ -53,9 +80,36 @@ def system_tests(session, python_version):
 
 
 @nox.session
+def lint(session):
+    """Run linters.
+
+    Returns a failure if the linters find linting errors or sufficiently
+    serious code quality issues.
+    """
+    session.interpreter = 'python3.6'
+    session.install('flake8', *LOCAL_DEPS)
+    session.install('.')
+    session.run('flake8', 'google', 'tests')
+
+    
+@nox.session
 def lint_setup_py(session):
     """Verify that setup.py is valid (including RST check)."""
     session.interpreter = 'python3.6'
     session.install('docutils', 'pygments')
     session.run('python', 'setup.py', 'check', '--restructuredtext',
                 '--strict')
+
+
+@nox.session
+def cover(session):
+    """Run the final coverage report.
+
+    This outputs the coverage report aggregating coverage from the unit
+    test runs (not system test runs), and then erases coverage data.
+    """
+    session.interpreter = 'python3.6'
+    session.chdir(os.path.dirname(__file__))
+    session.install('coverage', 'pytest-cov')
+    session.run('coverage', 'report', '--show-missing', '--fail-under=100')
+    session.run('coverage', 'erase')
