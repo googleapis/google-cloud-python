678cbcfec513ead351539a1f388812fccd1f2fa4
Alix Hamilton <ajhamilton@google.com>
2018-03-12 11:10:43 -0700

    Add IPython magics for running queries (#4983)

--- bigquery/google/cloud/bigquery/__init__.py
+++ bigquery/google/cloud/bigquery/__init__.py
@@ -110,3 +110,11 @@ __all__ = [
     'SourceFormat',
     'WriteDisposition'
 ]
+
+
+def load_ipython_extension(ipython):
+    """Called by IPython when this module is loaded as an IPython extension."""
+    from google.cloud.bigquery.magics import _cell_magic
+
+    ipython.register_magic_function(
+        _cell_magic, magic_kind='cell', magic_name='bigquery')
--- bigquery/tests/system.py
+++ bigquery/tests/system.py
@@ -22,12 +22,21 @@ import os
 import time
 import unittest
 import uuid
+import re
 
 import six
+import pytest
 try:
     import pandas
 except ImportError:  # pragma: NO COVER
     pandas = None
+try:
+    import IPython
+    from IPython.utils import io
+    from IPython.testing import tools
+    from IPython.terminal import interactiveshell
+except ImportError:  # pragma: NO COVER
+    IPython = None
 
 from google.api_core.exceptions import PreconditionFailed
 from google.cloud import bigquery
@@ -1571,6 +1580,37 @@ class TestBigQuery(unittest.TestCase):
         self.to_delete.append(dataset)
         return dataset
 
+    @pytest.mark.skipif(pandas is None, reason='Requires `pandas`')
+    @pytest.mark.skipif(IPython is None, reason='Requires `ipython`')
+    @pytest.mark.usefixtures('ipython_interactive')
+    def test_bigquery_magic(self):
+        ip = IPython.get_ipython()
+        ip.extension_manager.load_extension('google.cloud.bigquery')
+        sql = """
+            SELECT
+              CONCAT(
+                'https://stackoverflow.com/questions/',
+                CAST(id as STRING)) as url,
+              view_count
+            FROM `bigquery-public-data.stackoverflow.posts_questions`
+            WHERE tags like '%google-bigquery%'
+            ORDER BY view_count DESC
+            LIMIT 10
+        """
+        with io.capture_output() as captured:
+            result = ip.run_cell_magic('bigquery', '', sql)
+
+        lines = re.split('\n|\r', captured.stdout)
+        # Removes blanks & terminal code (result of display clearing)
+        updates = list(filter(lambda x: bool(x) and x != '\x1b[2K', lines))
+        assert re.match("Executing query with job ID: .*", updates[0])
+        assert all(re.match("Query executing: .*s", line)
+                   for line in updates[1:-1])
+        assert re.match("Query complete after .*s", updates[-1])
+        assert isinstance(result, pandas.DataFrame)
+        assert len(result) == 10                      # verify row count
+        assert list(result) == ['url', 'view_count']  # verify column names
+
 
 def _job_done(instance):
     return instance.state.lower() == 'done'
@@ -1591,3 +1631,21 @@ def _table_exists(t):
         return True
     except NotFound:
         return False
+
+
+@pytest.fixture(scope='session')
+def ipython():
+    config = tools.default_config()
+    config.TerminalInteractiveShell.simple_prompt = True
+    shell = interactiveshell.TerminalInteractiveShell.instance(config=config)
+    return shell
+
+
+@pytest.fixture()
+def ipython_interactive(request, ipython):
+    """Activate IPython's builtin hooks
+
+    for the duration of the test scope.
+    """
+    with ipython.builtin_trap:
+        yield ipython
