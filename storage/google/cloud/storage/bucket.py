c7effc572683ecad068d445bf0d0d491fe97c700
Tres Seaver <tseaver@palladion.com>
2018-03-14 15:06:45 -0400

    Add system tests for storage retention policy (#450)

--- storage/google/cloud/storage/bucket.py
+++ storage/google/cloud/storage/bucket.py
@@ -1346,7 +1346,9 @@ class Bucket(_PropertyMixin):
         :raises ValueError: if the bucket's retention policy is locked.
         """
         policy = self._properties.setdefault('retentionPolicy', {})
-        policy['retentionPeriod'] = str(value)
+        if value is not None:
+            value = str(value)
+        policy['retentionPeriod'] = value
         self._patch_property('retentionPolicy', policy)
 
     @property
@@ -1805,7 +1807,7 @@ class Bucket(_PropertyMixin):
 
         client = self._require_client(client)
 
-        query_params = {'metageneration': self.metageneration}
+        query_params = {'ifMetagenerationMatch': self.metageneration}
 
         if self.user_project is not None:
             query_params['userProject'] = self.user_project
--- storage/tests/unit/test_bucket.py
+++ storage/tests/unit/test_bucket.py
@@ -1570,7 +1570,18 @@ class Test_Bucket(unittest.TestCase):
 
         self.assertEqual(bucket.retention_period, period)
 
-    def test_retention_period_setter(self):
+    def test_retention_period_setter_w_none(self):
+        period = 86400 * 100  # 100 days
+        bucket = self._make_one()
+        policy = bucket._properties['retentionPolicy'] = {}
+        policy['retentionPeriod'] = period
+
+        bucket.retention_period = None
+
+        self.assertIsNone(
+            bucket._properties['retentionPolicy']['retentionPeriod'])
+
+    def test_retention_period_setter_w_int(self):
         period = 86400 * 100  # 100 days
         bucket = self._make_one()
 
@@ -2485,7 +2496,7 @@ class Test_Bucket(unittest.TestCase):
         kw, = connection._requested
         self.assertEqual(kw['method'], 'POST')
         self.assertEqual(kw['path'], '/b/{}/lockRetentionPolicy'.format(name))
-        self.assertEqual(kw['query_params'], {'metageneration': 1234})
+        self.assertEqual(kw['query_params'], {'ifMetagenerationMatch': 1234})
 
     def test_lock_retention_policy_w_user_project(self):
         name = 'name'
@@ -2518,7 +2529,7 @@ class Test_Bucket(unittest.TestCase):
         self.assertEqual(kw['path'], '/b/{}/lockRetentionPolicy'.format(name))
         self.assertEqual(
             kw['query_params'], {
-                'metageneration': 1234,
+                'ifMetagenerationMatch': 1234,
                 'userProject': user_project,
             })
 
