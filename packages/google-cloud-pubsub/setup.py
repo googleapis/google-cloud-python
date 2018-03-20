9245e55ac2be5b3f623d447571d8f48b486a9eb8
Jon Wayne Parrott <jonwayne@google.com>
2018-03-20 13:31:50 -0700

    Revert "Release 0.33.0 (#5074)" (#5077)
    This reverts commit 52ba3b7ef395ae7c408cddd22bb70d72b69cc72a.

--- packages/google-cloud-pubsub/setup.py
+++ packages/google-cloud-pubsub/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-pubsub'
 description = 'Google Cloud Pub/Sub API client library'
-version = '0.33.0'
+version = '0.32.1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
