7862cd8409cf5b1eca2bbb114a6b8ece83123a86
Tres Seaver <tseaver@palladion.com>
2018-03-23 16:43:10 -0400

    Export COMMIT_TIMESTAMP constant. (#5102)
    Magic string value for timestamp columns to be populated on the
    backend with the commit timestamp of the corresponding insert / update
    operation.
    
    Add a system test which shows using it with a table whose
    'commit_ts' column supports it.

--- spanner/google/cloud/spanner.py
+++ spanner/google/cloud/spanner.py
@@ -20,6 +20,7 @@ from google.cloud.spanner_v1 import __version__
 from google.cloud.spanner_v1 import AbstractSessionPool
 from google.cloud.spanner_v1 import BurstyPool
 from google.cloud.spanner_v1 import Client
+from google.cloud.spanner_v1 import COMMIT_TIMESTAMP
 from google.cloud.spanner_v1 import enums
 from google.cloud.spanner_v1 import FixedSizePool
 from google.cloud.spanner_v1 import KeyRange
@@ -33,6 +34,7 @@ __all__ = (
     'AbstractSessionPool',
     'BurstyPool',
     'Client',
+    'COMMIT_TIMESTAMP',
     'enums',
     'FixedSizePool',
     'KeyRange',
