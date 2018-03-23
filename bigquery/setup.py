278ba920bd737e4d5fe995d8c73d241a3a300e3f
Tim Swast <swast@google.com>
2018-03-23 13:14:28 -0700

    BigQuery: Add dev version for 0.32.0 release
    This will allow downstream packages (like pandas-gbq) to check for this
    version when using the package at HEAD in CI tests.

--- bigquery/setup.py
+++ bigquery/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-bigquery'
 description = 'Google BigQuery API client library'
-version = '0.31.0'
+version = '0.32.0.dev1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
