b67ab6aba181f6ca262c8c2b637d926342609c8e
Aneep Tandel <aneep@objectsbydesign.com>
2018-04-23 07:50:08 +0530

    Added cluster

---
+++ bigtable/google/cloud/bigtable/cluster.py
@@ -0,0 +1,174 @@
+# Copyright 2015 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""User friendly container for Google Cloud Bigtable Cluster."""
+
+
+import re
+
+
+_CLUSTER_NAME_RE = re.compile(r'^projects/(?P<project>[^/]+)/'
+                              r'instances/(?P<instance>[^/]+)/clusters/'
+                              r'(?P<cluster_id>[a-z][-a-z0-9]*)$')
+
+DEFAULT_SERVE_NODES = 3
+"""Default number of nodes to use when creating a cluster."""
+
+
+class Cluster(object):
+    """Representation of a Google Cloud Bigtable Cluster.
+
+    We can use a :class:`Cluster` to:
+
+    * :meth:`reload` itself
+    * :meth:`create` itself
+    * :meth:`update` itself
+    * :meth:`delete` itself
+
+    :type cluster_id: str
+    :param cluster_id: The ID of the cluster.
+
+    :type instance: :class:`~google.cloud.bigtable.instance.Instance`
+    :param instance: The instance where the cluster resides.
+
+    :type serve_nodes: int
+    :param serve_nodes: (Optional) The number of nodes in the cluster.
+                        Defaults to :data:`DEFAULT_SERVE_NODES`.
+    """
+
+    def __init__(self, cluster_id, instance,
+                 serve_nodes=DEFAULT_SERVE_NODES):
+        self.cluster_id = cluster_id
+        self._instance = instance
+        self.serve_nodes = serve_nodes
+        self.location = None
+
+    @property
+    def name(self):
+        """Cluster name used in requests.
+
+        .. note::
+          This property will not change if ``_instance`` and ``cluster_id``
+          do not, but the return value is not cached.
+
+        The cluster name is of the form
+
+            ``"projects/{project}/instances/{instance}/clusters/{cluster_id}"``
+
+        :rtype: str
+        :returns: The cluster name.
+        """
+        return self._instance._client._instance_admin_client.cluster_path(
+            self._instance._client.project, self._instance.instance_id,
+            self.cluster_id)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        # NOTE: This does not compare the configuration values, such as
+        #       the serve_nodes. Instead, it only compares
+        #       identifying values instance, cluster ID and client. This is
+        #       intentional, since the same cluster can be in different states
+        #       if not synchronized. Clusters with similar instance/cluster
+        #       settings but different clients can't be used in the same way.
+        return (other.cluster_id == self.cluster_id and
+                other._instance == self._instance)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def reload(self):
+        """Reload the metadata for this cluster."""
+        self._instance._client._instance_admin_client.get_cluster(self.name)
+
+    def create(self):
+        """Create this cluster.
+
+        .. note::
+
+            Uses the ``project``, ``instance`` and ``cluster_id`` on the
+            current :class:`Cluster` in addition to the ``serve_nodes``.
+            To change them before creating, reset the values via
+
+            .. code:: python
+
+                cluster.serve_nodes = 8
+                cluster.cluster_id = 'i-changed-my-mind'
+
+            before calling :meth:`create`.
+
+        :rtype: :class:`~google.cloud.bigtable_admin_v2.types._OperationFuture`
+        :returns: The long-running operation corresponding to the
+                  create operation.
+        """
+        client = self._instance._client
+        return client._instance_admin_client.create_cluster(
+            self._instance.name, self.cluster_id, {})
+
+    def update(self, location='', serve_nodes=0):
+        """Update this cluster.
+
+        .. note::
+
+            Updates the ``serve_nodes``. If you'd like to
+            change them before updating, reset the values via
+
+            .. code:: python
+
+                cluster.serve_nodes = 8
+
+            before calling :meth:`update`.
+
+        :type location: :str:``CreationOnly``
+        :param location: The location where this cluster's nodes and storage
+                reside. For best performance, clients should be located as
+                close as possible to this cluster. Currently only zones are
+                supported, so values should be of the form
+                ``projects/<project>/locations/<zone>``.
+
+        :type serve_nodes: :int
+        :param serve_nodes: The number of nodes allocated to this cluster.
+                More nodes enable higher throughput and more consistent
+                performance.
+
+        :rtype: :class:`Operation`
+        :returns: The long-running operation corresponding to the
+                  update operation.
+        """
+        client = self._instance._client
+        return client._instance_admin_client.update_cluster(
+            self.name, location, serve_nodes)
+
+    def delete(self):
+        """Delete this cluster.
+
+        Marks a cluster and all of its tables for permanent deletion in 7 days.
+
+        Immediately upon completion of the request:
+
+        * Billing will cease for all of the cluster's reserved resources.
+        * The cluster's ``delete_time`` field will be set 7 days in the future.
+
+        Soon afterward:
+
+        * All tables within the cluster will become unavailable.
+
+        At the cluster's ``delete_time``:
+
+        * The cluster and **all of its tables** will immediately and
+          irrevocably disappear from the API, and their data will be
+          permanently deleted.
+        """
+        client = self._instance._client
+        client._instance_admin_client.delete_cluster(self.name)
---
+++ bigtable/tests/unit/test_cluster.py
@@ -0,0 +1,300 @@
+# Copyright 2015 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+
+import mock
+
+
+class TestCluster(unittest.TestCase):
+
+    PROJECT = 'project'
+    INSTANCE_ID = 'instance-id'
+    CLUSTER_ID = 'cluster-id'
+    CLUSTER_NAME = ('projects/' + PROJECT +
+                    '/instances/' + INSTANCE_ID +
+                    '/clusters/' + CLUSTER_ID)
+
+    @mock.patch('google.auth.transport.grpc.secure_authorized_channel')
+    def _make_channel(self, secure_authorized_channel):
+        from google.api_core import grpc_helpers
+        target = 'example.com:443'
+
+        channel = grpc_helpers.create_channel(
+            target, credentials=mock.sentinel.credentials)
+
+        return channel
+
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.bigtable.cluster import Cluster
+
+        return Cluster
+
+    def _make_one(self, *args, **kwargs):
+        return self._get_target_class()(*args, **kwargs)
+
+    @staticmethod
+    def _get_target_client_class():
+        from google.cloud.bigtable.client import Client
+
+        return Client
+
+    def _make_client(self, *args, **kwargs):
+        return self._get_target_client_class()(*args, **kwargs)
+
+    def test_constructor_defaults(self):
+        from google.cloud.bigtable.cluster import DEFAULT_SERVE_NODES
+
+        client = _Client(self.PROJECT)
+        instance = _Instance(self.INSTANCE_ID, client)
+
+        cluster = self._make_one(self.CLUSTER_ID, instance)
+        self.assertEqual(cluster.cluster_id, self.CLUSTER_ID)
+        self.assertIs(cluster._instance, instance)
+        self.assertEqual(cluster.serve_nodes, DEFAULT_SERVE_NODES)
+
+    def test_constructor_non_default(self):
+        SERVE_NODES = 8
+        client = _Client(self.PROJECT)
+        instance = _Instance(self.INSTANCE_ID, client)
+
+        cluster = self._make_one(self.CLUSTER_ID, instance,
+                                 serve_nodes=SERVE_NODES)
+        self.assertEqual(cluster.cluster_id, self.CLUSTER_ID)
+        self.assertIs(cluster._instance, instance)
+        self.assertEqual(cluster.serve_nodes, SERVE_NODES)
+
+    def test_name_property(self):
+        from google.cloud.bigtable.instance import Instance
+
+        channel = self._make_channel()
+        client = self._make_client(project=self.PROJECT, channel=channel,
+                                   admin=True)
+        instance = Instance(self.INSTANCE_ID, client)
+        cluster = self._make_one(self.CLUSTER_ID, instance)
+        instance = Instance(self.INSTANCE_ID, client)
+
+        self.assertEqual(cluster.name, self.CLUSTER_NAME)
+
+    def test___eq__(self):
+        client = _Client(self.PROJECT)
+        instance = _Instance(self.INSTANCE_ID, client)
+        cluster1 = self._make_one(self.CLUSTER_ID, instance)
+        cluster2 = self._make_one(self.CLUSTER_ID, instance)
+        self.assertEqual(cluster1, cluster2)
+
+    def test___eq__type_differ(self):
+        client = _Client(self.PROJECT)
+        instance = _Instance(self.INSTANCE_ID, client)
+        cluster1 = self._make_one(self.CLUSTER_ID, instance)
+        cluster2 = object()
+        self.assertNotEqual(cluster1, cluster2)
+
+    def test___ne__same_value(self):
+        client = _Client(self.PROJECT)
+        instance = _Instance(self.INSTANCE_ID, client)
+        cluster1 = self._make_one(self.CLUSTER_ID, instance)
+        cluster2 = self._make_one(self.CLUSTER_ID, instance)
+        comparison_val = (cluster1 != cluster2)
+        self.assertFalse(comparison_val)
+
+    def test___ne__(self):
+        client = _Client(self.PROJECT)
+        instance = _Instance(self.INSTANCE_ID, client)
+        cluster1 = self._make_one('cluster_id1', instance)
+        cluster2 = self._make_one('cluster_id2', instance)
+        self.assertNotEqual(cluster1, cluster2)
+
+    def test_reload(self):
+        from google.cloud.bigtable.cluster import DEFAULT_SERVE_NODES
+        from google.cloud.bigtable.instance import Instance
+
+        LOCATION = 'LOCATION'
+        channel = self._make_channel()
+        client = self._make_client(project=self.PROJECT, channel=channel,
+                                   admin=True)
+        instance = Instance(self.INSTANCE_ID, client)
+        cluster = self._make_one(self.CLUSTER_ID, instance)
+
+        # Create response_pb
+        response_pb = _ClusterPB(
+            serve_nodes=DEFAULT_SERVE_NODES,
+            location=LOCATION,
+        )
+
+        # Patch the stub used by the API method.
+        instance_admin_client = client._instance_admin_client
+        instance_stub = instance_admin_client.bigtable_instance_admin_stub
+        instance_stub.GetCluster.side_effect = [response_pb]
+
+        # Create expected_result.
+        expected_result = None  # reload() has no return value.
+
+        # Check Cluster optional config values before.
+        self.assertEqual(cluster.serve_nodes, DEFAULT_SERVE_NODES)
+
+        # Perform the method and check the result.
+        result = cluster.reload()
+        self.assertEqual(result, expected_result)
+
+    def test_create(self):
+        from google.api_core import operation
+        from google.longrunning import operations_pb2
+        from google.cloud.bigtable.instance import Instance
+
+        channel = self._make_channel()
+        client = self._make_client(project=self.PROJECT, channel=channel,
+                                   admin=True)
+        instance = Instance(self.INSTANCE_ID, client)
+        cluster = self._make_one(self.CLUSTER_ID, instance)
+
+        # Create response_pb
+        OP_ID = 5678
+        OP_NAME = (
+            'operations/projects/%s/instances/%s/clusters/%s/operations/%d' %
+            (self.PROJECT, self.INSTANCE_ID, self.CLUSTER_ID, OP_ID))
+        response_pb = operations_pb2.Operation(name=OP_NAME)
+
+        # Patch the stub used by the API method.
+        instance_admin_client = client._instance_admin_client
+        instance_stub = instance_admin_client.bigtable_instance_admin_stub
+        instance_stub.CreateCluster.side_effect = [response_pb]
+
+        # Perform the method and check the result.
+        result = cluster.create()
+
+        self.assertIsInstance(result, operation.Operation)
+        self.assertEqual(result.operation.name, OP_NAME)
+        self.assertIsNone(result.metadata)
+
+    def test_update(self):
+        import datetime
+        from google.api_core import operation
+        from google.longrunning import operations_pb2
+        from google.protobuf.any_pb2 import Any
+        from google.cloud._helpers import _datetime_to_pb_timestamp
+        from google.cloud.bigtable.instance import Instance
+        from google.cloud.bigtable_admin_v2.proto import (
+            instance_pb2 as data_v2_pb2)
+        from google.cloud.bigtable_admin_v2.proto import (
+            bigtable_instance_admin_pb2 as messages_v2_pb2)
+
+        NOW = datetime.datetime.utcnow()
+        NOW_PB = _datetime_to_pb_timestamp(NOW)
+
+        SERVE_NODES = 81
+
+        channel = self._make_channel()
+        client = self._make_client(project=self.PROJECT, channel=channel,
+                                   admin=True)
+        instance = Instance(self.INSTANCE_ID, client)
+        cluster = self._make_one(self.CLUSTER_ID, instance,
+                                 serve_nodes=SERVE_NODES)
+
+        # Create request_pb
+        request_pb = _ClusterPB(
+            name=self.CLUSTER_NAME,
+            serve_nodes=SERVE_NODES,
+        )
+
+        # Create response_pb
+        OP_ID = 5678
+        OP_NAME = (
+            'operations/projects/%s/instances/%s/clusters/%s/operations/%d' %
+            (self.PROJECT, self.INSTANCE_ID, self.CLUSTER_ID, OP_ID))
+        metadata = messages_v2_pb2.UpdateClusterMetadata(request_time=NOW_PB)
+        type_url = 'type.googleapis.com/%s' % (
+            messages_v2_pb2.UpdateClusterMetadata.DESCRIPTOR.full_name,)
+        response_pb = operations_pb2.Operation(
+            name=OP_NAME,
+            metadata=Any(
+                type_url=type_url,
+                value=metadata.SerializeToString()
+            )
+        )
+
+        # Patch the stub used by the API method.
+        instance_admin_client = client._instance_admin_client
+        instance_stub = instance_admin_client.bigtable_instance_admin_stub
+        instance_stub.UpdateCluster.side_effect = [response_pb]
+
+        result = cluster.update()
+
+        self.assertIsInstance(result, operation.Operation)
+        self.assertIsInstance(result.metadata,
+                              messages_v2_pb2.UpdateClusterMetadata)
+
+        self.assertIsInstance(request_pb, data_v2_pb2.Cluster)
+        self.assertEqual(request_pb.name, self.CLUSTER_NAME)
+        self.assertEqual(request_pb.serve_nodes, SERVE_NODES)
+
+    def test_delete(self):
+        from google.protobuf import empty_pb2
+        from google.cloud.bigtable.cluster import DEFAULT_SERVE_NODES
+        from google.cloud.bigtable.instance import Instance
+
+        channel = self._make_channel()
+        client = self._make_client(project=self.PROJECT, channel=channel,
+                                   admin=True)
+        instance = Instance(self.INSTANCE_ID, client)
+        cluster = self._make_one(self.CLUSTER_ID, instance,
+                                 serve_nodes=DEFAULT_SERVE_NODES)
+
+        # Create response_pb
+        response_pb = empty_pb2.Empty()
+
+        # Patch the stub used by the API method.
+        instance_admin_client = client._instance_admin_client
+        instance_stub = instance_admin_client.bigtable_instance_admin_stub
+        instance_stub.DeleteCluster.side_effect = [response_pb]
+
+        # Create expected_result.
+        expected_result = None  # delete() has no return value.
+
+        # Perform the method and check the result.
+        result = cluster.delete()
+
+        self.assertEqual(result, expected_result)
+
+
+def _ClusterPB(*args, **kw):
+    from google.cloud.bigtable_admin_v2.proto import (
+        instance_pb2 as instance_v2_pb2)
+
+    return instance_v2_pb2.Cluster(*args, **kw)
+
+
+class _Instance(object):
+
+    def __init__(self, instance_id, client):
+        self.instance_id = instance_id
+        self._client = client
+
+    def __eq__(self, other):
+        return (other.instance_id == self.instance_id and
+                other._client == self._client)
+
+
+class _Client(object):
+
+    def __init__(self, project):
+        self.project = project
+        self.project_name = 'projects/' + self.project
+        self._operations_stub = mock.sentinel.operations_stub
+
+    def __eq__(self, other):
+        return (other.project == self.project and
+                other.project_name == self.project_name)
