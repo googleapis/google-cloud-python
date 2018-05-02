a1cb7619baedf9aa177a85b5f5f7042c1fea0514
Chris Wilcox <crwilcox@google.com>
2018-05-02 14:00:52 -0700

    modify setup.py to new template

--- redis/setup.py
+++ redis/setup.py
@@ -11,33 +11,61 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""A setup module for the GAPIC Google Cloud Memorystore for Redis API library.
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-from setuptools import setup, find_packages
 import io
-import sys
+import os
 
-install_requires = [
+import setuptools
+
+
+# Package metadata.
+
+name = 'google-cloud-redis'
+description = 'Cloud Redis API client library'
+version = '0.1.0'
+# Should be one of:
+# 'Development Status :: 3 - Alpha'
+# 'Development Status :: 4 - Beta'
+# 'Development Status :: 5 - Production/Stable'
+release_status = 'Development Status :: 3 - Alpha'
+dependencies = [
     'google-api-core >= 0.1.0, < 0.2.0dev',
     'googleapis-common-protos[grpc] >= 1.5.2, < 2.0dev',
 ]
+extras = {
+}
 
-with io.open('README.rst', 'r', encoding='utf-8') as readme_file:
-    long_description = readme_file.read()
+# Setup boilerplate below this line.
 
-setup(
-    name='google-cloud-redis',
-    version='0.1.0',
+package_root = os.path.abspath(os.path.dirname(__file__))
+
+readme_filename = os.path.join(package_root, 'README.rst')
+with io.open(readme_filename, encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+# Only include packages under the 'google' namespace. Do not include tests,
+# benchmarks, etc.
+packages = [
+    package for package in setuptools.find_packages()
+    if package.startswith('google')]
+
+# Determine which namespaces are needed.
+namespaces = ['google']
+if 'google.cloud' in packages:
+    namespaces.append('google.cloud')
+
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=readme,
     author='Google LLC',
     author_email='googleapis-packages@google.com',
+    license='Apache 2.0',
+    url='https://github.com/GoogleCloudPlatform/google-cloud-python',
     classifiers=[
-        'Intended Audience :: Developers',
-        'Development Status :: 3 - Alpha',
+        release_status,
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
@@ -47,14 +75,14 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Internet',
     ],
-    description='GAPIC library for the Google Cloud Memorystore for Redis API',
+    platforms='Posix; MacOS X; Windows',
+    packages=packages,
+    namespace_packages=namespaces,
+    install_requires=dependencies,
+    extras_require=extras,
     include_package_data=True,
-    long_description=long_description,
-    install_requires=install_requires,
-    license='Apache 2.0',
-    packages=find_packages(exclude=('tests*', )),
-    namespace_packages=['google', 'google.cloud'],
-    url='https://github.com/googleapis/googleapis',
     zip_safe=False,
 )
