16273cd4bb6467f4b8ce73646ae423e07cd07ca0
Chris Wilcox <crwilcox@google.com>
2018-05-18 13:31:26 -0700

    setup.py changes

--- tasks/setup.py
+++ tasks/setup.py
@@ -20,10 +20,11 @@ import setuptools
 name = 'google-cloud-cloudtasks'
 description = 'Cloud Tasks API API client library'
 version = '0.1.0'
-release_status = '3 - Alpha'
+release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'google-api-core[grpc] >= 1.1.0, < 2.0.0dev',
     'enum34; python_version < "3.4"',
+    'grpc-google-iam-v1<0.12dev,>=0.11.4',
 ]
 
 package_root = os.path.abspath(os.path.dirname(__file__))
