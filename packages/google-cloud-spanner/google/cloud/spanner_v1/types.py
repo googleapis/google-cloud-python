e554c29fd91bee7fb58818a54db0a41f72eed98d
Tres Seaver <tseaver@palladion.com>
2018-05-22 16:03:07 -0400

    Avoid overwriting '__module__' of messages from shared modules. (#5364)
    Note that we *are* still overwriting it for messages from modules defined
    within the current package.
    
    See #4715.

--- packages/google-cloud-spanner/google/cloud/spanner_v1/types.py
+++ packages/google-cloud-spanner/google/cloud/spanner_v1/types.py
@@ -15,9 +15,14 @@
 from __future__ import absolute_import
 import sys
 
-from google.api_core.protobuf_helpers import get_messages
-
 from google.api import http_pb2
+from google.protobuf import descriptor_pb2
+from google.protobuf import duration_pb2
+from google.protobuf import empty_pb2
+from google.protobuf import struct_pb2
+from google.protobuf import timestamp_pb2
+
+from google.api_core.protobuf_helpers import get_messages
 from google.cloud.spanner_v1.proto import keys_pb2
 from google.cloud.spanner_v1.proto import mutation_pb2
 from google.cloud.spanner_v1.proto import query_plan_pb2
@@ -25,28 +30,35 @@ from google.cloud.spanner_v1.proto import result_set_pb2
 from google.cloud.spanner_v1.proto import spanner_pb2
 from google.cloud.spanner_v1.proto import transaction_pb2
 from google.cloud.spanner_v1.proto import type_pb2
-from google.protobuf import descriptor_pb2
-from google.protobuf import duration_pb2
-from google.protobuf import empty_pb2
-from google.protobuf import struct_pb2
-from google.protobuf import timestamp_pb2
+
+
+_shared_modules = [
+    http_pb2,
+    descriptor_pb2,
+    duration_pb2,
+    empty_pb2,
+    struct_pb2,
+    timestamp_pb2,
+]
+
+_local_modules = [
+    keys_pb2,
+    mutation_pb2,
+    query_plan_pb2,
+    result_set_pb2,
+    spanner_pb2,
+    transaction_pb2,
+    type_pb2,
+]
 
 names = []
-for module in (
-        http_pb2,
-        keys_pb2,
-        mutation_pb2,
-        query_plan_pb2,
-        result_set_pb2,
-        spanner_pb2,
-        transaction_pb2,
-        type_pb2,
-        descriptor_pb2,
-        duration_pb2,
-        empty_pb2,
-        struct_pb2,
-        timestamp_pb2,
-):
+
+for module in _shared_modules:
+    for name, message in get_messages(module).items():
+        setattr(sys.modules[__name__], name, message)
+        names.append(name)
+
+for module in _local_modules:
     for name, message in get_messages(module).items():
         message.__module__ = 'google.cloud.spanner_v1.types'
         setattr(sys.modules[__name__], name, message)
