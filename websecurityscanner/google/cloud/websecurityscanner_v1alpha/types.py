61e5fac8005412e820c723f7159fef02a6f1a561
Tres Seaver <tseaver@palladion.com>
2018-05-22 16:03:07 -0400

    Avoid overwriting '__module__' of messages from shared modules. (#5364)
    Note that we *are* still overwriting it for messages from modules defined
    within the current package.
    
    See #4715.

--- websecurityscanner/google/cloud/websecurityscanner_v1alpha/types.py
+++ websecurityscanner/google/cloud/websecurityscanner_v1alpha/types.py
@@ -15,38 +15,50 @@
 from __future__ import absolute_import
 import sys
 
-from google.api_core.protobuf_helpers import get_messages
-
 from google.api import http_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import crawled_url_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import finding_addon_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import finding_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import \
-    finding_type_stats_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import scan_config_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import scan_run_pb2
-from google.cloud.websecurityscanner_v1alpha.proto import \
-    web_security_scanner_pb2
 from google.protobuf import descriptor_pb2
 from google.protobuf import empty_pb2
 from google.protobuf import field_mask_pb2
 from google.protobuf import timestamp_pb2
 
+from google.api_core.protobuf_helpers import get_messages
+from google.cloud.websecurityscanner_v1alpha.proto import crawled_url_pb2
+from google.cloud.websecurityscanner_v1alpha.proto import finding_addon_pb2
+from google.cloud.websecurityscanner_v1alpha.proto import finding_pb2
+from google.cloud.websecurityscanner_v1alpha.proto import (
+    finding_type_stats_pb2 )
+from google.cloud.websecurityscanner_v1alpha.proto import scan_config_pb2
+from google.cloud.websecurityscanner_v1alpha.proto import scan_run_pb2
+from google.cloud.websecurityscanner_v1alpha.proto import (
+    web_security_scanner_pb2 )
+
+
+_shared_modules = [
+    http_pb2,
+    descriptor_pb2,
+    empty_pb2,
+    field_mask_pb2,
+    timestamp_pb2,
+]
+
+_local_modules = [
+    crawled_url_pb2,
+    finding_addon_pb2,
+    finding_pb2,
+    finding_type_stats_pb2,
+    scan_config_pb2,
+    scan_run_pb2,
+    web_security_scanner_pb2,
+]
+
 names = []
-for module in (
-        http_pb2,
-        crawled_url_pb2,
-        finding_addon_pb2,
-        finding_pb2,
-        finding_type_stats_pb2,
-        scan_config_pb2,
-        scan_run_pb2,
-        web_security_scanner_pb2,
-        descriptor_pb2,
-        empty_pb2,
-        field_mask_pb2,
-        timestamp_pb2,
-):
+
+for module in _shared_modules:
+    for name, message in get_messages(module).items():
+        setattr(sys.modules[__name__], name, message)
+        names.append(name)
+
+for module in _local_modules:
     for name, message in get_messages(module).items():
         message.__module__ = 'google.cloud.websecurityscanner_v1alpha.types'
         setattr(sys.modules[__name__], name, message)
