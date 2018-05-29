613191158fd5b99f052739777d41a24352497a53
Alix Hamilton <ajhamilton@google.com>
2018-05-29 12:52:17 -0700

    BigQuery: Adds Python 3.7 and removes Python 3.4 (#5401)
    * BigQuery: Adds Python 3.7 and removes Python 3.4

--- packages/google-cloud-bigquery/nox.py
+++ packages/google-cloud-bigquery/nox.py
@@ -37,8 +37,8 @@ def default(session):
     # Install all test dependencies, then install this package in-place.
     session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
 
-    # Pandas does not support Python 3.4
-    if session.interpreter == 'python3.4':
+    # Pandas does not support Python 3.7
+    if session.interpreter == 'python3.7':
         session.install('-e', '.')
     else:
         session.install('-e', '.[pandas, pyarrow]')
@@ -65,7 +65,7 @@ def default(session):
 
 
 @nox.session
-@nox.parametrize('py', ['2.7', '3.5', '3.6'])
+@nox.parametrize('py', ['2.7', '3.5', '3.6', '3.7'])
 def unit(session, py):
     """Run the unit test suite."""
 
