602292c6a75a2fc69c9c01eeac852692226a3aa6
Alix Hamilton <ajhamilton@google.com>
2018-05-30 13:07:10 -0700

    adds snippets for adding/relaxing column through load or query job

--- docs/bigquery/snippets.py
+++ docs/bigquery/snippets.py
@@ -1365,6 +1365,126 @@ def test_load_table_from_uri_truncate(client, to_delete):
     # [END bigquery_load_table_gcs_parquet_truncate]
 
 
+def test_load_table_add_column(client, to_delete):
+    dataset_id = 'load_table_add_column_{}'.format(_millis())
+    dataset_ref = client.dataset(dataset_id)
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset.location = 'US'
+    dataset = client.create_dataset(dataset)
+    to_delete.append(dataset)
+
+    snippets_dir = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(
+        snippets_dir, '..', '..', 'bigquery', 'tests', 'data', 'people.csv')
+
+    # [START bigquery_add_column_load_append]
+    # from google.cloud import bigquery
+    # client = bigquery.Client()
+    # dataset_ref = client.dataset('my_dataset')
+    # filepath = 'path/to/your_file.csv'
+
+    table_ref = dataset_ref.table('my_table')
+    old_schema = [
+        bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+    ]
+    table = client.create_table(bigquery.Table(table_ref, schema=old_schema))
+
+    # Configure the load job to append the data to a destination table,
+    # allowing field addition
+    job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+    job_config.schema_update_options = [
+        bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+    ]
+    # 'REQUIRED' fields cannot be added to an existing schema, so the
+    # additional column must be 'NULLABLE'
+    job_config.schema = [
+        bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('age', 'INTEGER', mode='NULLABLE'),
+    ]
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.skip_leading_rows = 1
+
+    with open(filepath, 'rb') as source_file:
+        job = client.load_table_from_file(
+            source_file,
+            table_ref,
+            location='US',  # Must match the destination dataset location.
+            job_config=job_config)  # API request
+
+    job.result()  # Waits for table load to complete.
+
+    print('Loaded {} rows into {}:{}.'.format(
+        job.output_rows, dataset_id, table_ref.table_id))
+
+    # Verify that the column was added
+    table = client.get_table(table)
+    assert len(table.schema) == 2
+    # [END bigquery_add_column_load_append]
+    assert table.num_rows > 0
+
+
+def test_load_table_relax_column(client, to_delete):
+    dataset_id = 'load_table_relax_column_{}'.format(_millis())
+    dataset_ref = client.dataset(dataset_id)
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset.location = 'US'
+    dataset = client.create_dataset(dataset)
+    to_delete.append(dataset)
+
+    snippets_dir = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(
+        snippets_dir, '..', '..', 'bigquery', 'tests', 'data', 'people.csv')
+
+    # [START bigquery_relax_column_load_append]
+    # from google.cloud import bigquery
+    # client = bigquery.Client()
+    # dataset_ref = client.dataset('my_dataset')
+    # filepath = 'path/to/your_file.csv'
+
+    table_ref = dataset_ref.table('my_table')
+    old_schema = [
+        bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+        bigquery.SchemaField('favorite_color', 'STRING', mode='REQUIRED'),
+    ]
+    table = client.create_table(bigquery.Table(table_ref, schema=old_schema))
+
+    # Configure the load job to append the data to a destination table,
+    # allowing field relaxation
+    job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+    job_config.schema_update_options = [
+        bigquery.SchemaUpdateOption.ALLOW_FIELD_RELAXATION,
+    ]
+    # The source file data to load has one less column than the existing table
+    job_config.schema = [
+        bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+    ]
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.skip_leading_rows = 1
+
+    with open(filepath, 'rb') as source_file:
+        job = client.load_table_from_file(
+            source_file,
+            table_ref,
+            location='US',  # Must match the destination dataset location.
+            job_config=job_config)  # API request
+
+    job.result()  # Waits for table load to complete.
+
+    print('Loaded {} rows into {}:{}.'.format(
+        job.output_rows, dataset_id, table_ref.table_id))
+
+    # Verify that the third column was relaxed
+    table = client.get_table(table)
+    assert len(table.schema) == 3
+    assert table.schema[2].mode == 'NULLABLE'
+    # [END bigquery_relax_column_load_append]
+    assert table.num_rows > 0
+
+
 def _write_csv_to_storage(bucket_name, blob_name, header_row, data_rows):
     import csv
     from google.cloud._testing import _NamedTemporaryFile
@@ -1808,6 +1928,99 @@ def test_client_query_destination_table_cmek(client, to_delete):
     table = client.get_table(table_ref)
     assert table.encryption_configuration.kms_key_name == kms_key_name
     # [END bigquery_query_destination_table_cmek]
+
+
+def test_client_query_relax_column(client, to_delete):
+    dataset_id = 'query_relax_column_{}'.format(_millis())
+    dataset_ref = client.dataset(dataset_id)
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset.location = 'US'
+    dataset = client.create_dataset(dataset)
+    to_delete.append(dataset)
+
+    # [START bigquery_relax_column_query_append]
+    # from google.cloud import bigquery
+    # client = bigquery.Client()
+    # dataset_ref = client.dataset('my_dataset')
+
+    table_ref = dataset_ref.table('my_table')
+    schema = [
+        bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+    ]
+    table = client.create_table(bigquery.Table(table_ref, schema=schema))
+
+    # Configure the query to append the results to a destination table,
+    # allowing field relaxation
+    job_config = bigquery.QueryJobConfig()
+    job_config.schema_update_options = [
+        bigquery.SchemaUpdateOption.ALLOW_FIELD_RELAXATION,
+    ]
+    job_config.destination = table_ref
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+
+    query_job = client.query(
+        # Write a query that will omit a required field
+        'SELECT "Beyonce" as full_name;',
+        # Location must match that of the dataset(s) referenced in the query
+        # and of the destination table.
+        location='US',
+        job_config=job_config
+    )  # API request - starts the query
+
+    query_job.result()  # Waits for the query to finish
+
+    # Verify that the column was relaxed and the results were appended
+    table = client.get_table(table)
+    assert table.schema[1].mode == 'NULLABLE'
+    assert table.num_rows > 0
+    # [END bigquery_relax_column_query_append]
+
+def test_client_query_add_column(client, to_delete):
+    dataset_id = 'query_add_column_{}'.format(_millis())
+    dataset_ref = client.dataset(dataset_id)
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset.location = 'US'
+    dataset = client.create_dataset(dataset)
+    to_delete.append(dataset)
+
+    # [START bigquery_add_column_query_append]
+    # from google.cloud import bigquery
+    # client = bigquery.Client()
+    # dataset_ref = client.dataset('my_dataset')
+
+    table_ref = dataset_ref.table('my_table')
+    schema = [
+        bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+    ]
+    table = client.create_table(bigquery.Table(table_ref, schema=schema))
+
+    # Configure the query to append the results to a destination table,
+    # allowing field addition
+    job_config = bigquery.QueryJobConfig()
+    job_config.schema_update_options = [
+        bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+    ]
+    job_config.destination = table_ref
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+
+    query_job = client.query(
+        # Write a query including a column not found in the destination table
+        'SELECT "Timmy" as full_name, 85 as age, "Blue" as favorite_color;',
+        # Location must match that of the dataset(s) referenced in the query
+        # and of the destination table.
+        location='US',
+        job_config=job_config
+    )  # API request - starts the query
+
+    query_job.result()  # Waits for the query to finish
+
+    # Verify that the column was added and the results were appended
+    table = client.get_table(table)
+    assert len(table.schema) == 3
+    assert table.num_rows > 0
+    # [END bigquery_add_column_query_append]
 
 
 def test_client_query_w_named_params(client, capsys):
