4ce97adbb483c992c9678078f186c2ef58d3e545
Tres Seaver <tseaver@palladion.com>
2018-05-31 13:17:46 -0400

    Pass through 'session.posargs' when running Bigtable system tests. (#5418)
    While we're add it, use a develop install in system tests.

--- bigtable/nox.py
+++ bigtable/nox.py
@@ -88,10 +88,10 @@ def system(session, py):
     # virtualenv's dist-packages.
     session.install('mock', 'pytest', *LOCAL_DEPS)
     session.install('../test_utils/')
-    session.install('.')
+    session.install('-e', '.')
 
     # Run py.test against the system tests.
-    session.run('py.test', '--quiet', 'tests/system.py')
+    session.run('py.test', '--quiet', 'tests/system.py', *session.posargs)
 
 
 @nox.session
