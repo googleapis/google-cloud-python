dae65d59054aae7a07601d1f037178a7b02d34a5
Solomon Duskis <sduskis@google.com>
2018-07-03 17:30:57 -0400

    Update instance.py

--- bigtable/google/cloud/bigtable/instance.py
+++ bigtable/google/cloud/bigtable/instance.py
@@ -106,7 +106,7 @@ class Instance(object):
                              'project ID on the client')
         instance_id = match.group('instance_id')
 
-        if (instance_pb.display_name is not None:
+        if instance_pb.display_name is not None:
 	    display_name_len = len(instance_pb.display_name)
 	    if (display_name_len < _DISPLAY_NAME_MIN_LENGTH and
                 display_name_len > _DISPLAY_NAME_MAX_LENGTH):
