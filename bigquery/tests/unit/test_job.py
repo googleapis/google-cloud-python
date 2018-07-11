0b941f217d5a522e5159828048f8d83004fea2ec
Tim Swast <swast@google.com>
2018-07-11 15:13:28 -0700

    Allow listing rows on an empty table. (#5584)
    * Allow listing rows on an empty table.
    
    Removes check for missing schema on Client.list_rows. This allows the
    rows of an empty table to be listed. Such a table might be the
    "destination table" for a DML or DDL query.
    
    * Only skip schema check if table has no creation date.
    
    This is how we disambiguate (1) The table has no schema. list_rows()
    shouldn't cause errors but currently is, and it's affecting DML & DDL
    queries. (2) The Table object was constructed locally via the Table()
    constructor.
    
    * Add creationTime workaround to QueryJob.result()

--- bigquery/tests/unit/test_job.py
+++ bigquery/tests/unit/test_job.py
@@ -2712,6 +2712,25 @@ class TestQueryJob(unittest.TestCase, _Base):
 
         self.assertEqual(list(result), [])
 
+    def test_result_w_empty_schema(self):
+        # Destination table may have no schema for some DDL and DML queries.
+        query_resource = {
+            'jobComplete': True,
+            'jobReference': {
+                'projectId': self.PROJECT,
+                'jobId': self.JOB_ID,
+            },
+            'schema': {'fields': []},
+        }
+        connection = _make_connection(query_resource, query_resource)
+        client = _make_client(self.PROJECT, connection=connection)
+        resource = self._make_resource(ended=True)
+        job = self._get_target_class().from_api_repr(resource, client)
+
+        result = job.result()
+
+        self.assertEqual(list(result), [])
+
     def test_result_invokes_begins(self):
         begun_resource = self._make_resource()
         incomplete_resource = {
