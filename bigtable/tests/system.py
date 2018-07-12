1539c2a0a1015a38753f29d8ce30d356796b4aec
sumit m <sumit@qlogic.io>
2018-07-13 00:03:36 +0530

    Changed expected_row_keys to set, now test is passing.

--- bigtable/tests/system.py
+++ bigtable/tests/system.py
@@ -485,8 +485,8 @@ class TestDataAPI(unittest.TestCase):
 
         read_rows = self._table.yield_rows(row_set=row_set)
 
-        expected_row_keys = [b'row_key_1', b'row_key_3', b'row_key_4',
-                             b'row_key_5', b'row_key_6']
+        expected_row_keys = set([b'row_key_1', b'row_key_3', b'row_key_4',
+                                 b'row_key_5', b'row_key_6'])
         found_row_keys = set([row.row_key for row in read_rows])
         self.assertEqual(found_row_keys, expected_row_keys)
 
