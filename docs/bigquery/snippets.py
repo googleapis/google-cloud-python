173d71b58ba1c4b4eb5f40fabe5d7d3962ca3718
Alix Hamilton <ajhamilton@google.com>
2018-07-16 12:13:44 -0700

    adds snippet for get_job()

--- docs/bigquery/snippets.py
+++ docs/bigquery/snippets.py
@@ -1325,6 +1325,23 @@ def test_load_table_from_uri_csv(client, to_delete, capsys):
     out, _ = capsys.readouterr()
     assert 'Loaded 50 rows.' in out
 
+    job_id = load_job.job_id
+    location = 'us'
+    # [START bigquery_get_job]
+    # TODO(developer): Uncomment the lines below and replace with your values.
+    # from google.cloud import bigquery
+    # client = bigquery.Client()
+    # job_id = 'bq-job-123x456-123y123z123c'  # replace with your job ID
+    # location = 'us'                         # replace with your location
+
+    job = client.get_job(job_id, location=location)  # API request
+
+    # Print selected job properties
+    print('Details for job {} running in {}:'.format(job_id, location))
+    print('\tType: {}\n\tState: {}\n\tCreated: {}'.format(
+        job.job_type, job.state, job.created))
+    # [END bigquery_get_job]
+
 
 def test_load_table_from_uri_json(client, to_delete, capsys):
     dataset_id = 'load_table_dataset_{}'.format(_millis())
