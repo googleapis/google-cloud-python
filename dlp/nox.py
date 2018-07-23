50b961b0b0357f169281e44262ef3456af054816
Chris Wilcox <crwilcox@google.com>
2018-07-23 16:07:38 -0700

    revert nox changes

--- dlp/nox.py
+++ dlp/nox.py
@@ -1,10 +1,10 @@
-# Copyright 2018 Google LLC
+# Copyright 2017, Google LLC All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,9 +18,35 @@ import os
 import nox
 
 
+LOCAL_DEPS = (
+    os.path.join('..', 'api_core'),
+    os.path.join('..', 'core'),
+)
+
+
 @nox.session
 def default(session):
-    return unit(session, 'default')
+    """Run the unit test suite.
+
+    This is intended to be run **without** an interpreter set, so
+    that the current ``python`` (on the ``PATH``) or the version of
+    Python corresponding to the ``nox`` binary the ``PATH`` can
+    run the tests.
+    """
+    session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
+    session.install('-e', '.')
+
+    session.run(
+        'py.test',
+        '--quiet',
+        '--cov=google.cloud.dlp_v2',
+        '--cov-append',
+        '--cov-config=.coveragerc',
+        '--cov-report=',
+        '--cov-fail-under=97',
+        os.path.join('tests', 'unit'),
+        *session.posargs
+    )
 
 
 @nox.session
@@ -29,18 +55,47 @@ def unit(session, py):
     """Run the unit test suite."""
 
     # Run unit tests against all supported versions of Python.
-    if py != 'default':
-        session.interpreter = 'python{}'.format(py)
+    session.interpreter = 'python{}'.format(py)
 
-    # Set the virtualenv directory name.
+    # Set the virtualenv dirname.
     session.virtualenv_dirname = 'unit-' + py
 
-    # Install all test dependencies, then install this package in-place.
+    default(session)
+
+
+@nox.session
+@nox.parametrize('py', ['2.7', '3.6'])
+def system(session, py):
+    """Run the system test suite."""
+
+    if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
+        session.skip('Credentials must be set via environment variable.')
+
+    session.interpreter = 'python{}'.format(py)
+
+    session.virtualenv_dirname = 'sys-' + py
+
+    # Use pre-release gRPC for system tests.
+    session.install('--pre', 'grpcio')
+
     session.install('pytest')
     session.install('-e', '.')
 
-    # Run py.test against the unit tests.
-    session.run('py.test', '--quiet', os.path.join('tests', 'unit'))
+    session.run('py.test', '--quiet',
+                os.path.join('tests', 'system'), *session.posargs)
+
+
+@nox.session
+def lint(session):
+    """Run linters.
+
+    Returns a failure if the linters find linting errors or sufficiently
+    serious code quality issues.
+    """
+    session.interpreter = 'python3.6'
+    session.install('flake8', *LOCAL_DEPS)
+    session.install('.')
+    session.run('flake8', 'google', 'tests')
 
 
 @nox.session
@@ -50,3 +105,18 @@ def lint_setup_py(session):
     session.install('docutils', 'pygments')
     session.run('python', 'setup.py', 'check', '--restructuredtext',
                 '--strict')
+
+
+@nox.session
+def cover(session):
+    """Run the final coverage report.
+
+    This outputs the coverage report aggregating coverage from the unit
+    test runs (not system test runs), and then erases coverage data.
+    """
+    session.interpreter = 'python3.6'
+    session.chdir(os.path.dirname(__file__))
+    session.install('coverage', 'pytest-cov')
+    session.run('coverage', 'report', '--show-missing', '--fail-under=100')
+    session.run('coverage', 'erase')
+
