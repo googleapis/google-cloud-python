50d2356a4e3e2b34bf8fa77729d0487b40940d4d
Chris Wilcox <crwilcox@google.com>
2018-07-31 16:24:58 -0700

    move to using 3.7 for system tests

--- dataproc/nox.py
+++ dataproc/nox.py
@@ -63,7 +63,7 @@ def unit(session, py):
 
 
 @nox.session
-@nox.parametrize('py', ['2.7', '3.6'])
+@nox.parametrize('py', ['2.7', '3.7'])
 def system(session, py):
     """Run the system test suite."""
 
