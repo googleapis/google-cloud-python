0019ce359870bca9cc5861b38b543af0f14a658a
Christopher Wilcox <crwilcox@google.com>
2018-08-13 16:30:46 -0700

    Release 0.2.0 (#5794)

--- packages/google-cloud-bigquery-datatransfer/setup.py
+++ packages/google-cloud-bigquery-datatransfer/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-bigquery-datatransfer'
 description = 'BigQuery Data Transfer API client library'
-version = '0.1.1'
+version = '0.2.0'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
