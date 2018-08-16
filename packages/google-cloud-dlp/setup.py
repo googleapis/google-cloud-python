a5527c4314d46558031c45d866c46768604f6d43
Christopher Wilcox <crwilcox@google.com>
2018-08-16 10:35:01 -0700

    Release 0.7.0 (#5811)

--- packages/google-cloud-dlp/setup.py
+++ packages/google-cloud-dlp/setup.py
@@ -19,7 +19,7 @@ import setuptools
 
 name = 'google-cloud-dlp'
 description = 'Cloud Data Loss Prevention (DLP) API API client library'
-version = '0.6.0'
+version = '0.7.0'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'google-api-core[grpc] >= 1.1.0, < 2.0.0dev',
