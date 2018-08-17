75de6a53c9feee1bc37ee4bfd1369b3010403194
Christopher Wilcox <crwilcox@google.com>
2018-08-17 14:15:38 -0700

    Generate cloud asset library v1beta1 (#5817)

---
+++ asset/tests/unit/gapic/v1beta1/test_asset_service_client_v1beta1.py
@@ -0,0 +1,159 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests."""
+
+import pytest
+
+from google.rpc import status_pb2
+
+from google.cloud import asset_v1beta1
+from google.cloud.asset_v1beta1 import enums
+from google.cloud.asset_v1beta1.proto import asset_service_pb2
+from google.cloud.asset_v1beta1.proto import assets_pb2
+from google.longrunning import operations_pb2
+
+
+class MultiCallableStub(object):
+    """Stub for the grpc.UnaryUnaryMultiCallable interface."""
+
+    def __init__(self, method, channel_stub):
+        self.method = method
+        self.channel_stub = channel_stub
+
+    def __call__(self, request, timeout=None, metadata=None, credentials=None):
+        self.channel_stub.requests.append((self.method, request))
+
+        response = None
+        if self.channel_stub.responses:
+            response = self.channel_stub.responses.pop()
+
+        if isinstance(response, Exception):
+            raise response
+
+        if response:
+            return response
+
+
+class ChannelStub(object):
+    """Stub for the grpc.Channel interface."""
+
+    def __init__(self, responses=[]):
+        self.responses = responses
+        self.requests = []
+
+    def unary_unary(self,
+                    method,
+                    request_serializer=None,
+                    response_deserializer=None):
+        return MultiCallableStub(method, self)
+
+
+class CustomException(Exception):
+    pass
+
+
+class TestAssetServiceClient(object):
+    def test_export_assets(self):
+        # Setup Expected Response
+        expected_response = {}
+        expected_response = asset_service_pb2.ExportAssetsResponse(
+            **expected_response)
+        operation = operations_pb2.Operation(
+            name='operations/test_export_assets', done=True)
+        operation.response.Pack(expected_response)
+
+        # Mock the API response
+        channel = ChannelStub(responses=[operation])
+        client = asset_v1beta1.AssetServiceClient(channel=channel)
+
+        # Setup Request
+        parent = client.project_path('[PROJECT]')
+        content_types = []
+        output_config = {}
+
+        response = client.export_assets(parent, content_types, output_config)
+        result = response.result()
+        assert expected_response == result
+
+        assert len(channel.requests) == 1
+        expected_request = asset_service_pb2.ExportAssetsRequest(
+            parent=parent,
+            content_types=content_types,
+            output_config=output_config)
+        actual_request = channel.requests[0][1]
+        assert expected_request == actual_request
+
+    def test_export_assets_exception(self):
+        # Setup Response
+        error = status_pb2.Status()
+        operation = operations_pb2.Operation(
+            name='operations/test_export_assets_exception', done=True)
+        operation.error.CopyFrom(error)
+
+        # Mock the API response
+        channel = ChannelStub(responses=[operation])
+        client = asset_v1beta1.AssetServiceClient(channel=channel)
+
+        # Setup Request
+        parent = client.project_path('[PROJECT]')
+        content_types = []
+        output_config = {}
+
+        response = client.export_assets(parent, content_types, output_config)
+        exception = response.exception()
+        assert exception.errors[0] == error
+
+    def test_batch_get_assets_history(self):
+        # Setup Expected Response
+        expected_response = {}
+        expected_response = asset_service_pb2.BatchGetAssetsHistoryResponse(
+            **expected_response)
+
+        # Mock the API response
+        channel = ChannelStub(responses=[expected_response])
+        client = asset_v1beta1.AssetServiceClient(channel=channel)
+
+        # Setup Request
+        parent = client.project_path('[PROJECT]')
+        asset_names = []
+        content_type = enums.ContentType.CONTENT_TYPE_UNSPECIFIED
+        read_time_window = {}
+
+        response = client.batch_get_assets_history(
+            parent, asset_names, content_type, read_time_window)
+        assert expected_response == response
+
+        assert len(channel.requests) == 1
+        expected_request = asset_service_pb2.BatchGetAssetsHistoryRequest(
+            parent=parent,
+            asset_names=asset_names,
+            content_type=content_type,
+            read_time_window=read_time_window)
+        actual_request = channel.requests[0][1]
+        assert expected_request == actual_request
+
+    def test_batch_get_assets_history_exception(self):
+        # Mock the API response
+        channel = ChannelStub(responses=[CustomException()])
+        client = asset_v1beta1.AssetServiceClient(channel=channel)
+
+        # Setup request
+        parent = client.project_path('[PROJECT]')
+        asset_names = []
+        content_type = enums.ContentType.CONTENT_TYPE_UNSPECIFIED
+        read_time_window = {}
+
+        with pytest.raises(CustomException):
+            client.batch_get_assets_history(parent, asset_names, content_type,
+                                            read_time_window)
