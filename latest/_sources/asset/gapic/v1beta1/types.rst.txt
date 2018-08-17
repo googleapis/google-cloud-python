d67e429ba025e4928cc655686a1aca819c4d2fad
dpebot <dpebot@google.com>
2018-08-17 22:54:24 +0000

    Update docs after merge to master.

---
+++ latest/_sources/asset/gapic/v1beta1/types.rst.txt
@@ -0,0 +1,5 @@
+Types for Cloud Asset API Client
+================================
+
+.. automodule:: google.cloud.asset_v1beta1.types
+    :members:
--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2c6f69e',
+    VERSION: '75de6a5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
