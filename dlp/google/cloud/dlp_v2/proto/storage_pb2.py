37c32ccc2bab4fbf26bcc6beebac6d52458b82d0
Chris Wilcox <crwilcox@google.com>
2018-08-17 09:44:39 -0700

    fix #5668 by fixing docs issues in DLP generation

--- dlp/google/cloud/dlp_v2/proto/storage_pb2.py
+++ dlp/google/cloud/dlp_v2/proto/storage_pb2.py
@@ -1497,16 +1497,16 @@ CustomInfoType = _reflection.GeneratedProtocolMessageType('CustomInfoType', (_me
           hotword_regex: \
               Regular expression pattern defining what qualifies as a \
               hotword.
-          proximity: \
-              Proximity of the finding within which the entire hotword must \
-              reside. The total length of the window cannot exceed 1000 \
-              characters. Note that the finding itself will be included in \
-              the window, so that hotwords may be used to match substrings \
-              of the finding itself. For example, the certainty of a phone \
-              number regex "(`\d{3}`) `\d{3}`-:raw- \
-              latex:`\d{4}`" could be adjusted upwards if the area code is \
-              known to be the local area code of a company office using the \
-              hotword regex "(xxx)", where "xxx" is the area code in \
+          proximity:
+              Proximity of the finding within which the entire hotword must
+              reside. The total length of the window cannot exceed 1000
+              characters. Note that the finding itself will be included in
+              the window, so that hotwords may be used to match substrings
+              of the finding itself. For example, the certainty of a phone
+              number regex "(\d{3}) \d{3}-\d{4} "\
+              could be adjusted upwards if the area code is \
+              known to be the local area code of a company office using the
+              hotword regex "(xxx)", where "xxx" is the area code in
               question.
           likelihood_adjustment: \
               Likelihood adjustment to apply to all matching findings.
