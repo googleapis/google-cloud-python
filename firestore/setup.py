ee126081492b29821b42f61a89cc1c16a1e45ff4
Chris McDonough <chrismgoog@gmail.com>
2018-08-25 13:16:10 -0400

    depends directly on pytz now

--- firestore/setup.py
+++ firestore/setup.py
@@ -31,6 +31,7 @@ release_status = 'Development Status :: 4 - Beta'
 dependencies = [
     'google-cloud-core<0.29dev,>=0.28.0',
     'google-api-core[grpc]<2.0.0dev,>=0.1.1',
+    'pytz',
 ]
 extras = {
 }
