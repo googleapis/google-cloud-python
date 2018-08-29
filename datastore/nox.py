08fcc0d1d8a7ab02c0f7a520f7514421c89fefbb
Tres Seaver <tseaver@palladion.com>
2018-08-29 15:16:18 -0400

    datastore: install local packages inplace under nox.

--- datastore/nox.py
+++ datastore/nox.py
@@ -34,8 +34,9 @@ def default(session):
     Python corresponding to the ``nox`` binary the ``PATH`` can
     run the tests.
     """
-    # Install all test dependencies, then install this package in-place.
-    session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
+    # Install all test dependencies, then install local packages in-place.
+    session.install('mock', 'pytest', 'pytest-cov')
+    session.install('-e', *LOCAL_DEPS)
     session.install('-e', '.')
 
     # Run py.test against the unit tests.
@@ -85,11 +86,10 @@ def system(session, py):
     # Use pre-release gRPC for system tests.
     session.install('--pre', 'grpcio')
 
-    # Install all test dependencies, then install this package into the
-    # virtualenv's dist-packages.
-    session.install('mock', 'pytest', *LOCAL_DEPS)
-    session.install('../test_utils/')
-    session.install('.')
+    # Install all test dependencies, then install local packages in-place.
+    session.install('mock', 'pytest')
+    session.install('-e', '../test_utils/', *LOCAL_DEPS)
+    session.install('-e', '.')
 
     # Run py.test against the system tests.
     session.run('py.test', '--quiet', 'tests/system', *session.posargs)
@@ -111,9 +111,9 @@ def doctests(session):
 
     # Install all test dependencies, then install this package into the
     # virtualenv's dist-packages.
-    session.install('mock', 'pytest', 'sphinx', *LOCAL_DEPS)
-    session.install('../test_utils/')
-    session.install('.')
+    session.install('mock', 'pytest', 'sphinx')
+    session.install('-e', '../test_utils/', *LOCAL_DEPS)
+    session.install('-e', '.')
 
     # Run py.test against the system tests.
     session.run('py.test', '--quiet', 'tests/doctests.py')
