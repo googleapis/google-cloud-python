ec7a953b5019ff479b3be3bfe7b003bf38734739
Tres Seaver <tseaver@palladion.com>
2018-08-29 16:54:17 -0400

    spanner: install local packages inplace under nox.

--- spanner/nox.py
+++ spanner/nox.py
@@ -34,8 +34,9 @@ def default(session):
     Python corresponding to the ``nox`` binary the ``PATH`` can
     run the tests.
     """
-    # Install all test dependencies, then install this package in-place.
-    session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
+    # Install all test dependencies, then install local packages in-place.
+    session.install('mock', 'pytest', 'pytest-cov')
+    session.install('-e', *LOCAL_DEPS)
     session.install('-e', '.')
 
     # Run py.test against the unit tests.
@@ -89,10 +90,9 @@ def system_common(session):
     # Use pre-release gRPC for system tests.
     session.install('--pre', 'grpcio')
 
-    # Install all test dependencies, then install this package into the
-    # virtualenv's dist-packages.
-    session.install('mock', 'pytest', *LOCAL_DEPS)
-    session.install('../test_utils/')
+    # Install all test dependencies, then install local packages in-place.
+    session.install('mock', 'pytest')
+    session.install('-e', '../test_utils/', *LOCAL_DEPS)
     session.install('-e', '.')
 
     # Run py.test against the system tests.
