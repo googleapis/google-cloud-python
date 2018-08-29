0bb0653fba5ed79f53393869fb578a9c04ca31a8
Tres Seaver <tseaver@palladion.com>
2018-08-29 17:36:29 -0400

    storage: install local packages inplace under nox.

--- storage/nox.py
+++ storage/nox.py
@@ -34,8 +34,9 @@ def default(session):
     Python corresponding to the ``nox`` binary the ``PATH`` can
     run the tests.
     """
-    # Install all test dependencies, then install this package in-place.
-    session.install('mock', 'pytest', 'pytest-cov', *LOCAL_DEPS)
+    # Install all test dependencies, then install local packages in-place.
+    session.install('mock', 'pytest', 'pytest-cov')
+    session.install('-e', *LOCAL_DEPS)
     session.install('-e', '.')
 
     # Run py.test against the unit tests.
@@ -85,12 +86,13 @@ def system(session, py):
     # Use pre-release gRPC for system tests.
     session.install('--pre', 'grpcio')
 
-    # Install all test dependencies, then install this package into the
-    # virtualenv's dist-packages.
-    session.install('mock', 'pytest', *LOCAL_DEPS)
-    session.install('../test_utils/')
-    session.install('../pubsub')
-    session.install('../kms')
+    # Install all test dependencies, then install local packages in-place.
+    session.install('mock', 'pytest')
+    session.install('-e',
+        '../test_utils/',
+        '../pubsub',
+        '../kms',
+        *LOCAL_DEPS)
     session.install('-e', '.')
 
     # Run py.test against the system tests.
