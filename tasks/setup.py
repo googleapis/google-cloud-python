04245e91bff17adfe5321acdb50ce6519716cbd1
Chris Wilcox <crwilcox@google.com>
2018-09-03 12:12:44 -0700

    Release 0.3.0

--- tasks/setup.py
+++ tasks/setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 name = 'google-cloud-tasks'
 description = 'Cloud Tasks API API client library'
-version = '0.1.0'
+version = '0.3.0'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'google-api-core[grpc] >= 1.1.0, < 2.0.0dev',
