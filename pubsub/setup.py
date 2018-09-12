fcccfb08841783d9b8a960ac4e686d5025a0315a
Thea Flowers <theaflowers@google.com>
2018-09-12 15:15:35 -0700

    Release 0.38.0

--- pubsub/setup.py
+++ pubsub/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-pubsub'
 description = 'Google Cloud Pub/Sub API client library'
-version = '0.37.2'
+version = '0.38.0'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
