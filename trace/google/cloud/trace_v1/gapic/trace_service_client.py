f5285273018646ac19bffde7863db617caac5c8a
Tres Seaver <tseaver@palladion.com>
2018-09-19 11:41:31 -0400

    Trace: prep docs for repo split.
    - Remove contents of 'docs/trace/', and replace w/ symlink to 'trace/docs'.
    - Harmonize / DRY 'trace/README.rst' with 'trace/docs/index.rst'.
    - Adjust generated V1 docstrings to allow Sphinx to build.
    
    Toward #5912.

--- trace/google/cloud/trace_v1/gapic/trace_service_client.py
+++ trace/google/cloud/trace_v1/gapic/trace_service_client.py
@@ -293,41 +293,25 @@ class TraceServiceClient(object):
                 Multiple terms are ANDed. Syntax:
 
                 *   ``root:NAME_PREFIX`` or ``NAME_PREFIX``: Return traces where any root
-                ::
-
                     span starts with `NAME_PREFIX`.
                 *   ``+root:NAME`` or ``+NAME``: Return traces where any root span's name is
-                ::
-
                     exactly `NAME`.
                 *   ``span:NAME_PREFIX``: Return traces where any span starts with
-                ::
-
                     `NAME_PREFIX`.
                 *   ``+span:NAME``: Return traces where any span's name is exactly
-                ::
-
                     `NAME`.
                 *   ``latency:DURATION``: Return traces whose overall latency is
-                ::
-
                     greater or equal to than `DURATION`. Accepted units are nanoseconds
                     (`ns`), milliseconds (`ms`), and seconds (`s`). Default is `ms`. For
                     example, `latency:24ms` returns traces whose overall latency
                     is greater than or equal to 24 milliseconds.
                 *   ``label:LABEL_KEY``: Return all traces containing the specified
-                ::
-
                     label key (exact match, case-sensitive) regardless of the key:value
                     pair's value (including empty values).
                 *   ``LABEL_KEY:VALUE_PREFIX``: Return all traces containing the specified
-                ::
-
                     label key (exact match, case-sensitive) whose value starts with
                     `VALUE_PREFIX`. Both a key and a value must be specified.
                 *   ``+LABEL_KEY:VALUE``: Return all traces containing a key:value pair
-                ::
-
                     exactly matching the specified text. Both a key and a value must be
                     specified.
                 *   ``method:VALUE``: Equivalent to ``/http/method:VALUE``.
@@ -338,8 +322,6 @@ class TraceServiceClient(object):
                 *   ``trace_id``
                 *   ``name`` (``name`` field of root span in the trace)
                 *   ``duration`` (difference between ``end_time`` and ``start_time`` fields of
-                ::
-
                      the root span)
                 *   ``start`` (``start_time`` field of the root span)
 
