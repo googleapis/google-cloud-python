479e224c105c96b3cb3beffd8528a6f5fa9c8e05
Maxim Zudilov <maxim.zudilov@gmail.com>
2018-09-20 20:00:52 +1200

    Add tests

--- packages/sqlalchemy-bigquery/test/test_sqlalchemy_bigquery.py
+++ packages/sqlalchemy-bigquery/test/test_sqlalchemy_bigquery.py
@@ -92,6 +92,12 @@ def engine():
 
 
 @pytest.fixture(scope='session')
+def engine_using_test_dataset():
+    engine = create_engine('bigquery:///test_pybigquery', echo=True)
+    return engine
+
+
+@pytest.fixture(scope='session')
 def engine_with_location():
     engine = create_engine('bigquery://', echo=True, location="asia-northeast1")
     return engine
@@ -100,6 +106,11 @@ def engine_with_location():
 @pytest.fixture(scope='session')
 def table(engine):
     return Table('test_pybigquery.sample', MetaData(bind=engine), autoload=True)
+
+
+@pytest.fixture(scope='session')
+def table_using_test_dataset(engine_using_test_dataset):
+    return Table('sample', MetaData(bind=engine_using_test_dataset), autoload=True)
 
 
 @pytest.fixture(scope='session')
@@ -120,23 +131,32 @@ def session(engine):
 
 
 @pytest.fixture(scope='session')
+def session_using_test_dataset(engine_using_test_dataset):
+    Session = sessionmaker(bind=engine_using_test_dataset)
+    session = Session()
+    return session
+
+
+@pytest.fixture(scope='session')
 def inspector(engine):
     return inspect(engine)
 
 
 @pytest.fixture(scope='session')
-def query(table):
-    col1 = literal_column("TIMESTAMP_TRUNC(timestamp, DAY)").label("timestamp_label")
-    col2 = func.sum(table.c.integer)
-    query = (
-        select([
-            col1,
-            col2,
-        ])
-        .where(col1 < '2017-01-01 00:00:00')
-        .group_by(col1)
-        .order_by(col2)
-    )
+def query():
+    def query(table):
+        col1 = literal_column("TIMESTAMP_TRUNC(timestamp, DAY)").label("timestamp_label")
+        col2 = func.sum(table.c.integer)
+        query = (
+            select([
+                col1,
+                col2,
+            ])
+            .where(col1 < '2017-01-01 00:00:00')
+            .group_by(col1)
+            .order_by(col2)
+        )
+        return query
     return query
 
 
@@ -156,32 +176,47 @@ def test_dry_run(engine, api_client):
     assert 'Table name "sample_one_row" cannot be resolved: dataset name is missing.' in str(excinfo.value.message)
 
 
+def test_engine_with_dataset(engine_using_test_dataset):
+    # Raw queries should always include dataset
+    rows = engine_using_test_dataset.execute('SELECT * FROM test_pybigquery.sample_one_row').fetchall()
+    assert list(rows[0]) == ONE_ROW_CONTENTS
+
+    table_one_row = Table('sample_one_row', MetaData(bind=engine_using_test_dataset), autoload=True)
+    rows = table_one_row.select().execute().fetchall()
+    assert list(rows[0]) == ONE_ROW_CONTENTS_EXPANDED
+
+    # Table name shouldn't be included in the dataset
+    with pytest.raises(Exception):
+        table_one_row = Table('test_pybigquery.sample_one_row', MetaData(bind=engine_using_test_dataset), autoload=True)
+
+
 def test_dataset_location(engine_with_location):
     rows = engine_with_location.execute('SELECT * FROM test_pybigquery_location.sample_one_row').fetchall()
     assert list(rows[0]) == ONE_ROW_CONTENTS
 
 
-def test_reflect_select(engine, table):
-    assert len(table.c) == 14
+def test_reflect_select(table, table_using_test_dataset):
+    for table in [table, table_using_test_dataset]:
+        assert len(table.c) == 14
 
-    assert isinstance(table.c.integer, Column)
-    assert isinstance(table.c.integer.type, types.Integer)
-    assert isinstance(table.c.timestamp.type, types.TIMESTAMP)
-    assert isinstance(table.c.string.type, types.String)
-    assert isinstance(table.c.float.type, types.Float)
-    assert isinstance(table.c.boolean.type, types.Boolean)
-    assert isinstance(table.c.date.type, types.DATE)
-    assert isinstance(table.c.datetime.type, types.DATETIME)
-    assert isinstance(table.c.time.type, types.TIME)
-    assert isinstance(table.c.bytes.type, types.BINARY)
-    assert isinstance(table.c['record.age'].type, types.Integer)
-    assert isinstance(table.c['record.name'].type, types.String)
-    assert isinstance(table.c['nested_record.record.age'].type, types.Integer)
-    assert isinstance(table.c['nested_record.record.name'].type, types.String)
-    assert isinstance(table.c.array.type, types.ARRAY)
+        assert isinstance(table.c.integer, Column)
+        assert isinstance(table.c.integer.type, types.Integer)
+        assert isinstance(table.c.timestamp.type, types.TIMESTAMP)
+        assert isinstance(table.c.string.type, types.String)
+        assert isinstance(table.c.float.type, types.Float)
+        assert isinstance(table.c.boolean.type, types.Boolean)
+        assert isinstance(table.c.date.type, types.DATE)
+        assert isinstance(table.c.datetime.type, types.DATETIME)
+        assert isinstance(table.c.time.type, types.TIME)
+        assert isinstance(table.c.bytes.type, types.BINARY)
+        assert isinstance(table.c['record.age'].type, types.Integer)
+        assert isinstance(table.c['record.name'].type, types.String)
+        assert isinstance(table.c['nested_record.record.age'].type, types.Integer)
+        assert isinstance(table.c['nested_record.record.name'].type, types.String)
+        assert isinstance(table.c.array.type, types.ARRAY)
 
-    rows = table.select().execute().fetchall()
-    assert len(rows) == 1000
+        rows = table.select().execute().fetchall()
+        assert len(rows) == 1000
 
 
 def test_content_from_raw_queries(engine):
@@ -226,63 +261,83 @@ def test_reflect_dataset_does_not_exist(engine):
         Table('dataset_does_not_exist.table_does_not_exist', MetaData(bind=engine), autoload=True)
 
 
-def test_tables_list(engine):
+def test_tables_list(engine, engine_using_test_dataset):
     tables = engine.table_names()
     assert 'test_pybigquery.sample' in tables
     assert 'test_pybigquery.sample_one_row' in tables
     assert 'test_pybigquery.sample_dml' in tables
 
+    tables = engine_using_test_dataset.table_names()
+    assert 'test_pybigquery.sample' in tables
+    assert 'test_pybigquery.sample_one_row' in tables
+    assert 'test_pybigquery.sample_dml' in tables
 
-def test_group_by(session, table):
+
+def test_group_by(session, table, session_using_test_dataset, table_using_test_dataset):
     """labels in SELECT clause should be correclty formatted (dots are replaced with underscores)"""
-    result = session.query(table.c.string, func.count(table.c.integer)).group_by(table.c.string).all()
+    for session, table in [(session, table), (session_using_test_dataset, table_using_test_dataset)]:
+        result = session.query(table.c.string, func.count(table.c.integer)).group_by(table.c.string).all()
     assert len(result) > 0
 
 
-def test_session_query(session, table):
-    col_concat = func.concat(table.c.string).label('concat')
-    result = (
-        session
-        .query(
-            table.c.string,
-            col_concat,
-            func.avg(table.c.integer),
-            func.sum(case([(table.c.boolean == True, 1)], else_=0))
+def test_session_query(session, table, session_using_test_dataset, table_using_test_dataset):
+    for session, table in [(session, table), (session_using_test_dataset, table_using_test_dataset)]:
+        col_concat = func.concat(table.c.string).label('concat')
+        result = (
+            session
+            .query(
+                table.c.string,
+                col_concat,
+                func.avg(table.c.integer),
+                func.sum(case([(table.c.boolean == True, 1)], else_=0))
+            )
+            .group_by(table.c.string, col_concat)
+            .having(func.avg(table.c.integer) > 10)
+
+        ).all()
+        assert len(result) > 0
+
+
+def test_labels(session, table, session_using_test_dataset, table_using_test_dataset):
+    for session, table in [(session, table), (session_using_test_dataset, table_using_test_dataset)]:
+        result = (
+            session
+            .query(
+                # Valid
+                table.c.string.label('abc'),
+                # Invalid, needs to start with underscore
+                table.c.string.label('123'),
+                # Valid
+                table.c.string.label('_123abc'),
+                # Invalid, contains illegal characters
+                table.c.string.label('!@#$%^&*()~`'),
+            )
         )
-        .group_by(table.c.string, col_concat)
-        .having(func.avg(table.c.integer) > 10)
-
-    ).all()
-    assert len(result) > 0
+        result = result.all()
+        assert len(result) > 0
 
 
-def test_labels(session, table):
-    result = (
-        session
-        .query(
-            # Valid
-            table.c.string.label('abc'),
-            # Invalid, needs to start with underscore
-            table.c.string.label('123'),
-            # Valid
-            table.c.string.label('_123abc'),
-            # Invalid, contains illegal characters
-            table.c.string.label('!@#$%^&*()~`'),
-        )
-    )
-    result = result.all()
-    assert len(result) > 0
-
-
-def test_custom_expression(engine, query):
+def test_custom_expression(engine, engine_using_test_dataset, table, table_using_test_dataset, query):
     """GROUP BY clause should use labels instead of expressions"""
-    result = engine.execute(query).fetchall()
+    q = query(table)
+    result = engine.execute(q).fetchall()
+    assert len(result) > 0
+
+    q = query(table_using_test_dataset)
+    result = engine_using_test_dataset.execute(q).fetchall()
+
     assert len(result) > 0
 
 
-def test_compiled_query_literal_binds(engine, query):
-    compiled = query.compile(engine, compile_kwargs={"literal_binds": True})
+def test_compiled_query_literal_binds(engine, engine_using_test_dataset, table, table_using_test_dataset, query):
+    q = query(table)
+    compiled = q.compile(engine, compile_kwargs={"literal_binds": True})
     result = engine.execute(compiled).fetchall()
+    assert len(result) > 0
+
+    q = query(table_using_test_dataset)
+    compiled = q.compile(engine_using_test_dataset, compile_kwargs={"literal_binds": True})
+    result = engine_using_test_dataset.execute(compiled).fetchall()
     assert len(result) > 0
 
 
