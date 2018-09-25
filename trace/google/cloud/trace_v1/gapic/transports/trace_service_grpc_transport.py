7a5d47ca6b0686efa4a9552206e2dfb865dd8d14
Tres Seaver <tseaver@palladion.com>
2018-09-25 14:03:19 -0400

    Trace: add 'synth.py'. (#6083)
    Closes #6075.

---
+++ trace/google/cloud/trace_v1/gapic/transports/trace_service_grpc_transport.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import google.api_core.grpc_helpers
+
+from google.cloud.trace_v1.proto import trace_pb2_grpc
+
+
+class TraceServiceGrpcTransport(object):
+    """gRPC transport class providing stubs for
+    google.devtools.cloudtrace.v1 TraceService API.
+
+    The transport provides access to the raw gRPC stubs,
+    which can be used to take advantage of advanced
+    features of gRPC.
+    """
+    # The scopes needed to make gRPC calls to all of the methods defined
+    # in this service.
+    _OAUTH_SCOPES = (
+        'https://www.googleapis.com/auth/cloud-platform',
+        'https://www.googleapis.com/auth/trace.append',
+        'https://www.googleapis.com/auth/trace.readonly',
+    )
+
+    def __init__(self,
+                 channel=None,
+                 credentials=None,
+                 address='cloudtrace.googleapis.com:443'):
+        """Instantiate the transport class.
+
+        Args:
+            channel (grpc.Channel): A ``Channel`` instance through
+                which to make calls. This argument is mutually exclusive
+                with ``credentials``; providing both will raise an exception.
+            credentials (google.auth.credentials.Credentials): The
+                authorization credentials to attach to requests. These
+                credentials identify this application to the service. If none
+                are specified, the client will attempt to ascertain the
+                credentials from the environment.
+            address (str): The address where the service is hosted.
+        """
+        # If both `channel` and `credentials` are specified, raise an
+        # exception (channels come with credentials baked in already).
+        if channel is not None and credentials is not None:
+            raise ValueError(
+                'The `channel` and `credentials` arguments are mutually '
+                'exclusive.', )
+
+        # Create the channel.
+        if channel is None:
+            channel = self.create_channel(
+                address=address,
+                credentials=credentials,
+            )
+
+        # gRPC uses objects called "stubs" that are bound to the
+        # channel and provide a basic method for each RPC.
+        self._stubs = {
+            'trace_service_stub': trace_pb2_grpc.TraceServiceStub(channel),
+        }
+
+    @classmethod
+    def create_channel(cls,
+                       address='cloudtrace.googleapis.com:443',
+                       credentials=None):
+        """Create and return a gRPC channel object.
+
+        Args:
+            address (str): The host for the channel to use.
+            credentials (~.Credentials): The
+                authorization credentials to attach to requests. These
+                credentials identify this application to the service. If
+                none are specified, the client will attempt to ascertain
+                the credentials from the environment.
+
+        Returns:
+            grpc.Channel: A gRPC channel object.
+        """
+        return google.api_core.grpc_helpers.create_channel(
+            address,
+            credentials=credentials,
+            scopes=cls._OAUTH_SCOPES,
+        )
+
+    @property
+    def patch_traces(self):
+        """Return the gRPC stub for {$apiMethod.name}.
+
+        Sends new traces to Stackdriver Trace or updates existing traces. If the ID
+        of a trace that you send matches that of an existing trace, any fields
+        in the existing trace and its spans are overwritten by the provided values,
+        and any new fields provided are merged with the existing trace data. If the
+        ID does not match, a new trace is created.
+
+        Returns:
+            Callable: A callable which accepts the appropriate
+                deserialized request object and returns a
+                deserialized response object.
+        """
+        return self._stubs['trace_service_stub'].PatchTraces
+
+    @property
+    def get_trace(self):
+        """Return the gRPC stub for {$apiMethod.name}.
+
+        Gets a single trace by its ID.
+
+        Returns:
+            Callable: A callable which accepts the appropriate
+                deserialized request object and returns a
+                deserialized response object.
+        """
+        return self._stubs['trace_service_stub'].GetTrace
+
+    @property
+    def list_traces(self):
+        """Return the gRPC stub for {$apiMethod.name}.
+
+        Returns of a list of traces that match the specified filter conditions.
+
+        Returns:
+            Callable: A callable which accepts the appropriate
+                deserialized request object and returns a
+                deserialized response object.
+        """
+        return self._stubs['trace_service_stub'].ListTraces
--- trace/google/cloud/trace_v1/types.py
+++ trace/google/cloud/trace_v1/types.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,14 +17,13 @@
 from __future__ import absolute_import
 import sys
 
+from google.api_core.protobuf_helpers import get_messages
+
 from google.api import http_pb2
+from google.cloud.trace_v1.proto import trace_pb2
 from google.protobuf import descriptor_pb2
 from google.protobuf import empty_pb2
 from google.protobuf import timestamp_pb2
-
-from google.api_core.protobuf_helpers import get_messages
-from google.cloud.trace_v1.proto import trace_pb2
-
 
 _shared_modules = [
     http_pb2,
@@ -41,7 +42,6 @@ for module in _shared_modules:
     for name, message in get_messages(module).items():
         setattr(sys.modules[__name__], name, message)
         names.append(name)
-
 for module in _local_modules:
     for name, message in get_messages(module).items():
         message.__module__ = 'google.cloud.trace_v1.types'
