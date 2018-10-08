226eb8d98b4b9051ab417ba8f6a18f6d7fe37585
Danny Hermes <daniel.j.hermes@gmail.com>
2018-10-08 13:03:30 -0700

    Adding all arguments to Key() constructor.
    Added documentation for each argument to the docstring. For most
    arguments, just raising `NotImplementedError` for now, but the `app`
    and `namespace` inputs are supported. The current handling of `app`
    likely needs tweaking so I added a note to the README about how I think
    we might handle it.

--- ndb/README.md
+++ ndb/README.md
@@ -11,6 +11,26 @@ It was designed specifically to be used from within the
 Learn how to use the ``ndb`` library by visiting the Google Cloud Platform
 [documentation][2].
 
-[0]:https://cloud.google.com/datastore
-[1]:https://cloud.google.com/appengine
-[2]:https://cloud.google.com/appengine/docs/python/ndb/
+## Assumptions
+
+This is a running list of "compatibility" assumptions made for
+the rewrite.
+
+- In production, the `APPLICATION_ID` environment variable will be set to
+  a useful value (since there is no `dev_appserver.py` for
+  `runtime: python37`). This is used as a fallback for the `ndb.Key()`
+  constructor much like `google.cloud.datastore.Client()` determines a default
+  project via one of
+
+  - `DATASTORE_DATASET` environment variable (for `gcd` / emulator testing)
+  - `GOOGLE_CLOUD_PROJECT` environment variable
+  - Google App Engine application ID (this is legacy / standard GAE)
+  - Google Compute Engine project ID (from metadata server)
+
+  The correct fallback is likely different than this and should probably cache
+  the output of `google.cloud.datastore.client._determine_default_project()`
+  on the `ndb.Key` class or `ndb.key` module (at import time)
+
+[0]: https://cloud.google.com/datastore
+[1]: https://cloud.google.com/appengine
+[2]: https://cloud.google.com/appengine/docs/python/ndb/
--- ndb/tests/unit/test_key.py
+++ ndb/tests/unit/test_key.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest.mock
+
 import google.cloud.datastore
 import pytest
 
@@ -25,11 +27,91 @@ def test___all__():
 
 class TestKey:
     @staticmethod
-    def test_constructor():
-        key = key_module.Key("Kind", project="foo")
+    @unittest.mock.patch("os.environ", new={})
+    def test_constructor_default():
+        key = key_module.Key("Kind")
         ds_key = key._key
         assert isinstance(ds_key, google.cloud.datastore.Key)
         assert ds_key._flat_path == ("Kind",)
         assert ds_key._namespace is None
+        assert ds_key._parent is None
+        assert ds_key._project == key_module._APP_ID_DEFAULT
+        assert ds_key._path == [{"kind": "Kind"}]
+
+    @staticmethod
+    def test_constructor_with_reference():
+        with pytest.raises(NotImplementedError):
+            key_module.Key(reference=unittest.mock.sentinel.ref)
+
+    @staticmethod
+    def test_constructor_with_serialized():
+        with pytest.raises(NotImplementedError):
+            key_module.Key(serialized=b"foo")
+
+    @staticmethod
+    def test_constructor_with_urlsafe():
+        with pytest.raises(NotImplementedError):
+            key_module.Key(urlsafe="foo")
+
+    @staticmethod
+    def test_constructor_with_pairs():
+        with pytest.raises(NotImplementedError):
+            key_module.Key(pairs=[("Kind", 1)])
+
+    @staticmethod
+    def test_constructor_with_flat():
+        with pytest.raises(NotImplementedError):
+            key_module.Key(flat=["Kind", 1])
+
+    @staticmethod
+    def test_constructor_with_app():
+        key = key_module.Key("Kind", app="foo")
+        ds_key = key._key
+        assert isinstance(ds_key, google.cloud.datastore.Key)
+        assert ds_key._flat_path == ("Kind",)
+        assert ds_key._namespace is None
+        assert ds_key._parent is None
         assert ds_key._project == "foo"
         assert ds_key._path == [{"kind": "Kind"}]
+
+    @staticmethod
+    def test_constructor_with_namespace():
+        key = key_module.Key("Kind", namespace="foo", app="bar")
+        ds_key = key._key
+        assert isinstance(ds_key, google.cloud.datastore.Key)
+        assert ds_key._flat_path == ("Kind",)
+        assert ds_key._namespace == "foo"
+        assert ds_key._parent is None
+        assert ds_key._project == "bar"
+        assert ds_key._path == [{"kind": "Kind"}]
+
+    @staticmethod
+    def test_constructor_with_parent():
+        with pytest.raises(NotImplementedError):
+            key_module.Key(parent=unittest.mock.sentinel.key)
+
+
+class Test__project_from_app:
+    @staticmethod
+    def test_already_clean():
+        app = "my-prahjekt"
+        assert key_module._project_from_app(app) == app
+
+    @staticmethod
+    def test_prefixed():
+        project = "my-prahjekt"
+        for prefix in ("s", "e", "dev"):
+            app = "{}~{}".format(prefix, project)
+            assert key_module._project_from_app(app) == project
+
+    @staticmethod
+    @unittest.mock.patch("os.environ", new={})
+    def test_app_default():
+        assert key_module._project_from_app(None) == key_module._APP_ID_DEFAULT
+
+    @staticmethod
+    @unittest.mock.patch(
+        "os.environ", new={key_module._APP_ID_ENVIRONMENT: "s~jectpro"}
+    )
+    def test_app_fallback():
+        assert key_module._project_from_app(None) == "jectpro"
