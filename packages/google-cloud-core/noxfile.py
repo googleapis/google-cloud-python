6a27a76aadb33345b72d73dafaa1ea8a19546b60
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2018-10-10 11:04:44 -0700

    Use new Nox (#6175)

--- packages/google-cloud-core/noxfile.py
+++ packages/google-cloud-core/noxfile.py
@@ -22,16 +22,14 @@ LOCAL_DEPS = (
     os.path.join('..', 'api_core'),
 )
 
-
-@nox.session
 def default(session):
     """Default unit test session.
-
     This is intended to be run **without** an interpreter set, so
     that the current ``python`` (on the ``PATH``) or the version of
     Python corresponding to the ``nox`` binary the ``PATH`` can
     run the tests.
     """
+
     # Install all test dependencies, then install local packages in-place.
     session.install(
         'mock',
@@ -58,54 +56,39 @@ def default(session):
     )
 
 
-@nox.session
-@nox.parametrize('py', ['2.7', '3.5', '3.6', '3.7'])
-def unit(session, py):
-    """Run the unit test suite."""
-
-    # Run unit tests against all supported versions of Python.
-    session.interpreter = 'python{}'.format(py)
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'unit-' + py
-
+@nox.session(python=['2.7', '3.5', '3.6', '3.7'])
+def unit(session):
+    """Default unit test session."""
     default(session)
 
-
-@nox.session
+@nox.session(python='3.6')
 def lint(session):
     """Run linters.
 
     Returns a failure if the linters find linting errors or sufficiently
     serious code quality issues.
     """
-    session.interpreter = 'python3.6'
     session.install('flake8', 'flake8-import-order')
     session.install('.')
     session.run('flake8', 'google', 'tests')
 
 
-@nox.session
+@nox.session(python='3.6')
 def lint_setup_py(session):
     """Verify that setup.py is valid (including RST check)."""
-    session.interpreter = 'python3.6'
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'setup'
 
     session.install('docutils', 'Pygments')
     session.run(
         'python', 'setup.py', 'check', '--restructuredtext', '--strict')
 
 
-@nox.session
+@nox.session(python='3.6')
 def cover(session):
     """Run the final coverage report.
 
     This outputs the coverage report aggregating coverage from the unit
     test runs (not system test runs), and then erases coverage data.
     """
-    session.interpreter = 'python3.6'
     session.install('coverage', 'pytest-cov')
     session.run('coverage', 'report', '--show-missing', '--fail-under=100')
     session.run('coverage', 'erase')
