14710571051bccc6a6ce3b8dea64bcc7ba7bc6e2
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2018-10-10 11:04:44 -0700

    Use new Nox (#6175)

--- packages/google-cloud-bigquery/noxfile.py
+++ packages/google-cloud-bigquery/noxfile.py
@@ -25,7 +25,6 @@ LOCAL_DEPS = (
 )
 
 
-@nox.session
 def default(session):
     """Default unit test session.
 
@@ -40,14 +39,14 @@ def default(session):
         session.install('-e', local_dep)
 
     # Pyarrow does not support Python 3.7
-    if session.interpreter == 'python3.7':
+    if session.python == '3.7':
         dev_install = '.[pandas]'
     else:
         dev_install = '.[pandas, pyarrow]'
     session.install('-e', dev_install)
 
     # IPython does not support Python 2 after version 5.x
-    if session.interpreter == 'python2.7':
+    if session.python == '2.7':
         session.install('ipython==5.5')
     else:
         session.install('ipython')
@@ -67,34 +66,19 @@ def default(session):
     )
 
 
-@nox.session
-@nox.parametrize('py', ['2.7', '3.5', '3.6', '3.7'])
-def unit(session, py):
+@nox.session(python=['2.7', '3.5', '3.6', '3.7'])
+def unit(session):
     """Run the unit test suite."""
-
-    # Run unit tests against all supported versions of Python.
-    session.interpreter = 'python{}'.format(py)
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'unit-' + py
-
     default(session)
 
 
-@nox.session
-@nox.parametrize('py', ['2.7', '3.6'])
-def system(session, py):
+@nox.session(python=['2.7', '3.6'])
+def system(session):
     """Run the system test suite."""
 
     # Sanity check: Only run system tests if the environment variable is set.
     if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
         session.skip('Credentials must be set via environment variable.')
-
-    # Run the system tests against latest Python 2 and Python 3 only.
-    session.interpreter = 'python{}'.format(py)
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'sys-' + py
 
     # Use pre-release gRPC for system tests.
     session.install('--pre', 'grpcio')
@@ -108,7 +92,7 @@ def system(session, py):
     session.install('-e', '.[pandas]')
 
     # IPython does not support Python 2 after version 5.x
-    if session.interpreter == 'python2.7':
+    if session.python == '2.7':
         session.install('ipython==5.5')
     else:
         session.install('ipython')
@@ -122,20 +106,13 @@ def system(session, py):
     )
 
 
-@nox.session
-@nox.parametrize('py', ['2.7', '3.6'])
-def snippets(session, py):
+@nox.session(python=['2.7', '3.6'])
+def snippets(session):
     """Run the system test suite."""
 
     # Sanity check: Only run system tests if the environment variable is set.
     if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
         session.skip('Credentials must be set via environment variable.')
-
-    # Run the system tests against latest Python 2 and Python 3 only.
-    session.interpreter = 'python{}'.format(py)
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'snip-' + py
 
     # Install all test dependencies, then install local packages in place.
     session.install('mock', 'pytest')
@@ -150,14 +127,13 @@ def snippets(session, py):
         'py.test', os.path.join('docs', 'snippets.py'), *session.posargs)
 
 
-@nox.session
+@nox.session(python='3.6')
 def lint(session):
     """Run linters.
 
     Returns a failure if the linters find linting errors or sufficiently
     serious code quality issues.
     """
-    session.interpreter = 'python3.6'
 
     session.install('flake8', *LOCAL_DEPS)
     session.install('.')
@@ -167,28 +143,22 @@ def lint(session):
         'flake8', os.path.join('docs', 'snippets.py'))
 
 
-@nox.session
+@nox.session(python='3.6')
 def lint_setup_py(session):
     """Verify that setup.py is valid (including RST check)."""
-    session.interpreter = 'python3.6'
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'setup'
 
     session.install('docutils', 'Pygments')
     session.run(
         'python', 'setup.py', 'check', '--restructuredtext', '--strict')
 
 
-@nox.session
+@nox.session(python='3.6')
 def cover(session):
     """Run the final coverage report.
 
     This outputs the coverage report aggregating coverage from the unit
     test runs (not system test runs), and then erases coverage data.
     """
-    session.interpreter = 'python3.6'
-
     session.install('coverage', 'pytest-cov')
     session.run('coverage', 'report', '--show-missing', '--fail-under=100')
     session.run('coverage', 'erase')
