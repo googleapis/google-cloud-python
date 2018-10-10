5fb1a64b36af17ccf390416353b4ab69c40deac6
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2018-10-10 11:04:44 -0700

    Use new Nox (#6175)

--- packages/google-cloud-dlp/noxfile.py
+++ packages/google-cloud-dlp/noxfile.py
@@ -24,7 +24,6 @@ LOCAL_DEPS = (
 )
 
 
-@nox.session
 def default(session):
     """Run the unit test suite.
 
@@ -49,31 +48,19 @@ def default(session):
     )
 
 
-@nox.session
-@nox.parametrize('py', ['2.7', '3.5', '3.6', '3.7'])
-def unit(session, py):
+@nox.session(python=['2.7', '3.5', '3.6', '3.7'])
+def unit(session):
     """Run the unit test suite."""
-
-    # Run unit tests against all supported versions of Python.
-    session.interpreter = 'python{}'.format(py)
-
-    # Set the virtualenv dirname.
-    session.virtualenv_dirname = 'unit-' + py
 
     default(session)
 
 
-@nox.session
-@nox.parametrize('py', ['2.7', '3.6'])
-def system(session, py):
+@nox.session(python=['2.7', '3.6'])
+def system(session):
     """Run the system test suite."""
 
     if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
         session.skip('Credentials must be set via environment variable.')
-
-    session.interpreter = 'python{}'.format(py)
-
-    session.virtualenv_dirname = 'sys-' + py
 
     # Use pre-release gRPC for system tests.
     session.install('--pre', 'grpcio')
@@ -85,36 +72,33 @@ def system(session, py):
                 os.path.join('tests', 'system'), *session.posargs)
 
 
-@nox.session
+@nox.session(python='3.6')
 def lint(session):
     """Run linters.
 
     Returns a failure if the linters find linting errors or sufficiently
     serious code quality issues.
     """
-    session.interpreter = 'python3.6'
     session.install('flake8', *LOCAL_DEPS)
     session.install('.')
     session.run('flake8', 'google', 'tests')
 
 
-@nox.session
+@nox.session(python='3.6')
 def lint_setup_py(session):
     """Verify that setup.py is valid (including RST check)."""
-    session.interpreter = 'python3.6'
     session.install('docutils', 'pygments')
     session.run('python', 'setup.py', 'check', '--restructuredtext',
                 '--strict')
 
 
-@nox.session
+@nox.session(python='3.6')
 def cover(session):
     """Run the final coverage report.
 
     This outputs the coverage report aggregating coverage from the unit
     test runs (not system test runs), and then erases coverage data.
     """
-    session.interpreter = 'python3.6'
     session.chdir(os.path.dirname(__file__))
     session.install('coverage', 'pytest-cov')
     session.run('coverage', 'report', '--show-missing', '--fail-under=100')
