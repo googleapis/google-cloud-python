ffd29086c9949df2dbd66682c7412820c381d894
dpebot <dpebot@google.com>
2018-10-17 14:38:45 +0000

    Update docs after merge to master.

---
+++ latest/_sources/error-reporting/gapic/v1beta1/api.rst.txt
@@ -0,0 +1,6 @@
+Client for Stackdriver Error Reporting API
+==========================================
+
+.. automodule:: google.cloud.errorreporting_v1beta1
+    :members:
+    :inherited-members:
--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0ccd143a9a',
+    VERSION: 'ec5e02e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
