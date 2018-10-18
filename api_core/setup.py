6411c7c75e49ddde3ae816a5572fcf520cd6f3bf
Luke Sneeringer <lukesneeringer@google.com>
2018-10-18 10:47:14 -0700

    Bump version to 1.6.0a1

--- api_core/setup.py
+++ api_core/setup.py
@@ -22,12 +22,12 @@ import setuptools
 
 name = 'google-api-core'
 description = 'Google API client core library'
-version = '1.5.0'
+version = '1.6.0a1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
 # 'Development Status :: 5 - Production/Stable'
-release_status = 'Development Status :: 5 - Production/Stable'
+release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'googleapis-common-protos<2.0dev,>=1.5.3',
     'protobuf>=3.4.0',
