182edccf5fa02da81e06c50c693fc304d5324509
Danny Hermes <daniel.j.hermes@gmail.com>
2018-10-24 11:04:42 -0700

    Pulling commands into a build script.
    Also adding `crc32c_build.py` to actually create the foreign
    function interface.
    
    Unfortunately, this fails currently because `libcrc32c` can't be
    linked against dynamically.

---
+++ packages/google-crc32c/crc32c_build.py
@@ -0,0 +1,34 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cffi
+
+HEADER = """\
+uint32_t crc32c_extend(uint32_t crc, const uint8_t* data, size_t count);
+uint32_t crc32c_value(const uint8_t* data, size_t count);
+"""
+
+
+def main():
+    ffibuilder = cffi.FFI()
+    ffibuilder.cdef(HEADER)
+    ffibuilder.set_source(
+        "_crc32c_cffi",
+        '#include "crc32c/crc32c.h"',
+        libraries=["crc32c"],
+        include_dirs=["/usr/local/include"],
+    )
+    ffibuilder.compile(verbose=True)
+
+
+if __name__ == "__main__":
+    main()
