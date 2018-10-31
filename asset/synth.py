1ac2b0daed39566bc618389c2cd3595194de2744
Tres Seaver <tseaver@palladion.com>
2018-10-31 16:28:52 -0400

    Asset: fix docs build. (#6351)
    Broken in #6349.

--- asset/synth.py
+++ asset/synth.py
@@ -60,3 +60,24 @@ s.replace(
     "_ASSET.fields_by_name['iam_policy'].message_type = google_dot_iam_dot"
     "_v1_dot_policy__pb2.google_dot_iam_dot_v1_dot_policy__pb2._POLICY",
 )
+
+
+_BORKED_ASSET_DOCSTRING = """\
+          The full name of the asset. For example: ``//compute.googleapi
+          s.com/projects/my_project_123/zones/zone1/instances/instance1`
+          `. See `Resource Names <https://cloud.google.com/apis/design/r
+          esource_names#full_resource_name>`__ for more information.
+"""
+
+_FIXED_ASSET_DOCSTRING = """
+          The full name of the asset. For example:
+          ``//compute.googleapis.com/projects/my_project_123/zones/zone1/instances/instance1``.
+          See https://cloud.google.com/apis/design/resource_names#full_resource_name
+          for more information.
+"""
+
+s.replace(
+    "google/cloud/asset_v1beta1/proto/assets_pb2.py",
+    _BORKED_ASSET_DOCSTRING,
+    _FIXED_ASSET_DOCSTRING,
+)
