c69a532b64d1d6f98e6ce70a17d2691cba68ec5c
Chris Rossi <chris@archimedeanco.com>
2018-10-31 15:05:02 -0400

    First pass at ndb.eventloop.EvenLoop implementation.  Does not implement RPC integration.

--- ndb/src/google/cloud/ndb/eventloop.py
+++ ndb/src/google/cloud/ndb/eventloop.py
@@ -16,7 +16,8 @@
 
 This should handle both asynchronous ``ndb`` objects and arbitrary callbacks.
 """
-
+import collections
+import time
 
 __all__ = [
     "add_idle",
@@ -30,15 +31,227 @@ __all__ = [
 ]
 
 
-def add_idle(*args, **kwargs):
-    raise NotImplementedError
+def _noop(*args, **kw):
+    """Do nothing."""
+
+# TODO: Use utils.logging_debug when implemented
+_logging_debug = _noop
+
+
+_Event = collections.namedtuple('_Event', (
+    'when', 'callback', 'args', 'kwargs'))
+
+
+class _Clock(object):
+    """A clock to determine the current time, in seconds."""
+
+    def now(self):
+        """Returns the number of seconds since epoch."""
+        return time.time()
+
+    def sleep(self, seconds):
+        """Sleeps for the desired number of seconds."""
+        time.sleep(seconds)
 
 
 class EventLoop:
-    __slots__ = ()
+    """Constructor.
 
-    def __init__(self, *args, **kwargs):
+    Args:
+      clock: an eventloop._Clock object. Defaults to a time-based clock.
+
+    Fields:
+      current: a FIFO list of (callback, args, kwds). These callbacks
+        run immediately when the eventloop runs.
+      idlers: a FIFO list of (callback, args, kwds). Thes callbacks
+        run only when no other RPCs need to be fired first.
+        For example, AutoBatcher uses idler to fire a batch RPC even before
+        the batch is full.
+      queue: a sorted list of (absolute time in sec, callback, args, kwds),
+        sorted by time. These callbacks run only after the said time.
+      rpcs: a map from rpc to (callback, args, kwds). Callback is called
+        when the rpc finishes.
+    """
+    __slots__ = ('clock', 'current', 'idlers', 'inactive', 'queue', 'rpcs')
+
+    def __init__(self, clock=None):
+        self.clock = clock if clock else _Clock()
+        self.current = collections.deque()
+        self.idlers = collections.deque()
+        self.inactive = 0
+        self.queue = []
+        self.rpcs = {}
+
+    def clear(self):
+        """Remove all pending events without running any."""
+        while self.current or self.idlers or self.queue or self.rpcs:
+            current = self.current
+            idlers = self.idlers
+            queue = self.queue
+            rpcs = self.rpcs
+            _logging_debug('Clearing stale EventLoop instance...')
+            if current:
+                _logging_debug('  current = %s', current)
+            if idlers:
+                _logging_debug('  idlers = %s', idlers)
+            if queue:
+                _logging_debug('  queue = %s', queue)
+            if rpcs:
+                _logging_debug('  rpcs = %s', rpcs)
+            self.__init__(self.clock)
+            current.clear()
+            idlers.clear()
+            queue[:] = []
+            rpcs.clear()
+            _logging_debug('Cleared')
+
+    def insort_event_right(self, event):
+        """Insert event in queue, and keep it sorted by `event.when` assuming
+        queue is sorted.
+
+        For events with same `event.when`, new events are inserted to the
+        right, to keep FIFO order.
+
+        Args:
+          event: a (time in sec since unix epoch, callback, args, kwargs)
+          tuple.
+        """
+        queue = self.queue
+        lo = 0
+        hi = len(queue)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if event.when < queue[mid].when:
+                hi = mid
+            else:
+                lo = mid + 1
+        queue.insert(lo, event)
+
+    def queue_call(self, delay, callback, *args, **kwargs):
+        """Schedule a function call at a specific time in the future."""
+        if delay is None:
+            self.current.append((callback, args, kwargs))
+            return
+
+        # Times over a billion seconds are assumed to be absolute
+        when = self.clock.now() + delay if delay < 1e9 else delay
+        event = _Event(when, callback, args, kwargs)
+        self.insort_event_right(event)
+
+    def queue_rpc(self, rpc, callback=None, *args, **kwds):
+        """Schedule an RPC with an optional callback.
+
+        The caller must have previously sent the call to the service.
+        The optional callback is called with the remaining arguments.
+
+        NOTE: If the rpc is a MultiRpc, the callback will be called once
+        for each sub-RPC.  TODO: Is this a good idea?
+        """
+        # TODO Integrate with gRPC
         raise NotImplementedError
+
+    def add_idle(self, callback, *args, **kwargs):
+        """Add an idle callback.
+
+        An idle callback can return True, False or None.  These mean:
+
+        - None: remove the callback (don't reschedule)
+        - False: the callback did no work; reschedule later
+        - True: the callback did some work; reschedule soon
+
+        If the callback raises an exception, the traceback is logged and
+        the callback is removed.
+        """
+        self.idlers.append((callback, args, kwargs))
+
+    def run_idle(self):
+        """Run one of the idle callbacks.
+
+        Returns:
+          True if one was called, False if no idle callback was called.
+        """
+        if not self.idlers or self.inactive >= len(self.idlers):
+            return False
+        idler = self.idlers.popleft()
+        callback, args, kwargs = idler
+        _logging_debug('idler: %s', callback.__name__)
+        result = callback(*args, **kwargs)
+
+        # See add_idle() for meaning of callback return value.
+        if result is None:
+            _logging_debug('idler %s removed', callback.__name__)
+        else:
+            if result:
+                self.inactive = 0
+            else:
+                self.inactive += 1
+            self.idlers.append(idler)
+        return True
+
+    def _run_current(self):
+        """Run one current item.
+
+        Returns:
+            True if one was called, False if no callback was called.
+        """
+        if not self.current:
+            return False
+
+        self.inactive = 0
+        callback, args, kwargs = self.current.popleft()
+        _logging_debug('nowevent: %s', callback.__name__)
+        callback(*args, **kwargs)
+        return True
+
+    def run0(self):
+        """Run one item (a callback or an RPC wait_any).
+
+        Returns:
+          A time to sleep if something happened (may be 0);
+          None if all queues are empty.
+        """
+        if self._run_current() or self.run_idle():
+            return 0
+
+        delay = None
+        if self.queue:
+            delay = self.queue[0][0] - self.clock.now()
+            if delay <= 0:
+                self.inactive = 0
+                _, callback, args, kwargs = self.queue.pop(0)
+                _logging_debug('event: %s', callback.__name__)
+                callback(*args, **kwargs)
+                # TODO: What if it raises an exception?
+                return 0
+
+        if self.rpcs:
+            raise NotImplementedError
+
+        return delay
+
+    def run1(self):
+        """Run one item (a callback or an RPC wait_any) or sleep.
+
+        Returns:
+          True if something happened; False if all queues are empty.
+        """
+        delay = self.run0()
+        if delay is None:
+            return False
+        if delay > 0:
+            self.clock.sleep(delay)
+        return True
+
+    def run(self):
+        """Run until there's nothing left to do."""
+        self.inactive = 0
+        while True:
+            if not self.run1():
+                break
+
+
+def add_idle(*args, **kwargs):
+    raise NotImplementedError
 
 
 def get_event_loop(*args, **kwargs):
--- ndb/tests/unit/test_eventloop.py
+++ ndb/tests/unit/test_eventloop.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import collections
 import pytest
+import unittest.mock
 
 from google.cloud.ndb import eventloop
 import tests.unit.utils
@@ -22,16 +24,304 @@ def test___all__():
     tests.unit.utils.verify___all__(eventloop)
 
 
-def test_add_idle():
-    with pytest.raises(NotImplementedError):
-        eventloop.add_idle()
+def _Event(when=0, what='foo', args=(), kw={}):
+    return eventloop._Event(when, what, args, kw)
 
 
 class TestEventLoop:
     @staticmethod
-    def test_constructor():
+    def _make_one(**attrs):
+        """Use DummyClock"""
+        ev = eventloop.EventLoop(DummyClock())
+        for name, value in attrs.items():
+            setattr(ev, name, value)
+        return ev
+
+    @staticmethod
+    @unittest.mock.patch('google.cloud.ndb.eventloop.time')
+    def test_constructor(time):
+        time.time.return_value = 42
+        ev = eventloop.EventLoop()
+        assert ev.clock.now() == 42
+        ev.clock.sleep(27)
+        time.sleep.assert_called_once_with(27)
+
+        assert ev.current == collections.deque()
+        assert ev.idlers == collections.deque()
+        assert ev.inactive == 0
+        assert ev.queue == []
+        assert ev.rpcs == {}
+
+    def test_clear_all(self):
+        ev = self._make_one()
+        myclock = ev.clock
+        ev.current.append('foo')
+        ev.idlers.append('bar')
+        ev.queue.append('baz')
+        ev.rpcs['qux'] = 'quux'
+        ev.clear()
+        assert not len(ev.current)
+        assert not len(ev.idlers)
+        assert not len(ev.queue)
+        assert not len(ev.rpcs)
+        assert ev.clock is myclock
+
+        # idemptotence (branch coverage)
+        ev.clear()
+        assert not len(ev.current)
+        assert not len(ev.idlers)
+        assert not len(ev.queue)
+        assert not len(ev.rpcs)
+        assert ev.clock is myclock
+
+    def test_clear_current(self):
+        """ For branch coverage. """
+        ev = self._make_one()
+        myclock = ev.clock
+        ev.current.append('foo')
+        ev.clear()
+        assert not len(ev.current)
+        assert not len(ev.idlers)
+        assert not len(ev.queue)
+        assert not len(ev.rpcs)
+        assert ev.clock is myclock
+
+    def test_clear_idlers(self):
+        """ For branch coverage. """
+        ev = self._make_one()
+        myclock = ev.clock
+        ev.idlers.append('foo')
+        ev.clear()
+        assert not len(ev.current)
+        assert not len(ev.idlers)
+        assert not len(ev.queue)
+        assert not len(ev.rpcs)
+        assert ev.clock is myclock
+
+    def test_insert_event_right_empty_queue(self):
+        ev = self._make_one()
+        event = _Event()
+        ev.insort_event_right(event)
+        assert ev.queue == [event]
+
+    def test_insert_event_right_head(self):
+        ev = self._make_one(
+            queue=[_Event(1, 'bar')],
+        )
+        ev.insort_event_right(_Event(0, 'foo'))
+        assert ev.queue == [
+            _Event(0, 'foo'),
+            _Event(1, 'bar'),
+        ]
+
+    def test_insert_event_right_tail(self):
+        ev = self._make_one(
+            queue=[_Event(0, 'foo')],
+        )
+        ev.insort_event_right(_Event(1, 'bar'))
+        assert ev.queue == [
+            _Event(0, 'foo'),
+            _Event(1, 'bar'),
+        ]
+
+    def test_insert_event_right_middle(self):
+        ev = self._make_one(
+            queue=[
+                _Event(0, 'foo'),
+                _Event(2, 'baz'),
+            ],
+        )
+        ev.insort_event_right(_Event(1, 'bar'))
+        assert ev.queue == [
+            _Event(0, 'foo'),
+            _Event(1, 'bar'),
+            _Event(2, 'baz'),
+        ]
+
+    def test_insert_event_right_collision(self):
+        ev = self._make_one(
+            queue=[
+                _Event(0, 'foo'),
+                _Event(1, 'bar'),
+                _Event(2, 'baz'),
+            ],
+        )
+        ev.insort_event_right(_Event(1, 'barbar'))
+        assert ev.queue == [
+            _Event(0, 'foo'),
+            _Event(1, 'bar'),
+            _Event(1, 'barbar'),
+            _Event(2, 'baz'),
+        ]
+
+    def test_queue_call_now(self):
+        ev = self._make_one()
+        ev.queue_call(None, 'foo', 'bar', baz='qux')
+        assert list(ev.current) == [
+            ('foo', ('bar',), {'baz': 'qux'}),
+        ]
+        assert not len(ev.queue)
+
+    def test_queue_call_soon(self):
+        ev = self._make_one()
+        ev.clock.time = 5
+        ev.queue_call(5, 'foo', 'bar', baz='qux')
+        assert not len(ev.current)
+        assert ev.queue == [
+            _Event(10, 'foo', ('bar',), {'baz': 'qux'}),
+        ]
+
+    def test_queue_call_absolute(self):
+        ev = self._make_one()
+        ev.clock.time = 5
+        ev.queue_call(10e10, 'foo', 'bar', baz='qux')
+        assert not len(ev.current)
+        assert ev.queue == [
+            _Event(10e10, 'foo', ('bar',), {'baz': 'qux'}),
+        ]
+
+    def test_queue_rpc(self):
+        ev = self._make_one()
         with pytest.raises(NotImplementedError):
-            eventloop.EventLoop()
+            ev.queue_rpc('rpc')
+
+    def test_add_idle(self):
+        ev = self._make_one()
+        ev.add_idle('foo', 'bar', baz='qux')
+        assert list(ev.idlers) == [('foo', ('bar',), {'baz': 'qux'})]
+
+    def test_run_idle_no_idlers(self):
+        ev = self._make_one()
+        assert ev.run_idle() is False
+
+    def test_run_idle_all_inactive(self):
+        ev = self._make_one()
+        ev.add_idle('foo')
+        ev.inactive = 1
+        assert ev.run_idle() is False
+
+    def test_run_idle_remove_callback(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        callback.return_value = None
+        ev = self._make_one()
+        ev.add_idle(callback, 'foo', bar='baz')
+        ev.add_idle('foo')
+        assert ev.run_idle() is True
+        callback.assert_called_once_with('foo', bar='baz')
+        assert len(ev.idlers) == 1
+        assert ev.inactive == 0
+
+    def test_run_idle_did_work(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        callback.return_value = True
+        ev = self._make_one()
+        ev.add_idle(callback, 'foo', bar='baz')
+        ev.add_idle('foo')
+        ev.inactive = 1
+        assert ev.run_idle() is True
+        callback.assert_called_once_with('foo', bar='baz')
+        assert len(ev.idlers) == 2
+        assert ev.inactive == 0
+
+    def test_run_idle_did_no_work(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        callback.return_value = False
+        ev = self._make_one()
+        ev.add_idle(callback, 'foo', bar='baz')
+        ev.add_idle('foo')
+        ev.inactive = 1
+        assert ev.run_idle() is True
+        callback.assert_called_once_with('foo', bar='baz')
+        assert len(ev.idlers) == 2
+        assert ev.inactive == 2
+
+    def test_run0_nothing_to_do(self):
+        ev = self._make_one()
+        assert ev.run0() is None
+
+    def test_run0_current(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        ev = self._make_one()
+        ev.queue_call(None, callback, 'foo', bar='baz')
+        ev.inactive = 88
+        assert ev.run0() == 0
+        callback.assert_called_once_with('foo', bar='baz')
+        assert len(ev.current) == 0
+        assert ev.inactive == 0
+
+    def test_run0_idler(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        ev = self._make_one()
+        ev.add_idle(callback, 'foo', bar='baz')
+        assert ev.run0() == 0
+        callback.assert_called_once_with('foo', bar='baz')
+
+    def test_run0_next_later(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        ev = self._make_one()
+        ev.queue_call(5, callback, 'foo', bar='baz')
+        ev.inactive = 88
+        assert ev.run0() == 5
+        callback.assert_not_called()
+        assert len(ev.queue) == 1
+        assert ev.inactive == 88
+
+    def test_run0_next_now(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        ev = self._make_one()
+        ev.queue_call(6, 'foo')
+        ev.queue_call(5, callback, 'foo', bar='baz')
+        ev.inactive = 88
+        ev.clock.time = 10
+        assert ev.run0() == 0
+        callback.assert_called_once_with('foo', bar='baz')
+        assert len(ev.queue) == 1
+        assert ev.inactive == 0
+
+    def test_run0_rpc(self):
+        ev = self._make_one()
+        ev.rpcs['foo'] = 'bar'
+        with pytest.raises(NotImplementedError):
+            ev.run0()
+
+    def test_run1_nothing_to_do(self):
+        ev = self._make_one()
+        assert ev.run1() is False
+
+    def test_run1_has_work_now(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        ev = self._make_one()
+        ev.queue_call(None, callback)
+        assert ev.run1() is True
+        assert ev.clock.slept_for == 0
+        callback.assert_called_once_with()
+
+    def test_run1_has_work_later(self):
+        callback = unittest.mock.Mock(__name__='callback')
+        ev = self._make_one()
+        ev.queue_call(5, callback)
+        assert ev.run1() is True
+        assert ev.clock.slept_for == 5
+        callback.assert_not_called()
+
+    def test_run(self):
+        idler = unittest.mock.Mock(__name__='idler')
+        idler.return_value = None
+        runnow = unittest.mock.Mock(__name__='runnow')
+        runlater = unittest.mock.Mock(__name__='runlater')
+        ev = self._make_one()
+        ev.add_idle(idler)
+        ev.queue_call(None, runnow)
+        ev.queue_call(5, runlater)
+        ev.run()
+        idler.assert_called_once_with()
+        runnow.assert_called_once_with()
+        runlater.assert_called_once_with()
+
+
+def test_add_idle():
+    with pytest.raises(NotImplementedError):
+        eventloop.add_idle()
 
 
 def test_get_event_loop():
@@ -62,3 +352,20 @@ def test_run0():
 def test_run1():
     with pytest.raises(NotImplementedError):
         eventloop.run1()
+
+
+class DummyClock:
+    """Fake out clock class without having to actually read system clock or
+    sleep during tests.
+    """
+
+    def __init__(self, time=0):
+        self.time = time
+        self.slept_for = 0
+
+    def now(self):
+        return self.time
+
+    def sleep(self, t):
+        self.time += t
+        self.slept_for += t
