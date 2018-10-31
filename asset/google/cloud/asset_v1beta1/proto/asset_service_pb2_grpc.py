44eb097ee551c8b1ecf5767ba5600a23d0f3a98b
dpebot <dpebot@google.com>
2018-10-31 05:10:45 -0700

    [CHANGE ME] Re-generated asset to pick up changes in the API or client library generator.

--- asset/google/cloud/asset_v1beta1/proto/asset_service_pb2_grpc.py
+++ asset/google/cloud/asset_v1beta1/proto/asset_service_pb2_grpc.py
@@ -32,9 +32,9 @@ class AssetServiceServicer(object):
   """
 
   def ExportAssets(self, request, context):
-    """Exports assets with time and resource types to a given Google Cloud Storage
+    """Exports assets with time and resource types to a given Cloud Storage
     location. The output format is newline-delimited JSON.
-    This API implements the [google.longrunning.Operation][google.longrunning.Operation] API allowing users
+    This API implements the [google.longrunning.Operation][google.longrunning.Operation] API allowing you
     to keep track of the export.
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
@@ -42,11 +42,11 @@ class AssetServiceServicer(object):
     raise NotImplementedError('Method not implemented!')
 
   def BatchGetAssetsHistory(self, request, context):
-    """Batch gets assets update history that overlaps a time window.
+    """Batch gets the update history of assets that overlap a time window.
     For RESOURCE content, this API outputs history with asset in both
     non-delete or deleted status.
-    For IAM_POLICY content, this API only outputs history when asset and its
-    attached IAM POLICY both exist. So there may be gaps in the output history.
+    For IAM_POLICY content, this API outputs history when the asset and its
+    attached IAM POLICY both exist. This can create gaps in the output history.
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
     context.set_details('Method not implemented!')
