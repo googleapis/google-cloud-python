googleapis/google-cloud-python

commit 1b5fbe8184121bbe23ba247086d176ac467514f1

Asset: synth docstring changes generated from updated protos (#6349)
Also, exclude 'nox.py' / 'noxfile.py' from synth.

Closes #6348.

asset/google/cloud/asset_v1beta1/proto/assets_pb2.py
@@ -286,24 +286,24 @@ Asset = _reflection.GeneratedProtocolMessageType('Asset', (_message.Message,), d
   DESCRIPTOR = _ASSET,
   __module__ = 'google.cloud.asset_v1beta1.proto.assets_pb2'
   ,
-  __doc__ = """Cloud asset. This include all Google Cloud Platform resources, as well
-  as IAM policies and other non-GCP assets.
+  __doc__ = """Cloud asset. This includes all Google Cloud Platform resources, Cloud
+  IAM policies, and other non-GCP assets.
   
   
   Attributes:
       name:
-          The full name of the asset. See: https://cloud.google.com/apis
-          /design/resource\_names#full\_resource\_name Example: "//compu
-          te.googleapis.com/projects/my\_project\_123/zones/zone1/instan
-          ces/instance1".
+          The full name of the asset. For example: ``//compute.googleapi
+          s.com/projects/my_project_123/zones/zone1/instances/instance1`
+          `. See `Resource Names <https://cloud.google.com/apis/design/r
+          esource_names#full_resource_name>`__ for more information.
       asset_type:
           Type of the asset. Example: "google.compute.disk".
       resource:
           Representation of the resource.
       iam_policy:
-          Representation of the actual IAM policy set on a cloud
-          resource. For each resource, there must be at most one IAM
-          policy set on it.
+          Representation of the actual Cloud IAM policy set on a cloud
+          resource. For each resource, there must be at most one Cloud
+          IAM policy set on it.
   """,
   # @@protoc_insertion_point(class_scope:google.cloud.asset.v1beta1.Asset)
   ))
@@ -321,9 +321,9 @@ Resource = _reflection.GeneratedProtocolMessageType('Resource', (_message.Messag
           The API version. Example: "v1".
       discovery_document_uri:
           The URL of the discovery document containing the resource's
-          JSON schema. Example: "https://www.googleapis.com/discovery/v1
-          /apis/compute/v1/rest". It will be left unspecified for
-          resources without a discovery-based API, such as Cloud
+          JSON schema. For example: ``"https://www.googleapis.com/discov
+          ery/v1/apis/compute/v1/rest"``. It will be left unspecified
+          for resources without a discovery-based API, such as Cloud
           Bigtable.
       discovery_name:
           The JSON schema name listed in the discovery document.
@@ -336,14 +336,14 @@ Resource = _reflection.GeneratedProtocolMessageType('Resource', (_message.Messag
           project-123``. It will be left unspecified for resources
           without a REST API.
       parent:
-          The full name of the immediate parent of this resource. See: h
-          ttps://cloud.google.com/apis/design/resource\_names#full\_reso
-          urce\_name  For GCP assets, it is the parent resource defined
-          in the IAM policy hierarchy:
-          https://cloud.google.com/iam/docs/overview#policy\_hierarchy.
-          Example: "//cloudresourcemanager.googleapis.com/projects/my\_p
-          roject\_123".  For third-party assets, it is up to the users
-          to define.
+          The full name of the immediate parent of this resource. See
+          `Resource Names <https://cloud.google.com/apis/design/resource
+          _names#full_resource_name>`__ for more information.  For GCP
+          assets, it is the parent resource defined in the `Cloud IAM
+          policy hierarchy <https://cloud.google.com/iam/docs/overview#p
+          olicy_hierarchy>`__. For example: ``"//cloudresourcemanager.go
+          ogleapis.com/projects/my_project_123"``.  For third-party
+          assets, it is up to the users to define.
       data:
           The content of the resource, in which some sensitive fields
           are scrubbed away and may not be present.

asset/synth.py
@@ -23,6 +23,7 @@ versions = ["v1beta1"]
 
 excludes = [
     'setup.py',
+    'nox*.py',
     'README.rst',
     'docs/conf.py',
     'docs/index.rst',
@@ -37,12 +38,6 @@ for version in versions:
     )
 
     s.move(library, excludes=excludes)
-
-    s.replace(
-        f"google/cloud/asset_{version}/gapic/asset_service_client.py",
-        "'google-cloud-cloudasset', \).version",
-        "'google-cloud-asset', ).version",
-    )
 
 s.replace(
     "google/cloud/asset_v1beta1/proto/assets_pb2.py",

asset/google/cloud/asset_v1beta1/gapic/asset_service_client.py
@@ -178,9 +178,9 @@ class AssetServiceClient(object):
                       timeout=google.api_core.gapic_v1.method.DEFAULT,
                       metadata=None):
         """
-        Exports assets with time and resource types to a given Google Cloud Storage
+        Exports assets with time and resource types to a given Cloud Storage
         location. The output format is newline-delimited JSON.
-        This API implements the ``google.longrunning.Operation`` API allowing users
+        This API implements the ``google.longrunning.Operation`` API allowing you
         to keep track of the export.
 
         Example:
@@ -205,9 +205,9 @@ class AssetServiceClient(object):
             >>> metadata = response.metadata()
 
         Args:
-            parent (str): Required. The relative name of the root asset. Can only be an organization
-                number (such as \"organizations/123\"), or a project id (such as
-                \"projects/my-project-id\") or a project number (such as \"projects/12345\").
+            parent (str): Required. The relative name of the root asset. This can only be an organization
+                number (such as \"organizations/123\"), a project ID (such as
+                \"projects/my-project-id\"), or a project number (such as \"projects/12345\").
             output_config (Union[dict, ~google.cloud.asset_v1beta1.types.OutputConfig]): Required. Output configuration indicating where the results will be output
                 to. All results will be in newline delimited JSON format.
                 If a dict is provided, it must be of the same form as the protobuf
@@ -219,7 +219,7 @@ class AssetServiceClient(object):
                 results.
                 If a dict is provided, it must be of the same form as the protobuf
                 message :class:`~google.cloud.asset_v1beta1.types.Timestamp`
-            asset_types (list[str]): A list of asset types of which to take a snapshot for. Example:
+            asset_types (list[str]): A list of asset types of which to take a snapshot for. For example:
                 \"google.compute.disk\". If specified, only matching assets will be returned.
             content_type (~google.cloud.asset_v1beta1.types.ContentType): Asset content type. If not specified, no content but the asset name will be
                 returned.
@@ -279,11 +279,11 @@ class AssetServiceClient(object):
             timeout=google.api_core.gapic_v1.method.DEFAULT,
             metadata=None):
         """
-        Batch gets assets update history that overlaps a time window.
+        Batch gets the update history of assets that overlap a time window.
         For RESOURCE content, this API outputs history with asset in both
         non-delete or deleted status.
-        For IAM_POLICY content, this API only outputs history when asset and its
-        attached IAM POLICY both exist. So there may be gaps in the output history.
+        For IAM_POLICY content, this API outputs history when the asset and its
+        attached IAM POLICY both exist. This can create gaps in the output history.
 
         Example:
             >>> from google.cloud import asset_v1beta1
@@ -303,18 +303,18 @@ class AssetServiceClient(object):
 
         Args:
             parent (str): Required. The relative name of the root asset. It can only be an
-                organization number (such as \"organizations/123\"), or a project id (such as
-                \"projects/my-project-id\")\"or a project number (such as \"projects/12345\").
+                organization number (such as \"organizations/123\"), a project ID (such as
+                \"projects/my-project-id\")\", or a project number (such as \"projects/12345\").
             content_type (~google.cloud.asset_v1beta1.types.ContentType): Required. The content type.
             read_time_window (Union[dict, ~google.cloud.asset_v1beta1.types.TimeWindow]): Required. The time window for the asset history. The start time is
                 required. The returned results contain all temporal assets whose time
                 window overlap with read_time_window.
                 If a dict is provided, it must be of the same form as the protobuf
                 message :class:`~google.cloud.asset_v1beta1.types.TimeWindow`
-            asset_names (list[str]): A list of the full names of the assets. See:
-                https://cloud.google.com/apis/design/resource_names#full_resource_name
-                Example:
-                \"//compute.googleapis.com/projects/my_project_123/zones/zone1/instances/instance1\".
+            asset_names (list[str]): A list of the full names of the assets. For example:
+                ``//compute.googleapis.com/projects/my_project_123/zones/zone1/instances/instance1``.
+                See `Resource Names <https://cloud.google.com/apis/design/resource_names#full_resource_name>`_
+                for more info.
 
                 The request becomes a no-op if the asset name list is empty, and the max
                 size of the asset name list is 100 in one request.

asset/google/cloud/asset_v1beta1/gapic/transports/asset_service_grpc_transport.py
@@ -103,9 +103,9 @@ class AssetServiceGrpcTransport(object):
     def export_assets(self):
         """Return the gRPC stub for {$apiMethod.name}.
 
-        Exports assets with time and resource types to a given Google Cloud Storage
+        Exports assets with time and resource types to a given Cloud Storage
         location. The output format is newline-delimited JSON.
-        This API implements the ``google.longrunning.Operation`` API allowing users
+        This API implements the ``google.longrunning.Operation`` API allowing you
         to keep track of the export.
 
         Returns:
@@ -119,11 +119,11 @@ class AssetServiceGrpcTransport(object):
     def batch_get_assets_history(self):
         """Return the gRPC stub for {$apiMethod.name}.
 
-        Batch gets assets update history that overlaps a time window.
+        Batch gets the update history of assets that overlap a time window.
         For RESOURCE content, this API outputs history with asset in both
         non-delete or deleted status.
-        For IAM_POLICY content, this API only outputs history when asset and its
-        attached IAM POLICY both exist. So there may be gaps in the output history.
+        For IAM_POLICY content, this API outputs history when the asset and its
+        attached IAM POLICY both exist. This can create gaps in the output history.
 
         Returns:
             Callable: A callable which accepts the appropriate

asset/google/cloud/asset_v1beta1/proto/asset_service_pb2.py
@@ -24,7 +24,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='google/cloud/asset_v1beta1/proto/asset_service.proto',
   package='google.cloud.asset.v1beta1',
   syntax='proto3',
-  serialized_pb=_b('\n4google/cloud/asset_v1beta1/proto/asset_service.proto\x12\x1agoogle.cloud.asset.v1beta1\x1a\x1cgoogle/api/annotations.proto\x1a-google/cloud/asset_v1beta1/proto/assets.proto\x1a#google/longrunning/operations.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"\xe9\x01\n\x13\x45xportAssetsRequest\x12\x0e\n\x06parent\x18\x01 \x01(\t\x12-\n\tread_time\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x13\n\x0b\x61sset_types\x18\x03 \x03(\t\x12=\n\x0c\x63ontent_type\x18\x04 \x01(\x0e\x32\'.google.cloud.asset.v1beta1.ContentType\x12?\n\routput_config\x18\x05 \x01(\x0b\x32(.google.cloud.asset.v1beta1.OutputConfig\"\x86\x01\n\x14\x45xportAssetsResponse\x12-\n\tread_time\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12?\n\routput_config\x18\x02 \x01(\x0b\x32(.google.cloud.asset.v1beta1.OutputConfig\"\xc4\x01\n\x1c\x42\x61tchGetAssetsHistoryRequest\x12\x0e\n\x06parent\x18\x01 \x01(\t\x12\x13\n\x0b\x61sset_names\x18\x02 \x03(\t\x12=\n\x0c\x63ontent_type\x18\x03 \x01(\x0e\x32\'.google.cloud.asset.v1beta1.ContentType\x12@\n\x10read_time_window\x18\x04 \x01(\x0b\x32&.google.cloud.asset.v1beta1.TimeWindow\"Z\n\x1d\x42\x61tchGetAssetsHistoryResponse\x12\x39\n\x06\x61ssets\x18\x01 \x03(\x0b\x32).google.cloud.asset.v1beta1.TemporalAsset\"d\n\x0cOutputConfig\x12\x45\n\x0fgcs_destination\x18\x01 \x01(\x0b\x32*.google.cloud.asset.v1beta1.GcsDestinationH\x00\x42\r\n\x0b\x64\x65stination\"\x1d\n\x0eGcsDestination\x12\x0b\n\x03uri\x18\x01 \x01(\t*I\n\x0b\x43ontentType\x12\x1c\n\x18\x43ONTENT_TYPE_UNSPECIFIED\x10\x00\x12\x0c\n\x08RESOURCE\x10\x01\x12\x0e\n\nIAM_POLICY\x10\x02\x32\xdf\x03\n\x0c\x41ssetService\x12\xc9\x01\n\x0c\x45xportAssets\x12/.google.cloud.asset.v1beta1.ExportAssetsRequest\x1a\x1d.google.longrunning.Operation\"i\x82\xd3\xe4\x93\x02\x63\")/v1beta1/{parent=projects/*}:exportAssets:\x01*Z3\"./v1beta1/{parent=organizations/*}:exportAssets:\x01*\x12\x82\x02\n\x15\x42\x61tchGetAssetsHistory\x12\x38.google.cloud.asset.v1beta1.BatchGetAssetsHistoryRequest\x1a\x39.google.cloud.asset.v1beta1.BatchGetAssetsHistoryResponse\"t\x82\xd3\xe4\x93\x02n\x12\x32/v1beta1/{parent=projects/*}:batchGetAssetsHistoryZ8\x12\x36/v1beta1/{parent=organizations/*}:batchGetAssetHistoryB\xb0\x01\n\x1e\x63om.google.cloud.asset.v1beta1B\x11\x41ssetServiceProtoP\x01Z?google.golang.org/genproto/googleapis/cloud/asset/v1beta1;asset\xaa\x02\x1aGoogle.Cloud.Asset.V1Beta1\xca\x02\x1aGoogle\\Cloud\\Asset\\V1beta1b\x06proto3')
+  serialized_pb=_b('\n4google/cloud/asset_v1beta1/proto/asset_service.proto\x12\x1agoogle.cloud.asset.v1beta1\x1a\x1cgoogle/api/annotations.proto\x1a-google/cloud/asset_v1beta1/proto/assets.proto\x1a#google/longrunning/operations.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"\xe9\x01\n\x13\x45xportAssetsRequest\x12\x0e\n\x06parent\x18\x01 \x01(\t\x12-\n\tread_time\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x13\n\x0b\x61sset_types\x18\x03 \x03(\t\x12=\n\x0c\x63ontent_type\x18\x04 \x01(\x0e\x32\'.google.cloud.asset.v1beta1.ContentType\x12?\n\routput_config\x18\x05 \x01(\x0b\x32(.google.cloud.asset.v1beta1.OutputConfig\"\x86\x01\n\x14\x45xportAssetsResponse\x12-\n\tread_time\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12?\n\routput_config\x18\x02 \x01(\x0b\x32(.google.cloud.asset.v1beta1.OutputConfig\"\xc4\x01\n\x1c\x42\x61tchGetAssetsHistoryRequest\x12\x0e\n\x06parent\x18\x01 \x01(\t\x12\x13\n\x0b\x61sset_names\x18\x02 \x03(\t\x12=\n\x0c\x63ontent_type\x18\x03 \x01(\x0e\x32\'.google.cloud.asset.v1beta1.ContentType\x12@\n\x10read_time_window\x18\x04 \x01(\x0b\x32&.google.cloud.asset.v1beta1.TimeWindow\"Z\n\x1d\x42\x61tchGetAssetsHistoryResponse\x12\x39\n\x06\x61ssets\x18\x01 \x03(\x0b\x32).google.cloud.asset.v1beta1.TemporalAsset\"d\n\x0cOutputConfig\x12\x45\n\x0fgcs_destination\x18\x01 \x01(\x0b\x32*.google.cloud.asset.v1beta1.GcsDestinationH\x00\x42\r\n\x0b\x64\x65stination\"\x1d\n\x0eGcsDestination\x12\x0b\n\x03uri\x18\x01 \x01(\t*I\n\x0b\x43ontentType\x12\x1c\n\x18\x43ONTENT_TYPE_UNSPECIFIED\x10\x00\x12\x0c\n\x08RESOURCE\x10\x01\x12\x0e\n\nIAM_POLICY\x10\x02\x32\xe0\x03\n\x0c\x41ssetService\x12\xc9\x01\n\x0c\x45xportAssets\x12/.google.cloud.asset.v1beta1.ExportAssetsRequest\x1a\x1d.google.longrunning.Operation\"i\x82\xd3\xe4\x93\x02\x63\")/v1beta1/{parent=projects/*}:exportAssets:\x01*Z3\"./v1beta1/{parent=organizations/*}:exportAssets:\x01*\x12\x83\x02\n\x15\x42\x61tchGetAssetsHistory\x12\x38.google.cloud.asset.v1beta1.BatchGetAssetsHistoryRequest\x1a\x39.google.cloud.asset.v1beta1.BatchGetAssetsHistoryResponse\"u\x82\xd3\xe4\x93\x02o\x12\x32/v1beta1/{parent=projects/*}:batchGetAssetsHistoryZ9\x12\x37/v1beta1/{parent=organizations/*}:batchGetAssetsHistoryB\xb0\x01\n\x1e\x63om.google.cloud.asset.v1beta1B\x11\x41ssetServiceProtoP\x01Z?google.golang.org/genproto/googleapis/cloud/asset/v1beta1;asset\xaa\x02\x1aGoogle.Cloud.Asset.V1Beta1\xca\x02\x1aGoogle\\Cloud\\Asset\\V1beta1b\x06proto3')
   ,
   dependencies=[google_dot_api_dot_annotations__pb2.DESCRIPTOR,google_dot_cloud_dot_asset__v1beta1_dot_proto_dot_assets__pb2.DESCRIPTOR,google_dot_longrunning_dot_operations__pb2.DESCRIPTOR,google_dot_protobuf_dot_timestamp__pb2.DESCRIPTOR,])
 
@@ -335,9 +335,9 @@ ExportAssetsRequest = _reflection.GeneratedProtocolMessageType('ExportAssetsRequ
   
   Attributes:
       parent:
-          Required. The relative name of the root asset. Can only be an
-          organization number (such as "organizations/123"), or a
-          project id (such as "projects/my-project-id") or a project
+          Required. The relative name of the root asset. This can only
+          be an organization number (such as "organizations/123"), a
+          project ID (such as "projects/my-project-id"), or a project
           number (such as "projects/12345").
       read_time:
           Timestamp to take an asset snapshot. This can only be set to a
@@ -347,8 +347,8 @@ ExportAssetsRequest = _reflection.GeneratedProtocolMessageType('ExportAssetsRequ
           window during which running the same query may get different
           results.
       asset_types:
-          A list of asset types of which to take a snapshot for.
-          Example: "google.compute.disk". If specified, only matching
+          A list of asset types of which to take a snapshot for. For
+          example: "google.compute.disk". If specified, only matching
           assets will be returned.
       content_type:
           Asset content type. If not specified, no content but the asset
@@ -394,16 +394,17 @@ BatchGetAssetsHistoryRequest = _reflection.GeneratedProtocolMessageType('BatchGe
   Attributes:
       parent:
           Required. The relative name of the root asset. It can only be
-          an organization number (such as "organizations/123"), or a
-          project id (such as "projects/my-project-id")"or a project
+          an organization number (such as "organizations/123"), a
+          project ID (such as "projects/my-project-id")", or a project
           number (such as "projects/12345").
       asset_names:
-          A list of the full names of the assets. See: https://cloud.goo
-          gle.com/apis/design/resource\_names#full\_resource\_name
-          Example: "//compute.googleapis.com/projects/my\_project\_123/z
-          ones/zone1/instances/instance1".  The request becomes a no-op
-          if the asset name list is empty, and the max size of the asset
-          name list is 100 in one request.
+          A list of the full names of the assets. For example: ``//compu
+          te.googleapis.com/projects/my_project_123/zones/zone1/instance
+          s/instance1``. See `Resource Names <https://cloud.google.com/a
+          pis/design/resource_names#full_resource_name>`__ for more
+          info.  The request becomes a no-op if the asset name list is
+          empty, and the max size of the asset name list is 100 in one
+          request.
       content_type:
           Required. The content type.
       read_time_window:
@@ -441,7 +442,7 @@ OutputConfig = _reflection.GeneratedProtocolMessageType('OutputConfig', (_messag
       destination:
           Asset export destination.
       gcs_destination:
-          Destination on Google Cloud Storage (GCS).
+          Destination on Cloud Storage.
   """,
   # @@protoc_insertion_point(class_scope:google.cloud.asset.v1beta1.OutputConfig)
   ))
@@ -451,15 +452,17 @@ GcsDestination = _reflection.GeneratedProtocolMessageType('GcsDestination', (_me
   DESCRIPTOR = _GCSDESTINATION,
   __module__ = 'google.cloud.asset_v1beta1.proto.asset_service_pb2'
   ,
-  __doc__ = """A Google Cloud Storage (GCS) location.
+  __doc__ = """A Cloud Storage location.
   
   
   Attributes:
       uri:
-          The path of the GCS objects. It's the same path that is used
-          by gsutil, for example: "gs://bucket\_name/object\_path". See:
-          https://cloud.google.com/storage/docs/viewing-editing-metadata
-          for more information.
+          The path of the Cloud Storage objects. It's the same path that
+          is used by gsutil. For example:
+          "gs://bucket\_name/object\_path". See `Viewing and Editing
+          Object Metadata
+          <https://cloud.google.com/storage/docs/viewing-editing-
+          metadata>`__ for more information.
   """,
   # @@protoc_insertion_point(class_scope:google.cloud.asset.v1beta1.GcsDestination)
   ))
@@ -476,7 +479,7 @@ _ASSETSERVICE = _descriptor.ServiceDescriptor(
   index=0,
   options=None,
   serialized_start=1104,
-  serialized_end=1583,
+  serialized_end=1584,
   methods=[
   _descriptor.MethodDescriptor(
     name='ExportAssets',
@@ -494,7 +497,7 @@ _ASSETSERVICE = _descriptor.ServiceDescriptor(
     containing_service=None,
     input_type=_BATCHGETASSETSHISTORYREQUEST,
     output_type=_BATCHGETASSETSHISTORYRESPONSE,
-    options=_descriptor._ParseOptions(descriptor_pb2.MethodOptions(), _b('\202\323\344\223\002n\0222/v1beta1/{parent=projects/*}:batchGetAssetsHistoryZ8\0226/v1beta1/{parent=organizations/*}:batchGetAssetHistory')),
+    options=_descriptor._ParseOptions(descriptor_pb2.MethodOptions(), _b('\202\323\344\223\002o\0222/v1beta1/{parent=projects/*}:batchGetAssetsHistoryZ9\0227/v1beta1/{parent=organizations/*}:batchGetAssetsHistory')),
   ),
 ])
 _sym_db.RegisterServiceDescriptor(_ASSETSERVICE)

asset/google/cloud/asset_v1beta1/proto/asset_service_pb2_grpc.py
@@ -32,9 +32,9 @@ class AssetServiceServicer(object):
   """
 
   def ExportAssets(self, request, context):
-    """Exports assets with time and resource types to a given Google Cloud Storage
+    """Exports assets with time and resource types to a given Cloud Storage
     location. The output format is newline-delimited JSON.
-    This API implements the [google.longrunning.Operation][google.longrunning.Operation] API allowing users
+    This API implements the [google.longrunning.Operation][google.longrunning.Operation] API allowing you
     to keep track of the export.
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
@@ -42,11 +42,11 @@ class AssetServiceServicer(object):
     raise NotImplementedError('Method not implemented!')
 
   def BatchGetAssetsHistory(self, request, context):
-    """Batch gets assets update history that overlaps a time window.
+    """Batch gets the update history of assets that overlap a time window.
     For RESOURCE content, this API outputs history with asset in both
     non-delete or deleted status.
-    For IAM_POLICY content, this API only outputs history when asset and its
-    attached IAM POLICY both exist. So there may be gaps in the output history.
+    For IAM_POLICY content, this API outputs history when the asset and its
+    attached IAM POLICY both exist. This can create gaps in the output history.
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
     context.set_details('Method not implemented!')

commit 9bcdd21ce5da9a5239fc52739fda580ed9d5f9b3

Asset: fix docs build.
Broken in #6349.

asset/google/cloud/asset_v1beta1/proto/assets_pb2.py
@@ -292,10 +292,11 @@ Asset = _reflection.GeneratedProtocolMessageType('Asset', (_message.Message,), d
   
   Attributes:
       name:
-          The full name of the asset. For example: ``//compute.googleapi
-          s.com/projects/my_project_123/zones/zone1/instances/instance1`
-          `. See `Resource Names <https://cloud.google.com/apis/design/r
-          esource_names#full_resource_name>`__ for more information.
+
+          The full name of the asset. For example:
+          ``//compute.googleapis.com/projects/my_project_123/zones/zone1/instances/instance1``.
+          See https://cloud.google.com/apis/design/resource_names#full_resource_name
+          for more information.
       asset_type:
           Type of the asset. Example: "google.compute.disk".
       resource:

asset/synth.py
@@ -60,3 +60,24 @@ s.replace(
     "_ASSET.fields_by_name['iam_policy'].message_type = google_dot_iam_dot"
     "_v1_dot_policy__pb2.google_dot_iam_dot_v1_dot_policy__pb2._POLICY",
 )
+
+
+_BORKED_ASSET_DOCSTRING = """\
+          The full name of the asset. For example: ``//compute.googleapi
+          s.com/projects/my_project_123/zones/zone1/instances/instance1`
+          `. See `Resource Names <https://cloud.google.com/apis/design/r
+          esource_names#full_resource_name>`__ for more information.
+"""
+
+_FIXED_ASSET_DOCSTRING = """
+          The full name of the asset. For example:
+          ``//compute.googleapis.com/projects/my_project_123/zones/zone1/instances/instance1``.
+          See https://cloud.google.com/apis/design/resource_names#full_resource_name
+          for more information.
+"""
+
+s.replace(
+    "google/cloud/asset_v1beta1/proto/assets_pb2.py",
+    _BORKED_ASSET_DOCSTRING,
+    _FIXED_ASSET_DOCSTRING,
+)