11be972c06608466da68ebdeee2cab80e4e50695
Tres Seaver <tseaver@palladion.com>
2018-11-05 16:15:17 -0500

    Normalize dependencies:
    - Bump minimum 'google-api_core' version for all GAPIC libs to 1.4.1.
    
    - Lower bounds before upper bounds.
    
    - Whitespace for legibility.
    
    - Sort them topologically / consistently.

--- bigquery/setup.py
+++ bigquery/setup.py
@@ -29,9 +29,9 @@ version = '1.7.0'
 # 'Development Status :: 5 - Production/Stable'
 release_status = 'Development Status :: 5 - Production/Stable'
 dependencies = [
-    'google-cloud-core<0.29dev,>=0.28.0',
-    'google-api-core<2.0.0dev,>=1.0.0',
-    'google-resumable-media>=0.2.1',
+    'google-api-core >= 1.0.0, < 2.0.0dev',
+    'google-cloud-core >= 0.28.0, < 0.29dev',
+    'google-resumable-media >= 0.3.1',
 ]
 extras = {
     'pandas': 'pandas>=0.17.1',
