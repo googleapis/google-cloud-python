6c688b76a0029ed4572ba59aa1b72feb3fca9584
Tres Seaver <tseaver@palladion.com>
2018-11-07 14:03:35 -0500

    Bump minimum 'api_core' version for all GAPIC libs to 1.4.1. (#6391)
    Closes #6390.

--- google-cloud-speech/setup.py
+++ google-cloud-speech/setup.py
@@ -29,7 +29,7 @@ version = '0.36.0'
 # 'Development Status :: 5 - Production/Stable'
 release_status = 'Development Status :: 4 - Beta'
 dependencies = [
-    'google-api-core[grpc]<2.0.0dev,>=0.1.4',
+    'google-api-core[grpc] >= 1.4.1, < 2.0.0dev',
 ]
 extras = {
 }
