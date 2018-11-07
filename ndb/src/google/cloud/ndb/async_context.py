2d91a2b2746f4b08c7a09e1a8155cbb2d5017c74
Chris Rossi <chris@archimedeanco.com>
2018-11-07 11:00:45 -0500

    Add a docstring.

--- ndb/src/google/cloud/ndb/async_context.py
+++ ndb/src/google/cloud/ndb/async_context.py
@@ -24,7 +24,27 @@ __all__ = ["async_context"]
 def async_context():
     """Establish a context for a set of asynchronous API calls.
 
-    TODO
+    This function provides a context manager which establishes the event loop
+    that will be used for any asynchronous NDB calls that occur in the context.
+    For example::
+
+        from google.cloud.ndb import async_context
+
+        with async_context.async_context():
+            # Make some asynchronous calls
+            pass
+
+    Within the context, any calls to a ``*_async`` function or to an
+    ``ndb.tasklet``, will be added to the event loop established by the
+    context.  Upon exiting the context, execution will block until all
+    asynchronous calls loaded onto the event loop have finished execution.
+
+    Code within an ``async_context`` should be single threaded. Internally, a
+    ``threading.local`` instance is used to track the current event loop.
+
+    In the context of a web application, it is recommended that a single
+    ``async_context`` be used per HTTP request. This can typically be
+    accomplished in a middleware layer.
     """
     loop = _eventloop.contexts.push()
     yield
