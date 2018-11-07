27036720eac9f3ac10186ce0a39c30d6c1dcab6a
Chris Rossi <chris@archimedeanco.com>
2018-11-07 15:07:17 -0500

    Clean up test

--- ndb/tests/unit/test_async_.py
+++ ndb/tests/unit/test_async_.py
@@ -20,19 +20,22 @@ from google.cloud.ndb import async_
 
 @unittest.mock.patch("google.cloud.ndb.async_._eventloop.EventLoop")
 def test_async_context(EventLoop):
-    one = unittest.mock.Mock()
-    two = unittest.mock.Mock()
-    EventLoop.return_value = one
+    one = unittest.mock.Mock(spec=("run",))
+    two = unittest.mock.Mock(spec=("run",))
+    EventLoop.side_effect = [one, two]
     assert _eventloop.contexts.current() is None
+
     with async_.async_context():
         assert _eventloop.contexts.current() is one
         one.run.assert_not_called()
-        EventLoop.return_value = two
+
         with async_.async_context():
             assert _eventloop.contexts.current() is two
             two.run.assert_not_called()
+
         assert _eventloop.contexts.current() is one
         one.run.assert_not_called()
         two.run.assert_called_once_with()
+
     assert _eventloop.contexts.current() is None
     one.run.assert_called_once_with()
