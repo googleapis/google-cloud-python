fa2a76445e1c60aaef36bbc6ecb6506bc67bfe7f
yoshi-automation <yoshi-automation@google.com>
2018-11-14 05:26:22 -0800

    [CHANGE ME] Re-generated trace to pick up changes in the API or client library generator.

--- trace/google/cloud/trace_v2/gapic/transports/trace_service_grpc_transport.py
+++ trace/google/cloud/trace_v2/gapic/transports/trace_service_grpc_transport.py
@@ -65,6 +65,8 @@ class TraceServiceGrpcTransport(object):
                 credentials=credentials,
             )
 
+        self._channel = channel
+
         # gRPC uses objects called "stubs" that are bound to the
         # channel and provide a basic method for each RPC.
         self._stubs = {
@@ -93,6 +95,15 @@ class TraceServiceGrpcTransport(object):
             credentials=credentials,
             scopes=cls._OAUTH_SCOPES,
         )
+
+    @property
+    def channel(self):
+        """The gRPC channel used by the transport.
+
+        Returns:
+            grpc.Channel: A gRPC channel object.
+        """
+        return self._channel
 
     @property
     def batch_write_spans(self):
