a0bd8d0565e2a682760a113c59ce12b872bce9ab
Tres Seaver <tseaver@palladion.com>
2018-11-14 22:26:34 -0500

    Firestore: pick up fixes to GAPIC generator. (#6523)
    * Firestore: pick up fixes to GAPIC generator.
    
    Closes #6497.
    
    Includes changes to generated tests.
    
    Includes fixes from these PRs:
    
    - https://github.com/googleapis/gapic-generator/pull/2407
    - https://github.com/googleapis/gapic-generator/pull/2396
    
    * Fix overlong lines introduced in PR #6481.

--- firestore/google/cloud/firestore_v1beta1/gapic/transports/firestore_grpc_transport.py
+++ firestore/google/cloud/firestore_v1beta1/gapic/transports/firestore_grpc_transport.py
@@ -65,6 +65,8 @@ class FirestoreGrpcTransport(object):
                 credentials=credentials,
             )
 
+        self._channel = channel
+
         # gRPC uses objects called "stubs" that are bound to the
         # channel and provide a basic method for each RPC.
         self._stubs = {
@@ -93,6 +95,15 @@ class FirestoreGrpcTransport(object):
             credentials=credentials,
             scopes=cls._OAUTH_SCOPES,
         )
+
+    @property
+    def channel(self):
+        """The gRPC channel used by the transport.
+
+        Returns:
+            grpc.Channel: A gRPC channel object.
+        """
+        return self._channel
 
     @property
     def get_document(self):
