656a6a7fca3cafb36fa186325a4ab1b091bef1c8
Christopher Wilcox <crwilcox@google.com>
2018-11-28 13:55:23 -0800

    Add templates for flake8, coveragerc, noxfile, and black. (#6642)

--- packages/google-cloud-speech/synth.py
+++ packages/google-cloud-speech/synth.py
@@ -20,37 +20,43 @@ import synthtool as s
 from synthtool import gcp
 
 gapic = gcp.GAPICGenerator()
+common = gcp.CommonTemplates()
+versions = ["v1p1beta1", "v1"]
 
-versions = ['v1p1beta1', 'v1']
 
-
+# ----------------------------------------------------------------------------
+# Generate speech GAPIC layer
+# ----------------------------------------------------------------------------
 for version in versions:
-    library = gapic.py_library('speech', version)
+    library = gapic.py_library("speech", version)
 
     # Don't move over __init__.py, as we modify it to make the generated client
     # use helpers.py.
-    s.move(library / f'google/cloud/speech_{version}/types.py')
-    s.move(library / f'google/cloud/speech_{version}/gapic')
-    s.move(library / f'google/cloud/speech_{version}/proto')
-    s.move(library / f'tests/unit/gapic/{version}')
-    s.move(library / f'docs/gapic/{version}')
+    s.move(library / f"google/cloud/speech_{version}/types.py")
+    s.move(library / f"google/cloud/speech_{version}/gapic")
+    s.move(library / f"google/cloud/speech_{version}/proto")
+    s.move(library / f"tests/unit/gapic/{version}")
+    s.move(library / f"docs/gapic/{version}")
 
 
 # Use the highest version library to generate documentation import alias.
-s.move(library / 'google/cloud/speech.py')
-
+s.move(library / "google/cloud/speech.py")
 
 # Issues exist where python files should define the source encoding
 # https://github.com/googleapis/gapic-generator/issues/2097
-s.replace(
-    '**/proto/*_pb2.py',
-    r"(^.*$\n)*",
-    r"# -*- coding: utf-8 -*-\n\g<0>")
+s.replace("**/proto/*_pb2.py", r"(^.*$\n)*", r"# -*- coding: utf-8 -*-\n\g<0>")
 
 
 # Fix tests to use the direct gapic client instead of the wrapped helper
 # client.
 s.replace(
-    'tests/unit/**/test*client*.py',
-    r'from google\.cloud import speech_(.+?)$',
-    r'from google.cloud.speech_\1.gapic import speech_client as speech_\1')
+    "tests/unit/**/test*client*.py",
+    r"from google\.cloud import speech_(.+?)$",
+    r"from google.cloud.speech_\1.gapic import speech_client as speech_\1",
+)
+
+# ----------------------------------------------------------------------------
+# Add templated files
+# ----------------------------------------------------------------------------
+templated_files = common.py_library(unit_cov_level=97, cov_level=100)
+s.move(templated_files)
