ac7b4ddd3152132ccc57e04564215bcd661a60c1
Christopher Wilcox <crwilcox@google.com>
2018-11-28 13:55:23 -0800

    Add templates for flake8, coveragerc, noxfile, and black. (#6642)

--- packages/google-cloud-os-login/google/cloud/oslogin_v1/types.py
+++ packages/google-cloud-os-login/google/cloud/oslogin_v1/types.py
@@ -26,17 +26,9 @@ from google.protobuf import descriptor_pb2
 from google.protobuf import empty_pb2
 from google.protobuf import field_mask_pb2
 
-_shared_modules = [
-    http_pb2,
-    common_pb2,
-    descriptor_pb2,
-    empty_pb2,
-    field_mask_pb2,
-]
+_shared_modules = [http_pb2, common_pb2, descriptor_pb2, empty_pb2, field_mask_pb2]
 
-_local_modules = [
-    oslogin_pb2,
-]
+_local_modules = [oslogin_pb2]
 
 names = []
 
@@ -46,7 +38,7 @@ for module in _shared_modules:
         names.append(name)
 for module in _local_modules:
     for name, message in get_messages(module).items():
-        message.__module__ = 'google.cloud.oslogin_v1.types'
+        message.__module__ = "google.cloud.oslogin_v1.types"
         setattr(sys.modules[__name__], name, message)
         names.append(name)
 
