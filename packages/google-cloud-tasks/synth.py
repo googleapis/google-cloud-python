656b72b9dad8df91ecfe5c98bd3d8825b5766355
Christopher Wilcox <crwilcox@google.com>
2018-11-28 13:55:23 -0800

    Add templates for flake8, coveragerc, noxfile, and black. (#6642)

--- packages/google-cloud-tasks/synth.py
+++ packages/google-cloud-tasks/synth.py
@@ -22,46 +22,50 @@ logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
 common = gcp.CommonTemplates()
-excludes = [
-    'README.rst',
-    'setup.py',
-    'nox*.py',
-    'docs/conf.py',
-    'docs/index.rst',
-]
+excludes = ["README.rst", "setup.py", "nox*.py", "docs/conf.py", "docs/index.rst"]
 
-for version in ['v2beta2', 'v2beta3']:
+# ----------------------------------------------------------------------------
+# Generate tasks GAPIC layer
+# ----------------------------------------------------------------------------
+for version in ["v2beta2", "v2beta3"]:
     library = gapic.py_library(
-        'tasks', version,
-        config_path=f'artman_cloudtasks_{version}.yaml')
+        "tasks", version, config_path=f"artman_cloudtasks_{version}.yaml"
+    )
 
     s.copy(library, excludes=excludes)
 
     # Fix unindentation of bullet list second line
     s.replace(
-        f'google/cloud/tasks_{version}/gapic/cloud_tasks_client.py',
-        '(        \* .*\n        )([^\s*])',
-        '\g<1>  \g<2>')
+        f"google/cloud/tasks_{version}/gapic/cloud_tasks_client.py",
+        "(        \* .*\n        )([^\s*])",
+        "\g<1>  \g<2>",
+    )
 
     s.replace(
-        f'google/cloud/tasks_{version}/gapic/cloud_tasks_client.py',
-        '(Google IAM .*?_) ',
-        '\g<1>_ ')
+        f"google/cloud/tasks_{version}/gapic/cloud_tasks_client.py",
+        "(Google IAM .*?_) ",
+        "\g<1>_ ",
+    )
 
     # Issues with Anonymous ('__') links. Change to named.
-    s.replace(
-        f"google/cloud/tasks_{version}/proto/*.py",
-        ">`__",
-        ">`_")
+    s.replace(f"google/cloud/tasks_{version}/proto/*.py", ">`__", ">`_")
 
 # Issue in v2beta2
 s.replace(
-    f'google/cloud/tasks_v2beta2/gapic/cloud_tasks_client.py',
+    f"google/cloud/tasks_v2beta2/gapic/cloud_tasks_client.py",
     r'(Sample filter \\"app_engine_http_target: )\*\\".',
-    '\g<1>\\*\\".')
+    '\g<1>\\*\\".',
+)
 
 # Wrapped link fails due to space in link (v2beta2)
 s.replace(
     f"google/cloud/tasks_v2beta2/proto/queue_pb2.py",
-    '(uests in queue.yaml/xml) <\n\s+',
-    '\g<1>\n          <')
+    "(uests in queue.yaml/xml) <\n\s+",
+    "\g<1>\n          <",
+)
+
+# ----------------------------------------------------------------------------
+# Add templated files
+# ----------------------------------------------------------------------------
+templated_files = common.py_library(unit_cov_level=97, cov_level=100)
+s.move(templated_files)
