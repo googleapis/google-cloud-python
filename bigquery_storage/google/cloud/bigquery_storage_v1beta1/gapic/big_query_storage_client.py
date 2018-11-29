34eaa651b8d9c7246a8571b2d353e31f0239dbac
yoshi-automation <yoshi-automation@google.com>
2018-11-29 15:12:09 -0800

    [CHANGE ME] Re-generated bigquery_storage to pick up changes in the API or client library generator.

--- bigquery_storage/google/cloud/bigquery_storage_v1beta1/gapic/big_query_storage_client.py
+++ bigquery_storage/google/cloud/bigquery_storage_v1beta1/gapic/big_query_storage_client.py
@@ -79,7 +79,7 @@ class BigQueryStorageClient(object):
                  transport=None,
                  channel=None,
                  credentials=None,
-                 client_config=big_query_storage_client_config.config,
+                 client_config=None,
                  client_info=None):
         """Constructor.
 
@@ -112,13 +112,20 @@ class BigQueryStorageClient(object):
                 your own client library.
         """
         # Raise deprecation warnings for things we want to go away.
-        if client_config:
-            warnings.warn('The `client_config` argument is deprecated.',
-                          PendingDeprecationWarning)
+        if client_config is not None:
+            warnings.warn(
+                'The `client_config` argument is deprecated.',
+                PendingDeprecationWarning,
+                stacklevel=2)
+        else:
+            client_config = big_query_storage_client_config.config
+
         if channel:
             warnings.warn(
                 'The `channel` argument is deprecated; use '
-                '`transport` instead.', PendingDeprecationWarning)
+                '`transport` instead.',
+                PendingDeprecationWarning,
+                stacklevel=2)
 
         # Instantiate the transport.
         # The transport is responsible for handling serialization and
