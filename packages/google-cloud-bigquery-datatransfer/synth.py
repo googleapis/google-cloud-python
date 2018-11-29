6e7f75764d5ac43d66df69c04d6b2cd0b80cea8c
Christopher Wilcox <crwilcox@google.com>
2018-11-29 13:13:54 -0800

    Run black at end of synth.py (#6698)
    * Run black at end of synth.py
    * blacken logging

--- packages/google-cloud-bigquery-datatransfer/synth.py
+++ packages/google-cloud-bigquery-datatransfer/synth.py
@@ -63,3 +63,5 @@ s.replace(
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(unit_cov_level=80, cov_level=80)
 s.move(templated_files)
+
+s.shell.run(["nox", "-s", "blacken"], hide_output=False)
