ae879f7a980289cbc111fa643b05e4ba65fe4dd6
yoshi-automation <yoshi-automation@google.com>
2018-11-30 11:29:56 -0800

    [CHANGE ME] Re-generated scheduler to pick up changes in the API or client library generator.

--- scheduler/google/cloud/scheduler_v1beta1/gapic/cloud_scheduler_client_config.py
+++ scheduler/google/cloud/scheduler_v1beta1/gapic/cloud_scheduler_client_config.py
@@ -3,7 +3,7 @@ config = {
         "google.cloud.scheduler.v1beta1.CloudScheduler": {
             "retry_codes": {
                 "idempotent": ["DEADLINE_EXCEEDED", "UNAVAILABLE"],
-                "non_idempotent": [],
+                "non_idempotent": []
             },
             "retry_params": {
                 "default": {
@@ -13,51 +13,51 @@ config = {
                     "initial_rpc_timeout_millis": 20000,
                     "rpc_timeout_multiplier": 1.0,
                     "max_rpc_timeout_millis": 20000,
-                    "total_timeout_millis": 600000,
+                    "total_timeout_millis": 600000
                 }
             },
             "methods": {
                 "ListJobs": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "GetJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "CreateJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "UpdateJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "DeleteJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "PauseJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "ResumeJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "RunJob": {
                     "timeout_millis": 10000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
-                },
-            },
+                    "retry_params_name": "default"
+                }
+            }
         }
     }
 }
