d665036a24a58cff326937bd3ff03a937215f57d
Chris Wilcox <crwilcox@google.com>
2018-11-30 15:35:01 -0800

    add blacken to noxfile

--- runtimeconfig/noxfile.py
+++ runtimeconfig/noxfile.py
@@ -25,6 +25,45 @@ LOCAL_DEPS = (
 )
 
 
+@nox.session(python="3.7")
+def lint(session):
+    """Run linters.
+    Returns a failure if the linters find linting errors or sufficiently
+    serious code quality issues.
+    """
+    session.install("flake8", "black", *LOCAL_DEPS)
+    session.run(
+        "black",
+        "--check",
+        "google",
+        "tests",
+        "docs",
+    )
+    session.run("flake8", "google", "tests")
+
+
+@nox.session(python="3.6")
+def blacken(session):
+    """Run black.
+    Format code to uniform standard.
+    """
+    session.install("black")
+    session.run(
+        "black",
+        "google",
+        "tests",
+        "docs",
+    )
+
+
+@nox.session(python='3.6')
+def lint_setup_py(session):
+    """Verify that setup.py is valid (including RST check)."""
+    session.install('docutils', 'Pygments')
+    session.run(
+        'python', 'setup.py', 'check', '--restructuredtext', '--strict')
+
+
 def default(session):
     """Default unit test session.
     """
@@ -50,26 +89,6 @@ def default(session):
 def unit(session):
     """Run the unit test suite."""
     default(session)
-
-
-@nox.session(python='3.6')
-def lint(session):
-    """Run linters.
-
-    Returns a failure if the linters find linting errors or sufficiently
-    serious code quality issues.
-    """
-    session.install('flake8', *LOCAL_DEPS)
-    session.install('.')
-    session.run('flake8', 'google', 'tests')
-
-
-@nox.session(python='3.6')
-def lint_setup_py(session):
-    """Verify that setup.py is valid (including RST check)."""
-    session.install('docutils', 'Pygments')
-    session.run(
-        'python', 'setup.py', 'check', '--restructuredtext', '--strict')
 
 
 @nox.session(python='3.6')
