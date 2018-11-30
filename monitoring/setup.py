2db5500975c8049250b481674052c0e1d18c4079
Christopher Wilcox <crwilcox@google.com>
2018-11-29 16:45:59 -0800

    Release 0.31.0 (#6700)

--- monitoring/setup.py
+++ monitoring/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-monitoring'
 description = 'Stackdriver Monitoring API client library'
-version = '0.30.1'
+version = '0.31.0'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
