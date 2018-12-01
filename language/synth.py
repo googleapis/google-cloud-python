3953c316e7c794b4afeb50ac87cb5dcec6dadf03
Chris Wilcox <crwilcox@google.com>
2018-11-30 16:38:15 -0800

    adjust coverage numbers specified in synth.py

--- language/synth.py
+++ language/synth.py
@@ -41,7 +41,7 @@ for version in versions:
 # ----------------------------------------------------------------------------
 # Add templated files
 # ----------------------------------------------------------------------------
-templated_files = common.py_library(unit_cov_level=87, cov_level=87)
+templated_files = common.py_library(unit_cov_level=97, cov_level=100)
 s.move(templated_files)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
