cc23a6bc0d8a7918375357905f096b85ea1fdbcd
Danny Hermes <daniel.j.hermes@gmail.com>
2018-12-03 10:53:15 -0800

    Update ndb/tests/unit/test_query.py
    Co-Authored-By: crwilcox <crwilcox@google.com>

--- ndb/tests/unit/test_query.py
+++ ndb/tests/unit/test_query.py
@@ -143,7 +143,7 @@ class TestNode:
     def test___eq__(self):
         node = self._make_one()
         with pytest.raises(NotImplementedError):
-            node == None  # noqa: E711
+            node == unittest.mock.sentinel.other
 
     def test___ne__(self):
         node = self._make_one()
