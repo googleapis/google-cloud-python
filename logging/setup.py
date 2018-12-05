3d9bb9cd0d110cf515799e1e34b768f9ab8a9fa9
Brian Quinlan <brian@sweetapp.com>
2018-12-04 17:25:07 -0800

    Change the url to the canonical one

--- logging/setup.py
+++ logging/setup.py
@@ -64,7 +64,7 @@ setuptools.setup(
     author='Google LLC',
     author_email='googleapis-packages@google.com',
     license='Apache 2.0',
-    url='https://github.com/GoogleCloudPlatform/google-cloud-python',
+    url='https://github.com/googleapis/google-cloud-python',
     classifiers=[
         release_status,
         'Intended Audience :: Developers',
