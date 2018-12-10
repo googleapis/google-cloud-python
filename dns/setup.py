a6c2b1314f92644a8a15ef3bf06f0c46c9b9179e
Christopher Wilcox <crwilcox@google.com>
2018-12-10 13:48:07 -0800

    Release dns 0.29.1 (#6892)
    * Release 0.29.1

--- dns/setup.py
+++ dns/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-dns'
 description = 'Google Cloud DNS API client library'
-version = '0.29.0'
+version = '0.29.1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
