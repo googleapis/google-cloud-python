3761e2b66d422a23b8ece20ffeb90f84074bbb46
Christopher Wilcox <crwilcox@google.com>
2018-12-10 15:27:36 -0800

    Release runtimeconfig 0.28.2 (#6895)
    * Release 0.28.2

--- runtimeconfig/setup.py
+++ runtimeconfig/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-runtimeconfig'
 description = 'Google Cloud RuntimeConfig API client library'
-version = '0.28.1'
+version = '0.28.2'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
