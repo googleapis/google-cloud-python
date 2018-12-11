b11c124015fa7369069613775c364a8a9e019b4b
Christopher Wilcox <crwilcox@google.com>
2018-12-10 16:24:54 -0800

    Release translate 1.3.2 (#6898)
    * Release 1.3.2
    
    * Update translate/CHANGELOG.md
    
    Co-Authored-By: crwilcox <crwilcox@google.com>
    
    * Update translate/CHANGELOG.md
    
    Co-Authored-By: crwilcox <crwilcox@google.com>

--- translate/setup.py
+++ translate/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-translate'
 description = 'Google Cloud Translation API client library'
-version = '1.3.1'
+version = '1.3.2'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
