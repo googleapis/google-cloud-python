6a4381075ee90e1750cd6e1fb8c4f707c3781893
Chris Wilcox <crwilcox@google.com>
2018-12-11 10:49:11 -0800

    Release Firestore 0.30.1

--- firestore/setup.py
+++ firestore/setup.py
@@ -22,15 +22,15 @@ import setuptools
 
 name = 'google-cloud-firestore'
 description = 'Google Cloud Firestore API client library'
-version = '0.30.0'
+version = '0.30.1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
 # 'Development Status :: 5 - Production/Stable'
 release_status = 'Development Status :: 4 - Beta'
 dependencies = [
-    'google-cloud-core<0.29dev,>=0.28.0',
-    'google-api-core[grpc]<2.0.0dev,>=0.1.1',
+    'google-cloud-core >= 0.29.0, < 0.30dev',
+    'google-api-core[grpc] >= 1.6.0, < 2.0.0dev',
 ]
 extras = {
 }
