091abe9c784ecb0bae709e924a5b85815c7f1fbf
Tres Seaver <tseaver@palladion.com>
2018-12-12 13:19:28 -0500

    Use current copyright header pattern.
    [ci skip]

--- firestore/tests/unit/test_field_path.py
+++ firestore/tests/unit/test_field_path.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Google LLC All rights reserved.
+# Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
