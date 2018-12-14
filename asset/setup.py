9cb9f6357e7c19d404f29ca50a814372b3da1440
Christopher Wilcox <crwilcox@google.com>
2018-12-14 12:25:37 -0800

    Document Python 2 deprecation (#6910)

--- asset/setup.py
+++ asset/setup.py
@@ -80,6 +80,7 @@ setuptools.setup(
     packages=packages,
     namespace_packages=namespaces,
     install_requires=dependencies,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     include_package_data=True,
     zip_safe=False,
 )
