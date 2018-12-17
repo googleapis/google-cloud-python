acad4a059249baec7e09ac975e2117b2be4e2506
Tres Seaver <tseaver@palladion.com>
2018-12-17 16:48:46 -0500

    Enable use of 'WriteBatch' as a context manager. (#6912)
    Closes #6548.

--- packages/google-cloud-firestore/tests/unit/test_batch.py
+++ packages/google-cloud-firestore/tests/unit/test_batch.py
@@ -32,6 +32,8 @@ class TestWriteBatch(unittest.TestCase):
         batch = self._make_one(mock.sentinel.client)
         self.assertIs(batch._client, mock.sentinel.client)
         self.assertEqual(batch._write_pbs, [])
+        self.assertIsNone(batch.write_results)
+        self.assertIsNone(batch.commit_time)
 
     def test__add_write_pbs(self):
         batch = self._make_one(mock.sentinel.client)
@@ -151,13 +153,16 @@ class TestWriteBatch(unittest.TestCase):
         self.assertEqual(batch._write_pbs, [new_write_pb])
 
     def test_commit(self):
+        from google.protobuf import timestamp_pb2
         from google.cloud.firestore_v1beta1.proto import firestore_pb2
         from google.cloud.firestore_v1beta1.proto import write_pb2
 
         # Create a minimal fake GAPIC with a dummy result.
         firestore_api = mock.Mock(spec=["commit"])
+        timestamp = timestamp_pb2.Timestamp(seconds=1234567, nanos=123456798)
         commit_response = firestore_pb2.CommitResponse(
-            write_results=[write_pb2.WriteResult(), write_pb2.WriteResult()]
+            write_results=[write_pb2.WriteResult(), write_pb2.WriteResult()],
+            commit_time=timestamp,
         )
         firestore_api.commit.return_value = commit_response
 
@@ -175,6 +180,8 @@ class TestWriteBatch(unittest.TestCase):
 
         write_results = batch.commit()
         self.assertEqual(write_results, list(commit_response.write_results))
+        self.assertEqual(batch.write_results, write_results)
+        self.assertEqual(batch.commit_time, timestamp)
         # Make sure batch has no more "changes".
         self.assertEqual(batch._write_pbs, [])
 
@@ -185,6 +192,64 @@ class TestWriteBatch(unittest.TestCase):
             transaction=None,
             metadata=client._rpc_metadata,
         )
+
+    def test_as_context_mgr_wo_error(self):
+        from google.protobuf import timestamp_pb2
+        from google.cloud.firestore_v1beta1.proto import firestore_pb2
+        from google.cloud.firestore_v1beta1.proto import write_pb2
+
+        firestore_api = mock.Mock(spec=["commit"])
+        timestamp = timestamp_pb2.Timestamp(seconds=1234567, nanos=123456798)
+        commit_response = firestore_pb2.CommitResponse(
+            write_results=[write_pb2.WriteResult(), write_pb2.WriteResult()],
+            commit_time=timestamp,
+        )
+        firestore_api.commit.return_value = commit_response
+        client = _make_client()
+        client._firestore_api_internal = firestore_api
+        batch = self._make_one(client)
+        document1 = client.document("a", "b")
+        document2 = client.document("c", "d", "e", "f")
+
+        with batch as ctx_mgr:
+            self.assertIs(ctx_mgr, batch)
+            ctx_mgr.create(document1, {"ten": 10, "buck": u"ets"})
+            ctx_mgr.delete(document2)
+            write_pbs = batch._write_pbs[::]
+
+        self.assertEqual(batch.write_results, list(commit_response.write_results))
+        self.assertEqual(batch.commit_time, timestamp)
+        # Make sure batch has no more "changes".
+        self.assertEqual(batch._write_pbs, [])
+
+        # Verify the mocks.
+        firestore_api.commit.assert_called_once_with(
+            client._database_string,
+            write_pbs,
+            transaction=None,
+            metadata=client._rpc_metadata,
+        )
+
+    def test_as_context_mgr_w_error(self):
+        firestore_api = mock.Mock(spec=["commit"])
+        client = _make_client()
+        client._firestore_api_internal = firestore_api
+        batch = self._make_one(client)
+        document1 = client.document("a", "b")
+        document2 = client.document("c", "d", "e", "f")
+
+        with self.assertRaises(RuntimeError):
+            with batch as ctx_mgr:
+                ctx_mgr.create(document1, {"ten": 10, "buck": u"ets"})
+                ctx_mgr.delete(document2)
+                raise RuntimeError("testing")
+
+        self.assertIsNone(batch.write_results)
+        self.assertIsNone(batch.commit_time)
+        # batch still has its changes
+        self.assertEqual(len(batch._write_pbs), 2)
+
+        firestore_api.commit.assert_not_called()
 
 
 def _value_pb(**kwargs):
