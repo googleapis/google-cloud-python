50257c3aedab814d97f9bb4b61283a58ec6faf78
Chris Rossi <chris@archimedeanco.com>
2018-12-28 14:02:09 -0500

    NDB: Implement ``Future`` base class.

--- ndb/src/google/cloud/ndb/tasklets.py
+++ ndb/src/google/cloud/ndb/tasklets.py
@@ -18,6 +18,7 @@ Tasklets are a way to write concurrently running functions without
 threads.
 """
 
+from google.cloud.ndb import _eventloop
 
 __all__ = [
     "add_flow_exception",
@@ -39,15 +40,164 @@ __all__ = [
 ]
 
 
+class Future:
+    """Represents a task to be completed at an unspecified time in the future.
+
+    This is the abstract base class from which all NDB ``Future`` classes are
+    derived. A future represents a task that is to be performed
+    asynchronously with the current flow of program control.
+
+    Provides interface defined by :class:`concurrent.futures.Future` as well as
+    that of the legacy Google App Engine NDB ``Future`` class.
+    """
+
+    def __init__(self):
+        self._done = False
+        self._result = None
+        self._callbacks = []
+        self._exception = None
+
+    def done(self):
+        """Get whether future has finished its task.
+
+        Returns:
+            bool: True if task has finished, False otherwise.
+        """
+        return self._done
+
+    def running(self):
+        """Get whether future's task is still running.
+
+        Returns:
+            bool: False if task has finished, True otherwise.
+        """
+        return not self._done
+
+    def wait(self):
+        """Wait for this future's task to complete.
+
+        This future will be done and will have either a result or an exception
+        after a call to this method.
+        """
+        while not self._done:
+            _eventloop.run1()
+
+    def check_success(self):
+        """Check whether a future has completed without raising an exception.
+
+        This will wait for the future to finish its task and will then raise
+        the future's exception, if there is one, or else do nothing.
+        """
+        self.wait()
+
+        if self._exception:
+            raise self._exception
+
+    def set_result(self, result):
+        """Set the result for this future.
+
+        Signals that this future has completed its task and sets the result.
+
+        Should not be called from user code.
+        """
+        if self._done:
+            raise RuntimeError("Cannot set result on future that is done.")
+
+        self._result = result
+        self._done = True
+
+        for callback in self._callbacks:
+            callback(self)
+
+    def set_exception(self, exception):
+        """Set an exception for this future.
+
+        Signals that this future's task has resulted in an exception. The
+        future is considered done but has no result. Once the exception is set,
+        calls to :meth:`done` will return True, and calls to :meth:`result`
+        will raise the exception.
+
+        Should not be called from user code.
+
+        Args:
+            exception (Exception): The exception that was raised.
+        """
+        if self._done:
+            raise RuntimeError("Cannot set exception on future that is done.")
+
+        self._exception = exception
+        self._done = True
+
+    def result(self):
+        """Return the result of this future's task.
+
+        If the task is finished, this will return immediately. Otherwise, this
+        will block until a result is ready.
+
+        Returns:
+            Any: The result
+        """
+        while not self._done:
+            _eventloop.run1()
+
+        return self._result
+
+    get_result = result  # Legacy NDB interface
+
+    def exception(self):
+        """Get the exception for this future, if there is one.
+
+        If the task has not yet finished, this will block until the task has
+        finished. When the task has finished, this will get the exception
+        raised during the task, or None, if no exception was raised.
+
+        Returns:
+            Union[Exception, None]: The exception, or None.
+        """
+        return self._exception
+
+    get_exception = exception  # Legacy NDB interface
+
+    def get_traceback(self):
+        """Get the traceback for this future, if there is one.
+
+        Included for backwards compatibility with legacy NDB. If there is an
+        exception for this future, this just returns the ``__traceback__``
+        attribute of that exception.
+
+        Returns:
+            Union[traceback, None]: The traceback, or None.
+        """
+        if self._exception:
+            return self._exception.__traceback__
+
+    def add_done_callback(self, callback):
+        """Add a callback function to be run upon task completion.
+
+        Args:
+            callback (Callable): The function to execute.
+        """
+        self._callbacks.append(callback)
+
+    def cancel(self):
+        """Cancel the task for this future.
+
+        Raises:
+            NotImplementedError: Always, not supported.
+        """
+        raise NotImplementedError
+
+    def cancelled(self):
+        """Get whether task for this future has been cancelled.
+
+        Returns:
+            False: Always.
+        """
+        return False
+
+
 def add_flow_exception(*args, **kwargs):
     raise NotImplementedError
-
-
-class Future:
-    __slots__ = ()
-
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
 
 
 def get_context(*args, **kwargs):
