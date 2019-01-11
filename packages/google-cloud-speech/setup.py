797b443536a7190517228144f38551cb002c4a0c
Christopher Wilcox <crwilcox@google.com>
2019-01-11 09:18:34 -0800

    Release 0.36.2 (#7116)

--- packages/google-cloud-speech/setup.py
+++ packages/google-cloud-speech/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-speech'
 description = 'Google Cloud Speech API client library'
-version = '0.36.1'
+version = '0.36.2'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
