295d6be28a32ebcdfc6bc581dbd866aa487dc93b
Tim Swast <swast@google.com>
2019-01-25 13:55:24 -0800

    Release 0.2.0

--- bigquery_storage/setup.py
+++ bigquery_storage/setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 name = 'google-cloud-bigquery-storage'
 description = 'BigQuery Storage API API client library'
-version = '0.2.0dev1'
+version = '0.2.0'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'google-api-core[grpc] >= 1.6.0, < 2.0.0dev',
