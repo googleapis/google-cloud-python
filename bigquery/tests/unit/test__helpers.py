fd4a0f28cbf7034a58a528e7fab91bfd78569cc7
Tres Seaver <tseaver@palladion.com>
2019-02-06 15:54:35 -0500

    Add explicit unit tests for '_scalar_field_to_json' helper.

--- bigquery/tests/unit/test__helpers.py
+++ bigquery/tests/unit/test__helpers.py
@@ -776,6 +776,31 @@ class Test_time_to_json(unittest.TestCase):
         self.assertEqual(self._call_fut(when), "12:13:41")
 
 
+class Test_scalar_field_to_json(unittest.TestCase):
+    def _call_fut(self, field, value):
+        from google.cloud.bigquery._helpers import _scalar_field_to_json
+
+        return _scalar_field_to_json(field, value)
+
+    @staticmethod
+    def _make_field(field_type):
+        from google.cloud.bigquery.schema import SchemaField
+
+        return SchemaField(name='testing', field_type=field_type)
+
+    def test_w_unknown_field_type(self):
+        field = self._make_field('UNKNOWN')
+        original = object()
+        converted = self._call_fut(field, original)
+        self.assertIs(converted, original)
+
+    def test_w_known_field_type(self):
+        field = self._make_field('INT64')
+        original = 42
+        converted = self._call_fut(field, original)
+        self.assertEqual(converted, str(original))
+
+
 class Test_snake_to_camel_case(unittest.TestCase):
     def _call_fut(self, value):
         from google.cloud.bigquery._helpers import _snake_to_camel_case
