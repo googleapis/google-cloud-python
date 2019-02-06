950cca40dc3574b1a84f5b8085e684da6cb5af25
Tres Seaver <tseaver@palladion.com>
2019-02-06 16:23:30 -0500

    Add explicit unit tests for '_field_to_json' helper.

--- bigquery/tests/unit/test__helpers.py
+++ bigquery/tests/unit/test__helpers.py
@@ -776,10 +776,10 @@ class Test_time_to_json(unittest.TestCase):
         self.assertEqual(self._call_fut(when), "12:13:41")
 
 
-def _make_field(field_type, mode='NULLABLE', name='testing'):
+def _make_field(field_type, mode='NULLABLE', name='testing', fields=()):
     from google.cloud.bigquery.schema import SchemaField
 
-    return SchemaField(name=name, field_type=field_type, mode=mode)
+    return SchemaField(name=name, field_type=field_type, mode=mode, fields=fields)
 
 
 class Test_scalar_field_to_json(unittest.TestCase):
@@ -860,6 +860,41 @@ class Test_record_field_to_json(unittest.TestCase):
         original = {'one': 42}
         converted = self._call_fut(fields, original)
         self.assertEqual(converted, {'one': '42', 'two': None})
+
+
+class Test_field_to_json(unittest.TestCase):
+    def _call_fut(self, field, value):
+        from google.cloud.bigquery._helpers import _field_to_json
+
+        return _field_to_json(field, value)
+
+    def test_w_none(self):
+        field = _make_field('INT64')
+        original = None
+        converted = self._call_fut(field, original)
+        self.assertIsNone(converted)
+
+    def test_w_repeated(self):
+        field = _make_field('INT64', mode='REPEATED')
+        original = [42, 17]
+        converted = self._call_fut(field, original)
+        self.assertEqual(converted, [str(value) for value in original])
+
+    def test_w_record(self):
+        subfields = [
+            _make_field('INT64', name='one'),
+            _make_field('STRING', name='two'),
+        ]
+        field = _make_field('RECORD', fields=subfields)
+        original = {'one': 42, 'two': 'two'}
+        converted = self._call_fut(field, original)
+        self.assertEqual(converted, {'one': '42', 'two': 'two'})
+
+    def test_w_scalar(self):
+        field = _make_field('INT64')
+        original = 42
+        converted = self._call_fut(field, original)
+        self.assertEqual(converted, str(original))
 
 
 class Test_snake_to_camel_case(unittest.TestCase):
