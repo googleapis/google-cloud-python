9f625e1ec526f9b23e718d4a812f3f2550e50483
yoshi-automation <yoshi-automation@google.com>
2019-02-14 05:22:50 -0800

    [CHANGE ME] Re-generated securitycenter to pick up changes in the API or client library generator.

--- securitycenter/google/cloud/securitycenter_v1beta1/gapic/enums.py
+++ securitycenter/google/cloud/securitycenter_v1beta1/gapic/enums.py
@@ -28,7 +28,6 @@ class NullValue(enum.IntEnum):
     Attributes:
       NULL_VALUE (int): Null value.
     """
-
     NULL_VALUE = 0
 
 
@@ -43,7 +42,6 @@ class Finding(object):
           INACTIVE (int): The finding has been fixed, triaged as a non-issue or otherwise addressed
           and is no longer active.
         """
-
         STATE_UNSPECIFIED = 0
         ACTIVE = 1
         INACTIVE = 2
@@ -66,7 +64,6 @@ class ListAssetsResponse(object):
               REMOVED (int): Asset was removed between the points in time.
               ACTIVE (int): Asset was active at both point(s) in time.
             """
-
             STATE_UNSPECIFIED = 0
             UNUSED = 1
             ADDED = 2
@@ -94,7 +91,6 @@ class OrganizationSettings(object):
               EXCLUDE (int): Asset Discovery will ignore all resources under the projects specified.
               All other resources will be retrieved.
             """
-
             INCLUSION_MODE_UNSPECIFIED = 0
             INCLUDE_ONLY = 1
             EXCLUDE = 2
--- securitycenter/google/cloud/securitycenter_v1beta1/gapic/security_center_client_config.py
+++ securitycenter/google/cloud/securitycenter_v1beta1/gapic/security_center_client_config.py
@@ -3,7 +3,7 @@ config = {
         "google.cloud.securitycenter.v1beta1.SecurityCenter": {
             "retry_codes": {
                 "idempotent": ["DEADLINE_EXCEEDED", "UNAVAILABLE"],
-                "non_idempotent": [],
+                "non_idempotent": []
             },
             "retry_params": {
                 "default": {
@@ -13,101 +13,101 @@ config = {
                     "initial_rpc_timeout_millis": 20000,
                     "rpc_timeout_multiplier": 1.0,
                     "max_rpc_timeout_millis": 20000,
-                    "total_timeout_millis": 600000,
+                    "total_timeout_millis": 600000
                 }
             },
             "methods": {
                 "CreateSource": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "CreateFinding": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "GetIamPolicy": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "GetOrganizationSettings": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "GetSource": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "GroupAssets": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "GroupFindings": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "ListAssets": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "ListFindings": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "ListSources": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "RunAssetDiscovery": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "SetFindingState": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "SetIamPolicy": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "TestIamPermissions": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "UpdateFinding": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "UpdateOrganizationSettings": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "UpdateSource": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
+                    "retry_params_name": "default"
                 },
                 "UpdateSecurityMarks": {
                     "timeout_millis": 60000,
                     "retry_codes_name": "non_idempotent",
-                    "retry_params_name": "default",
-                },
-            },
+                    "retry_params_name": "default"
+                }
+            }
         }
     }
 }
