13b51fe4591453fd0029a7de8ef0cc48873c9372
yoshi-automation <yoshi-automation@google.com>
2019-02-14 05:21:48 -0800

    [CHANGE ME] Re-generated redis to pick up changes in the API or client library generator.

--- redis/google/cloud/redis_v1/types.py
+++ redis/google/cloud/redis_v1/types.py
@@ -26,9 +26,17 @@ from google.protobuf import field_mask_pb2
 from google.protobuf import timestamp_pb2
 from google.rpc import status_pb2
 
-_shared_modules = [operations_pb2, any_pb2, field_mask_pb2, timestamp_pb2, status_pb2]
+_shared_modules = [
+    operations_pb2,
+    any_pb2,
+    field_mask_pb2,
+    timestamp_pb2,
+    status_pb2,
+]
 
-_local_modules = [cloud_redis_pb2]
+_local_modules = [
+    cloud_redis_pb2,
+]
 
 names = []
 
@@ -38,7 +46,7 @@ for module in _shared_modules:
         names.append(name)
 for module in _local_modules:
     for name, message in get_messages(module).items():
-        message.__module__ = "google.cloud.redis_v1.types"
+        message.__module__ = 'google.cloud.redis_v1.types'
         setattr(sys.modules[__name__], name, message)
         names.append(name)
 
--- redis/google/cloud/redis_v1beta1/__init__.py
+++ redis/google/cloud/redis_v1beta1/__init__.py
@@ -26,4 +26,8 @@ class CloudRedisClient(cloud_redis_client.CloudRedisClient):
     enums = enums
 
 
-__all__ = ("enums", "types", "CloudRedisClient")
+__all__ = (
+    'enums',
+    'types',
+    'CloudRedisClient',
+)
