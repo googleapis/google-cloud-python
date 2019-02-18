45c08f25d29acce24fdf1c9568ed068a9e19265d
Carlos de la Guardia <cguardia@yahoo.com>
2019-02-18 16:09:23 -0600

    NDB: First pass at porting metadata module (#7364)
    * port classes and set up initial tests
    
    * format comments and add types to docstrings
    
    * add tests and do some minor doc fixes

--- src/google/cloud/ndb/metadata.py
+++ src/google/cloud/ndb/metadata.py
@@ -14,63 +14,270 @@
 
 """Access datastore metadata."""
 
+from google.cloud.ndb import model
+
 
 __all__ = [
-    "EntityGroup",
     "get_entity_group_version",
     "get_kinds",
     "get_namespaces",
     "get_properties_of_kind",
     "get_representations_of_kind",
+    "EntityGroup",
     "Kind",
     "Namespace",
     "Property",
 ]
 
 
-class EntityGroup:
+class _BaseMetadata(model.Model):
+    """Base class for all metadata models."""
+
     __slots__ = ()
 
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
+    _use_cache = False
+    _use_memcache = False
+
+    KIND_NAME = ""
+
+    def __new__(cls, *args, **kwargs):
+        """override to prevent instantiation"""
+        if cls is _BaseMetadata:
+            raise TypeError("This base class cannot be instantiated")
+        return super(_BaseMetadata, cls).__new__(cls)
+
+    @classmethod
+    def _get_kind(cls):
+        """Kind name override."""
+        return cls.KIND_NAME
+
+
+class Namespace(_BaseMetadata):
+    """Model for __namespace__ metadata query results."""
+
+    __slots__ = ()
+
+    KIND_NAME = "__namespace__"
+    EMPTY_NAMESPACE_ID = 1
+
+    @property
+    def namespace_name(self):
+        """Return the namespace name specified by this entity's key.
+
+        Returns:
+            str: the namespace name.
+        """
+        return self.key_to_namespace(self.key)
+
+    @classmethod
+    def key_for_namespace(cls, namespace):
+        """Return the Key for a namespace.
+
+        Args:
+            namespace (str): A string giving the namespace whose key is
+                requested.
+
+        Returns:
+            key.Key: The Key for the namespace.
+        """
+        if namespace:
+            return model.Key(cls.KIND_NAME, namespace)
+        else:
+            return model.Key(cls.KIND_NAME, cls.EMPTY_NAMESPACE_ID)
+
+    @classmethod
+    def key_to_namespace(cls, key):
+        """Return the namespace specified by a given __namespace__ key.
+
+        Args:
+            key (key.Key): key whose name is requested.
+
+        Returns:
+            str: The namespace specified by key.
+        """
+        return key.string_id() or ""
+
+
+class Kind(_BaseMetadata):
+    """Model for __kind__ metadata query results."""
+
+    __slots__ = ()
+
+    KIND_NAME = "__kind__"
+
+    @property
+    def kind_name(self):
+        """Return the kind name specified by this entity's key.
+
+        Returns:
+            str: the kind name.
+        """
+        return self.key_to_kind(self.key)
+
+    @classmethod
+    def key_for_kind(cls, kind):
+        """Return the __kind__ key for kind.
+
+        Args:
+            kind (str): kind whose key is requested.
+
+        Returns:
+            key.Key: key for kind.
+        """
+        return model.Key(cls.KIND_NAME, kind)
+
+    @classmethod
+    def key_to_kind(cls, key):
+        """Return the kind specified by a given __kind__ key.
+
+        Args:
+            key (key.Key): key whose name is requested.
+
+        Returns:
+            str: The kind specified by key.
+        """
+        return key.id()
+
+
+class Property(_BaseMetadata):
+    """Model for __property__ metadata query results."""
+
+    __slots__ = ()
+
+    KIND_NAME = "__property__"
+
+    @property
+    def property_name(self):
+        """Return the property name specified by this entity's key.
+
+        Returns:
+            str: the property name.
+        """
+        return self.key_to_property(self.key)
+
+    @property
+    def kind_name(self):
+        """Return the kind name specified by this entity's key.
+
+        Returns:
+            str: the kind name.
+        """
+        return self.key_to_kind(self.key)
+
+    property_representation = model.StringProperty(repeated=True)
+
+    @classmethod
+    def key_for_kind(cls, kind):
+        """Return the __property__ key for kind.
+
+        Args:
+            kind (str): kind whose key is requested.
+
+        Returns:
+            key.Key: The parent key for __property__ keys of kind.
+        """
+        return model.Key(Kind.KIND_NAME, kind)
+
+    @classmethod
+    def key_for_property(cls, kind, property):
+        """Return the __property__ key for property of kind.
+
+        Args:
+            kind (str): kind whose key is requested.
+            property (str): property whose key is requested.
+
+        Returns:
+            key.Key: The key for property of kind.
+        """
+        return model.Key(Kind.KIND_NAME, kind, Property.KIND_NAME, property)
+
+    @classmethod
+    def key_to_kind(cls, key):
+        """Return the kind specified by a given __property__ key.
+
+        Args:
+            key (key.Key): key whose kind name is requested.
+
+        Returns:
+            str: The kind specified by key.
+        """
+        if key.kind() == Kind.KIND_NAME:
+            return key.id()
+        else:
+            return key.parent().id()
+
+    @classmethod
+    def key_to_property(cls, key):
+        """Return the property specified by a given __property__ key.
+
+        Args:
+            key (key.Key): key whose property name is requested.
+
+        Returns:
+            str: property specified by key, or None if the key specified
+                only a kind.
+        """
+        if key.kind() == Kind.KIND_NAME:
+            return None
+        else:
+            return key.id()
+
+
+class EntityGroup(_BaseMetadata):
+    """Model for __entity_group__ metadata, available in HR datastore only.
+
+    This metadata contains a numeric __version__ property that is guaranteed
+    to increase on every change to the entity group. The version may increase
+    even in the absence of user-visible changes to the entity group. The
+    __entity_group__ entity may not exist if the entity group was never
+    written to.
+
+    Attributes:
+        version (int): counter for changes in entity group.
+    """
+
+    __slots__ = ()
+
+    KIND_NAME = "__entity_group__"
+    ID = 1
+
+    version = model.IntegerProperty(name="__version__")
+
+    @classmethod
+    def key_for_entity_group(cls, key):
+        """Return the key for the entity group containing key.
+
+        Args:
+            key (key.Key): a key for an entity group whose __entity_group__ key
+                you want.
+
+        Returns:
+            key.Key: The __entity_group__ key for the entity group containing
+                key.
+        """
+        return model.Key(cls.KIND_NAME, cls.ID, parent=key.root())
 
 
 def get_entity_group_version(*args, **kwargs):
+    """Need query for this"""
     raise NotImplementedError
 
 
 def get_kinds(*args, **kwargs):
+    """Need query for this"""
     raise NotImplementedError
 
 
 def get_namespaces(*args, **kwargs):
+    """Need query for this"""
     raise NotImplementedError
 
 
 def get_properties_of_kind(*args, **kwargs):
+    """Need query for this"""
     raise NotImplementedError
 
 
 def get_representations_of_kind(*args, **kwargs):
+    """Need query for this"""
     raise NotImplementedError
-
-
-class Kind:
-    __slots__ = ()
-
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
-
-
-class Namespace:
-    __slots__ = ()
-
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
-
-
-class Property:
-    __slots__ = ()
-
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
