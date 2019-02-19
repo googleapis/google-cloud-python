8ee84a987b42ed024aba4ed00ac5b674a1e1bdb6
Chris Rossi <chris@archimedeanco.com>
2019-02-19 10:54:56 -0500

    NDB: Stub out Context class. (#7379)

--- tests/unit/test_context.py
+++ tests/unit/test_context.py
@@ -14,37 +14,162 @@
 
 import pytest
 
-from google.cloud.ndb import context
+from google.cloud.ndb import context as context_module
 import tests.unit.utils
 
 
 def test___all__():
-    tests.unit.utils.verify___all__(context)
+    tests.unit.utils.verify___all__(context_module)
+
+
+class TestContext:
+    def test_clear_cache(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.clear_cache()
+
+    def test_flush(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.flush()
+
+    def test_get_cache_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.get_cache_policy()
+
+    def test_get_datastore_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.get_datastore_policy()
+
+    def test_get_memcache_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.get_memcache_policy()
+
+    def test_get_memcache_timeout_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.get_memcache_timeout_policy()
+
+    def test_set_cache_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.set_cache_policy(None)
+
+    def test_set_datastore_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.set_datastore_policy(None)
+
+    def test_set_memcache_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.set_memcache_policy(None)
+
+    def test_set_memcache_timeout_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.set_memcache_timeout_policy(None)
+
+    def test_call_on_commit(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.call_on_commit(None)
+
+    def test_in_transaction(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.in_transaction()
+
+    def test_default_cache_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.default_cache_policy(None)
+
+    def test_default_datastore_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.default_datastore_policy(None)
+
+    def test_default_memcache_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.default_memcache_policy(None)
+
+    def test_default_memcache_timeout_policy(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.default_memcache_timeout_policy(None)
+
+    def test_memcache_add(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_add()
+
+    def test_memcache_cas(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_cas()
+
+    def test_memcache_decr(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_decr()
+
+    def test_memcache_replace(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_replace()
+
+    def test_memcache_set(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_set()
+
+    def test_memcache_delete(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_delete()
+
+    def test_memcache_get(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_get()
+
+    def test_memcache_gets(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_gets()
+
+    def test_memcache_incr(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.memcache_incr()
+
+    def test_urlfetch(self):
+        context = context_module.Context()
+        with pytest.raises(NotImplementedError):
+            context.urlfetch()
 
 
 class TestAutoBatcher:
     @staticmethod
     def test_constructor():
         with pytest.raises(NotImplementedError):
-            context.AutoBatcher()
-
-
-class TestContext:
-    @staticmethod
-    def test_constructor():
-        with pytest.raises(NotImplementedError):
-            context.Context()
+            context_module.AutoBatcher()
 
 
 class TestContextOptions:
     @staticmethod
     def test_constructor():
         with pytest.raises(NotImplementedError):
-            context.ContextOptions()
+            context_module.ContextOptions()
 
 
 class TestTransactionOptions:
     @staticmethod
     def test_constructor():
         with pytest.raises(NotImplementedError):
-            context.TransactionOptions()
+            context_module.TransactionOptions()
