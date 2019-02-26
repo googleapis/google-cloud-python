5416803e10076d3fc7660a99ffe971edc90491ed
Chris Wilcox <crwilcox@google.com>
2019-02-25 16:03:37 -0800

    lint

--- talent/docs/conf.py
+++ talent/docs/conf.py
@@ -265,7 +265,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, "google-cloud-talent", u"google-cloud-talent Documentation", [author], 1)
+    (
+        master_doc,
+        "google-cloud-talent",
+        u"google-cloud-talent Documentation",
+        [author],
+        1,
+    )
 ]
 
 # If true, show URL addresses after external links.
