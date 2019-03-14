56c518e4bc81ccb420acc6133d7441d8af967a9b
Tres Seaver <tseaver@palladion.com>
2019-03-14 12:55:51 -0400

    Blacken.

--- storage/tests/system.py
+++ storage/tests/system.py
@@ -44,7 +44,9 @@ def _bad_copy(bad_request):
 
 
 retry_429 = RetryErrors(exceptions.TooManyRequests, max_tries=6)
-retry_429_503 = RetryErrors([exceptions.TooManyRequests, exceptions.ServiceUnavailable], max_tries=6)
+retry_429_503 = RetryErrors(
+    [exceptions.TooManyRequests, exceptions.ServiceUnavailable], max_tries=6
+)
 retry_bad_copy = RetryErrors(exceptions.BadRequest, error_predicate=_bad_copy)
 
 
