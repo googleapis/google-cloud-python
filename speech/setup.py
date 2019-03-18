42707bbf2e4ce401c85da93ab572d951cc4d08c0
Christopher Wilcox <crwilcox@google.com>
2019-03-18 07:59:33 -0700

    Promote google-cloud-speech to GA (#7525)

--- speech/setup.py
+++ speech/setup.py
@@ -27,7 +27,7 @@ version = '0.36.3'
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
 # 'Development Status :: 5 - Production/Stable'
-release_status = 'Development Status :: 4 - Beta'
+release_status = 'Development Status :: 5 - Production/Stable'
 dependencies = [
     'google-api-core[grpc] >= 1.6.0, < 2.0.0dev',
 ]
