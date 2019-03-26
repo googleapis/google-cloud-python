b970a01564b7ac0c95018a6f7fb3f7eec599b08a
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-03-26 13:52:02 -0700

    Release 0.6.0 (#7561)

--- tasks/setup.py
+++ tasks/setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 name = 'google-cloud-tasks'
 description = 'Cloud Tasks API API client library'
-version = '0.5.0'
+version = '0.6.0'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'google-api-core[grpc] >= 1.6.0, < 2.0.0dev',
