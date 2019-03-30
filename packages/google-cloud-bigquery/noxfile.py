8b28574de03560e88ab576d7a54af00ad87bfeae
Tim Swast <swast@google.com>
2019-03-29 20:26:53 -0700

    Blacken noxfile and setup file in nox session (#7619)

--- packages/google-cloud-bigquery/noxfile.py
+++ packages/google-cloud-bigquery/noxfile.py
@@ -21,12 +21,14 @@ import nox
 
 
 LOCAL_DEPS = (
-    os.path.join('..', 'api_core[grpc]'),
-    os.path.join('..', 'core'),
+    os.path.join("..", "api_core[grpc]"),
+    os.path.join("..", "core"),
     # TODO: Move bigquery_storage back to dev_install once dtypes feature is
     #       released. Issue #7049
-    os.path.join('..', 'bigquery_storage[pandas,fastavro]'),
+    os.path.join("..", "bigquery_storage[pandas,fastavro]"),
 )
+
+BLACK_PATHS = ("google", "tests", "docs", "noxfile.py", "setup.py")
 
 
 def default(session):
@@ -38,112 +40,108 @@ def default(session):
     run the tests.
     """
     # Install all test dependencies, then install local packages in-place.
-    session.install('mock', 'pytest', 'pytest-cov')
+    session.install("mock", "pytest", "pytest-cov")
     for local_dep in LOCAL_DEPS:
-        session.install('-e', local_dep)
+        session.install("-e", local_dep)
 
     # Pyarrow does not support Python 3.7
-    if session.python == '3.7':
-        dev_install = '.[pandas, tqdm]'
+    if session.python == "3.7":
+        dev_install = ".[pandas, tqdm]"
     else:
-        dev_install = '.[pandas, pyarrow, tqdm]'
-    session.install('-e', dev_install)
+        dev_install = ".[pandas, pyarrow, tqdm]"
+    session.install("-e", dev_install)
 
     # IPython does not support Python 2 after version 5.x
-    if session.python == '2.7':
-        session.install('ipython==5.5')
+    if session.python == "2.7":
+        session.install("ipython==5.5")
     else:
-        session.install('ipython')
+        session.install("ipython")
 
     # Run py.test against the unit tests.
     session.run(
-        'py.test',
-        '--quiet',
-        '--cov=google.cloud.bigquery',
-        '--cov=tests.unit',
-        '--cov-append',
-        '--cov-config=.coveragerc',
-        '--cov-report=',
-        '--cov-fail-under=97',
-        os.path.join('tests', 'unit'),
+        "py.test",
+        "--quiet",
+        "--cov=google.cloud.bigquery",
+        "--cov=tests.unit",
+        "--cov-append",
+        "--cov-config=.coveragerc",
+        "--cov-report=",
+        "--cov-fail-under=97",
+        os.path.join("tests", "unit"),
         *session.posargs
     )
 
 
-@nox.session(python=['2.7', '3.5', '3.6', '3.7'])
+@nox.session(python=["2.7", "3.5", "3.6", "3.7"])
 def unit(session):
     """Run the unit test suite."""
     default(session)
 
 
-@nox.session(python=['2.7', '3.6'])
+@nox.session(python=["2.7", "3.6"])
 def system(session):
     """Run the system test suite."""
 
     # Sanity check: Only run system tests if the environment variable is set.
-    if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
-        session.skip('Credentials must be set via environment variable.')
+    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
+        session.skip("Credentials must be set via environment variable.")
 
     # Use pre-release gRPC for system tests.
-    session.install('--pre', 'grpcio')
+    session.install("--pre", "grpcio")
 
     # Install all test dependencies, then install local packages in place.
-    session.install('mock', 'pytest')
+    session.install("mock", "pytest")
     for local_dep in LOCAL_DEPS:
-        session.install('-e', local_dep)
-    session.install('-e', os.path.join('..', 'storage'))
-    session.install('-e', os.path.join('..', 'test_utils'))
-    session.install('-e', '.[pandas]')
+        session.install("-e", local_dep)
+    session.install("-e", os.path.join("..", "storage"))
+    session.install("-e", os.path.join("..", "test_utils"))
+    session.install("-e", ".[pandas]")
 
     # IPython does not support Python 2 after version 5.x
-    if session.python == '2.7':
-        session.install('ipython==5.5')
+    if session.python == "2.7":
+        session.install("ipython==5.5")
     else:
-        session.install('ipython')
+        session.install("ipython")
 
     # Run py.test against the system tests.
     session.run(
-        'py.test',
-        '--quiet',
-        os.path.join('tests', 'system.py'),
-        *session.posargs
+        "py.test", "--quiet", os.path.join("tests", "system.py"), *session.posargs
     )
 
 
-@nox.session(python=['2.7', '3.6'])
+@nox.session(python=["2.7", "3.6"])
 def snippets(session):
     """Run the snippets test suite."""
 
     # Sanity check: Only run snippets tests if the environment variable is set.
-    if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', ''):
-        session.skip('Credentials must be set via environment variable.')
+    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
+        session.skip("Credentials must be set via environment variable.")
 
     # Install all test dependencies, then install local packages in place.
-    session.install('mock', 'pytest')
+    session.install("mock", "pytest")
     for local_dep in LOCAL_DEPS:
-        session.install('-e', local_dep)
-    session.install('-e', os.path.join('..', 'storage'))
-    session.install('-e', os.path.join('..', 'test_utils'))
-    session.install('-e', '.[pandas, pyarrow, fastparquet]')
+        session.install("-e", local_dep)
+    session.install("-e", os.path.join("..", "storage"))
+    session.install("-e", os.path.join("..", "test_utils"))
+    session.install("-e", ".[pandas, pyarrow, fastparquet]")
 
     # Run py.test against the snippets tests.
-    session.run(
-        'py.test', os.path.join('docs', 'snippets.py'), *session.posargs)
+    session.run("py.test", os.path.join("docs", "snippets.py"), *session.posargs)
 
 
-@nox.session(python='3.6')
+@nox.session(python="3.6")
 def cover(session):
     """Run the final coverage report.
 
     This outputs the coverage report aggregating coverage from the unit
     test runs (not system test runs), and then erases coverage data.
     """
-    session.install('coverage', 'pytest-cov')
-    session.run('coverage', 'report', '--show-missing', '--fail-under=100')
-    session.run('coverage', 'erase')
+    session.install("coverage", "pytest-cov")
+    session.run("coverage", "report", "--show-missing", "--fail-under=100")
+    session.run("coverage", "erase")
 
 
-@nox.session(python='3.6')
+@nox.session(python="3.6")
 def lint(session):
     """Run linters.
 
@@ -151,22 +149,20 @@ def lint(session):
     serious code quality issues.
     """
 
-    session.install('flake8', *LOCAL_DEPS)
-    session.install('.')
-    session.run('flake8', os.path.join('google', 'cloud', 'bigquery'))
-    session.run('flake8', 'tests')
-    session.run(
-        'flake8', os.path.join('docs', 'snippets.py'))
+    session.install("black", "flake8", *LOCAL_DEPS)
+    session.install(".")
+    session.run("flake8", os.path.join("google", "cloud", "bigquery"))
+    session.run("flake8", "tests")
+    session.run("flake8", os.path.join("docs", "snippets.py"))
+    session.run("black", "--check", *BLACK_PATHS)
 
 
-@nox.session(python='3.6')
+@nox.session(python="3.6")
 def lint_setup_py(session):
     """Verify that setup.py is valid (including RST check)."""
 
-    session.install('docutils', 'Pygments')
-    session.run(
-        'python', 'setup.py', 'check', '--restructuredtext', '--strict')
-
+    session.install("docutils", "Pygments")
+    session.run("python", "setup.py", "check", "--restructuredtext", "--strict")
 
 
 @nox.session(python="3.6")
@@ -175,32 +171,29 @@ def blacken(session):
     Format code to uniform standard.
     """
     session.install("black")
-    session.run(
-        "black",
-        "google",
-        "tests",
-        "docs",
-    )
+    session.run("black", *BLACK_PATHS)
 
 
-@nox.session(python='3.6')
+@nox.session(python="3.6")
 def docs(session):
     """Build the docs."""
 
-    session.install('ipython', 'recommonmark', 'sphinx', 'sphinx_rtd_theme')
+    session.install("ipython", "recommonmark", "sphinx", "sphinx_rtd_theme")
     for local_dep in LOCAL_DEPS:
-        session.install('-e', local_dep)
-    session.install('-e', os.path.join('..', 'storage'))
-    session.install('-e', '.[pandas, pyarrow]')
+        session.install("-e", local_dep)
+    session.install("-e", os.path.join("..", "storage"))
+    session.install("-e", ".[pandas, pyarrow]")
 
-    shutil.rmtree(os.path.join('docs', '_build'), ignore_errors=True)
+    shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
-        'sphinx-build',
-        '-W',  # warnings as errors
-        '-T',  # show full traceback on exception
-        '-N',  # no colors
-        '-b', 'html',
-        '-d', os.path.join('docs', '_build', 'doctrees', ''),
-        os.path.join('docs', ''),
-        os.path.join('docs', '_build', 'html', ''),
+        "sphinx-build",
+        "-W",  # warnings as errors
+        "-T",  # show full traceback on exception
+        "-N",  # no colors
+        "-b",
+        "html",
+        "-d",
+        os.path.join("docs", "_build", "doctrees", ""),
+        os.path.join("docs", ""),
+        os.path.join("docs", "_build", "html", ""),
     )
