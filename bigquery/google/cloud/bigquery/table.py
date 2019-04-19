f2bdd1cbab029dfae08a126194f650cf65aef820
Tim Swast <swast@google.com>
2019-04-19 11:41:16 -0700

    Close the to_dataframe progress bar when finished.

--- bigquery/google/cloud/bigquery/table.py
+++ bigquery/google/cloud/bigquery/table.py
@@ -1384,6 +1384,10 @@ class RowIterator(HTTPIterator):
                 progress_bar.total = progress_bar.total or self.total_rows
                 progress_bar.update(len(current_frame))
 
+        if progress_bar is not None:
+            # Indicate that the download has finished.
+            progress_bar.close()
+
         return pandas.concat(frames)
 
     def _to_dataframe_bqstorage(self, bqstorage_client, dtypes):
