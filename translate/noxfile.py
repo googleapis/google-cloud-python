74ed955cfad96da440bac3acec773f8cb23a89bf
yoshi-automation <yoshi-automation@google.com>
2019-04-23 05:32:43 -0700

    [CHANGE ME] Re-generated translate to pick up changes in the API or client library generator.

--- translate/noxfile.py
+++ translate/noxfile.py
@@ -140,12 +140,11 @@ def cover(session):
 
     session.run("coverage", "erase")
 
-@nox.session(python='3.6')
+@nox.session(python="3.7")
 def docs(session):
-    """Build the docs."""
+    """Build the docs for this library."""
 
     session.install('sphinx', 'alabaster', 'recommonmark')
-    session.install('-e', '.')
 
     shutil.rmtree(os.path.join('docs', '_build'), ignore_errors=True)
     session.run(
