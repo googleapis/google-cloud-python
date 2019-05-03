4bd8a3f11a48af4a24682b2b492e863b0e429ecd
Bu Sun Kim <busunkim@google.com>
2019-05-03 10:43:40 -0700

    Fix classifier in setup.py.

--- recaptchaenterprise/setup.py
+++ recaptchaenterprise/setup.py
@@ -22,7 +22,7 @@ import setuptools
 name = 'google-cloud-recaptcha-enterprise'
 description = 'reCAPTCHA Enterprise API client library'
 version = '0.1.0'
-release_status = '3 - Alpha'
+release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'google-api-core[grpc] >= 1.4.1, < 2.0.0dev',
     'enum34; python_version < "3.4"',
