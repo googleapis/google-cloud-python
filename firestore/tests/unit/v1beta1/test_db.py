71e86a5a62f711f23c17057c175216dd7b69e617
Billcountry <me@billcountry.tech>
2019-05-08 16:27:04 +0300

    Initial tests

--- firestore/tests/unit/v1beta1/test_db.py
+++ firestore/tests/unit/v1beta1/test_db.py
@@ -1,12 +1,24 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from google.cloud.firestore_v1beta1 import db
 
 
-class TestModel(db.FirestoreModel):
+class UserModel(db.FirestoreModel):
     name = db.StringField(required=True)
     age = db.IntegerField()
     tags = db.ListField(db.StringField)
 
 
+
+class Messages(db.FirestoreModel):
+    user = db.ReferenceField(UserModel)
+
+
 class TestDB(TestCase):
-    pass
+    def test_fox(self):
+        print("pass")
+        user = UserModel.get("billcountry")
+        user.name = "Billcountry Mwaniki"
+        user.age = 24
+        user.put()
+
+
