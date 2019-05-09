189f2f02c14bee9d4b10129e92634e766c6f49de
dpebot <dpebot@google.com>
2019-05-09 16:53:15 +0000

    Update docs after merge to master.

--- latest/_sources/bigquery_datatransfer/index.rst.txt
+++ latest/_sources/bigquery_datatransfer/index.rst.txt
@@ -1,4 +1,4 @@
-.. include:: /../bigquery_datatransfer/README.rst
+.. include:: README.rst
 
 
 API Reference
--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'db583fc',
+    VERSION: 'f290d4c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
