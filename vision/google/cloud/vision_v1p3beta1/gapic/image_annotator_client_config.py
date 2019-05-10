5c7c90f479c77763d6f233d875c47a8073b19609
yoshi-automation <yoshi-automation@google.com>
2019-05-10 05:47:39 -0700

    [CHANGE ME] Re-generated vision to pick up changes in the API or client library generator.

--- vision/google/cloud/vision_v1p3beta1/gapic/image_annotator_client_config.py
+++ vision/google/cloud/vision_v1p3beta1/gapic/image_annotator_client_config.py
@@ -1,7 +1,10 @@
 config = {
     "interfaces": {
         "google.cloud.vision.v1p3beta1.ImageAnnotator": {
-            "retry_codes": {"idempotent": ["UNAVAILABLE"], "non_idempotent": []},
+            "retry_codes": {
+                "idempotent": ["DEADLINE_EXCEEDED", "UNAVAILABLE"],
+                "non_idempotent": [],
+            },
             "retry_params": {
                 "default": {
                     "initial_retry_delay_millis": 100,
