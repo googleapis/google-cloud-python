d3048e98e7d1f3a7f8e7e2bf1248dbd921ef39e6
Tres Seaver <tseaver@palladion.com>
2019-05-14 15:13:22 -0400

    Widen range for 'google-cloud-core'.

--- storage/setup.py
+++ storage/setup.py
@@ -30,7 +30,7 @@ version = '1.15.0'
 release_status = 'Development Status :: 5 - Production/Stable'
 dependencies = [
     'google-api-core >= 1.6.0, < 2.0.0dev',
-    'google-cloud-core >= 0.29.0, < 0.30dev',
+    'google-cloud-core >= 0.29.0, < 2.0dev',
     'google-resumable-media >= 0.3.1',
 ]
 extras = {
