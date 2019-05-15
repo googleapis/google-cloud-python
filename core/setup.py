c95f9b220feb457804757d15cc4e5ab73d028c44
Tres Seaver <tseaver@palladion.com>
2019-05-15 17:02:56 -0400

    Release core-1.0.0 (#7990)

--- core/setup.py
+++ core/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = "google-cloud-core"
 description = "Google Cloud API client core library"
-version = "0.29.1"
+version = "1.0.0"
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
