bf1e6785daabdd69b38acbdd2de08651d7be2ea2
Tres Seaver <tseaver@palladion.com>
2019-05-15 13:22:25 -0400

    Widen range for 'google-cloud-core'. (#7980)
    Prep translate-1.4.1 release.

--- translate/setup.py
+++ translate/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-translate'
 description = 'Google Cloud Translation API client library'
-version = '1.4.0'
+version = '1.4.1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
@@ -30,7 +30,7 @@ version = '1.4.0'
 release_status = 'Development Status :: 5 - Production/Stable'
 dependencies = [
     'google-api-core[grpc] >= 1.6.0, < 2.0.0dev',
-    'google-cloud-core >= 0.29.0, < 0.30dev',
+    'google-cloud-core >= 0.29.0, < 2.0dev',
 ]
 extras = {
 }
