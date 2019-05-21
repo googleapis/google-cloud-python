6e6ef5ec9f9825d1ecde404b8e6a8cd6fe862dc7
yoshi-automation <yoshi-automation@google.com>
2019-05-21 05:13:36 -0700

    [CHANGE ME] Re-generated bigquery_datatransfer to pick up changes in the API or client library generator.

--- bigquery_datatransfer/google/cloud/bigquery_datatransfer_v1/gapic/data_transfer_service_client.py
+++ bigquery_datatransfer/google/cloud/bigquery_datatransfer_v1/gapic/data_transfer_service_client.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Accesses the google.cloud.bigquery.datatransfer.v1 DataTransferService API."""
 
 import functools
@@ -43,6 +44,7 @@ from google.cloud.bigquery_datatransfer_v1.proto import transfer_pb2
 from google.protobuf import empty_pb2
 from google.protobuf import field_mask_pb2
 from google.protobuf import timestamp_pb2
+
 
 _GAPIC_LIBRARY_VERSION = pkg_resources.get_distribution(
     "google-cloud-bigquery-datatransfer"
