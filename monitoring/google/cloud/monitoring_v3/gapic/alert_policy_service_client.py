d33223cebd7a73f491e1c98fa8b2a1751651dcf5
yoshi-automation <yoshi-automation@google.com>
2019-05-21 05:26:20 -0700

    [CHANGE ME] Re-generated monitoring to pick up changes in the API or client library generator.

--- monitoring/google/cloud/monitoring_v3/gapic/alert_policy_service_client.py
+++ monitoring/google/cloud/monitoring_v3/gapic/alert_policy_service_client.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Accesses the google.monitoring.v3 AlertPolicyService API."""
 
 import functools
@@ -39,6 +40,7 @@ from google.cloud.monitoring_v3.proto import alert_service_pb2
 from google.cloud.monitoring_v3.proto import alert_service_pb2_grpc
 from google.protobuf import empty_pb2
 from google.protobuf import field_mask_pb2
+
 
 _GAPIC_LIBRARY_VERSION = pkg_resources.get_distribution(
     "google-cloud-monitoring"
