c5a9b4cf8371dddcd037d073c1f8e4cde4f60168
Andrew Gorcester <gorcester@google.com>
2019-05-24 14:10:45 -0700

    Fix url in setup.py

--- packages/google-cloud-ndb/setup.py
+++ packages/google-cloud-ndb/setup.py
@@ -34,7 +34,7 @@ def main():
         author="Google LLC",
         author_email="googleapis-packages@google.com",
         license="Apache 2.0",
-        url="https://github.com/GoogleCloudPlatform/google-cloud-python",
+        url="https://github.com/googleapis/python-ndb",
         classifiers=[
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Developers",
