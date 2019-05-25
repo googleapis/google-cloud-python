0d9a6c4fedc4bf4d9e30dc352a22de3cb3b4400b
dpebot <dpebot@google.com>
2019-05-25 17:07:25 +0000

    Update docs after merge to master.

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8a525eb',
+    VERSION: '0574678',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
