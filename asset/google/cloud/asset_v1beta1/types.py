d4bc15846b8061b745cdffbdd1c06b54fd362173
yoshi-automation <yoshi-automation@google.com>
2019-05-25 05:11:35 -0700

    [CHANGE ME] Re-generated asset to pick up changes in the API or client library generator.

--- asset/google/cloud/asset_v1beta1/types.py
+++ asset/google/cloud/asset_v1beta1/types.py
@@ -45,7 +45,7 @@ _local_modules = [asset_service_pb2, assets_pb2]
 
 names = []
 
-for module in _shared_modules:
+for module in _shared_modules:  # pragma: NO COVER
     for name, message in get_messages(module).items():
         setattr(sys.modules[__name__], name, message)
         names.append(name)
