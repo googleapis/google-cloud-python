ef4c72865af7abe6c707f455b50c202bea98adb2
yoshi-automation <yoshi-automation@google.com>
2019-05-25 05:35:25 -0700

    [CHANGE ME] Re-generated trace to pick up changes in the API or client library generator.

--- trace/google/cloud/trace_v2/types.py
+++ trace/google/cloud/trace_v2/types.py
@@ -35,7 +35,7 @@ _local_modules = [trace_pb2, tracing_pb2]
 
 names = []
 
-for module in _shared_modules:
+for module in _shared_modules:  # pragma: NO COVER
     for name, message in get_messages(module).items():
         setattr(sys.modules[__name__], name, message)
         names.append(name)
