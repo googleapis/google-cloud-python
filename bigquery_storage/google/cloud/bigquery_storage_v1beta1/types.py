ea9616fb388733e2b971ea7da68cfe5e992888b8
yoshi-automation <yoshi-automation@google.com>
2019-05-25 05:14:31 -0700

    [CHANGE ME] Re-generated bigquery_storage to pick up changes in the API or client library generator.

--- bigquery_storage/google/cloud/bigquery_storage_v1beta1/types.py
+++ bigquery_storage/google/cloud/bigquery_storage_v1beta1/types.py
@@ -34,7 +34,7 @@ _local_modules = [avro_pb2, read_options_pb2, storage_pb2, table_reference_pb2]
 
 names = []
 
-for module in _shared_modules:
+for module in _shared_modules:  # pragma: NO COVER
     for name, message in get_messages(module).items():
         setattr(sys.modules[__name__], name, message)
         names.append(name)
