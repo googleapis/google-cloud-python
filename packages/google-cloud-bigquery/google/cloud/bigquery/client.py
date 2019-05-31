625c1d1ff561e733fddb915513f993b9a07ae024
Tim Swast <swast@google.com>
2019-05-30 17:23:48 -0700

    Use `job_config.schema` for data type conversion if specified in `load_table_from_dataframe`. (#8105)
    * Use `job_config.schema` for data type conversion if specified in `load_table_from_dataframe`.
    
    Use the BigQuery schema to inform encoding of file used in load job.
    This fixes an issue where a dataframe with ambiguous types (such as an
    `object` column containing all `None` values) could not be appended to
    an existing table, since the schemas wouldn't match in most cases.
    
    * Add system test for loading dataframe with non-nulls and explicit schema.

--- packages/google-cloud-bigquery/google/cloud/bigquery/client.py
+++ packages/google-cloud-bigquery/google/cloud/bigquery/client.py
@@ -44,6 +44,7 @@ from google.cloud.client import ClientWithProject
 from google.cloud.bigquery._helpers import _record_field_to_json
 from google.cloud.bigquery._helpers import _str_or_none
 from google.cloud.bigquery._http import Connection
+from google.cloud.bigquery import _pandas_helpers
 from google.cloud.bigquery.dataset import Dataset
 from google.cloud.bigquery.dataset import DatasetListItem
 from google.cloud.bigquery.dataset import DatasetReference
@@ -1271,8 +1272,15 @@ class Client(ClientWithProject):
             project (str, optional):
                 Project ID of the project of where to run the job. Defaults
                 to the client's project.
-            job_config (google.cloud.bigquery.job.LoadJobConfig, optional):
+            job_config (~google.cloud.bigquery.job.LoadJobConfig, optional):
                 Extra configuration options for the job.
+
+                To override the default pandas data type conversions, supply
+                a value for
+                :attr:`~google.cloud.bigquery.job.LoadJobConfig.schema` with
+                column names matching those of the dataframe. The BigQuery
+                schema is used to determine the correct data type conversion.
+                Indexes are not loaded. Requires the :mod:`pyarrow` library.
 
         Returns:
             google.cloud.bigquery.job.LoadJob: A new load job.
@@ -1296,7 +1304,10 @@ class Client(ClientWithProject):
         os.close(tmpfd)
 
         try:
-            dataframe.to_parquet(tmppath)
+            if job_config.schema:
+                _pandas_helpers.to_parquet(dataframe, job_config.schema, tmppath)
+            else:
+                dataframe.to_parquet(tmppath)
 
             with open(tmppath, "rb") as parquet_file:
                 return self.load_table_from_file(
