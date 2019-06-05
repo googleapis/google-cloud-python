0dfb041d93dc7c926afd2a64a445fa2bb8af7925
Paul Adams <paul.adams@evogro.com>
2019-06-05 19:04:29 +0100

    set the actual timestamp of the log record, fixes #8222

--- logging/google/cloud/logging/handlers/transports/background_thread.py
+++ logging/google/cloud/logging/handlers/transports/background_thread.py
@@ -267,6 +267,7 @@ class _Worker(object):
                 "labels": labels,
                 "trace": trace,
                 "span_id": span_id,
+                "timestamp": datetime.utcfromtimestamp(record.created),
             }
         )
 
