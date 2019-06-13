929b66aa9a15aeffd8b255c4522223561c37cce5
Bu Sun Kim <busunkim@google.com>
2019-06-13 12:21:43 -0700

    Blacken.

--- monitoring/tests/system/test_vpcsc.py
+++ monitoring/tests/system/test_vpcsc.py
@@ -28,7 +28,9 @@ from google.cloud import monitoring_v3
 from google.cloud.monitoring_v3 import enums
 
 PROJECT_INSIDE = os.environ.get("PROJECT_INSIDE", None)
-PROJECT_OUTSIDE = os.environ.get("GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_PROJECT", None)
+PROJECT_OUTSIDE = os.environ.get(
+    "GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_PROJECT", None
+)
 IS_INSIDE_VPCSC = os.environ.get("GOOGLE_CLOUD_TESTS_IN_VPCSC", "false")
 
 
