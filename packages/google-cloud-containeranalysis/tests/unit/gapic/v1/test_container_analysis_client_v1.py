96b3fa3b75175a59a8d363fc2926b4f7fe4f27e0
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-06-17 10:40:57 -0700

    Generate grafeas and container analysis. (#8186)

---
+++ packages/google-cloud-containeranalysis/tests/unit/gapic/v1/test_container_analysis_client_v1.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests."""
+
+import mock
+import pytest
+
+from google.cloud.devtools import containeranalysis_v1
+from google.iam.v1 import iam_policy_pb2
+from google.iam.v1 import policy_pb2
+
+
+class MultiCallableStub(object):
+    """Stub for the grpc.UnaryUnaryMultiCallable interface."""
+
+    def __init__(self, method, channel_stub):
+        self.method = method
+        self.channel_stub = channel_stub
+
+    def __call__(self, request, timeout=None, metadata=None, credentials=None):
+        self.channel_stub.requests.append((self.method, request))
+
+        response = None
+        if self.channel_stub.responses:
+            response = self.channel_stub.responses.pop()
+
+        if isinstance(response, Exception):
+            raise response
+
+        if response:
+            return response
+
+
+class ChannelStub(object):
+    """Stub for the grpc.Channel interface."""
+
+    def __init__(self, responses=[]):
+        self.responses = responses
+        self.requests = []
+
+    def unary_unary(self, method, request_serializer=None, response_deserializer=None):
+        return MultiCallableStub(method, self)
+
+
+class CustomException(Exception):
+    pass
+
+
+class TestContainerAnalysisClient(object):
+    def test_set_iam_policy(self):
+        # Setup Expected Response
+        version = 351608024
+        etag = b"21"
+        expected_response = {"version": version, "etag": etag}
+        expected_response = policy_pb2.Policy(**expected_response)
+
+        # Mock the API response
+        channel = ChannelStub(responses=[expected_response])
+        patch = mock.patch("google.api_core.grpc_helpers.create_channel")
+        with patch as create_channel:
+            create_channel.return_value = channel
+            client = containeranalysis_v1.ContainerAnalysisClient()
+
+        # Setup Request
+        resource = client.note_path("[PROJECT]", "[NOTE]")
+        policy = {}
+
+        response = client.set_iam_policy(resource, policy)
+        assert expected_response == response
+
+        assert len(channel.requests) == 1
+        expected_request = iam_policy_pb2.SetIamPolicyRequest(
+            resource=resource, policy=policy
+        )
+        actual_request = channel.requests[0][1]
+        assert expected_request == actual_request
+
+    def test_set_iam_policy_exception(self):
+        # Mock the API response
+        channel = ChannelStub(responses=[CustomException()])
+        patch = mock.patch("google.api_core.grpc_helpers.create_channel")
+        with patch as create_channel:
+            create_channel.return_value = channel
+            client = containeranalysis_v1.ContainerAnalysisClient()
+
+        # Setup request
+        resource = client.note_path("[PROJECT]", "[NOTE]")
+        policy = {}
+
+        with pytest.raises(CustomException):
+            client.set_iam_policy(resource, policy)
+
+    def test_get_iam_policy(self):
+        # Setup Expected Response
+        version = 351608024
+        etag = b"21"
+        expected_response = {"version": version, "etag": etag}
+        expected_response = policy_pb2.Policy(**expected_response)
+
+        # Mock the API response
+        channel = ChannelStub(responses=[expected_response])
+        patch = mock.patch("google.api_core.grpc_helpers.create_channel")
+        with patch as create_channel:
+            create_channel.return_value = channel
+            client = containeranalysis_v1.ContainerAnalysisClient()
+
+        # Setup Request
+        resource = client.note_path("[PROJECT]", "[NOTE]")
+
+        response = client.get_iam_policy(resource)
+        assert expected_response == response
+
+        assert len(channel.requests) == 1
+        expected_request = iam_policy_pb2.GetIamPolicyRequest(resource=resource)
+        actual_request = channel.requests[0][1]
+        assert expected_request == actual_request
+
+    def test_get_iam_policy_exception(self):
+        # Mock the API response
+        channel = ChannelStub(responses=[CustomException()])
+        patch = mock.patch("google.api_core.grpc_helpers.create_channel")
+        with patch as create_channel:
+            create_channel.return_value = channel
+            client = containeranalysis_v1.ContainerAnalysisClient()
+
+        # Setup request
+        resource = client.note_path("[PROJECT]", "[NOTE]")
+
+        with pytest.raises(CustomException):
+            client.get_iam_policy(resource)
+
+    def test_test_iam_permissions(self):
+        # Setup Expected Response
+        expected_response = {}
+        expected_response = iam_policy_pb2.TestIamPermissionsResponse(
+            **expected_response
+        )
+
+        # Mock the API response
+        channel = ChannelStub(responses=[expected_response])
+        patch = mock.patch("google.api_core.grpc_helpers.create_channel")
+        with patch as create_channel:
+            create_channel.return_value = channel
+            client = containeranalysis_v1.ContainerAnalysisClient()
+
+        # Setup Request
+        resource = client.note_path("[PROJECT]", "[NOTE]")
+        permissions = []
+
+        response = client.test_iam_permissions(resource, permissions)
+        assert expected_response == response
+
+        assert len(channel.requests) == 1
+        expected_request = iam_policy_pb2.TestIamPermissionsRequest(
+            resource=resource, permissions=permissions
+        )
+        actual_request = channel.requests[0][1]
+        assert expected_request == actual_request
+
+    def test_test_iam_permissions_exception(self):
+        # Mock the API response
+        channel = ChannelStub(responses=[CustomException()])
+        patch = mock.patch("google.api_core.grpc_helpers.create_channel")
+        with patch as create_channel:
+            create_channel.return_value = channel
+            client = containeranalysis_v1.ContainerAnalysisClient()
+
+        # Setup request
+        resource = client.note_path("[PROJECT]", "[NOTE]")
+        permissions = []
+
+        with pytest.raises(CustomException):
+            client.test_iam_permissions(resource, permissions)
