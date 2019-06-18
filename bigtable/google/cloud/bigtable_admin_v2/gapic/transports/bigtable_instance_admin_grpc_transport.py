b7b7ba12f373e621ab7bde2662e844ab4c86e0a9
yoshi-automation <yoshi-automation@google.com>
2019-06-18 05:12:19 -0700

    [CHANGE ME] Re-generated bigtable to pick up changes in the API or client library generator.

--- bigtable/google/cloud/bigtable_admin_v2/gapic/transports/bigtable_instance_admin_grpc_transport.py
+++ bigtable/google/cloud/bigtable_admin_v2/gapic/transports/bigtable_instance_admin_grpc_transport.py
@@ -90,7 +90,7 @@ class BigtableInstanceAdminGrpcTransport(object):
 
     @classmethod
     def create_channel(
-        cls, address="bigtableadmin.googleapis.com:443", credentials=None
+        cls, address="bigtableadmin.googleapis.com:443", credentials=None, **kwargs
     ):
         """Create and return a gRPC channel object.
 
@@ -101,12 +101,14 @@ class BigtableInstanceAdminGrpcTransport(object):
                 credentials identify this application to the service. If
                 none are specified, the client will attempt to ascertain
                 the credentials from the environment.
+            kwargs (dict): Keyword arguments, which are passed to the
+                channel creation.
 
         Returns:
             grpc.Channel: A gRPC channel object.
         """
         return google.api_core.grpc_helpers.create_channel(
-            address, credentials=credentials, scopes=cls._OAUTH_SCOPES
+            address, credentials=credentials, scopes=cls._OAUTH_SCOPES, **kwargs
         )
 
     @property
