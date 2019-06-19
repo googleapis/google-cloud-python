292030dd7cc97f9fe98388a6912061d3c7bcc5ca
Tres Seaver <tseaver@palladion.com>
2019-06-19 18:06:00 -0400

    Add synth support for v1beta1 API version (via manual synth). (#8436)
    Closes #8208.

--- packages/google-cloud-container/google/cloud/container_v1beta1/__init__.py
+++ packages/google-cloud-container/google/cloud/container_v1beta1/__init__.py
@@ -17,9 +17,14 @@
 
 from __future__ import absolute_import
 
-from google.cloud.container_v1 import ClusterManagerClient
-from google.cloud.container_v1 import enums
-from google.cloud.container_v1 import types
+from google.cloud.container_v1beta1 import types
+from google.cloud.container_v1beta1.gapic import cluster_manager_client
+from google.cloud.container_v1beta1.gapic import enums
+
+
+class ClusterManagerClient(cluster_manager_client.ClusterManagerClient):
+    __doc__ = cluster_manager_client.ClusterManagerClient.__doc__
+    enums = enums
 
 
 __all__ = ("enums", "types", "ClusterManagerClient")
