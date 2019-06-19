efe6b6015c98662d709b7d8c14d3eea22a296ac6
Tim Swast <swast@google.com>
2019-06-19 14:39:37 -0700

    Blacken. Remove link and location auto-detect information from comments.

--- bigquery/samples/create_job.py
+++ bigquery/samples/create_job.py
@@ -22,19 +22,13 @@ def create_job(client):
 
     query_job = client.query(
         "SELECT country_name from `bigquery-public-data.utility_us.country_code_iso`",
-
-        # Generally, location can be inferred based on the location of the
-        # referenced dataset. However, it can also be set explicitly to force
-        # job execution to be routed to a specific processing location. See
-        # https://cloud.google.com/bigquery/docs/locations for more info.
+        # Explicitly force job execution to be routed to a specific processing
+        # location.
         location="US",
-
         # Specify a job configuration to set optional job resource properties.
         job_config=bigquery.QueryJobConfig(
-            labels={"example-label": "example-value"},
-            maximum_bytes_billed=1000000,
+            labels={"example-label": "example-value"}, maximum_bytes_billed=1000000
         ),
-
         # The client libraries automatically generate a job ID. Override the
         # generated ID with either the job_id_prefix or job_id parameters.
         job_id_prefix="code_sample_",
