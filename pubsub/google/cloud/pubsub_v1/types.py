9a18efe5aa4817aa95d02b54a26236e7ec4bebdd
yoshi-automation <yoshi-automation@google.com>
2019-06-20 05:24:38 -0700

    [CHANGE ME] Re-generated pubsub to pick up changes in the API or client library generator.

--- pubsub/google/cloud/pubsub_v1/types.py
+++ pubsub/google/cloud/pubsub_v1/types.py
@@ -170,6 +170,39 @@ if sys.version_info >= (3, 5):
     )
 
 
+if sys.version_info >= (3, 5):
+    FlowControl.__doc__ = (
+        "The settings for controlling the rate at which messages are pulled "
+        "with an asynchronous subscription."
+    )
+    FlowControl.max_bytes.__doc__ = (
+        "The maximum total size of received - but not yet processed - messages "
+        "before pausing the message stream."
+    )
+    FlowControl.max_messages.__doc__ = (
+        "The maximum number of received - but not yet processed - messages before "
+        "pausing the message stream."
+    )
+    FlowControl.resume_threshold.__doc__ = (
+        "The relative threshold of the ``max_bytes`` and ``max_messages`` limits "
+        "below which to resume the message stream. Must be a positive number not "
+        "greater than ``1.0``."
+    )
+    FlowControl.max_requests.__doc__ = "Currently not in use."
+    FlowControl.max_request_batch_size.__doc__ = (
+        "The maximum number of requests scheduled by callbacks to process and "
+        "dispatch at a time."
+    )
+    FlowControl.max_request_batch_latency.__doc__ = (
+        "The maximum amount of time in seconds to wait for additional request "
+        "items before processing the next batch of requests."
+    )
+    FlowControl.max_lease_duration.__doc__ = (
+        "The maximum amount of time in seconds to hold a lease on a message "
+        "before dropping it from the lease management."
+    )
+
+
 _shared_modules = [
     http_pb2,
     iam_policy_pb2,
