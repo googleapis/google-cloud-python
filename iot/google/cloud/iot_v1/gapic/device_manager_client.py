8c6af56e107d1f802e1dab05fb8c3002f1b5123a
Bu Sun Kim <busunkim@google.com>
2019-06-21 16:13:55 -0700

    Revert iot changes.

--- iot/google/cloud/iot_v1/gapic/device_manager_client.py
+++ iot/google/cloud/iot_v1/gapic/device_manager_client.py
@@ -22,7 +22,6 @@ import warnings
 
 from google.oauth2 import service_account
 import google.api_core.gapic_v1.client_info
-import google.api_core.client_options
 import google.api_core.gapic_v1.config
 import google.api_core.gapic_v1.method
 import google.api_core.gapic_v1.routing_header
@@ -113,7 +112,6 @@ class DeviceManagerClient(object):
         credentials=None,
         client_config=None,
         client_info=None,
-        client_options=None,
     ):
         """Constructor.
 
@@ -144,9 +142,6 @@ class DeviceManagerClient(object):
                 API requests. If ``None``, then default info will be used.
                 Generally, you only need to set this if you're developing
                 your own client library.
-            client_options (Union[dict, google.api_core.client_options.ClientOptions):
-                Client options used to set user options on the client. API Endpoint
-                should be set through client_options.
         """
         # Raise deprecation warnings for things we want to go away.
         if client_config is not None:
@@ -165,13 +160,6 @@ class DeviceManagerClient(object):
                 stacklevel=2,
             )
 
-        api_endpoint = self.SERVICE_ADDRESS
-        if client_options:
-            if type(client_options) == dict:
-                client_options = google.api_core.client_options.from_dict(client_options)
-            if client_options.api_endpoint:
-                api_endpoint = client_options.api_endpoint
-
         # Instantiate the transport.
         # The transport is responsible for handling serialization and
         # deserialization and actually sending data to the service.
@@ -180,7 +168,6 @@ class DeviceManagerClient(object):
                 self.transport = transport(
                     credentials=credentials,
                     default_class=device_manager_grpc_transport.DeviceManagerGrpcTransport,
-                    address=api_endpoint,
                 )
             else:
                 if credentials:
@@ -191,7 +178,7 @@ class DeviceManagerClient(object):
                 self.transport = transport
         else:
             self.transport = device_manager_grpc_transport.DeviceManagerGrpcTransport(
-                address=api_endpoint, channel=channel, credentials=credentials
+                address=self.SERVICE_ADDRESS, channel=channel, credentials=credentials
             )
 
         if client_info is None:
