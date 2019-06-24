48b422dab9277d7d1ed136a75f9f7c02c26a857b
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-06-24 10:18:46 -0700

    Add client options. (#8265)

---
+++ packages/google-api-core/tests/unit/test_client_options.py
@@ -0,0 +1,36 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from google.api_core import client_options
+
+
+def test_constructor():
+    options = client_options.ClientOptions(api_endpoint="foo.googleapis.com")
+
+    assert options.api_endpoint == "foo.googleapis.com"
+
+
+def test_from_dict():
+    options = client_options.from_dict({"api_endpoint": "foo.googleapis.com"})
+
+    assert options.api_endpoint == "foo.googleapis.com"
+
+
+def test_from_dict_bad_argument():
+    with pytest.raises(ValueError):
+        client_options.from_dict(
+            {"api_endpoint": "foo.googleapis.com", "bad_arg": "1234"}
+        )
