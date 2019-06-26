e7047c83232f98e826d367b239149e14626ba6c5
Bu Sun Kim <busunkim@google.com>
2019-06-26 13:41:49 -0700

    Blacken.

--- dns/docs/conf.py
+++ dns/docs/conf.py
@@ -293,13 +293,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (
-        master_doc,
-        "google-cloud-dns",
-        u"google-cloud-dns Documentation",
-        [author],
-        1,
-    )
+    (master_doc, "google-cloud-dns", u"google-cloud-dns Documentation", [author], 1)
 ]
 
 # If true, show URL addresses after external links.
