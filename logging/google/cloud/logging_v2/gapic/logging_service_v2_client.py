87c08d67ba2ef0fe9a65b700a6f7c975d256267a
Tres Seaver <tseaver@palladion.com>
2019-06-28 13:39:40 -0400

    Add 'client_options' support, update list method docstrings (via synth).
    Supersedes #8320.

--- logging/google/cloud/logging_v2/gapic/logging_service_v2_client.py
+++ logging/google/cloud/logging_v2/gapic/logging_service_v2_client.py
@@ -21,6 +21,7 @@ import pkg_resources
 import warnings
 
 from google.oauth2 import service_account
+import google.api_core.client_options
 import google.api_core.gapic_v1.client_info
 import google.api_core.gapic_v1.config
 import google.api_core.gapic_v1.method
@@ -94,6 +95,7 @@ class LoggingServiceV2Client(object):
         credentials=None,
         client_config=None,
         client_info=None,
+        client_options=None,
     ):
         """Constructor.
 
@@ -124,6 +126,9 @@ class LoggingServiceV2Client(object):
                 API requests. If ``None``, then default info will be used.
                 Generally, you only need to set this if you're developing
                 your own client library.
+            client_options (Union[dict, google.api_core.client_options.ClientOptions]):
+                Client options used to set user options on the client. API Endpoint
+                should be set through client_options.
         """
         # Raise deprecation warnings for things we want to go away.
         if client_config is not None:
@@ -142,6 +147,15 @@ class LoggingServiceV2Client(object):
                 stacklevel=2,
             )
 
+        api_endpoint = self.SERVICE_ADDRESS
+        if client_options:
+            if type(client_options) == dict:
+                client_options = google.api_core.client_options.from_dict(
+                    client_options
+                )
+            if client_options.api_endpoint:
+                api_endpoint = client_options.api_endpoint
+
         # Instantiate the transport.
         # The transport is responsible for handling serialization and
         # deserialization and actually sending data to the service.
@@ -150,6 +164,7 @@ class LoggingServiceV2Client(object):
                 self.transport = transport(
                     credentials=credentials,
                     default_class=logging_service_v2_grpc_transport.LoggingServiceV2GrpcTransport,
+                    address=api_endpoint,
                 )
             else:
                 if credentials:
@@ -160,7 +175,7 @@ class LoggingServiceV2Client(object):
                 self.transport = transport
         else:
             self.transport = logging_service_v2_grpc_transport.LoggingServiceV2GrpcTransport(
-                address=self.SERVICE_ADDRESS, channel=channel, credentials=credentials
+                address=api_endpoint, channel=channel, credentials=credentials
             )
 
         if client_info is None:
@@ -500,10 +515,10 @@ class LoggingServiceV2Client(object):
                 that is provided to the method.
 
         Returns:
-            A :class:`~google.gax.PageIterator` instance. By default, this
-            is an iterable of :class:`~google.cloud.logging_v2.types.LogEntry` instances.
-            This object can also be configured to iterate over the pages
-            of the response through the `options` parameter.
+            A :class:`~google.api_core.page_iterator.PageIterator` instance.
+            An iterable of :class:`~google.cloud.logging_v2.types.LogEntry` instances.
+            You can also iterate over the pages of the response
+            using its `pages` property.
 
         Raises:
             google.api_core.exceptions.GoogleAPICallError: If the request
@@ -590,10 +605,10 @@ class LoggingServiceV2Client(object):
                 that is provided to the method.
 
         Returns:
-            A :class:`~google.gax.PageIterator` instance. By default, this
-            is an iterable of :class:`~google.cloud.logging_v2.types.MonitoredResourceDescriptor` instances.
-            This object can also be configured to iterate over the pages
-            of the response through the `options` parameter.
+            A :class:`~google.api_core.page_iterator.PageIterator` instance.
+            An iterable of :class:`~google.cloud.logging_v2.types.MonitoredResourceDescriptor` instances.
+            You can also iterate over the pages of the response
+            using its `pages` property.
 
         Raises:
             google.api_core.exceptions.GoogleAPICallError: If the request
@@ -692,10 +707,10 @@ class LoggingServiceV2Client(object):
                 that is provided to the method.
 
         Returns:
-            A :class:`~google.gax.PageIterator` instance. By default, this
-            is an iterable of :class:`str` instances.
-            This object can also be configured to iterate over the pages
-            of the response through the `options` parameter.
+            A :class:`~google.api_core.page_iterator.PageIterator` instance.
+            An iterable of :class:`str` instances.
+            You can also iterate over the pages of the response
+            using its `pages` property.
 
         Raises:
             google.api_core.exceptions.GoogleAPICallError: If the request
