51b63f1621cb3a80cc2292c1096613430f5bb6ad
yoshi-automation <yoshi-automation@google.com>
2019-06-28 05:16:07 -0700

    [CHANGE ME] Re-generated container to pick up changes in the API or client library generator.

--- container/google/cloud/container_v1/gapic/cluster_manager_client.py
+++ container/google/cloud/container_v1/gapic/cluster_manager_client.py
@@ -20,6 +20,7 @@ import pkg_resources
 import warnings
 
 from google.oauth2 import service_account
+import google.api_core.client_options
 import google.api_core.gapic_v1.client_info
 import google.api_core.gapic_v1.config
 import google.api_core.gapic_v1.method
@@ -77,6 +78,7 @@ class ClusterManagerClient(object):
         credentials=None,
         client_config=None,
         client_info=None,
+        client_options=None,
     ):
         """Constructor.
 
@@ -107,6 +109,9 @@ class ClusterManagerClient(object):
                 API requests. If ``None``, then default info will be used.
                 Generally, you only need to set this if you're developing
                 your own client library.
+            client_options (Union[dict, google.api_core.client_options.ClientOptions]):
+                Client options used to set user options on the client. API Endpoint
+                should be set through client_options.
         """
         # Raise deprecation warnings for things we want to go away.
         if client_config is not None:
@@ -125,6 +130,15 @@ class ClusterManagerClient(object):
                 stacklevel=2,
             )
 
+        api_endpoint = self.SERVICE_ADDRESS
+        if client_options:
+            if type(client_options) == dict:
+                client_options = google.api_core.client_options.from_dict(
+                    client_options
+                )
+            if client_options.api_endpoint:
+                api_endpoint = client_options.api_endpoint
+
         # Instantiate the transport.
         # The transport is responsible for handling serialization and
         # deserialization and actually sending data to the service.
@@ -133,6 +147,7 @@ class ClusterManagerClient(object):
                 self.transport = transport(
                     credentials=credentials,
                     default_class=cluster_manager_grpc_transport.ClusterManagerGrpcTransport,
+                    address=api_endpoint,
                 )
             else:
                 if credentials:
@@ -143,7 +158,7 @@ class ClusterManagerClient(object):
                 self.transport = transport
         else:
             self.transport = cluster_manager_grpc_transport.ClusterManagerGrpcTransport(
-                address=self.SERVICE_ADDRESS, channel=channel, credentials=credentials
+                address=api_endpoint, channel=channel, credentials=credentials
             )
 
         if client_info is None:
