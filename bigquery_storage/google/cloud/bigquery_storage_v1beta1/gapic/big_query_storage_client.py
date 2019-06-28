dc4834ccbb0bf054c7f7e56fc08153ddf69301c3
Tres Seaver <tseaver@palladion.com>
2019-06-28 14:17:47 -0400

    Add 'client_options' support (via synth). (#8536)
    Supersedes #8499.

--- bigquery_storage/google/cloud/bigquery_storage_v1beta1/gapic/big_query_storage_client.py
+++ bigquery_storage/google/cloud/bigquery_storage_v1beta1/gapic/big_query_storage_client.py
@@ -20,6 +20,7 @@ import pkg_resources
 import warnings
 
 from google.oauth2 import service_account
+import google.api_core.client_options
 import google.api_core.gapic_v1.client_info
 import google.api_core.gapic_v1.config
 import google.api_core.gapic_v1.method
@@ -86,6 +87,7 @@ class BigQueryStorageClient(object):
         credentials=None,
         client_config=None,
         client_info=None,
+        client_options=None,
     ):
         """Constructor.
 
@@ -116,6 +118,9 @@ class BigQueryStorageClient(object):
                 API requests. If ``None``, then default info will be used.
                 Generally, you only need to set this if you're developing
                 your own client library.
+            client_options (Union[dict, google.api_core.client_options.ClientOptions]):
+                Client options used to set user options on the client. API Endpoint
+                should be set through client_options.
         """
         # Raise deprecation warnings for things we want to go away.
         if client_config is not None:
@@ -134,6 +139,15 @@ class BigQueryStorageClient(object):
                 stacklevel=2,
             )
 
+        api_endpoint = self.SERVICE_ADDRESS
+        if client_options:
+            if type(client_options) == dict:
+                client_options = google.api_core.client_options.from_dict(
+                    client_options
+                )
+            if client_options.api_endpoint:
+                api_endpoint = client_options.api_endpoint
+
         # Instantiate the transport.
         # The transport is responsible for handling serialization and
         # deserialization and actually sending data to the service.
@@ -142,6 +156,7 @@ class BigQueryStorageClient(object):
                 self.transport = transport(
                     credentials=credentials,
                     default_class=big_query_storage_grpc_transport.BigQueryStorageGrpcTransport,
+                    address=api_endpoint,
                 )
             else:
                 if credentials:
@@ -152,7 +167,7 @@ class BigQueryStorageClient(object):
                 self.transport = transport
         else:
             self.transport = big_query_storage_grpc_transport.BigQueryStorageGrpcTransport(
-                address=self.SERVICE_ADDRESS, channel=channel, credentials=credentials
+                address=api_endpoint, channel=channel, credentials=credentials
             )
 
         if client_info is None:
