9f2ae84755a394f75f4dd51a7fd2c38e221f5a97
Chris Wilcox <crwilcox@google.com>
2019-07-01 09:25:32 -0700

    add _target to reuse gapic configured service address

--- firestore/google/cloud/firestore_v1/client.py
+++ firestore/google/cloud/firestore_v1/client.py
@@ -114,18 +114,16 @@ class Client(ClientWithProject):
             <The GAPIC client with the credentials of the current client.
         """
         if self._firestore_api_internal is None:
-            SERVICE_ADDRESS = "firestore.googleapis.com:443"
-
             # Use a custom channel.
             # We need this in order to set appropriate keepalive options.
             channel = firestore_grpc_transport.FirestoreGrpcTransport.create_channel(
-                target=SERVICE_ADDRESS,
+                target=self._target,
                 credentials=self._credentials,
                 options={"grpc.keepalive_time_ms": 30000}.items(),
             )
 
             self._transport = firestore_grpc_transport.FirestoreGrpcTransport(
-                address=SERVICE_ADDRESS, channel=channel
+                address=self._target, channel=channel
             )
 
             self._firestore_api_internal = firestore_client.FirestoreClient(
@@ -133,6 +131,15 @@ class Client(ClientWithProject):
             )
 
         return self._firestore_api_internal
+
+    @property
+    def _target(self):
+        """Return the target (where the API is).
+
+        Returns:
+            str: The location of the API.
+        """
+        return firestore_client.FirestoreClient.SERVICE_ADDRESS
 
     @property
     def _database_string(self):
