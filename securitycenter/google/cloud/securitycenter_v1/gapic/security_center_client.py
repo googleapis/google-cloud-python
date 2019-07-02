96e0bca31d0fce1575642e29e2cb49fdc06949d7
yoshi-automation <yoshi-automation@google.com>
2019-07-02 05:30:05 -0700

    [CHANGE ME] Re-generated securitycenter to pick up changes in the API or client library generator.

--- securitycenter/google/cloud/securitycenter_v1/gapic/security_center_client.py
+++ securitycenter/google/cloud/securitycenter_v1/gapic/security_center_client.py
@@ -46,6 +46,7 @@ from google.cloud.securitycenter_v1.proto import securitycenter_service_pb2
 from google.cloud.securitycenter_v1.proto import securitycenter_service_pb2_grpc
 from google.cloud.securitycenter_v1.proto import source_pb2
 from google.iam.v1 import iam_policy_pb2
+from google.iam.v1 import options_pb2
 from google.iam.v1 import policy_pb2
 from google.longrunning import operations_pb2
 from google.protobuf import duration_pb2
@@ -444,6 +445,7 @@ class SecurityCenterClient(object):
     def get_iam_policy(
         self,
         resource,
+        options_=None,
         retry=google.api_core.gapic_v1.method.DEFAULT,
         timeout=google.api_core.gapic_v1.method.DEFAULT,
         metadata=None,
@@ -463,6 +465,11 @@ class SecurityCenterClient(object):
         Args:
             resource (str): REQUIRED: The resource for which the policy is being requested.
                 See the operation documentation for the appropriate value for this field.
+            options_ (Union[dict, ~google.cloud.securitycenter_v1.types.GetPolicyOptions]): OPTIONAL: A ``GetPolicyOptions`` object for specifying options to
+                ``GetIamPolicy``. This field is only used by Cloud IAM.
+
+                If a dict is provided, it must be of the same form as the protobuf
+                message :class:`~google.cloud.securitycenter_v1.types.GetPolicyOptions`
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will not
                 be retried.
@@ -493,7 +500,9 @@ class SecurityCenterClient(object):
                 client_info=self._client_info,
             )
 
-        request = iam_policy_pb2.GetIamPolicyRequest(resource=resource)
+        request = iam_policy_pb2.GetIamPolicyRequest(
+            resource=resource, options=options_
+        )
         if metadata is None:
             metadata = []
         metadata = list(metadata)
