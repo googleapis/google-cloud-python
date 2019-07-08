70b4bd23456f6d6a30ac4fefdec174b4c7ed52b9
gaogaogiraffe <52432515+gaogaogiraffe@users.noreply.github.com>
2019-07-08 10:50:23 -0700

    Add VPCSC tests. (#8613)
    The tests can be run inside or outside of VPC service perimeter. The input to the script should be the following environment variables.
    
    PROJECT_ID: a project that is inside the VPC perimeter.
    GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_PROJECT: a project that is outside the VPC perimeter.

--- asset/synth.py
+++ asset/synth.py
@@ -84,6 +84,6 @@ s.replace(
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = gcp.CommonTemplates().py_library(unit_cov_level=79, cov_level=80)
-s.move(templated_files)
+s.move(templated_files, excludes=["noxfile.py"])
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
