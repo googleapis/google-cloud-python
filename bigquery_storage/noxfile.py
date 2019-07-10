5e7a403313d434e481c2ecd269a546471550e617
Tim Swast <swast@google.com>
2019-07-10 14:31:27 -0500

    Add pyarrow to system tests deps.

--- bigquery_storage/noxfile.py
+++ bigquery_storage/noxfile.py
@@ -121,7 +121,7 @@ def system(session):
     session.install('-e', os.path.join('..', 'test_utils'))
     for local_dep in LOCAL_DEPS:
         session.install('-e', local_dep)
-    session.install('-e', '.[pandas,fastavro]')
+    session.install('-e', '.[fastavro,pandas,pyarrow]')
 
     # Run py.test against the system tests.
     session.run('py.test', '--quiet', 'tests/system/')
