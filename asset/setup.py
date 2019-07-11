2d60554dc0db6cf467cf874f2920aa75bc9d4b5b
Tres Seaver <tseaver@palladion.com>
2019-07-11 13:22:10 -0400

    Update pin for 'grpc-google-iam-v1' to 0.12.3+.
    For pubsub / kms, also update the import of the 'IAMPolicy' stub, which
    is no longer exported from the same location.
    
    Supersedes: #8639
    Supersedes: #8640
    
    Closes: #8574
    Closes: #8576
    Closes: #8577
    Closes: #8585
    Closes: #8587
    Closes: #8591
    Closes: #8594
    Closes: #8595
    Closes: #8598

--- asset/setup.py
+++ asset/setup.py
@@ -30,7 +30,7 @@ release_status = "Development Status :: 3 - Alpha"
 dependencies = [
     "google-api-core[grpc] >= 1.6.0, < 2.0.0dev",
     'enum34; python_version < "3.4"',
-    "grpc-google-iam-v1 >= 0.11.4, < 0.12dev",
+    "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
 ]
 
 # Setup boilerplate below this line.
