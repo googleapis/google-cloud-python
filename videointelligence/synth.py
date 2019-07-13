1dd90d8ff7f6b3a69c03a0a5a7f73405f203b7fe
Bu Sun Kim <busunkim@google.com>
2019-07-12 17:10:27 -0700

    Exclude noxfile in copy.

--- videointelligence/synth.py
+++ videointelligence/synth.py
@@ -66,6 +66,6 @@ s.replace(
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(unit_cov_level=97, cov_level=100)
-s.move(templated_files)
+s.move(templated_files, excludes="noxfile.py")
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
