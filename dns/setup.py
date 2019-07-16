0f06c341e242d28f9c23c8bf0eddaea1256e95e2
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-07-16 10:14:30 -0700

    Release 0.30.2 (#8645)

--- dns/setup.py
+++ dns/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-dns'
 description = 'Google Cloud DNS API client library'
-version = '0.30.1'
+version = '0.30.2'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
