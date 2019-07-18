a5a3056037c7997b478d66921fe835aa65200510
Tres Seaver <tseaver@palladion.com>
2019-07-18 16:19:04 -0400

    Bump minimum version for google-api-core to 1.14.0.

--- datacatalog/setup.py
+++ datacatalog/setup.py
@@ -28,7 +28,7 @@ version = "0.2.0"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 3 - Alpha"
 dependencies = [
-    "google-api-core[grpc] >= 1.4.1, < 2.0.0dev",
+    "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
     'enum34; python_version < "3.4"',
 ]
--- spanner/setup.py
+++ spanner/setup.py
@@ -29,7 +29,7 @@ version = "1.9.0"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc, grpcgcp] >= 1.4.1, < 2.0.0dev",
+    "google-api-core[grpc, grpcgcp] >= 1.14.0, < 2.0.0dev",
     "google-cloud-core >= 1.0.0, < 2.0dev",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
 ]
