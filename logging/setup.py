2d0c0948dfb37afcea876bdb38de7f11d8f2c941
Tres Seaver <tseaver@palladion.com>
2019-07-19 13:31:47 -0400

    Bump minimum version for google-api-core to 1.14.0. (#8709)

--- logging/setup.py
+++ logging/setup.py
@@ -29,7 +29,7 @@ version = '1.11.0'
 # 'Development Status :: 5 - Production/Stable'
 release_status = 'Development Status :: 5 - Production/Stable'
 dependencies = [
-    'google-api-core[grpc] >= 1.6.0, < 2.0.0dev',
+    'google-api-core[grpc] >= 1.14.0, < 2.0.0dev',
     "google-cloud-core >= 1.0.0, < 2.0dev",
 ]
 extras = {
