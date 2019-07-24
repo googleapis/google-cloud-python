e10e552b463c1a8b8dc9eaee76847c56e99dbfa2
Bu Sun Kim <busunkim@google.com>
2019-07-23 17:40:01 -0700

    Use translation.googleapis.com as default endpoint.

--- translate/tests/unit/test__http.py
+++ translate/tests/unit/test__http.py
@@ -30,7 +30,7 @@ class TestConnection(unittest.TestCase):
     def test_build_api_url_no_extra_query_params(self):
         conn = self._make_one(object())
         URI = "/".join(
-            [conn.API_BASE_URL, "language", "translate", conn.API_VERSION, "foo"]
+            [conn.DEFAULT_API_ENDPOINT, "language", "translate", conn.API_VERSION, "foo"]
         )
         self.assertEqual(conn.build_api_url("/foo"), URI)
 
