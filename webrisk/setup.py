e7882d2321a410387dda5ffb09611b11fdd0d74b
Bu Sun Kim <busunkim@google.com>
2019-07-24 17:56:16 -0700

    Release 0.2.0

--- webrisk/setup.py
+++ webrisk/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = 'google-cloud-webrisk'
 description = 'Google Cloud Web Risk API client library'
-version = '0.1.0'
+version = '0.2.0'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
