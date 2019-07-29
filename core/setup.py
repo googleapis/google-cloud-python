b3acf87e2ddb9a9406198c2627edd67b78c70dc1
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-07-29 10:17:37 -0700

    Release core 1.0.3 (#8800)

--- core/setup.py
+++ core/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = "google-cloud-core"
 description = "Google Cloud API client core library"
-version = "1.0.2"
+version = "1.0.3"
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
