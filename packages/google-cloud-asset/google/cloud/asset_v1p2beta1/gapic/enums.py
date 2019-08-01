53b4b4ffb637a6b45b54b23c5fead7d558a854c9
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-08-01 14:23:59 -0700

    Generate asset v1p2beta1. (#8888)

---
+++ packages/google-cloud-asset/google/cloud/asset_v1p2beta1/gapic/enums.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Wrappers for protocol buffer enum types."""
+
+import enum
+
+
+class ContentType(enum.IntEnum):
+    """
+    Asset content type.
+
+    Attributes:
+      CONTENT_TYPE_UNSPECIFIED (int): Unspecified content type.
+      RESOURCE (int): Resource metadata.
+      IAM_POLICY (int): The actual IAM policy set on a resource.
+      IAM_POLICY_NAME (int): The IAM policy name for the IAM policy set on a resource.
+      ORG_POLICY (int)
+      ACCESS_POLICY (int): The Cloud Access context mananger Policy set on an asset.
+    """
+
+    CONTENT_TYPE_UNSPECIFIED = 0
+    RESOURCE = 1
+    IAM_POLICY = 2
+    IAM_POLICY_NAME = 3
+    ORG_POLICY = 4
+    ACCESS_POLICY = 5
+
+
+class NullValue(enum.IntEnum):
+    """
+    ``NullValue`` is a singleton enumeration to represent the null value for
+    the ``Value`` type union.
+
+    The JSON representation for ``NullValue`` is JSON ``null``.
+
+    Attributes:
+      NULL_VALUE (int): Null value.
+    """
+
+    NULL_VALUE = 0
