0f77db7fc8ea854a84868779aa386ce310ec8add
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-08-01 14:23:59 -0700

    Generate asset v1p2beta1. (#8888)

--- asset/synth.py
+++ asset/synth.py
@@ -19,7 +19,7 @@ from synthtool import gcp
 
 gapic = gcp.GAPICGenerator()
 common = gcp.CommonTemplates()
-versions = ["v1beta1", "v1"]
+versions = ["v1beta1", "v1p2beta1", "v1"]
 
 excludes = ["setup.py", "nox*.py", "README.rst", "docs/conf.py", "docs/index.rst"]
 
