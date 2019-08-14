8de1289d5996f7ba3fbaa183ad61f00f77c60242
Lars Wander <lwander@users.noreply.github.com>
2019-08-14 19:35:03 +0200

    AutoML: Add a TablesClient for automl-tables specific behavior. (#8720)
    * Checking in staged client helper code
    
    Additional test, docs & proposed cleanup needs to happen on top of this.
    
    * update create_model to allow user to specify included or excluded col… (#16)
    
    * update create_model to allow user to specify included or excluded columns
    
    * made minor changes stylistically and with added ValueError outputs
    
    * Update doc gen & module structure. Add unit & system tests
    
    * added two new func: set time, get table address (#23)
    
    * added two new func: set time, get table address
    
    * changed indentation
    
    * Add system tests
    
    * Address linter & python2.7 import errors
    
    * Passes **kwargs through to client & implements missing methods
    
    * Support BQ as input/output in batch_predict
    
    * Address first round of feedback
    
    * Switch to pytest.raises, fix .rst formatting exception
    
    * Make list system tests more stringent

--- automl/synth.py
+++ automl/synth.py
@@ -33,6 +33,22 @@ for version in versions:
     s.move(library / f"tests/unit/gapic/{version}")
     s.move(library / f"docs/gapic/{version}")
 
+    s.replace(
+        f"google/cloud/automl_{version}/__init__.py",
+        f"from google.cloud.automl_v1beta1.gapic import prediction_service_client",
+        f"from google.cloud.automl_v1beta1.gapic import prediction_service_client"
+        f"from google.cloud.automl_v1beta1.tables import tables_client"
+        f"\n\n"
+        f"class TablesClient(tables_client.TablesClient):"
+        f"    __doc__ = tables_client.TablesClient.__doc__"
+    )
+
+    s.replace(
+        f"google/cloud/automl_{version}/__init__.py",
+        f"__all__ = (\"enums\", \"types\", \"AutoMlClient\", \"PredictionServiceClient\")",
+        f"__all__ = (\"enums\", \"types\", \"AutoMlClient\", \"PredictionServiceClient\", \"TablesClient\")"
+    )
+
 s.move(library / f"docs/conf.py")
 
 # Use the highest version library to generate import alias.
