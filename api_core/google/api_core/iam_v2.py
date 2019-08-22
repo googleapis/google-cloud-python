8d9f7e31f0ed2450e93d6f5ee06cf2023c5bcc27
Jonathan Lui <jonathanlui@google.com>
2019-08-22 14:53:46 -0700

    add set_iam_policy_v2 and get_iam_policy_v2 methods

--- api_core/google/api_core/iam_v2.py
+++ api_core/google/api_core/iam_v2.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Non-API-specific IAM policy v3 definitions
+"""Non-API-specific IAM policy v2 definitions
 
 For allowed roles / permissions, see:
 https://cloud.google.com/iam/docs/understanding-roles
