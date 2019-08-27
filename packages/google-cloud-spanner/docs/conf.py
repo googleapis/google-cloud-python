7c34214f3f554c2e9c0aae1c93a257a3a1d96ac8
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-08-27 16:35:22 -0700

    Docs: Remove CI for gh-pages, use googleapis.dev for api_core refs. (#9085)

--- packages/google-cloud-spanner/docs/conf.py
+++ packages/google-cloud-spanner/docs/conf.py
@@ -337,10 +337,7 @@ intersphinx_mapping = {
     "gax": ("https://gax-python.readthedocs.org/en/latest/", None),
     "google-auth": ("https://google-auth.readthedocs.io/en/stable", None),
     "google-gax": ("https://gax-python.readthedocs.io/en/latest/", None),
-    "google.api_core": (
-        "https://googleapis.github.io/google-cloud-python/latest",
-        None,
-    ),
+    "google.api_core": ("https://googleapis.dev/python/google-api-core/latest", None),
     "grpc": ("https://grpc.io/grpc/python/", None),
     "requests": ("https://2.python-requests.org/en/master/", None),
     "fastavro": ("https://fastavro.readthedocs.io/en/stable/", None),
