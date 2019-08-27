6a50f8e29a1a636b9a65076aa8e668071c66ddc1
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-08-27 10:27:42 -0700

    Release error_reporting 0.32.1 (#9094)

--- packages/google-cloud-error-reporting/setup.py
+++ packages/google-cloud-error-reporting/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = "google-cloud-error-reporting"
 description = "Stackdriver Error Reporting API client library"
-version = "0.32.0"
+version = "0.32.1"
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
