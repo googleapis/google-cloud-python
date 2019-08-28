36e717902d19c81a38a76bc3efdfea50b456a75d
Bamboo Le <8941262+TrucHLe@users.noreply.github.com>
2019-08-28 13:49:41 -0400

    Enable users to pass in Pandas Dataframe when calling import_data() and batch_predict() from AutoML Tables client (#9116)

--- automl/noxfile.py
+++ automl/noxfile.py
@@ -70,6 +70,7 @@ def default(session):
     for local_dep in LOCAL_DEPS:
         session.install("-e", local_dep)
     session.install("-e", ".")
+    session.install("-e", ".[pandas,storage]")
 
     # Run py.test against the unit tests.
     session.run(
@@ -117,6 +118,7 @@ def system(session):
         session.install("-e", local_dep)
     session.install("-e", "../test_utils/")
     session.install("-e", ".")
+    session.install("-e", ".[pandas,storage]")
 
     # Run py.test against the system tests.
     if system_test_exists:
