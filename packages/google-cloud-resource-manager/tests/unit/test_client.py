9a29d2f25fbb482ff3d432618616f63a9c29f9b1
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-08-28 09:33:58 -0700

    Add client_options. (#9043)

--- packages/google-cloud-resource-manager/tests/unit/test_client.py
+++ packages/google-cloud-resource-manager/tests/unit/test_client.py
@@ -60,6 +60,41 @@ class TestClient(unittest.TestCase):
         self.assertIs(client._http_internal, http)
         self.assertIs(client._connection._client_info, client_info)
 
+    def test_ctor_w_empty_client_options(self):
+        from google.api_core.client_options import ClientOptions
+
+        http = object()
+        client_options = ClientOptions()
+        client = self._make_one(_http=http, client_options=client_options)
+        self.assertEqual(
+            client._connection.API_BASE_URL, client._connection.DEFAULT_API_ENDPOINT
+        )
+
+    def test_ctor_w_client_options_object(self):
+        from google.api_core.client_options import ClientOptions
+
+        http = object()
+        client_options = ClientOptions(
+            api_endpoint="https://foo-cloudresourcemanager.googleapis.com"
+        )
+        client = self._make_one(_http=http, client_options=client_options)
+        self.assertEqual(
+            client._connection.API_BASE_URL,
+            "https://foo-cloudresourcemanager.googleapis.com",
+        )
+
+    def test_ctor_w_client_options_dict(self):
+        http = object()
+        client_options = {
+            "api_endpoint": "https://foo-cloudresourcemanager.googleapis.com"
+        }
+
+        client = self._make_one(_http=http, client_options=client_options)
+        self.assertEqual(
+            client._connection.API_BASE_URL,
+            "https://foo-cloudresourcemanager.googleapis.com",
+        )
+
     def test_new_project_factory(self):
         from google.cloud.resource_manager.project import Project
 
