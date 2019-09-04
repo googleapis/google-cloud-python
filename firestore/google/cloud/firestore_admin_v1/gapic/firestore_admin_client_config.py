54281a30005ec9bdbb04b5b4ae5bd00c7af5aebb
yoshi-automation <yoshi-automation@google.com>
2019-09-04 05:19:38 -0700

    [CHANGE ME] Re-generated firestore to pick up changes in the API or client library generator.

--- firestore/google/cloud/firestore_admin_v1/gapic/firestore_admin_client_config.py
+++ firestore/google/cloud/firestore_admin_v1/gapic/firestore_admin_client_config.py
@@ -2,7 +2,7 @@ config = {
     "interfaces": {
         "google.firestore.admin.v1.FirestoreAdmin": {
             "retry_codes": {
-                "idempotent": ["DEADLINE_EXCEEDED", "UNAVAILABLE"],
+                "idempotent": ["DEADLINE_EXCEEDED", "INTERNAL", "UNAVAILABLE"],
                 "non_idempotent": [],
             },
             "retry_params": {
@@ -10,9 +10,9 @@ config = {
                     "initial_retry_delay_millis": 100,
                     "retry_delay_multiplier": 1.3,
                     "max_retry_delay_millis": 60000,
-                    "initial_rpc_timeout_millis": 20000,
+                    "initial_rpc_timeout_millis": 60000,
                     "rpc_timeout_multiplier": 1.0,
-                    "max_rpc_timeout_millis": 20000,
+                    "max_rpc_timeout_millis": 60000,
                     "total_timeout_millis": 600000,
                 }
             },
