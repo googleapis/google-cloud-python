98ff8094125b834fd27a4a82c0a8816a4abc5fa0
Christopher Wilcox <crwilcox@google.com>
2019-09-16 13:45:20 -0700

    fix(test): introduce local redis for tests (#191)

--- packages/google-cloud-ndb/.kokoro/build.sh
+++ packages/google-cloud-ndb/.kokoro/build.sh
@@ -40,10 +40,9 @@ export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
 # Setup project id.
 export PROJECT_ID=$(cat "${KOKORO_GFILE_DIR}/project-id.json")
 
-# Some system tests require indexes. Use gcloud to create them.
-gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS --project=$PROJECT_ID
-gcloud --quiet --verbosity=debug datastore indexes create tests/system/index.yaml
-
+# Configure Local Redis to be used
+export REDIS_CACHE_URL=redis://localhost
+redis-server &
 
 # Some system tests require indexes. Use gcloud to create them.
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS --project=$PROJECT_ID
