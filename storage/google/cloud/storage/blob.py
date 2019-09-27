1a54003ab3000e85f1630b50d8eadc6f090b0808
Tres Seaver <tseaver@palladion.com>
2019-09-27 11:26:11 -0400

    Drop reference to 'boto'.

--- storage/google/cloud/storage/blob.py
+++ storage/google/cloud/storage/blob.py
@@ -714,10 +714,6 @@ class Blob(_PropertyMixin):
     def download_as_string(self, client=None, start=None, end=None):
         """Download the contents of this blob as a bytes object.
 
-        .. note::
-            the method name, ``download_as_string`` was chosen for
-            compatibility with the ``boto`` library's API.
-
         If :attr:`user_project` is set on the bucket, bills the API request
         to that project.
 
