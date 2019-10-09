68d97b0f8cb5f937a0d0e2009bf4fab1cfc10336
Music Li <lyzmusic@google.com>
2019-10-08 17:37:40 -0700

    test(translate): reformat vpc-sc test

--- translate/tests/system/test_vpcsc.py
+++ translate/tests/system/test_vpcsc.py
@@ -23,132 +23,135 @@ from google.cloud import translate_v3beta1
 from google.cloud.translate_v3beta1.proto import translation_service_pb2
 from google.longrunning import operations_pb2
 
-IS_INSIDE_VPCSC = 'GOOGLE_CLOUD_TESTS_IN_VPCSC' in os.environ
+IS_INSIDE_VPCSC = "GOOGLE_CLOUD_TESTS_IN_VPCSC" in os.environ
 # If IS_INSIDE_VPCSC is set, these environment variables should also be set
 if IS_INSIDE_VPCSC:
-  PROJECT_INSIDE = os.environ['PROJECT_ID']
-  PROJECT_OUTSIDE = os.environ[
-      'GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_PROJECT']
+    PROJECT_INSIDE = os.environ["PROJECT_ID"]
+    PROJECT_OUTSIDE = os.environ["GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_PROJECT"]
 
 
 class TestVPCServiceControl(object):
+    @classmethod
+    def setup(self):
+        self._client = translate_v3beta1.TranslationServiceClient()
+        self._parent_inside = self._client.location_path(PROJECT_INSIDE, "us-central1")
+        self._parent_outside = self._client.location_path(
+            PROJECT_OUTSIDE, "us-central1"
+        )
 
-  @classmethod
-  def setup(self):
-    self._client = translate_v3beta1.TranslationServiceClient()
-    self._parent_inside = self._client.location_path(PROJECT_INSIDE,
-                                                     'us-central1')
-    self._parent_outside = self._client.location_path(PROJECT_OUTSIDE,
-                                                      'us-central1')
+        def make_glossary_name(project_id):
+            return "projects/{0}/locations/us-central1/glossaries/fake_glossary".format(
+                project_id
+            )
 
-    def make_glossary_name(project_id):
-      return 'projects/{0}/locations/us-central1/glossaries/fake_glossary'.format(
-          project_id)
+        self._glossary_name_inside = make_glossary_name(PROJECT_INSIDE)
+        self._glossary_name_outside = make_glossary_name(PROJECT_OUTSIDE)
 
-    self._glossary_name_inside = make_glossary_name(PROJECT_INSIDE)
-    self._glossary_name_outside = make_glossary_name(PROJECT_OUTSIDE)
+    @staticmethod
+    def _is_rejected(call):
+        try:
+            responses = call()
+            print("responses: ", responses)
+        except exceptions.PermissionDenied as e:
+            print("PermissionDenied Exception: ", e)
+            return e.message == "Request is prohibited by organization's policy"
+        except Exception as e:
+            print("Other Exception: ", e)
+            pass
+        return False
 
-  @staticmethod
-  def _is_rejected(call):
-    try:
-      responses = call()
-      print('responses: ', responses)
-    except exceptions.PermissionDenied as e:
-      print('PermissionDenied Exception: ', e)
-      return e.message == "Request is prohibited by organization's policy"
-    except Exception as e:
-      print('Other Exception: ', e)
-      pass
-    return False
+    @staticmethod
+    def _do_test(delayed_inside, delayed_outside):
+        assert TestVPCServiceControl._is_rejected(delayed_outside)
+        assert not (TestVPCServiceControl._is_rejected(delayed_inside))
 
-  @staticmethod
-  def _do_test(delayed_inside, delayed_outside):
-    assert TestVPCServiceControl._is_rejected(delayed_outside)
-    assert not (TestVPCServiceControl._is_rejected(delayed_inside))
+    @pytest.mark.skipif(
+        not IS_INSIDE_VPCSC,
+        reason="This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True",
+    )
+    def test_create_glossary(self):
+        def make_glossary(project_id):
+            return {
+                "name": "projects/{0}/locations/us-central1/glossaries/fake_glossary".format(
+                    project_id
+                ),
+                "language_codes_set": {"language_codes": ["en", "ja"]},
+                "input_config": {
+                    "gcs_source": {"input_uri": "gs://fake-bucket/fake_glossary.csv"}
+                },
+            }
 
-  @pytest.mark.skipif(
-      not IS_INSIDE_VPCSC,
-      reason='This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True',
-  )
-  def test_create_glossary(self):
+        glossary_inside = make_glossary(PROJECT_INSIDE)
+        delayed_inside = lambda: self._client.create_glossary(
+            self._parent_inside, glossary_inside
+        )
 
-    def make_glossary(project_id):
-      return {
-          'name':
-              'projects/{0}/locations/us-central1/glossaries/fake_glossary'
-              .format(project_id),
-          'language_codes_set': {
-              'language_codes': ['en', 'ja']
-          },
-          'input_config': {
-              'gcs_source': {
-                  'input_uri': 'gs://fake-bucket/fake_glossary.csv'
-              }
-          }
-      }
+        glossary_outside = make_glossary(PROJECT_OUTSIDE)
+        delayed_outside = lambda: self._client.create_glossary(
+            self._parent_outside, glossary_outside
+        )
+        TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
 
-    glossary_inside = make_glossary(PROJECT_INSIDE)
-    delayed_inside = lambda: self._client.create_glossary(
-        self._parent_inside, glossary_inside)
+    @pytest.mark.skipif(
+        not IS_INSIDE_VPCSC,
+        reason="This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True",
+    )
+    def test_list_glossaries(self):
+        # list_glossaries() returns an GRPCIterator instance, and we need to actually iterate through it
+        # by calling _next_page() to get real response.
+        delayed_inside = lambda: self._client.list_glossaries(
+            self._parent_inside
+        )._next_page()
+        delayed_outside = lambda: self._client.list_glossaries(
+            self._parent_outside
+        )._next_page()
+        TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
 
-    glossary_outside = make_glossary(PROJECT_OUTSIDE)
-    delayed_outside = lambda: self._client.create_glossary(
-        self._parent_outside, glossary_outside)
-    TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
+    @pytest.mark.skipif(
+        not IS_INSIDE_VPCSC,
+        reason="This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True",
+    )
+    def test_get_glossary(self):
+        delayed_inside = lambda: self._client.get_glossary(self._glossary_name_inside)
+        delayed_outside = lambda: self._client.get_glossary(self._glossary_name_outside)
+        TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
 
-  @pytest.mark.skipif(
-      not IS_INSIDE_VPCSC,
-      reason='This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True',
-  )
-  def test_list_glossaries(self):
-    # list_glossaries() returns an GRPCIterator instance, and we need to actually iterate through it
-    # by calling _next_page() to get real response.
-    delayed_inside = lambda: self._client.list_glossaries(self._parent_inside
-                                                         )._next_page()
-    delayed_outside = lambda: self._client.list_glossaries(self._parent_outside
-                                                          )._next_page()
-    TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
+    @pytest.mark.skipif(
+        not IS_INSIDE_VPCSC,
+        reason="This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True",
+    )
+    def test_delete_glossary(self):
+        delayed_inside = lambda: self._client.delete_glossary(
+            self._glossary_name_inside
+        )
+        delayed_outside = lambda: self._client.delete_glossary(
+            self._glossary_name_outside
+        )
+        TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
 
-  @pytest.mark.skipif(
-      not IS_INSIDE_VPCSC,
-      reason='This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True',
-  )
-  def test_get_glossary(self):
-    delayed_inside = lambda: self._client.get_glossary(self.
-                                                       _glossary_name_inside)
-    delayed_outside = lambda: self._client.get_glossary(self.
-                                                        _glossary_name_outside)
-    TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
-
-  @pytest.mark.skipif(
-      not IS_INSIDE_VPCSC,
-      reason='This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True',
-  )
-  def test_delete_glossary(self):
-    delayed_inside = lambda: self._client.delete_glossary(self.
-                                                          _glossary_name_inside)
-    delayed_outside = lambda: self._client.delete_glossary(
-        self._glossary_name_outside)
-    TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
-
-  @pytest.mark.skipif(
-      not IS_INSIDE_VPCSC,
-      reason='This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True',
-  )
-  def test_batch_translate_text(self):
-    source_language_code = 'en'
-    target_language_codes = ['es']
-    input_configs = [{'gcs_source': {'input_uri': 'gs://fake-bucket/*'}}]
-    output_config = {
-        'gcs_destination': {
-            'output_uri_prefix': 'gs://fake-bucket/output/'
+    @pytest.mark.skipif(
+        not IS_INSIDE_VPCSC,
+        reason="This test must be run in VPCSC. To enable this test, set the environment variable GOOGLE_CLOUD_TESTS_IN_VPCSC to True",
+    )
+    def test_batch_translate_text(self):
+        source_language_code = "en"
+        target_language_codes = ["es"]
+        input_configs = [{"gcs_source": {"input_uri": "gs://fake-bucket/*"}}]
+        output_config = {
+            "gcs_destination": {"output_uri_prefix": "gs://fake-bucket/output/"}
         }
-    }
-    delayed_inside = lambda: self._client.batch_translate_text(
-        self._parent_inside, source_language_code, target_language_codes,
-        input_configs, output_config)
-    delayed_outside = lambda: self._client.batch_translate_text(
-        self._parent_outside, source_language_code, target_language_codes,
-        input_configs, output_config)
-    TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
-
+        delayed_inside = lambda: self._client.batch_translate_text(
+            self._parent_inside,
+            source_language_code,
+            target_language_codes,
+            input_configs,
+            output_config,
+        )
+        delayed_outside = lambda: self._client.batch_translate_text(
+            self._parent_outside,
+            source_language_code,
+            target_language_codes,
+            input_configs,
+            output_config,
+        )
+        TestVPCServiceControl._do_test(delayed_inside, delayed_outside)
