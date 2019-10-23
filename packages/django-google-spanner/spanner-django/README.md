ffd8e9c3e01580616d11a5e869b3ab7aaadc4ef4
Emmanuel T Odeke <emmanuel@orijtech.com>
2019-10-22 21:10:46 -0700

    django: start code setup

---
+++ packages/django-google-spanner/spanner-django/README.md
@@ -0,0 +1,3 @@
+### spanner-django
+
+A bridge to enable Django applications to use Spanner.
---
+++ packages/django-google-spanner/spanner-django/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+        name='spanner-django',
+        version='0.0.1',
+        author='Google LLC',
+        author_email='cloud-spanner-developers@googlegroups.com',
+        description=('Bridge to enable using Django with Spanner.'),
+        license='Apache 2.0',
+        packages=find_packages(exclude=['tests']),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache 2 License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Topic :: Utilities',
+            'Framework :: Django',
+            'Framework :: Django :: 2.1',
+            'Framework :: Django :: 2.2',
+            'Framework :: Django :: 3.0',
+        ],
+)
