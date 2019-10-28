bc28c2a56279ae9df292517a40acf53fd0f87399
Christopher Wilcox <crwilcox@google.com>
2019-10-28 16:00:31 -0700

    fix: change to public bug

--- storage/google/cloud/storage/_http.py
+++ storage/google/cloud/storage/_http.py
@@ -36,10 +36,10 @@ class Connection(_http.JSONConnection):
         self.API_BASE_URL = api_endpoint
         self._client_info.client_library_version = __version__
         
-        # TODO: When metrics all use gccl, this should be removed b/143493862
+        # TODO: When metrics all use gccl, this should be removed #9552
         if self._client_info.user_agent is None:
             self._client_info.user_agent = ""
-        self._client_info.user_agent += " gcloud-python/{client_library_version} "
+        self._client_info.user_agent += " gcloud-python/{} ".format(__version__)
 
     API_VERSION = "v1"
     """The version of the API, used in building the API call's URL."""
