b4a9475740e5e5b21b728542ae754b9525d5820f
Tres Seaver <tseaver@palladion.com>
2019-11-05 15:13:44 -0500

    fix: bring tests into line with spec.
    See:
    https://docs.google.com/document/d/1qCRRNqzKn7YnssbB1g-RTkJH11S3vmJ97JjsLWo6alA
    
    In particular:
    
    - Ensure 'inside_vpcxc' returns True IFF env var is set, regardless of value.
    - Add 'bucket_outside' test and corresponding skip decorator.
    - Rename 'skip_*' decorators to clarify semantics.
    - Expand reason messages in skip decorators to clarify semantics, explain
      how to enable the skipped test.

--- test_utils/test_utils/vpcsc_config.py
+++ test_utils/test_utils/vpcsc_config.py
@@ -22,23 +22,25 @@ import pytest
 INSIDE_VPCSC_ENVVAR = "GOOGLE_CLOUD_TESTS_IN_VPCSC"
 PROJECT_INSIDE_ENVVAR = "PROJECT_ID"
 PROJECT_OUTSIDE_ENVVAR = "GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_PROJECT"
+BUCKET_OUTSIDE_ENVVVAR = "GOOGLE_CLOUD_TESTS_VPCSC_OUTSIDE_PERIMETER_BUCKET"
 
 
 class VPCSCTestConfig(object):
     """System test utility for VPCSC detection.
-    
+
     See: https://cloud.google.com/vpc-service-controls/docs/
     """
 
     @property
     def inside_vpcsc(self):
-        """Is the test environment inside VPCSC.
+        """Test whether the test environment is configured to run inside VPCSC.
 
         Returns:
-            bool: true if the environment is inside VPCSC, else false.
+            bool:
+                true if the environment is configured to run inside VPCSC,
+                else false.
         """
-        value = os.environ.get(INSIDE_VPCSC_ENVVAR, "true").lower()
-        return value == "true"
+        return INSIDE_VPCSC_ENVVAR in os.environ
 
     @property
     def project_inside(self):
@@ -58,34 +60,58 @@ class VPCSCTestConfig(object):
         """
         return os.environ.get(PROJECT_OUTSIDE_ENVVAR, None)
 
-    def skip_if_running_inside_vpcsc(self, testcase):
+    @property
+    def bucket_outside(self):
+        """GCS bucket for testing inside access.
+
+        Returns:
+            str: bucket ID used for testing inside access; None if undefined.
+        """
+        return os.environ.get(BUCKET_OUTSIDE_ENVVAR, None)
+
+    def skip_if_inside_vpcsc(self, testcase):
         """Test decorator: skip if running inside VPCSC."""
-        reason = "Running inside VPCSC"
+        reason = (
+            "Running inside VPCSC.  "
+            "Set the {} environment variable to enable this test."
+        ).format(INSIDE_VPCSC_ENVVAR)
         skip = pytest.mark.skipif(self.inside_vpcsc, reason=reason)
         return skip(testcase)
 
-    def skip_if_running_outside_vpcsc(self, testcase):
+    def skip_unless_inside_vpcsc(self, testcase):
         """Test decorator: skip if running outside VPCSC."""
-        reason = "Running outside VPCSC"
+        reason = (
+            "Running outside VPCSC.  "
+            "Unset the {} environment variable to enable this test."
+        ).format(INSIDE_VPCSC_ENVVAR)
         skip = pytest.mark.skipif(not self.inside_vpcsc, reason=reason)
         return skip(testcase)
 
-    def skip_if_inside_vpcsc(self, testcase):
+    def skip_unless_inside_project(self, testcase):
         """Test decorator: skip if inside project env var not set."""
-        reason = "Running inside VPCSC"
-        skip = pytest.mark.skipif(self.inside_vpcsc, reason=reason)
-        return skip(testcase)
-
-    def skip_if_no_inside_project(self, testcase):
-        """Test decorator: skip if inside project env var not set."""
-        reason = "Missing envvar: {}".format(PROJECT_INSIDE_ENVVAR)
+        reason = (
+            "Project ID for running inside VPCSC not set.  "
+            "Set the {} environment variable to enable this test."
+        ).format(PROJECT_INSIDE_ENVVAR)
         skip = pytest.mark.skipif(self.project_inside is None, reason=reason)
         return skip(testcase)
 
-    def skip_if_no_outside_project(self, testcase):
+    def skip_unless_outside_project(self, testcase):
         """Test decorator: skip if outside project env var not set."""
-        reason = "Missing envvar: {}".format(PROJECT_OUTSIDE_ENVVAR)
+        reason = (
+            "Project ID for running outside VPCSC not set. "
+            "Set the {} environment variable to enable this test."
+        ).format(PROJECT_OUTSIDE_ENVVAR)
         skip = pytest.mark.skipif(self.project_outside is None, reason=reason)
+        return skip(testcase)
+
+    def skip_unless_outside_bucket(self, testcase):
+        """Test decorator: skip if outside bucket env var not set."""
+        reason = (
+            "Bucket ID for running outside VPCSC not set. "
+            "Set the {} environment variable to enable this test."
+        ).format(BUCKET_OUTSIDE_ENVVAR)
+        skip = pytest.mark.skipif(self.bucket_outside is None, reason=reason)
         return skip(testcase)
 
 
