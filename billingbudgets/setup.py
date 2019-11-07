c054f7720766ef68e3cbbac1b9caf77c73e70643
Bu Sun Kim <busunkim@google.com>
2019-11-07 13:21:41 -0800

    docs: drop 2.7 classifier, symlink README

--- billingbudgets/setup.py
+++ billingbudgets/setup.py
@@ -59,8 +59,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
