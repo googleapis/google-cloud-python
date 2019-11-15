ac5454742f8c404d207a0ec5a7bc6b7c1be64f05
Emmanuel T Odeke <emmanuel@orijtech.com>
2019-11-15 15:39:29 -0800

    django: implement bulk_insert_sql
    Implements the standard bulk_insert_sql method as
    MySQL's Django backend does.
    
    Fixes #54

--- packages/django-google-spanner/spanner/django/operations.py
+++ packages/django-google-spanner/spanner/django/operations.py
@@ -4,3 +4,8 @@ from django.db.backends.base.operations import BaseDatabaseOperations
 class DatabaseOperations(BaseDatabaseOperations):
     def quote_name(self, name):
         return name
+
+    def bulk_insert_sql(self, fields, placeholder_rows):
+        placeholder_rows_sql = (", ".join(row) for row in placeholder_rows)
+        values_sql = ", ".join("(%s)" % sql for sql in placeholder_rows_sql)
+        return "VALUES " + values_sql
