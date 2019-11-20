8a7df02b4b4ebfc77ca57607f531f3f325bc9abe
Tim Graham <timograham@gmail.com>
2019-11-19 20:49:51 -0500

    django: add DatabaseCreation._destroy_test_db()

--- packages/django-google-spanner/spanner/django/creation.py
+++ packages/django-google-spanner/spanner/django/creation.py
@@ -2,4 +2,6 @@ from django.db.backends.base.creation import BaseDatabaseCreation
 
 
 class DatabaseCreation(BaseDatabaseCreation):
-    pass
+    def _destroy_test_db(self, test_database_name, verbosity):
+        # Cloud Spanner doesn't support dropping a database.
+        pass
