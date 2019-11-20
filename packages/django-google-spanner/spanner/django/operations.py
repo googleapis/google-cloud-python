94655785e4915cfb156202ad400768f6079414ca
Tim Graham <timograham@gmail.com>
2019-11-20 18:02:31 -0500

    django: convert DateTimeFields when fetching from the database
    Fixes #72

--- packages/django-google-spanner/spanner/django/operations.py
+++ packages/django-google-spanner/spanner/django/operations.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db.backends.base.operations import BaseDatabaseOperations
 
 
@@ -15,3 +17,18 @@ class DatabaseOperations(BaseDatabaseOperations):
         # need to be implemented somehow to support the normal way of testing
         # in Django: https://github.com/orijtech/spanner-orm/issues/67
         return []
+
+    def get_db_converters(self, expression):
+        converters = super().get_db_converters(expression)
+        internal_type = expression.output_field.get_internal_type()
+        if internal_type == 'DateTimeField':
+            converters.append(self.convert_datetimefield_value)
+        return converters
+
+    def convert_datetimefield_value(self, value, expression, connection):
+        if value is not None:
+            try:
+                value = datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f')
+            except ValueError:  # time data does not match format (no microsecond?)
+                value = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+        return value
