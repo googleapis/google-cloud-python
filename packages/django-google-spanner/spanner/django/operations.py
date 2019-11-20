b5383d7fc8022d2c36dc09c785b007bf39cf119b
Tim Graham <timograham@gmail.com>
2019-11-19 20:50:20 -0500

    django: add DatabaseOperations.sql_flush() stub

--- packages/django-google-spanner/spanner/django/operations.py
+++ packages/django-google-spanner/spanner/django/operations.py
@@ -9,3 +9,9 @@ class DatabaseOperations(BaseDatabaseOperations):
         placeholder_rows_sql = (", ".join(row) for row in placeholder_rows)
         values_sql = ", ".join("(%s)" % sql for sql in placeholder_rows_sql)
         return "VALUES " + values_sql
+
+    def sql_flush(self, style, tables, sequences, allow_cascade=False):
+        # TODO: Cloud Spanner doesn't support TRUNCATE, however, this will
+        # need to be implemented somehow to support the normal way of testing
+        # in Django: https://github.com/orijtech/spanner-orm/issues/67
+        return []
