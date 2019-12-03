563930cac8d5a511ead6e8150e75edabc991c24b
Tim Graham <timograham@gmail.com>
2019-12-03 11:02:55 -0500

    django: allow skipping unsupported tests

--- packages/django-google-spanner/spanner/django/creation.py
+++ packages/django-google-spanner/spanner/django/creation.py
@@ -1,7 +1,30 @@
+import os
+from unittest import skip
+
+from django.conf import settings
 from django.db.backends.base.creation import BaseDatabaseCreation
+from django.utils.module_loading import import_string
 
 
 class DatabaseCreation(BaseDatabaseCreation):
+    def mark_skips(self):
+        """Skip tests that don't work on Spanner."""
+        for test_name in self.connection.features.skip_tests:
+            test_case_name, _, method_name = test_name.rpartition('.')
+            test_app = test_name.split('.')[0]
+            # Importing a test app that isn't installed raises RuntimeError.
+            if test_app in settings.INSTALLED_APPS:
+                test_case = import_string(test_case_name)
+                method = getattr(test_case, method_name)
+                setattr(test_case, method_name, skip('unsupported by Spanner')(method))
+
+    def create_test_db(self, *args, **kwargs):
+        # This environment variable is set by the Travis build script or
+        # by a developer running the tests locally.
+        if os.environ.get('RUNNING_SPANNER_BACKEND_TESTS'):
+            self.mark_skips()
+        super().create_test_db(*args, **kwargs)
+
     def _destroy_test_db(self, test_database_name, verbosity):
         # Cloud Spanner doesn't support dropping a database.
         pass
--- packages/django-google-spanner/spanner/django/features.py
+++ packages/django-google-spanner/spanner/django/features.py
@@ -4,3 +4,11 @@ from django.db.backends.base.features import BaseDatabaseFeatures
 class DatabaseFeatures(BaseDatabaseFeatures):
     supports_foreign_keys = False
     supports_transactions = False
+
+    # Django tests that aren't supported by Spanner.
+    skip_tests = (
+        # No Django transaction management in Spanner.
+        'basic.tests.SelectOnSaveTests.test_select_on_save_lying_update',
+        # spanner.django monkey patches AutoField to have a default value.
+        'basic.tests.ModelTest.test_hash',
+    )
