9aa54ecfa2db730f2b28967375aa29777d7b5e3d
Christopher Wilcox <crwilcox@google.com>
2019-12-26 10:30:38 -0800

    feat: add timeout parameter to Batch interface to match google-cloud-core (#10010)
    * fix: increase minimum version of google-cloud-core after a required field is introduced
    * feat: extend batch interface to have timeout to match new google-cloud-core interface

--- packages/google-cloud-storage/tests/unit/test__http.py
+++ packages/google-cloud-storage/tests/unit/test__http.py
@@ -51,7 +51,11 @@ class TestConnection(unittest.TestCase):
         }
         expected_uri = conn.build_api_url("/rainbow")
         http.request.assert_called_once_with(
-            data=req_data, headers=expected_headers, method="GET", url=expected_uri
+            data=req_data,
+            headers=expected_headers,
+            method="GET",
+            url=expected_uri,
+            timeout=None,
         )
 
     def test_build_api_url_no_extra_query_params(self):
