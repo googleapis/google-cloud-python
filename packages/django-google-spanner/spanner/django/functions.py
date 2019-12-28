13aac508fa0d5ac9665e95c503c46ad611ba8695
Tim Graham <timograham@gmail.com>
2019-12-28 08:50:20 -0500

    django: fix Left, Right, StrIndex, and Substr database functions

--- packages/django-google-spanner/spanner/django/functions.py
+++ packages/django-google-spanner/spanner/django/functions.py
@@ -1,6 +1,8 @@
 import math
 
-from django.db.models.functions import Cot, Degrees, Log, Pi, Radians
+from django.db.models.functions import (
+    Cot, Degrees, Left, Log, Pi, Radians, Right, StrIndex, Substr,
+)
 
 
 def cot(self, compiler, connection, **extra_context):
@@ -13,6 +15,10 @@ def degrees(self, compiler, connection, **extra_context):
         template='((%%(expressions)s) * 180 / %s)' % math.pi,
         **extra_context
     )
+
+
+def left_and_right(self, compiler, connection, **extra_context):
+    return self.get_substr().as_spanner(compiler, connection, **extra_context)
 
 
 def log(self, compiler, connection, **extra_context):
@@ -35,9 +41,21 @@ def radians(self, compiler, connection, **extra_context):
     )
 
 
+def strindex(self, compiler, connection, **extra_context):
+    return self.as_sql(compiler, connection, function='STRPOS', **extra_context)
+
+
+def substr(self, compiler, connection, **extra_context):
+    return self.as_sql(compiler, connection, function='SUBSTR', **extra_context)
+
+
 def register_functions():
     Cot.as_spanner = cot
     Degrees.as_spanner = degrees
+    Left.as_spanner = left_and_right
     Log.as_spanner = log
     Pi.as_spanner = pi
     Radians.as_spanner = radians
+    Right.as_spanner = left_and_right
+    StrIndex.as_spanner = strindex
+    Substr.as_spanner = substr
