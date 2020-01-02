de673650f7bb0a554cf89efaa10be797f7ea511d
Tim Swast <swast@google.com>
2020-01-02 17:16:22 -0600

    lint

--- bigquery/samples/tests/test_load_table_dataframe.py
+++ bigquery/samples/tests/test_load_table_dataframe.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datetime
-
 import pytest
-import pytz
 
 from .. import load_table_dataframe
 
