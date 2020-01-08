2f3145c0b936fe981ef0da87bf266bb8b8d017c5
yoshi-automation <yoshi-automation@google.com>
2020-01-08 05:17:56 -0800

    [CHANGE ME] Re-generated dataproc to pick up changes in the API or client library generator.

--- dataproc/google/cloud/dataproc_v1/__init__.py
+++ dataproc/google/cloud/dataproc_v1/__init__.py
@@ -16,12 +16,23 @@
 
 
 from __future__ import absolute_import
+import sys
+import warnings
 
 from google.cloud.dataproc_v1 import types
 from google.cloud.dataproc_v1.gapic import cluster_controller_client
 from google.cloud.dataproc_v1.gapic import enums
 from google.cloud.dataproc_v1.gapic import job_controller_client
 from google.cloud.dataproc_v1.gapic import workflow_template_service_client
+
+
+if sys.version_info[:2] == (2, 7):
+    message = (
+        "A future version of this library will drop support for Python 2.7."
+        "More details about Python 2 support for Google Cloud Client Libraries"
+        "can be found at https://cloud.google.com/python/docs/python2-sunset/"
+    )
+    warnings.warn(message, DeprecationWarning)
 
 
 class ClusterControllerClient(cluster_controller_client.ClusterControllerClient):
