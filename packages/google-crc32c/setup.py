29d8d9c06f8fdbe18ac7f3becc167ae9c4822611
Christopher Wilcox <crwilcox@google.com>
2020-01-17 20:46:16 -0800

    fix: update setup.py to include 3.8

--- packages/google-crc32c/setup.py
+++ packages/google-crc32c/setup.py
@@ -82,6 +82,7 @@ def main():
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
         ],
         cmdclass={"build_ext": BuildExtWithDLL},
         extras_require={
