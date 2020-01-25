6ebce55fc268bb4f0b222d4c9b6cef4c657cd2ab
Emmanuel T Odeke <emmanuel@orijtech.com>
2020-01-25 10:38:49 +0300

    all: ensure proper license headers
    Reported offline by Vikram and then I wrote
    a script
    https://gist.github.com/odeke-em/b471edcb5c0c43c5d1ad732cf1a6fb56
    to check and report.

--- packages/django-google-spanner/spanner/django/creation.py
+++ packages/django-google-spanner/spanner/django/creation.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2020 Google LLC. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import os
 import sys
 from unittest import skip
--- packages/django-google-spanner/spanner/django/operations.py
+++ packages/django-google-spanner/spanner/django/operations.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2020 Google LLC. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import os
 import re
 from base64 import b64decode
