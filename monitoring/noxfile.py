f2f7dcba917842105240cb40f49a235724aee812
Bu Sun Kim <busunkim@google.com>
2020-01-29 14:25:39 -0800

    test: remove from noxfile

--- monitoring/noxfile.py
+++ monitoring/noxfile.py
@@ -83,7 +83,6 @@ def default(session):
         "--cov-append",
         "--cov-config=.coveragerc",
         "--cov-report=",
-        "--cov-fail-under=97",
         os.path.join("tests", "unit"),
         *session.posargs,
     )
