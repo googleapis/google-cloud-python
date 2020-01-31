f518fc7c6c95facd42380a47e06e79200cf3575a
Leah Cole <coleleah@google.com>
2020-01-31 13:36:16 -0800

    chore: remove translate from monorepo

---
+++ speech/samples/v1/test0.py
@@ -0,0 +1,42 @@
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+
+
+def sample_recognize(storage_uri):
+    """
+    Transcribe short audio file from Cloud Storage using synchronous speech
+    recognition
+
+    Args:
+      storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
+    """
+
+    client = speech_v1.SpeechClient()
+
+    # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
+
+    # Sample rate in Hertz of the audio data sent
+    sample_rate_hertz = 16000
+
+    # The language of the supplied audio
+    language_code = "en-US"
+
+    # Encoding of audio data sent. This sample sets this explicitly.
+    # This field is optional for FLAC and WAV audio formats.
+    encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    config = {
+        "sample_rate_hertz": sample_rate_hertz,
+        "language_code": language_code,
+        "encoding": encoding,
+    }
+    print(storage_uri)
+    if not storage_uri:
+       print("NO STORAGE URI ASSHOLE")
+    audio = {"uri": storage_uri}
+
+    response = client.recognize(config, audio)
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        print(u"Transcript: {}".format(alternative.transcript))
+
--- translate/docs/gapic/v3beta1/api.rst
+++
@@ -1,6 +0,0 @@
-Client for Cloud Translation API
-================================
-
-.. automodule:: google.cloud.translate_v3beta1
-    :members:
-    :inherited-members:
