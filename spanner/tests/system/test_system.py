b4906e6328acd360e68d129cb5de98fa73e31070
larkee <larkee@users.noreply.github.com>
2020-01-31 13:59:59 +1100

    allow emulator configs when creating an instance for system tests

--- spanner/tests/system/test_system.py
+++ spanner/tests/system/test_system.py
@@ -56,6 +56,7 @@ from tests._fixtures import DDL_STATEMENTS
 
 
 CREATE_INSTANCE = os.getenv("GOOGLE_CLOUD_TESTS_CREATE_SPANNER_INSTANCE") is not None
+USE_EMULATOR = os.getenv("SPANNER_EMULATOR_HOST") is not None
 
 if CREATE_INSTANCE:
     INSTANCE_ID = "google-cloud" + unique_resource_id("-")
@@ -102,10 +103,12 @@ def setUpModule():
     EXISTING_INSTANCES[:] = instances
 
     if CREATE_INSTANCE:
-
-        # Defend against back-end returning configs for regions we aren't
-        # actually allowed to use.
-        configs = [config for config in configs if "-us-" in config.name]
+        if USE_EMULATOR:
+            configs = [config for config in configs if "emulator" in config.name]
+        else:
+            # Defend against back-end returning configs for regions we aren't
+            # actually allowed to use.
+            configs = [config for config in configs if "-us-" in config.name]
 
         if not configs:
             raise ValueError("List instance configs failed in module set up.")
