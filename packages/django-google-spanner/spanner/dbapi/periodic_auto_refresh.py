9e2a326fcab612a0087d85f039bf92552f32de88
Emmanuel T Odeke <emmanuel@orijtech.com>
2020-02-07 18:32:59 -0800

    dbapi: revert to event-oriented auto-refresher instead of mutex
    The mutex approach apparently has a gaping hole
    that allows idle transactions as seen by quickly
    failing tests.
    
    For now slower shall suffice as correctness matters
    until we have a proper and fast implementation for the
    auto refreshing Transaction.

--- packages/django-google-spanner/spanner/dbapi/periodic_auto_refresh.py
+++ packages/django-google-spanner/spanner/dbapi/periodic_auto_refresh.py
@@ -9,7 +9,6 @@ The code in this file provides a drop-in replacement for spanner_v1.Transaction,
 but one that auto-refreshes every 8.5 seconds, to deal with Cloud Spanner's server's
 max idle time of 10 seconds, per:
     https://cloud.google.com/spanner/docs/reference/rest/v1/TransactionOptions#idle-transactions
-
 It handles concurrency concerns by using an event loop, a queue and callbacks. If the queue
 is empty for 8.5 seconds, it'll ping Cloud Spanner by sending the recommended:
     execute_sql('SELECT 1')
@@ -22,8 +21,6 @@ import time
 
 
 class PeriodicAutoRefresher:
-    TICK = 'tick'
-
     def __init__(self, period_secs=10, ping_fn=None):
         self.__period_secs = period_secs
         self.__done = threading.Event()
@@ -35,6 +32,25 @@ class PeriodicAutoRefresher:
         pth.start()
         self.__pth = pth
 
+    def __event_loop(self):
+        while True:
+            if not self.__still_running():
+                return
+
+            try:
+                callback, fn, args, kwargs = self.__Q.get(block=True, timeout=self.__period_secs)
+                res, exc = None, None
+
+                try:
+                    res = fn(*args, **kwargs)
+                except Exception as e:
+                    exc = e
+                finally:
+                    callback(res, exc)
+            except queue.Empty:
+                if self.__still_running():
+                    self.__ping_fn()
+
     def __still_running(self):
         return not self.__done.is_set()
 
@@ -42,8 +58,8 @@ class PeriodicAutoRefresher:
         self.__done.set()
         self.__pth.join()
 
-    def record_tick(self):
-        self.__Q.put_nowait(self.TICK)
+    def run_op(self, callback, fn, *args, **kwargs):
+        self.__Q.put_nowait((callback, fn, args, kwargs))
 
 
 class PeriodicAutoRefreshingTransaction:
@@ -51,82 +67,63 @@ class PeriodicAutoRefreshingTransaction:
     PeriodicAutoRefreshingTransaction is the drop-in replacement for spanner_v1.Transaction
     but with a max-idle duration of 8.5 seconds, since the last use time of the underlying
     Transaction, else we'll perform a ping to Cloud Spanner with 'SELECT 1'.
-
     It becomes active after .begin() has been invoked.
     """
 
     def __init__(self, txn):
         self.__txn = txn
-        self.__running = False
-        self.__lock = threading.Lock()
-        self.__period_secs = 8.5
 
     def begin(self):
         res = self.__txn.begin()
-        self.__running = True
-        self.__start_time = time.time()
-        self.__last_active_time = 0
-
-        pth = threading.Thread(target=self.__event_loop, name='periodic-auto-refresh')
-        pth.start()
-        self.__pth = pth
-
+        self.__par = PeriodicAutoRefresher(period_secs=8.5, ping_fn=self.__ping)
         return res
 
-    def __event_loop(self):
-        while True:
-            time.sleep(self.__period_secs)
-
-            self.__lock.acquire()
-            running = self.__running
-            diff_secs = time.time() - self.__last_active_time
-            if not running:
-                self.__lock.release()
-                return
-
-            if diff_secs >= self.__period_secs:
-                self.__ping_locked()
-            self.__lock.release()
-
-    def __ping_locked(self):
-        print('Pinging Cloud Spanner at %s' % time.time() - self.__start_time)
+    def __ping(self):
+        print('Pinging Cloud Spanner at %s' % time.time())
         res = self.__txn.execute_sql('SELECT 1')
         if res:
             for it in res:
                 _ = it
 
     def execute_sql(self, *args, **kwargs):
-        self.__record_last_active_time()
-        return self.__txn.execute_sql(*args, **kwargs)
+        return self.__on_event_queue(self.__txn.execute_sql, *args, **kwargs)
 
     def execute_update(self, *args, **kwargs):
-        self.__record_last_active_time()
-        return self.__txn.execute_update(*args, **kwargs)
+        return self.__on_event_queue(self.__txn.execute_update, *args, **kwargs)
 
     def commit(self):
-        self.__stop()
-        return self.__txn.commit()
+        res = self.__on_event_queue(self.__txn.commit)
+        self.__par.stop()
+        return res
 
     def rollback(self):
-        self.__stop()
-        return self.__txn.rollback()
-
-    def __stop(self):
-        self.__lock.acquire()
-        self.__running = False
-        self.__lock.release()
-        self.__pth.join()
-
-    def __record_last_active_time(self):
-        self.__lock.acquire()
-        self.__last_active_time = time.time()
-        self.__lock.release()
+        res = self.__on_event_queue(self.__txn.rollback)
+        self.__par.stop()
+        return res
 
     def was_committed_or_rolledback(self):
         # For now it is alright to access Transaction._rolled_back
         # even though it is unexported. We've filed a follow-up issue:
         #   https://github.com/googleapis/python-spanner/issues/13
-        self.__lock.acquire()
-        ok = self.__txn.committed or self.__txn._rolled_back
-        self.__lock.release()
-        return ok
+        return self.__on_event_queue(lambda txn: txn.committed or txn._rolled_back, self.__txn)
+
+    def __on_event_queue(self, fn, *args, **kwargs):
+        ready = threading.Event()
+        res_exc = {}
+
+        # Using a lambda here because a defined closure would have scope/visibility
+        # problems trying to set res_exc, even if we used 'global res_exc'. A lambda solves
+        # the issue due to different scoping.
+        # We have to propagate the underlying results and exceptions from
+        # the asynchronously running callback, converting it to a synchronous call.
+        callback = lambda in_res, in_exc: (res_exc.setdefault('res', in_res), res_exc.setdefault('exc', in_exc), ready.set())  # noqa
+
+        self.__par.run_op(callback, fn, *args, **kwargs)
+        ready.wait()
+
+        res, exc = res_exc['res'], res_exc['exc']
+
+        if exc:
+            raise exc
+
+        return res
