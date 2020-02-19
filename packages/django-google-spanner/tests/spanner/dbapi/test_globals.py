f5363a10f1919e59d6bc932ca334ffd331e6c834
Emmanuel T Odeke <emmanuel@orijtech.com>
2020-02-19 11:42:33 -0800

    dbapi: update threadsafety to 1
    With this setting, threads may share the module, but
    not connections. This is gradual upgrade towards
    one day achieving the highest thread safety, if possible.

--- packages/django-google-spanner/tests/spanner/dbapi/test_globals.py
+++ packages/django-google-spanner/tests/spanner/dbapi/test_globals.py
@@ -13,4 +13,4 @@ class DBAPIGlobalsTests(TestCase):
     def test_apilevel(self):
         self.assertEqual(apilevel, '2.0', 'We implement PEP-0249 version 2.0')
         self.assertEqual(paramstyle, 'format', 'Cloud Spanner uses @param')
-        self.assertEqual(threadsafety, 0, 'Downgraded to the least concurrency')
+        self.assertEqual(threadsafety, 1, 'Threads may share module but not connections')
