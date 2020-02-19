c406e648d61f7bfb63c8f29bd472b201b177dc83
larkee <31196561+larkee@users.noreply.github.com>
2020-02-19 16:06:14 +1300

    fix: remove erroneous timeouts for batch_create_session calls (#18)
    * fix: remove erroneous timeouts for batch_create_session calls in session pools
    
    * blacken

--- packages/google-cloud-spanner/google/cloud/spanner_v1/pool.py
+++ packages/google-cloud-spanner/google/cloud/spanner_v1/pool.py
@@ -171,10 +171,7 @@ class FixedSizePool(AbstractSessionPool):
 
         while not self._sessions.full():
             resp = api.batch_create_sessions(
-                database.name,
-                self.size - self._sessions.qsize(),
-                timeout=self.default_timeout,
-                metadata=metadata,
+                database.name, self.size - self._sessions.qsize(), metadata=metadata
             )
             for session_pb in resp.session:
                 session = self._new_session()
@@ -365,10 +362,7 @@ class PingingPool(AbstractSessionPool):
 
         while created_session_count < self.size:
             resp = api.batch_create_sessions(
-                database.name,
-                self.size - created_session_count,
-                timeout=self.default_timeout,
-                metadata=metadata,
+                database.name, self.size - created_session_count, metadata=metadata
             )
             for session_pb in resp.session:
                 session = self._new_session()
