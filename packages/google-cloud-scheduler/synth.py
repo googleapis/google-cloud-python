090237633aae015dfca63f4c590b9f1fc6204027
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2020-02-19 17:46:32 -0800

    chore: add split repo templates (#2)
    * chore: add split repo templates
    
    * test: increase coverage values

--- packages/google-cloud-scheduler/synth.py
+++ packages/google-cloud-scheduler/synth.py
@@ -49,7 +49,7 @@ for version in versions:
 # ----------------------------------------------------------------------------
 # Add templated files
 # ----------------------------------------------------------------------------
-templated_files = common.py_library(unit_cov_level=79, cov_level=80)
+templated_files = common.py_library(cov_level=86)
 s.move(templated_files)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
