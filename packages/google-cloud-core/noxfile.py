b6f9c3349b6d74d784d02f23e3ba1c191a787dc5
Tres Seaver <tseaver@palladion.com>
2020-02-25 15:21:13 -0500

    test: drop majyk per-lang-version coverage level (#6)
    Closes #5.

--- packages/google-cloud-core/noxfile.py
+++ packages/google-cloud-core/noxfile.py
@@ -41,7 +41,7 @@ def default(session):
         "--cov-append",
         "--cov-config=.coveragerc",
         "--cov-report=",
-        "--cov-fail-under=97",
+        "--cov-fail-under=0",
         os.path.join("tests", "unit"),
         *session.posargs
     )
