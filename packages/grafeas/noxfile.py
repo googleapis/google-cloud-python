c6a037293dc5ceb43ea33ae5a0585d5961339bc3
Peter Lamut <plamut@users.noreply.github.com>
2020-03-03 01:06:27 +0000

    test: drop per-language-version coverage levels (#10)

--- packages/grafeas/noxfile.py
+++ packages/grafeas/noxfile.py
@@ -79,7 +79,7 @@ def default(session):
         "--cov-append",
         "--cov-config=.coveragerc",
         "--cov-report=",
-        "--cov-fail-under=78",
+        "--cov-fail-under=0",
         os.path.join("tests", "unit"),
         *session.posargs,
     )
