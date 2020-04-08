796759efa5196d2100d7af81c121a83960a24cf5
larkee <31196561+larkee@users.noreply.github.com>
2020-04-08 18:34:32 +1200

    fix: Pin Sphnix version to last working release (#51)
    Co-authored-by: larkee <larkee@users.noreply.github.com>

--- packages/google-cloud-spanner/noxfile.py
+++ packages/google-cloud-spanner/noxfile.py
@@ -143,7 +143,7 @@ def docs(session):
     """Build the docs for this library."""
 
     session.install("-e", ".")
-    session.install("sphinx", "alabaster", "recommonmark")
+    session.install("sphinx==2.4.4", "alabaster", "recommonmark")
 
     shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
