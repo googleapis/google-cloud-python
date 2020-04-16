e95dbd9f5ab05aa994dcdfa90f55d3cb3ab1e6d0
Peter Lamut <plamut@users.noreply.github.com>
2020-04-16 19:18:05 +0200

    fix: restrict api-core dependency to < 1.17.0 (#76)
    Version 1.17.0 causes problems on re-establishing streams on retryable
    errors, thus restricting it.

--- packages/google-cloud-pubsub/setup.py
+++ packages/google-cloud-pubsub/setup.py
@@ -29,7 +29,10 @@ version = "1.4.2"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
+    # google-api-core[grpc] 1.17.0 causes problems, thus restricting its
+    # version until the issue gets fixed.
+    # https://github.com/googleapis/python-pubsub/issues/74
+    "google-api-core[grpc] >= 1.14.0, < 1.17.0",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
     'enum34; python_version < "3.4"',
 ]
