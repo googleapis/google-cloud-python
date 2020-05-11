d4b9dafc35835bd955616e6a94c67eade24e0f9b
arithmetic1728 <58957152+arithmetic1728@users.noreply.github.com>
2020-05-11 14:22:32 -0700

    fix: catch exceptions.RefreshError (#508)

--- packages/google-auth/google/oauth2/id_token.py
+++ packages/google-auth/google/oauth2/id_token.py
@@ -212,7 +212,7 @@ def fetch_id_token(request, audience):
         )
         credentials.refresh(request)
         return credentials.token
-    except (ImportError, exceptions.TransportError):
+    except (ImportError, exceptions.TransportError, exceptions.RefreshError):
         pass
 
     # 2. Try to use service account credentials to get ID token.
