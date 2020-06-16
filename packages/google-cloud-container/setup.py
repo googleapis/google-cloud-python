3fa9e17efe7ff5317d9f7879577de31ed0063f26
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2020-06-16 16:54:08 -0700

    fix: fix `release_status` in `setup.py` (#27)

--- packages/google-cloud-container/setup.py
+++ packages/google-cloud-container/setup.py
@@ -27,7 +27,7 @@ version = "1.0.0"
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
 # 'Development Status :: 5 - Production/Stable'
-release_status = "Development Status :: 5 - Production/stable"
+release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
     "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
