00e3f9bed860b493e6e405fef407b570a4d730b1
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2020-06-26 07:33:54 -0700

    chore: use GOOGLE_CLOUD_PROJECT and update reads/filters region tags (#60)
    * chore: use GOOGLE_CLOUD_PROJECT and update region tags
    
    * chore: add newlines to appease lint
    
    * fix: remove dulicate env var

--- packages/google-cloud-bigtable/samples/metricscaler/metricscaler_test.py
+++ packages/google-cloud-bigtable/samples/metricscaler/metricscaler_test.py
@@ -21,6 +21,7 @@ import uuid
 from google.cloud import bigtable
 from google.cloud.bigtable import enums
 from mock import patch
+
 import pytest
 
 from metricscaler import get_cpu_load
@@ -28,7 +29,8 @@ from metricscaler import get_storage_utilization
 from metricscaler import main
 from metricscaler import scale_bigtable
 
-PROJECT = os.environ['GCLOUD_PROJECT']
+
+PROJECT = os.environ['GOOGLE_CLOUD_PROJECT']
 BIGTABLE_ZONE = os.environ['BIGTABLE_ZONE']
 SIZE_CHANGE_STEP = 3
 INSTANCE_ID_FORMAT = 'metric-scale-test-{}'
