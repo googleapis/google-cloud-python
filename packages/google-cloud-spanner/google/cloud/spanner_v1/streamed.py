67364851765ce6407022ce4f48685d10771b1489
Gurov Ilya <ilya.faer@mail.ru>
2020-07-14 13:49:22 +0300

    refactor: remove unused _count attribute from StreamedResultSet class (#113)

--- packages/google-cloud-spanner/google/cloud/spanner_v1/streamed.py
+++ packages/google-cloud-spanner/google/cloud/spanner_v1/streamed.py
@@ -42,7 +42,6 @@ class StreamedResultSet(object):
     def __init__(self, response_iterator, source=None):
         self._response_iterator = response_iterator
         self._rows = []  # Fully-processed rows
-        self._counter = 0  # Counter for processed responses
         self._metadata = None  # Until set from first PRS
         self._stats = None  # Until set from last PRS
         self._current_row = []  # Accumulated values for incomplete row
@@ -114,7 +113,6 @@ class StreamedResultSet(object):
         Parse the result set into new/existing rows in :attr:`_rows`
         """
         response = six.next(self._response_iterator)
-        self._counter += 1
 
         if self._metadata is None:  # first response
             metadata = self._metadata = response.metadata
