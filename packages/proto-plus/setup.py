12ba7d12bb994cbc58e64d85540bbcf215124f7b
Dov Shlachter <dovs@google.com>
2020-07-16 13:35:45 -0700

    chore: bump version to 1.2.0 (#44)

--- packages/proto-plus/setup.py
+++ packages/proto-plus/setup.py
@@ -25,7 +25,7 @@ with io.open(os.path.join(PACKAGE_ROOT, "README.rst")) as file_obj:
 
 setup(
     name="proto-plus",
-    version="1.1.0",
+    version="1.2.0",
     license="Apache 2.0",
     author="Dov Shlachter",
     author_email="dovs@google.com",
