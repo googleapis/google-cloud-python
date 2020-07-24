40dd7f852d4e0691169579c65e942d79cf29ce2b
Tres Seaver <tseaver@palladion.com>
2020-07-24 13:18:11 -0400

    chore(packaging): prep for grmp-1.0.0 release (#189)
    Fixes #188

--- packages/google-cloud-bigquery/setup.py
+++ packages/google-cloud-bigquery/setup.py
@@ -32,7 +32,7 @@ dependencies = [
     'enum34; python_version < "3.4"',
     "google-api-core >= 1.21.0, < 2.0dev",
     "google-cloud-core >= 1.1.0, < 2.0dev",
-    "google-resumable-media >= 0.5.0, < 0.6dev",
+    "google-resumable-media >= 0.5.0, < 2.0dev",
     "six >=1.13.0,< 2.0.0dev",
 ]
 extras = {
