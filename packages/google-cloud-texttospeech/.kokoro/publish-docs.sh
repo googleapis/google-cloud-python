92b5f87fadbabdd7d48da6a676bac7d5f9b638b6
Takashi Matsuo <tmatsuo@google.com>
2020-07-28 13:11:29 -0700

    docs: finishing touches for the cloudsite docs pipeline (#71)
    * add a new envvar for cloudsite docs pipeline
    * modify sys.path for the new plugin

--- packages/google-cloud-texttospeech/.kokoro/publish-docs.sh
+++ packages/google-cloud-texttospeech/.kokoro/publish-docs.sh
@@ -62,4 +62,4 @@ python3 -m docuploader create-metadata \
 cat docs.metadata
 
 # upload docs
-python3 -m docuploader upload docs/_build/html/docfx_yaml --metadata-file docs.metadata --destination-prefix docfx --staging-bucket "${STAGING_BUCKET}"
+python3 -m docuploader upload docs/_build/html/docfx_yaml --metadata-file docs.metadata --destination-prefix docfx --staging-bucket "${V2_STAGING_BUCKET}"
