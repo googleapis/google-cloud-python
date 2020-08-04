f6b6ffb177725b1b0041d1526cd7765f33da1255
Takashi Matsuo <tmatsuo@google.com>
2020-08-04 16:34:04 -0700

    docs: update docs build (via synth) (#155)
    * fix(docs-presubmit): changes needed for docs-presubmit build
    
    * docs: update docs build (via synth)
    
    By manually running synthtool
    
    * add docfx nox session

--- packages/google-resumable-media/noxfile.py
+++ packages/google-resumable-media/noxfile.py
@@ -71,6 +71,38 @@ def docs(session):
         os.path.join("docs", "_build", "html", ""),
     )
 
+@nox.session(python="3.8")
+def docfx(session):
+    """Build the docfx yaml files for this library."""
+
+    session.install("-e", ".")
+    session.install("sphinx", "alabaster", "recommonmark", "sphinx-docfx-yaml")
+
+    shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
+    session.run(
+        "sphinx-build",
+        "-T",  # show full traceback on exception
+        "-N",  # no colors
+        "-D",
+        (
+            "extensions=sphinx.ext.autodoc,"
+            "sphinx.ext.autosummary,"
+            "docfx_yaml.extension,"
+            "sphinx.ext.intersphinx,"
+            "sphinx.ext.coverage,"
+            "sphinx.ext.napoleon,"
+            "sphinx.ext.todo,"
+            "sphinx.ext.viewcode,"
+            "recommonmark"
+        ),
+        "-b",
+        "html",
+        "-d",
+        os.path.join("docs", "_build", "doctrees", ""),
+        os.path.join("docs", ""),
+        os.path.join("docs", "_build", "html", ""),
+    )
+
 
 @nox.session(python='3.8')
 def doctest(session):
--- packages/google-resumable-media/synth.py
+++ packages/google-resumable-media/synth.py
@@ -8,6 +8,7 @@ common = gcp.CommonTemplates()
 # ----------------------------------------------------------------------------
 templated_files = common.py_library()
 s.move(templated_files / ".kokoro")
+s.move(templated_files / ".trampolinerc")
 s.move(templated_files / "docs", excludes=["multiprocessing.rst", "conf.py"])
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
