c802716324101349d53f5534c3bf85470391f166
Billy Jacobson <billyjacobson@users.noreply.github.com>
2020-08-06 11:39:20 -0400

    docs: add sample for writing data with Beam (#80)
    Co-authored-by: Tres Seaver <tseaver@palladion.com>

---
+++ packages/google-cloud-bigtable/samples/beam/hello_world_write.py
@@ -0,0 +1,64 @@
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datetime
+
+import apache_beam as beam
+from apache_beam.io.gcp.bigtableio import WriteToBigTable
+from apache_beam.options.pipeline_options import PipelineOptions
+from google.cloud.bigtable import row
+
+
+class BigtableOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument(
+            '--bigtable-project',
+            help='The Bigtable project ID, this can be different than your '
+                 'Dataflow project',
+            default='bigtable-project')
+        parser.add_argument(
+            '--bigtable-instance',
+            help='The Bigtable instance ID',
+            default='bigtable-instance')
+        parser.add_argument(
+            '--bigtable-table',
+            help='The Bigtable table ID in the instance.',
+            default='bigtable-table')
+
+
+class CreateRowFn(beam.DoFn):
+    def process(self, key):
+        direct_row = row.DirectRow(row_key=key)
+        direct_row.set_cell(
+            "stats_summary",
+            b"os_build",
+            b"android",
+            datetime.datetime.now())
+        return [direct_row]
+
+
+def run(argv=None):
+    """Build and run the pipeline."""
+    options = BigtableOptions(argv)
+    with beam.Pipeline(options=options) as p:
+        p | beam.Create(["phone#4c410523#20190501",
+                         "phone#4c410523#20190502"]) | beam.ParDo(
+            CreateRowFn()) | WriteToBigTable(
+            project_id=options.bigtable_project,
+            instance_id=options.bigtable_instance,
+            table_id=options.bigtable_table)
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/google-cloud-bigtable/samples/beam/hello_world_write_test.py
@@ -0,0 +1,57 @@
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import uuid
+
+from google.cloud import bigtable
+import pytest
+
+import hello_world_write
+
+PROJECT = os.environ['GOOGLE_CLOUD_PROJECT']
+BIGTABLE_INSTANCE = os.environ['BIGTABLE_INSTANCE']
+TABLE_ID_PREFIX = 'mobile-time-series-{}'
+
+
+@pytest.fixture(scope="module", autouse=True)
+def table_id():
+    client = bigtable.Client(project=PROJECT, admin=True)
+    instance = client.instance(BIGTABLE_INSTANCE)
+
+    table_id = TABLE_ID_PREFIX.format(str(uuid.uuid4())[:16])
+    table = instance.table(table_id)
+    if table.exists():
+        table.delete()
+
+    table.create(column_families={'stats_summary': None})
+    yield table_id
+
+    table.delete()
+
+
+def test_hello_world_write(table_id):
+    hello_world_write.run([
+        '--bigtable-project=%s' % PROJECT,
+        '--bigtable-instance=%s' % BIGTABLE_INSTANCE,
+        '--bigtable-table=%s' % table_id])
+
+    client = bigtable.Client(project=PROJECT, admin=True)
+    instance = client.instance(BIGTABLE_INSTANCE)
+    table = instance.table(table_id)
+
+    rows = table.read_rows()
+    count = 0
+    for _ in rows:
+        count += 1
+    assert count == 2
