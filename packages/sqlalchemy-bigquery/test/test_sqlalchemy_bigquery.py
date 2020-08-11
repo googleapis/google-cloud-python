a2e0da76917bdeb28b5c1c9f66d208d0b1bd85b7
Vince Broz <vince.broz@bluelabs.com>
2020-08-11 18:25:43 -0400

    Remove redundant example

--- packages/sqlalchemy-bigquery/test/test_sqlalchemy_bigquery.py
+++ packages/sqlalchemy-bigquery/test/test_sqlalchemy_bigquery.py
@@ -488,7 +488,6 @@ def test_get_columns(inspector, inspector_using_test_dataset):
                              ('dataset', 'table', 'project'),
                              (None, 'dataset.table', 'project'),
                              (None, 'project.dataset.table', 'other_project'),
-                             ('dataset', 'table', 'project'),
                              ('project', 'dataset.table', 'other_project'),
                          ])
 def test_table_reference(dialect, provided_schema_name,
