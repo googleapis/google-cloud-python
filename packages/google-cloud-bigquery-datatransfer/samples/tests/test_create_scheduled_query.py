03baedea097cbfc34f2cb01af3d953d4ece4ca96
Dan O'Meara <danom@google.com>
2020-08-28 18:29:22 +0000

    chore: corrects import order (lint fix)

--- packages/google-cloud-bigquery-datatransfer/samples/tests/test_create_scheduled_query.py
+++ packages/google-cloud-bigquery-datatransfer/samples/tests/test_create_scheduled_query.py
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 import os
+import time
 
 import google.api_core.exceptions
 import google.auth
