googleapis/python-language

Fixed name of model [(#1779)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/1779)
* Fixed name of model

* update model ids

samples/snippets/automl/predict_test.py
@@ -23,7 +23,7 @@ compute_region = "us-central1"
 
 
 def test_predict(capsys):
-    model_id = "3472481026502981088"
+    model_id = "TCN3472481026502981088"
     automl_natural_language_predict.predict(
         project_id, compute_region, model_id, "resources/test.txt"
     )

googleapis/google-cloud-python

packages/google-cloud-translate/samples/snippets/predict_test.py
@@ -23,7 +23,7 @@ compute_region = "us-central1"
 
 
 def test_predict(capsys):
-    model_id = "3128559826197068699"
+    model_id = "TRL3128559826197068699"
     automl_translation_predict.predict(
         project_id, compute_region, model_id, "resources/input.txt"
     )