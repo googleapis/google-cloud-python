dea7cb9e68bdcab9095efd3145d1dd7b5be6b050
MF2199 <38331387+mf2199@users.noreply.github.com>
2020-09-08 15:33:03 -0400

    feat: Stage 6 of `nox` implementation - enabling system tests (#480)
    Add googleapis/python-test-utils and system test stub

--- packages/django-google-spanner/noxfile.py
+++ packages/django-google-spanner/noxfile.py
@@ -88,6 +88,37 @@ def unit(session):
 
 
 @nox.session(python="3.8")
+def system(session):
+    """Run the system test suite."""
+    system_test_path = os.path.join("tests", "system.py")
+    system_test_folder_path = os.path.join("tests", "system")
+
+    # Sanity check: Only run tests if the environment variable is set.
+    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
+        session.skip("Credentials must be set via environment variable")
+
+    system_test_exists = os.path.exists(system_test_path)
+    system_test_folder_exists = os.path.exists(system_test_folder_path)
+
+    # Sanity check: only run tests if found.
+    if not system_test_exists and not system_test_folder_exists:
+        session.skip("System tests were not found")
+
+    # Install all test dependencies, then install this package into the
+    # virtualenv's dist-packages.
+    session.install("mock", "pytest", "google-cloud-testutils")
+    session.install("-e", ".")
+
+    # Run py.test against the system tests.
+    if system_test_exists:
+        session.run("py.test", "--quiet", system_test_path, *session.posargs)
+    if system_test_folder_exists:
+        session.run(
+            "py.test", "--quiet", system_test_folder_path, *session.posargs
+        )
+
+
+@nox.session(python="3.8")
 def cover(session):
     """Run the final coverage report.
 
---
+++ packages/django-google-spanner/tests/system/test_system.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Google LLC
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+import unittest
+
+
+class TestSpannerDjangoDBAPI(unittest.TestCase):
+    def setUp(self):
+        # TODO: Implement this method
+        pass
+
+    def tearDown(self):
+        # TODO: Implement this method
+        pass
+
+    def test_api(self):
+        # An dummy stub to avoid `exit code 5` errors
+        # TODO: Replace this with an actual system test method
+        self.assertTrue(True)
