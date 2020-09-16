9d01d0fc6597e2a90053c3f204db591667fecaf8
HemangChothani <50404902+HemangChothani@users.noreply.github.com>
2020-09-16 14:45:06 +0530

    fix: add requests as a dependency (#271)
    Co-authored-by: Peter Lamut <plamut@users.noreply.github.com>

--- packages/google-cloud-storage/setup.py
+++ packages/google-cloud-storage/setup.py
@@ -32,6 +32,7 @@ dependencies = [
     "google-auth >= 1.11.0, < 2.0dev",
     "google-cloud-core >= 1.4.1, < 2.0dev",
     "google-resumable-media >= 1.0.0, < 2.0dev",
+    "requests >= 2.18.0, < 3.0.0dev",
 ]
 extras = {}
 
