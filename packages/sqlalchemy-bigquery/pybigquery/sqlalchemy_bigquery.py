3802a71a9fdcb671a52b944f8c44490593d2122e
Jacob Hayes <jacob.r.hayes@gmail.com>
2020-09-25 22:59:29 -0400

    Fix rendering of nested label expressions

--- packages/sqlalchemy-bigquery/pybigquery/sqlalchemy_bigquery.py
+++ packages/sqlalchemy-bigquery/pybigquery/sqlalchemy_bigquery.py
@@ -183,12 +183,17 @@ class BigQueryCompiler(SQLCompiler):
                 self.preparer.quote(tablename) + \
                 "." + name
 
-    def visit_label(self, *args, **kwargs):
-        # Use labels in GROUP BY clause
-        if len(kwargs) == 0 or len(kwargs) == 1:
+    def visit_label(self, *args, within_group_by=False, **kwargs):
+        # Use labels in GROUP BY clause.
+        #
+        # Flag set in the group_by_clause method. Works around missing equivalent to
+        # supports_simple_order_by_label for group by.
+        if within_group_by:
             kwargs['render_label_as_label'] = args[0]
-        result = super(BigQueryCompiler, self).visit_label(*args, **kwargs)
-        return result
+        return super(BigQueryCompiler, self).visit_label(*args, **kwargs)
+
+    def group_by_clause(self, select, **kw):
+        return super(BigQueryCompiler, self).group_by_clause(select, **kw, within_group_by=True)
 
 
 class BigQueryTypeCompiler(GenericTypeCompiler):
