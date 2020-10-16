497ea31ef1015b749e6e0d3d576cf431ea0b64a6
Kumar Anirudha <5357586+anistark@users.noreply.github.com>
2020-10-16 20:02:03 +0530

    deps: add protobuf dependency (#306)
    Thank you for opening a Pull Request! Before submitting your PR, there are a few things you can do to make sure it goes smoothly:
    - [x] Make sure to open an issue as a [bug/issue](https://github.com/googleapis/python-bigquery/issues/new/choose) before writing your code!  That way we can discuss the change, evaluate designs, and agree on the general idea
    - [x] Ensure the tests and linter pass
    - [x] Code coverage does not decrease (if any source code was changed)
    - [x] Appropriate docs were updated (if necessary)
    
    Fixes #305

--- packages/google-cloud-bigquery/setup.py
+++ packages/google-cloud-bigquery/setup.py
@@ -34,6 +34,7 @@ dependencies = [
     "google-cloud-core >= 1.4.1, < 2.0dev",
     "google-resumable-media >= 0.6.0, < 2.0dev",
     "six >=1.13.0,< 2.0.0dev",
+    "protobuf >= 3.12.0",
 ]
 extras = {
     "bqstorage": [
