0d9b3cafe196182796359c0d5b2e14bad6e19843
Daniel Sanche <d.sanche14@gmail.com>
2020-12-08 11:36:43 -0800

    fix: Add submodule imports for handlers to logging alias (#117)

--- packages/google-cloud-logging/tests/unit/test_logging_shim.py
+++ packages/google-cloud-logging/tests/unit/test_logging_shim.py
@@ -17,7 +17,7 @@ import unittest
 
 
 class TestLoggingShim(unittest.TestCase):
-    def test_shim_matches_logging_v2(self):
+    def test_root_shim_matches_logging_v2(self):
         from google.cloud import logging
         from google.cloud import logging_v2
 
@@ -26,4 +26,43 @@ class TestLoggingShim(unittest.TestCase):
         for name in logging.__all__:
             found = getattr(logging, name)
             expected = getattr(logging_v2, name)
+            if name == "handlers":
+                # handler has separate shim
+                self.assertTrue(found)
+                self.assertIs(type(found), type(expected))
+            else:
+                # other attributes should be identical
+                self.assertIs(found, expected)
+
+    def test_handler_shim_matches_logging_v2(self):
+        from google.cloud.logging import handlers
+        from google.cloud.logging_v2 import handlers as handlers_2
+
+        self.assertEqual(handlers.__all__, handlers_2.__all__)
+
+        for name in handlers.__all__:
+            found = getattr(handlers, name)
+            expected = getattr(handlers_2, name)
+            self.assertIs(found, expected)
+
+    def test_middleware_shim_matches_logging_v2(self):
+        from google.cloud.logging.handlers import middleware
+        from google.cloud.logging_v2.handlers import middleware as middleware_2
+
+        self.assertEqual(middleware.__all__, middleware_2.__all__)
+
+        for name in middleware.__all__:
+            found = getattr(middleware, name)
+            expected = getattr(middleware_2, name)
+            self.assertIs(found, expected)
+
+    def test_transports_shim_matches_logging_v2(self):
+        from google.cloud.logging.handlers import transports
+        from google.cloud.logging_v2.handlers import transports as transports_2
+
+        self.assertEqual(transports.__all__, transports_2.__all__)
+
+        for name in transports.__all__:
+            found = getattr(transports, name)
+            expected = getattr(transports_2, name)
             self.assertIs(found, expected)
