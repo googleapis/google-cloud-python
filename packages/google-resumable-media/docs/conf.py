c803e4387d77acc9149d667d13f7dc5abf82968f
Tres Seaver <tseaver@palladion.com>
2020-12-10 16:22:57 -0500

    chore: allow Sphinx 1.5.5 FBO docfx (#190)

--- packages/google-resumable-media/docs/conf.py
+++ packages/google-resumable-media/docs/conf.py
@@ -29,7 +29,7 @@ __version__ = ""
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "1.6.3"
+needs_sphinx = "1.5.5"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
