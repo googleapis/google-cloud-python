e1cf6fcd6219b36c99402f5f0ab59ef3fc8f090e
Tres Seaver <tseaver@palladion.com>
2020-12-12 14:34:26 -0500

    feat: add Python 3.9 support, drop Python 3.5 support (#42)
    * feat: build Python 3.9 wheels on manylinux
    
    * chore: drop Python 3.5 support
    
    Closes #38.

--- packages/google-crc32c/scripts/osx/build.sh
+++ packages/google-crc32c/scripts/osx/build.sh
@@ -24,10 +24,10 @@ SCRIPTS_DIR=$(dirname ${OSX_DIR})
 export REPO_ROOT=$(dirname ${SCRIPTS_DIR})
 
 # NOTE: These are the Python.org versions of Python.
-PYTHON35="/Library/Frameworks/Python.framework/Versions/3.5/bin"
 PYTHON36="/Library/Frameworks/Python.framework/Versions/3.6/bin"
 PYTHON37="/Library/Frameworks/Python.framework/Versions/3.7/bin"
 PYTHON38="/Library/Frameworks/Python.framework/Versions/3.8/bin"
+PYTHON39="/Library/Frameworks/Python.framework/Versions/3.9/bin"
 
 # Build and install `libcrc32c`
 export PY_BIN="python3"
@@ -37,11 +37,6 @@ cd ${REPO_ROOT}
 git submodule update --init --recursive
 
 ${OSX_DIR}/build_c_lib.sh
-
-# Build wheel for Python 3.5.
-export PY_BIN="python3.5"
-export PY_TAG="cp35-cp35m"
-${OSX_DIR}/build_python_wheel.sh
 
 # Build wheel for Python 3.6.
 export PY_BIN="python3.6"
@@ -53,11 +48,17 @@ export PY_BIN="python3.7"
 export PY_TAG="cp37-cp37m"
 ${OSX_DIR}/build_python_wheel.sh
 
-# TODO: As of 2019-Dec-30, 3.8 is not available on our CI
-# Build wheel for Python 3.7.
-# export PY_BIN="python3.8"
-# export PY_TAG="cp38-cp38m"
-# ${OSX_DIR}/build_python_wheel.sh
+# Build wheel for Python 3.8.
+# Note that the 'm' SOABI flag is no longer supported for Python >= 3.8
+export PY_BIN="python3.8"
+export PY_TAG="cp38-cp38"
+${OSX_DIR}/build_python_wheel.sh
+
+# TODO: As of 2020-12-11, 3.9 is not available on our CI
+# Build wheel for Python 3.9.
+#export PY_BIN="python3.9"
+#export PY_TAG="cp39-cp39"
+#${OSX_DIR}/build_python_wheel.sh
 
 
 # Clean up.
