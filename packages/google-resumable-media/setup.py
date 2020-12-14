7f7ebe68023b88c5b25005b56ea215727b807e02
Tres Seaver <tseaver@palladion.com>
2020-12-14 12:41:16 -0500

    feat: add support for Python 3.9, drop support for Python 3.5 (#191)
    * feat: add support for Python 3.9
    
    Closes #189.
    
    * chore: drop Python 3.5 support
    
    * fix: overlooked python-requires change

--- packages/google-resumable-media/setup.py
+++ packages/google-resumable-media/setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
     zip_safe=False,
     install_requires=REQUIREMENTS,
     extras_require=EXTRAS_REQUIRE,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -62,10 +62,10 @@ setuptools.setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet',
     ],
 )
