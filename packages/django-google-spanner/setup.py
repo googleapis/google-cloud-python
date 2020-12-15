094f190b009be1e3bf736b9983f90cf4f0d1b045
Chris Kleinknecht <libc@google.com>
2020-12-15 15:26:40 -0800

    chore: Add missing py3.8 classifier (#567)

--- packages/django-google-spanner/setup.py
+++ packages/django-google-spanner/setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Utilities",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
