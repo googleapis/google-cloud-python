b6598a15b96181adac05e830afc13105209a4f99
Tim Swast <swast@google.com>
2020-12-22 16:20:48 -0600

    test: add session to test with nightly dependencies (#449)
    This should catch errors introduced in the next versions of dependency
    packages.

--- packages/google-cloud-bigquery/noxfile.py
+++ packages/google-cloud-bigquery/noxfile.py
@@ -169,6 +169,38 @@ def cover(session):
 
 
 @nox.session(python="3.8")
+def prerelease_deps(session):
+    """Run all tests with prerelease versions of dependencies installed.
+
+    https://github.com/googleapis/python-bigquery/issues/95
+    """
+    # PyArrow prerelease packages are published to an alternative PyPI host.
+    # https://arrow.apache.org/docs/python/install.html#installing-nightly-packages
+    session.install(
+        "--extra-index-url", "https://pypi.fury.io/arrow-nightlies/", "--pre", "pyarrow"
+    )
+    session.install("--pre", "grpcio", "pandas")
+    session.install(
+        "mock",
+        "pytest",
+        "google-cloud-testutils",
+        "pytest-cov",
+        "freezegun",
+        "IPython",
+    )
+    session.install("-e", ".[all]")
+
+    # Print out prerelease package versions.
+    session.run("python", "-c", "import grpc; print(grpc.__version__)")
+    session.run("python", "-c", "import pandas; print(pandas.__version__)")
+    session.run("python", "-c", "import pyarrow; print(pyarrow.__version__)")
+
+    # Run all tests, except a few samples tests which require extra dependencies.
+    session.run("py.test", "tests")
+    session.run("py.test", "samples/tests")
+
+
+@nox.session(python="3.8")
 def lint(session):
     """Run linters.
 
