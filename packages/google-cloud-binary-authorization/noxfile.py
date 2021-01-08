4bcf9a579458d750bb90c371ae2d5029c9e30c9d
Bu Sun Kim <busunkim@google.com>
2021-01-08 22:47:59 +0000

    chore: fix templates

--- packages/google-cloud-binary-authorization/noxfile.py
+++ packages/google-cloud-binary-authorization/noxfile.py
@@ -28,7 +28,18 @@ BLACK_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 
 DEFAULT_PYTHON_VERSION = "3.8"
 SYSTEM_TEST_PYTHON_VERSIONS = ["3.8"]
-UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
+UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8"]
+
+# 'docfx' is excluded since it only needs to run in 'docs-presubmit'
+nox.options.sessions = [
+    "unit",
+    "system",
+    "cover",
+    "lint",
+    "lint_setup_py",
+    "blacken",
+    "docs",
+]
 
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
