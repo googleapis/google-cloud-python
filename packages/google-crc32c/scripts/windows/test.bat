b66a0fcbd5ed17d29a1fa3a043da2b183d3f75c9
Christopher Wilcox <crwilcox@google.com>
2021-01-12 23:10:27 -0800

    fix: Update CI to use GitHub actions and build for osx, windows, linux (#51)
    * fix: Update CI to use GitHub actions and build for osx, windows, linux
    
    * fix: cleanup commented out code
    
    * fix: trigger on release, use pypi.org
    
    * fix: add blankline for formatting
    
    * fix: add presubmit job as well
    
    * fix: remove unnecessary conftest
    
    * fix: remove presubmits from kokoro
    
    * fix: remove continuous, release from kokoro
    
    * fix: update workflow names
    
    * fix: remove unused kokoro scripts

--- packages/google-crc32c/scripts/windows/test.bat
+++ packages/google-crc32c/scripts/windows/test.bat
@@ -12,13 +12,19 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 
+@rem python version should be set as an argument, if not, default to python 3.9
+set PYTHON_VERSION=%1
+if "%PYTHON_VERSION%"=="" (
+  echo "Python version was not provided, using Python 3.9"
+  set PYTHON_VERSION=3.9
+)
 
 @rem update python deps and build wheels (requires CRC32C_INSTALL_PREFIX is set)
 @rem FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
-@rem FOR %%V IN (3.5-64,3.6-64,3.7-64) DO (
-FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
-
-    py -%%V -m pip install --no-index --find-links=wheels google-crc32c
+@REM FOR %%V IN (3.9-64,3.9-32) DO (
+FOR %%V IN (%PYTHON_VERSION%-32, %PYTHON_VERSION%-64) DO (
+    py -%%V -m pip install cffi pyparser
+    py -%%V -m pip install --no-index --find-links=wheels google-crc32c --force-reinstall
     py -%%V -m pip install pytest
     py -%%V -m pytest tests
 )
