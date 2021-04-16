1cb4c1287f23f90f223c0bc7019119e07a7216e4
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-16 17:24:29 -0400

    chore: prevent normalization of semver versioning (#141)
    * chore: prevent normalization of semver versioning
    
    * chore: update workaround to make sic work

--- packages/google-cloud-datacatalog/setup.py
+++ packages/google-cloud-datacatalog/setup.py
@@ -19,6 +19,21 @@ import os
 
 import setuptools
 
+# Disable version normalization performed by setuptools.setup()
+try:
+    # Try the approach of using sic(), added in setuptools 46.1.0
+    from setuptools import sic
+except ImportError:
+    # Try the approach of replacing packaging.version.Version
+    sic = lambda v: v
+    try:
+        # setuptools >=39.0.0 uses packaging from setuptools.extern
+        from setuptools.extern import packaging
+    except ImportError:
+        # setuptools <39.0.0 uses packaging from pkg_resources.extern
+        from pkg_resources.extern import packaging
+    packaging.version.Version = packaging.version.LegacyVersion
+
 name = "google-cloud-datacatalog"
 description = "Google Cloud Data Catalog API API client library"
 version = "3.1.1"
@@ -52,7 +67,7 @@ if "google.cloud" in packages:
 
 setuptools.setup(
     name=name,
-    version=version,
+    version=sic(version),
     description=description,
     long_description=readme,
     author="Google LLC",
