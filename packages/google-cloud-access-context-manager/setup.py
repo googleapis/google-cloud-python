944c5beb4e5241c07075158e1f0ba710b7bb7196
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-16 17:37:48 -0400

    chore: prevent normalization of semver versioning (#29)
    * chore: prevent normalization of semver versioning
    
    * chore: update workaround to make sic work

--- packages/google-cloud-access-context-manager/setup.py
+++ packages/google-cloud-access-context-manager/setup.py
@@ -17,6 +17,21 @@ import os
 
 import setuptools
 
+# Disable version normalization performed by setuptools.setup()
+try:
+    # Try the approach of using sic(), added in setuptools 46.1.0
+    from setuptools import sic
+except ImportError:
+    # Try the approach of replacing packaging.version.Version
+    sic = lambda v: v
+    try:
+        # setuptools >=39.0.0 uses packaging from setuptools.extern
+        from setuptools.extern import packaging
+    except ImportError:
+        # setuptools <39.0.0 uses packaging from pkg_resources.extern
+        from pkg_resources.extern import packaging
+    packaging.version.Version = packaging.version.LegacyVersion
+
 # Package metadata.
 
 name = "google-cloud-access-context-manager"
@@ -49,7 +64,7 @@ namespaces.append("google.identity")
 
 setuptools.setup(
     name=name,
-    version=version,
+    version=sic(version),
     description=description,
     long_description=readme,
     author="Google LLC",
