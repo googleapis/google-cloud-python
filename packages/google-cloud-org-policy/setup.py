8c57394ca1975c8b2fe494b4702755e7c67f1514
Jonathan Beaulieu <123.jonathan@gmail.com>
2021-04-19 10:50:04 -0400

    fix: Fixed broken url for package. (#38)
    Fixes #37

--- packages/google-cloud-org-policy/setup.py
+++ packages/google-cloud-org-policy/setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     author="Google LLC",
     author_email="googleapis-packages@google.com",
     license="Apache 2.0",
-    url="https://github.com/googleapis/python-orgpolicy",
+    url="https://github.com/googleapis/python-org-policy",
     packages=setuptools.PEP420PackageFinder.find(),
     namespace_packages=("google", "google.cloud"),
     platforms="Posix; MacOS X; Windows",
