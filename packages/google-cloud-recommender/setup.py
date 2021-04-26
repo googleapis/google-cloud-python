17d44b1bf731150472abc37db105b136a5cbdb5e
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-26 13:35:48 -0400

    chore(revert): revert preventing normalization (#83)

--- packages/google-cloud-recommender/setup.py
+++ packages/google-cloud-recommender/setup.py
@@ -19,21 +19,6 @@ import os
 
 import setuptools
 
-# Disable version normalization performed by setuptools.setup()
-try:
-    # Try the approach of using sic(), added in setuptools 46.1.0
-    from setuptools import sic
-except ImportError:
-    # Try the approach of replacing packaging.version.Version
-    sic = lambda v: v
-    try:
-        # setuptools >=39.0.0 uses packaging from setuptools.extern
-        from setuptools.extern import packaging
-    except ImportError:
-        # setuptools <39.0.0 uses packaging from pkg_resources.extern
-        from pkg_resources.extern import packaging
-    packaging.version.Version = packaging.version.LegacyVersion
-
 name = "google-cloud-recommender"
 description = "Cloud Recommender API client library"
 version = "2.1.0"
@@ -63,7 +48,7 @@ if "google.cloud" in packages:
 
 setuptools.setup(
     name=name,
-    version=sic(version),
+    version=version,
     description=description,
     long_description=readme,
     author="Google LLC",
