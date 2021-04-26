809856d60af8f38c6bdc8beb91814aa309d26378
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-26 10:41:53 -0400

    chore(revert): revert preventing normalization (#396)

--- packages/google-cloud-pubsub/setup.py
+++ packages/google-cloud-pubsub/setup.py
@@ -17,20 +17,6 @@ import os
 
 import setuptools
 
-# Disable version normalization performed by setuptools.setup()
-try:
-    # Try the approach of using sic(), added in setuptools 46.1.0
-    from setuptools import sic
-except ImportError:
-    # Try the approach of replacing packaging.version.Version
-    sic = lambda v: v
-    try:
-        # setuptools >=39.0.0 uses packaging from setuptools.extern
-        from setuptools.extern import packaging
-    except ImportError:
-        # setuptools <39.0.0 uses packaging from pkg_resources.extern
-        from pkg_resources.extern import packaging
-    packaging.version.Version = packaging.version.LegacyVersion
 
 # Package metadata.
 
@@ -75,7 +61,7 @@ if "google.cloud" in packages:
 
 setuptools.setup(
     name=name,
-    version=sic(version),
+    version=version,
     description=description,
     long_description=readme,
     author="Google LLC",
