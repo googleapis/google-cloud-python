051091b8b07175c29b593c26ec46d636a7d8b0d5
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-26 13:15:22 -0400

    chore(revert): revert preventing normalization (#62)

--- packages/grafeas/setup.py
+++ packages/grafeas/setup.py
@@ -19,21 +19,6 @@ import os
 
 import setuptools
 
-# Disable version normalization performed by setuptools.setup()
-try:
-    # Try the approach of using sic(), added in setuptools 46.1.0
-    from setuptools import sic
-except ImportError:
-    # Try the approach of replacing packaging.version.Version
-    sic = lambda v: v
-    try:
-        # setuptools >=39.0.0 uses packaging from setuptools.extern
-        from setuptools.extern import packaging
-    except ImportError:
-        # setuptools <39.0.0 uses packaging from pkg_resources.extern
-        from pkg_resources.extern import packaging
-    packaging.version.Version = packaging.version.LegacyVersion
-
 name = "grafeas"
 description = "Grafeas API client library"
 version = "1.0.1"
@@ -60,7 +45,7 @@ namespaces = ["grafeas"]
 
 setuptools.setup(
     name=name,
-    version=sic(version),
+    version=version,
     description=description,
     long_description=readme,
     author="Google LLC",
