da91401181562affdacc6f9eecf153128ecdee5e
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-26 21:12:18 -0400

    chore(revert): revert preventing normalization (#139)
    reverts previous commit for preventing normalization of versioning

--- packages/google-cloud-vision/setup.py
+++ packages/google-cloud-vision/setup.py
@@ -17,21 +17,6 @@ import os
 
 import setuptools
 
-# Disable version normalization performed by setuptools.setup()
-try:
-    # Try the approach of using sic(), added in setuptools 46.1.0
-    from setuptools import sic
-except ImportError:
-    # Try the approach of replacing packaging.version.Version
-    sic = lambda v: v
-    try:
-        # setuptools >=39.0.0 uses packaging from setuptools.extern
-        from setuptools.extern import packaging
-    except ImportError:
-        # setuptools <39.0.0 uses packaging from pkg_resources.extern
-        from pkg_resources.extern import packaging
-    packaging.version.Version = packaging.version.LegacyVersion
-
 name = "google-cloud-vision"
 description = "Cloud Vision API API client library"
 version = "2.3.1"
@@ -61,7 +46,7 @@ if "google.cloud" in packages:
 
 setuptools.setup(
     name=name,
-    version=sic(version),
+    version=version,
     description=description,
     long_description=readme,
     author="Google LLC",
