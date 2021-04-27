93a2afd42216bd98b7ce6920c8372e0d928f672a
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-26 21:00:36 -0400

    chore(revert): revert preventing normalization (#15)
    reverts previous commit for preventing normalization of versioning

--- packages/google-cloud-dataproc-metastore/setup.py
+++ packages/google-cloud-dataproc-metastore/setup.py
@@ -19,21 +19,6 @@ import io
 import os
 import setuptools  # type: ignore
 
-# Disable version normalization performed by setuptools.setup()
-try:
-    # Try the approach of using sic(), added in setuptools 46.1.0
-    from setuptools import sic
-except ImportError:
-    # Try the approach of replacing packaging.version.Version
-    sic = lambda v: v
-    try:
-        # setuptools >=39.0.0 uses packaging from setuptools.extern
-        from setuptools.extern import packaging
-    except ImportError:
-        # setuptools <39.0.0 uses packaging from pkg_resources.extern
-        from pkg_resources.extern import packaging
-    packaging.version.Version = packaging.version.LegacyVersion
-
 name = "google-cloud-dataproc-metastore"
 version = "0.1.0"
 description = "Dataproc Metastore API client library"
@@ -52,7 +37,7 @@ with io.open(readme_filename, encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name=name,
-    version=sic(version),
+    version=version,
     description=description,
     long_description=readme,
     author="Google LLC",
