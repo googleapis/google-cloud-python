890a74334a39e7266e0ca05ab149a93589359ceb
Chris Rossi <chris@archimedeanco.com>
2021-05-06 12:56:59 -0400

    test: add tests for instanceadmin sample (#290)
    * test: add tests for instanceadmin sample
    
    Closes #201.
    
    * lint
    
    * Fix CLI.
    
    * Remove anything to do with deprecated dev instances.
    
    * Make sure we don't litter our test project with instances.
    
    * Rearrange imports to make linter happy.

---
+++ packages/google-cloud-bigtable/samples/instanceadmin/test_instanceadmin.py
@@ -0,0 +1,152 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import random
+import warnings
+
+from google.cloud import bigtable
+
+import pytest
+
+import instanceadmin
+
+
+PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
+ID_FORMAT = "instanceadmin-test-{:06}"
+ID_RANGE = 1000000
+
+INSTANCE = ID_FORMAT.format(random.randrange(ID_RANGE))
+CLUSTER1 = ID_FORMAT.format(random.randrange(ID_RANGE))
+CLUSTER2 = ID_FORMAT.format(random.randrange(ID_RANGE))
+
+
+@pytest.fixture(scope="module", autouse=True)
+def preclean():
+    """In case any test instances weren't cleared out in a previous run."""
+    client = bigtable.Client(project=PROJECT, admin=True)
+    for instance in client.list_instances()[0]:
+        if instance.instance_id.startswith("instanceadmin-test-"):
+            warnings.warn(f"Deleting leftover test instance: {instance.instance_id}")
+            instance.delete()
+
+
+@pytest.fixture
+def dispose_of():
+    instances = []
+
+    def disposal(instance):
+        instances.append(instance)
+
+    yield disposal
+
+    client = bigtable.Client(project=PROJECT, admin=True)
+    for instance_id in instances:
+        instance = client.instance(instance_id)
+        if instance.exists():
+            instanceadmin.delete_instance(PROJECT, INSTANCE)
+
+
+def test_run_instance_operations(capsys, dispose_of):
+    dispose_of(INSTANCE)
+
+    instanceadmin.run_instance_operations(PROJECT, INSTANCE, CLUSTER1)
+    out = capsys.readouterr().out
+    assert f"Instance {INSTANCE} does not exist." in out
+    assert "Creating an instance" in out
+    assert f"Created instance: {INSTANCE}" in out
+    assert "Listing instances" in out
+    assert f"\n{INSTANCE}\n" in out
+    assert f"Name of instance: {INSTANCE}" in out
+    assert "Labels: {'prod-label': 'prod-label'}" in out
+    assert "Listing clusters..." in out
+    assert f"\n{CLUSTER1}\n" in out
+
+    instanceadmin.run_instance_operations(PROJECT, INSTANCE, CLUSTER1)
+    out = capsys.readouterr().out
+    assert f"Instance {INSTANCE} already exists." in out
+    assert "Listing instances" in out
+    assert f"\n{INSTANCE}\n" in out
+    assert f"Name of instance: {INSTANCE}" in out
+    assert "Labels: {'prod-label': 'prod-label'}" in out
+    assert "Listing clusters..." in out
+    assert f"\n{CLUSTER1}\n" in out
+
+
+def test_delete_instance(capsys, dispose_of):
+    dispose_of(INSTANCE)
+
+    # Can't delete it, it doesn't exist
+    instanceadmin.delete_instance(PROJECT, INSTANCE)
+    out = capsys.readouterr().out
+    assert "Deleting instance" in out
+    assert f"Instance {INSTANCE} does not exist" in out
+
+    # Ok, create it then
+    instanceadmin.run_instance_operations(PROJECT, INSTANCE, CLUSTER1)
+    capsys.readouterr()  # throw away output
+
+    # Now delete it
+    instanceadmin.delete_instance(PROJECT, INSTANCE)
+    out = capsys.readouterr().out
+    assert "Deleting instance" in out
+    assert f"Deleted instance: {INSTANCE}" in out
+
+
+def test_add_and_delete_cluster(capsys, dispose_of):
+    dispose_of(INSTANCE)
+
+    # This won't work, because the instance isn't created yet
+    instanceadmin.add_cluster(PROJECT, INSTANCE, CLUSTER2)
+    out = capsys.readouterr().out
+    assert f"Instance {INSTANCE} does not exist" in out
+
+    # Get the instance created
+    instanceadmin.run_instance_operations(PROJECT, INSTANCE, CLUSTER1)
+    capsys.readouterr()  # throw away output
+
+    # Add a cluster to that instance
+    instanceadmin.add_cluster(PROJECT, INSTANCE, CLUSTER2)
+    out = capsys.readouterr().out
+    assert f"Adding cluster to instance {INSTANCE}" in out
+    assert "Listing clusters..." in out
+    assert f"\n{CLUSTER1}\n" in out
+    assert f"Cluster created: {CLUSTER2}" in out
+
+    # Try to add the same cluster again, won't work
+    instanceadmin.add_cluster(PROJECT, INSTANCE, CLUSTER2)
+    out = capsys.readouterr().out
+    assert "Listing clusters..." in out
+    assert f"\n{CLUSTER1}\n" in out
+    assert f"\n{CLUSTER2}\n" in out
+    assert f"Cluster not created, as {CLUSTER2} already exists."
+
+    # Now delete it
+    instanceadmin.delete_cluster(PROJECT, INSTANCE, CLUSTER2)
+    out = capsys.readouterr().out
+    assert "Deleting cluster" in out
+    assert f"Cluster deleted: {CLUSTER2}" in out
+
+    # Verify deletion
+    instanceadmin.run_instance_operations(PROJECT, INSTANCE, CLUSTER1)
+    out = capsys.readouterr().out
+    assert "Listing clusters..." in out
+    assert f"\n{CLUSTER1}\n" in out
+    assert f"\n{CLUSTER2}\n" not in out
+
+    # Try deleting it again, for fun (and coverage)
+    instanceadmin.delete_cluster(PROJECT, INSTANCE, CLUSTER2)
+    out = capsys.readouterr().out
+    assert "Deleting cluster" in out
+    assert f"Cluster {CLUSTER2} does not exist" in out
