c08e0b439e949c18c040fb52d26c7366ee11ac91
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-05-14 19:02:05 -0600

    fix(deps): add packaging requirement (#97)
    Add packaging requirement. packaging.version
                  is used for a version comparison in transports/base.py and is needed after the upgrade to gapic-generator-python 0.46.3

--- packages/google-cloud-container/setup.py
+++ packages/google-cloud-container/setup.py
@@ -32,6 +32,7 @@ dependencies = [
     "google-api-core[grpc] >= 1.21.2, < 2.0.0dev",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
     "proto-plus >= 1.4.0",
+    "packaging >= 14.3",
     "libcst >= 0.2.5",
 ]
 extras = {}
