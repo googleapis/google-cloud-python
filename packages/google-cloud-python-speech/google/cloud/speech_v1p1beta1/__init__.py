7311f2d248ad0b2543d5827b2837c7c58fc1859a
Anthonios Partheniou <partheniou@google.com>
2021-05-17 10:42:52 -0400

    chore: migrate to owl bot (#158)
    This PR migrates from autosynth to owl bot. owl bot will save time for maintainers as it will automatically open PRs when there are updates in googleapis-gen without requiring maintainers to run synthtool to build the client from protos. Additionally, similar to autosynth, PRs will be automatically opened when there are template updates.

--- packages/google-cloud-python-speech/google/cloud/speech_v1p1beta1/__init__.py
+++ packages/google-cloud-python-speech/google/cloud/speech_v1p1beta1/__init__.py
@@ -60,7 +60,6 @@ class SpeechClient(SpeechHelpers, SpeechClient):
 
 
 __all__ = (
-    "AdaptationClient",
     "CreateCustomClassRequest",
     "CreatePhraseSetRequest",
     "CustomClass",
@@ -83,6 +82,7 @@ __all__ = (
     "RecognizeResponse",
     "SpeakerDiarizationConfig",
     "SpeechAdaptation",
+    "SpeechClient",
     "SpeechContext",
     "SpeechRecognitionAlternative",
     "SpeechRecognitionResult",
@@ -94,5 +94,5 @@ __all__ = (
     "UpdateCustomClassRequest",
     "UpdatePhraseSetRequest",
     "WordInfo",
-    "SpeechClient",
+    "AdaptationClient",
 )
