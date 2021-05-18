7eb73b431998abe343cf277f3ed8034adf38880e
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-05-17 18:52:04 -0600

    fix(deps): add packaging requirement (#20)
    Add packaging requirement. packaging.version
                  is used for a version comparison in transports/base.py and is needed after the upgrade to gapic-generator-python 0.46.3

--- packages/google-cloud-appengine-admin/setup.py
+++ packages/google-cloud-appengine-admin/setup.py
@@ -27,7 +27,7 @@ url = "https://github.com/googleapis/python-appengine-admin"
 dependencies = [
     "google-api-core[grpc] >= 1.22.2, < 2.0.0dev",
     "proto-plus >= 1.18.1",
-    "grpc-google-iam-v1",
+    "packaging >= 14.3" "grpc-google-iam-v1",
     "six",
 ]
 
