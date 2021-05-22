3a1fb08468b75d3cd8a91f3a7aa8e03174adb1ec
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-05-22 02:58:01 -0600

    fix(deps): add packaging requirement (#75)

--- packages/google-cloud-billing/setup.py
+++ packages/google-cloud-billing/setup.py
@@ -43,6 +43,7 @@ setuptools.setup(
         "google-api-core[grpc] >= 1.22.2, < 2.0.0dev",
         "grpc-google-iam-v1 >= 0.12.3, < 0.13.0",
         "proto-plus >= 1.10.0",
+        "packaging >= 14.3",
     ),
     python_requires=">=3.6",
     classifiers=[
