9b121f3034d6eca2968100aaefe12decc7da75f7
Anthonios Partheniou <partheniou@google.com>
2021-06-01 14:56:07 -0400

    chore: migrate to owl bot  (#31)
    This PR migrates from autosynth to [owl-bot](https://github.com/googleapis/repo-automation-bots/blob/master/packages/owl-bot/README.md).

--- packages/google-auth-httplib2/owlbot.py
+++ packages/google-auth-httplib2/owlbot.py
@@ -10,4 +10,4 @@ templated_files = common.py_library(unit_cov_level=100, cov_level=100)
 
 paths = [".kokoro", ".github", ".flake8"]
 for p in paths:
-    s.move(templated_files)
+    s.move(templated_files / p)
