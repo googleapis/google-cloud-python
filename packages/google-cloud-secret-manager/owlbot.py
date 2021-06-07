4c7a8ad9aa3a22faaf140807a3cff29519234172
Anthonios Partheniou <partheniou@google.com>
2021-06-07 11:00:58 -0400

    chore: remove obsolete replacement in owlbot.py (#125)

--- packages/google-cloud-secret-manager/owlbot.py
+++ packages/google-cloud-secret-manager/owlbot.py
@@ -44,7 +44,4 @@ s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .
 # ----------------------------------------------------------------------------
 python.py_samples()
 
-# TODO(busunkim): Use latest sphinx after microgenerator transition
-s.replace("noxfile.py", """['"]sphinx['"]""", '"sphinx<3.0.0"')
-
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
