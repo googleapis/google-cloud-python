e307f3f134bfcdc9e66d3061a8e86ea341129310
Anthonios Partheniou <partheniou@google.com>
2021-06-16 11:08:06 -0400

    fix(deps): add packaging requirement (#35)

--- packages/google-cloud-dataproc-metastore/setup.py
+++ packages/google-cloud-dataproc-metastore/setup.py
@@ -27,6 +27,7 @@ url = "https://github.com/googleapis/python-dataproc-metastore"
 dependencies = [
     "google-api-core[grpc] >= 1.22.2, < 2.0.0dev",
     "proto-plus >= 1.4.0",
+    "packaging >= 14.3",
 ]
 
 package_root = os.path.abspath(os.path.dirname(__file__))
