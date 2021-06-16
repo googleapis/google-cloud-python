a564b9165d89dd85ade5eef4d9d40ca19ea9329d
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-06-15 18:52:19 -0600

    fix: exclude docs and tests from package (#33)
    Only include packages that start with google in the published artifact

--- packages/google-cloud-dataproc-metastore/setup.py
+++ packages/google-cloud-dataproc-metastore/setup.py
@@ -44,7 +44,11 @@ setuptools.setup(
     author_email="googleapis-packages@google.com",
     license="Apache 2.0",
     url=url,
-    packages=setuptools.PEP420PackageFinder.find(),
+    packages=[
+        package
+        for package in setuptools.PEP420PackageFinder.find()
+        if package.startswith("google")
+    ],
     namespace_packages=("google", "google.cloud"),
     platforms="Posix; MacOS X; Windows",
     include_package_data=True,
