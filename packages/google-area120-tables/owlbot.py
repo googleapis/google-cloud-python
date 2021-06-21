4ebe91d4815611b824639d6765a46b35e24f7479
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-06-21 15:27:10 -0400

    chore: move non-cloud package to staging bucket (#55)
    * chore: move non-cloud package to staging bucket
    
    * chore: update branch to staging
    
    * chore: update Owlbot.py
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/master/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/google-area120-tables/owlbot.py
+++ packages/google-area120-tables/owlbot.py
@@ -41,7 +41,9 @@ s.remove_staging_dirs()
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(cov_level=98, microgenerator=True)
 s.move(
-    templated_files, excludes=[".coveragerc"]
+    templated_files, excludes=[
+        ".coveragerc"
+    ]
 )  # the microgenerator has a good coveragerc file
 
 # fix coverage target
@@ -49,6 +51,13 @@ s.replace(
     "noxfile.py",
     """[""]--cov=google/cloud["'],""",
     '''"--cov=google/area120",''',
+)
+
+# Block pushing non-cloud libraries to Cloud RAD
+s.replace(
+    ".kokoro/docs/common.cfg",
+    r'value: "docs-staging-v2"',
+    r'value: "docs-staging-v2-staging"'
 )
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
