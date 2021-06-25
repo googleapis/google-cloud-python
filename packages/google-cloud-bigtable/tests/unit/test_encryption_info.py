be16bc0adfd2bb6861da6ffd027e1298f1069870
Tres Seaver <tseaver@palladion.com>
2021-06-25 13:40:06 -0400

    tests: add coverage for 'encryption_info' module (#342)
    Toward #335.

---
+++ packages/google-cloud-bigtable/tests/unit/test_encryption_info.py
@@ -0,0 +1,167 @@
+# Copyright 2021 Google LLC All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import mock
+
+from google.cloud.bigtable import enums
+
+
+EncryptionType = enums.EncryptionInfo.EncryptionType
+_STATUS_CODE = 123
+_STATUS_MESSAGE = "message"
+_KMS_KEY_VERSION = 345
+
+
+def _make_status_pb(code=_STATUS_CODE, message=_STATUS_MESSAGE):
+    from google.rpc.status_pb2 import Status
+
+    return Status(code=code, message=message)
+
+
+def _make_status(code=_STATUS_CODE, message=_STATUS_MESSAGE):
+    from google.cloud.bigtable.error import Status
+
+    status_pb = _make_status_pb(code=code, message=message)
+    return Status(status_pb)
+
+
+def _make_info_pb(
+    encryption_type=EncryptionType.GOOGLE_DEFAULT_ENCRYPTION,
+    code=_STATUS_CODE,
+    message=_STATUS_MESSAGE,
+    kms_key_version=_KMS_KEY_VERSION,
+):
+    encryption_status = _make_status_pb(code=code, message=message)
+
+    spec = ["encryption_type", "encryption_status", "kms_key_version"]
+    return mock.Mock(
+        spec=spec,
+        encryption_type=encryption_type,
+        encryption_status=encryption_status,
+        kms_key_version=kms_key_version,
+    )
+
+
+class TestEncryptionInfo(unittest.TestCase):
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.bigtable.encryption_info import EncryptionInfo
+
+        return EncryptionInfo
+
+    def _make_one(self, encryption_type, encryption_status, kms_key_version):
+        return self._get_target_class()(
+            encryption_type, encryption_status, kms_key_version,
+        )
+
+    def _make_one_defaults(
+        self,
+        encryption_type=EncryptionType.GOOGLE_DEFAULT_ENCRYPTION,
+        code=_STATUS_CODE,
+        message=_STATUS_MESSAGE,
+        kms_key_version=_KMS_KEY_VERSION,
+    ):
+        encryption_status = _make_status(code=code, message=message)
+        return self._make_one(encryption_type, encryption_status, kms_key_version)
+
+    def test__from_pb(self):
+        klass = self._get_target_class()
+        info_pb = _make_info_pb()
+
+        info = klass._from_pb(info_pb)
+
+        self.assertEqual(
+            info.encryption_type, EncryptionType.GOOGLE_DEFAULT_ENCRYPTION,
+        )
+        self.assertEqual(info.encryption_status.code, _STATUS_CODE)
+        self.assertEqual(info.encryption_status.message, _STATUS_MESSAGE)
+        self.assertEqual(info.kms_key_version, _KMS_KEY_VERSION)
+
+    def test_ctor(self):
+        encryption_type = EncryptionType.GOOGLE_DEFAULT_ENCRYPTION
+        encryption_status = _make_status()
+
+        info = self._make_one(
+            encryption_type=encryption_type,
+            encryption_status=encryption_status,
+            kms_key_version=_KMS_KEY_VERSION,
+        )
+
+        self.assertEqual(info.encryption_type, encryption_type)
+        self.assertEqual(info.encryption_status, encryption_status)
+        self.assertEqual(info.kms_key_version, _KMS_KEY_VERSION)
+
+    def test___eq___identity(self):
+        info = self._make_one_defaults()
+        self.assertTrue(info == info)
+
+    def test___eq___wrong_type(self):
+        info = self._make_one_defaults()
+        other = object()
+        self.assertFalse(info == other)
+
+    def test___eq___same_values(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults()
+        self.assertTrue(info == other)
+
+    def test___eq___different_encryption_type(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults(
+            encryption_type=EncryptionType.CUSTOMER_MANAGED_ENCRYPTION,
+        )
+        self.assertFalse(info == other)
+
+    def test___eq___different_encryption_status(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults(code=456)
+        self.assertFalse(info == other)
+
+    def test___eq___different_kms_key_version(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults(kms_key_version=789)
+        self.assertFalse(info == other)
+
+    def test___ne___identity(self):
+        info = self._make_one_defaults()
+        self.assertFalse(info != info)
+
+    def test___ne___wrong_type(self):
+        info = self._make_one_defaults()
+        other = object()
+        self.assertTrue(info != other)
+
+    def test___ne___same_values(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults()
+        self.assertFalse(info != other)
+
+    def test___ne___different_encryption_type(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults(
+            encryption_type=EncryptionType.CUSTOMER_MANAGED_ENCRYPTION,
+        )
+        self.assertTrue(info != other)
+
+    def test___ne___different_encryption_status(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults(code=456)
+        self.assertTrue(info != other)
+
+    def test___ne___different_kms_key_version(self):
+        info = self._make_one_defaults()
+        other = self._make_one_defaults(kms_key_version=789)
+        self.assertTrue(info != other)
