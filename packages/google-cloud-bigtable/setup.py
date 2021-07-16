70b3b9b5f7bc6ab8ae02a6c79b867b3c16bb43ae
Tres Seaver <tseaver@palladion.com>
2021-07-16 10:37:26 -0400

    chore: pin 'google-{api,cloud}-core' to allow 2.x versions (#377)

--- packages/google-cloud-bigtable/setup.py
+++ packages/google-cloud-bigtable/setup.py
@@ -29,8 +29,8 @@ version = "2.3.1"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.26.0, < 2.0.0dev",
-    "google-cloud-core >= 1.4.1, < 2.0dev",
+    "google-api-core[grpc] >= 1.26.0, < 3.0.0dev",
+    "google-cloud-core >= 1.4.1, < 3.0dev",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
     "proto-plus >= 1.13.0",
     "libcst >= 0.2.5",
