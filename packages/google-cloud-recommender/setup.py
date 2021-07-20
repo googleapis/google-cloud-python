d941e5c00277e4a7d289341e92147ce3d52a0352
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-07-20 03:46:30 -0600

    fix(deps): pin 'google-{api,cloud}-core', 'google-auth' to allow 2.x versions (#109)
    Expand pins on library dependencies in preparation for these dependencies taking a new major version. See https://github.com/googleapis/google-cloud-python/issues/10566.

--- packages/google-cloud-recommender/setup.py
+++ packages/google-cloud-recommender/setup.py
@@ -24,7 +24,10 @@ description = "Cloud Recommender API client library"
 version = "2.3.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.26.0, <2.0.0dev",
+    # NOTE: Maintainers, please do not require google-api-core>=2.x.x
+    # Until this issue is closed
+    # https://github.com/googleapis/google-cloud-python/issues/10566
+    "google-api-core[grpc] >= 1.26.0, <3.0.0dev",
     "proto-plus >= 1.10.0",
     "packaging >= 14.3",
 ]
