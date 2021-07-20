5bf99cd0e719cd655646d220ccca301f2f28a3ea
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-07-20 03:44:12 -0600

    fix(deps): pin 'google-{api,cloud}-core', 'google-auth' to allow 2.x versions (#240)
    Expand pins on library dependencies in preparation for these dependencies taking a new major version. See https://github.com/googleapis/google-cloud-python/issues/10566.

--- packages/google-cloud-bigquery-storage/setup.py
+++ packages/google-cloud-bigquery-storage/setup.py
@@ -24,7 +24,10 @@ description = "BigQuery Storage API API client library"
 version = "2.6.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.26.0, < 2.0.0dev",
+    # NOTE: Maintainers, please do not require google-api-core>=2.x.x
+    # Until this issue is closed
+    # https://github.com/googleapis/google-cloud-python/issues/10566
+    "google-api-core[grpc] >= 1.26.0, <3.0.0dev",
     "proto-plus >= 1.4.0",
     "packaging >= 14.3",
     "libcst >= 0.2.5",
