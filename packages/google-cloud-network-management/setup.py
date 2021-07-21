cea944ff722ab3f07afb793a7aace8a7a5ce9791
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-07-21 10:52:37 -0600

    fix(deps): pin 'google-{api,cloud}-core', 'google-auth' to allow 2.x versions (#7)
    Expand pins on library dependencies in preparation for these dependencies taking a new major version. See https://github.com/googleapis/google-cloud-python/issues/10566.

--- packages/google-cloud-network-management/setup.py
+++ packages/google-cloud-network-management/setup.py
@@ -26,7 +26,10 @@ version = "0.1.0"
 release_status = "Development Status :: 4 - Beta"
 url = "https://github.com/googleapis/python-network-management"
 dependencies = [
-    "google-api-core[grpc] >= 1.26.0, <2.0.0dev",
+    # NOTE: Maintainers, please do not require google-api-core>=2.x.x
+    # Until this issue is closed
+    # https://github.com/googleapis/google-cloud-python/issues/10566
+    "google-api-core[grpc] >= 1.26.0, <3.0.0dev",
     "proto-plus >= 1.15.0",
     "packaging >= 14.3",
 ]
