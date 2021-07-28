1da8ec50991cff6de7531c1efb2e056b56f4c1f3
larkee <31196561+larkee@users.noreply.github.com>
2021-07-29 10:03:54 +1200

    fix: avoid bad version of `opentelemetry-instrumentation` (#429)
    Versions >= `0.23b0` break us.
    
    Co-authored-by: larkee <larkee@users.noreply.github.com>
    Co-authored-by: Tres Seaver <tseaver@palladion.com>

--- packages/google-cloud-spanner/setup.py
+++ packages/google-cloud-spanner/setup.py
@@ -46,7 +46,7 @@ extras = {
     "tracing": [
         "opentelemetry-api >= 1.1.0",
         "opentelemetry-sdk >= 1.1.0",
-        "opentelemetry-instrumentation >= 0.20b0",
+        "opentelemetry-instrumentation >= 0.20b0, < 0.23dev",
     ],
     "libcst": "libcst >= 0.2.5",
 }
