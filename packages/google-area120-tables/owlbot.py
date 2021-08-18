e784b1e39102e3c2d715b0dbd74e349838a95771
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-18 07:40:59 -0600

    chore: generate python samples templates in owlbot.py (#79)
    Generate python samples templates in owlbot.py

--- packages/google-area120-tables/owlbot.py
+++ packages/google-area120-tables/owlbot.py
@@ -40,6 +40,7 @@ s.remove_staging_dirs()
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(cov_level=98, microgenerator=True)
+python.py_samples(skip_readmes=True)
 s.move(
     templated_files, excludes=[
         ".coveragerc"
