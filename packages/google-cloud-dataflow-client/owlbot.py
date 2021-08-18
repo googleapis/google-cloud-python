e97d23bd81da5fae947f51015717812d7dac0576
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-18 07:40:28 -0600

    chore: generate python samples templates in owlbot.py (#24)
    Generate python samples templates in owlbot.py

--- packages/google-cloud-dataflow-client/owlbot.py
+++ packages/google-cloud-dataflow-client/owlbot.py
@@ -31,6 +31,7 @@ s.remove_staging_dirs()
 # ----------------------------------------------------------------------------
 
 templated_files = gcp.CommonTemplates().py_library(microgenerator=True)
+python.py_samples(skip_readmes=True)
 s.move(templated_files, excludes=[".coveragerc"]) # the microgenerator has a good coveragerc file
 
 # ----------------------------------------------------------------------------
