a9e40bfcd75a0f8285175f67b6aa2f2daed98ef6
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-18 07:41:10 -0600

    chore: generate python samples templates in owlbot.py (#103)
    Generate python samples templates in owlbot.py

--- packages/google-cloud-monitoring-dashboards/owlbot.py
+++ packages/google-cloud-monitoring-dashboards/owlbot.py
@@ -54,6 +54,7 @@ templated_files = common.py_library(
     microgenerator=True,
     cov_level=96
 )
+python.py_samples(skip_readmes=True)
 s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .coveragerc file
 
 
