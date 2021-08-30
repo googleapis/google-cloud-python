bcd01b30b1247fa65aeb2bfe23fa929de8289866
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-08-30 16:40:00 -0400

    chore: migrate to main branch (#242)

--- google-cloud-speech/owlbot.py
+++ google-cloud-speech/owlbot.py
@@ -69,3 +69,32 @@ s.move(
 python.py_samples(skip_readmes=True)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
+
+# ----------------------------------------------------------------------------
+# Main Branch migration
+# ----------------------------------------------------------------------------
+
+s.replace(
+  "*.rst",
+  "master",
+  "main"
+)
+
+s.replace(
+  "*.rst",
+  "google-cloud-python/blob/main",
+  "google-cloud-python/blob/master"
+)
+
+s.replace(
+  "CONTRIBUTING.rst",
+  "kubernetes/community/blob/main",
+  "kubernetes/community/blob/master"
+)
+
+s.replace(
+  ".kokoro/*",
+  "master",
+  "main"
+)
+
