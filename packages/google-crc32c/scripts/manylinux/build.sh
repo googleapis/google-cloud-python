356444a3e8a17bf68683fd00fb169622066a854f
Tres Seaver <tseaver@palladion.com>
2021-09-01 17:09:58 -0400

    fix: restore building 'manylinux1' wheels (#87)
    * ci: restore building 'manylinux1' wheels.
    
    Toward #83.
    
    * ci: skip 'manylinux1' build on CI for Python 3.10
    
    That image does not support Python 3.10, nor will it ever.

--- packages/google-crc32c/scripts/manylinux/build.sh
+++ packages/google-crc32c/scripts/manylinux/build.sh
@@ -21,12 +21,25 @@ MANYLINUX_DIR=$(echo $(cd $(dirname ${0}); pwd))
 SCRIPTS_DIR=$(dirname ${MANYLINUX_DIR})
 REPO_ROOT=$(dirname ${SCRIPTS_DIR})
 
-docker pull quay.io/pypa/manylinux2010_x86_64	
-docker pull quay.io/pypa/manylinux2014_x86_64
 
 cd $REPO_ROOT
 git submodule update --init --recursive 
 
+# Note:  PyPA's support for the 'manylinux1' image ends on 2022-01-01.
+#        See: https://github.com/pypa/manylinux/issues/994
+#        No 3.10 support for 'manylinux1'.
+if [[ "${BUILD_PYTHON}" != "3.10"* ]]; then
+    docker pull quay.io/pypa/manylinux1_x86_64
+    docker run \
+        --rm \
+        --interactive \
+        --volume ${REPO_ROOT}:/var/code/python-crc32c/ \
+        --env BUILD_PYTHON=${BUILD_PYTHON} \
+        quay.io/pypa/manylinux1_x86_64 \
+        /var/code/python-crc32c/scripts/manylinux/build_on_centos.sh
+fi
+
+docker pull quay.io/pypa/manylinux2010_x86_64	
 docker run \
     --rm \
     --interactive \
@@ -35,6 +48,7 @@ docker run \
     quay.io/pypa/manylinux2010_x86_64 \
     /var/code/python-crc32c/scripts/manylinux/build_on_centos.sh
 
+docker pull quay.io/pypa/manylinux2014_x86_64
 docker run \
     --rm \
     --interactive \
@@ -44,6 +58,7 @@ docker run \
     /var/code/python-crc32c/scripts/manylinux/build_on_centos.sh
 
 docker run --rm --privileged hypriot/qemu-register
+docker pull quay.io/pypa/manylinux2014_aarch64
 docker run \
     --rm \
     --interactive \
