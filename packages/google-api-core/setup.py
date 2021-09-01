2ab556f18c3c3e407ac3898bd499ef7ac1afd225
Anthonios Partheniou <partheniou@google.com>
2021-09-01 12:31:34 -0400

    chore: remove obsolete dependency in setup.py (#269)
    Co-authored-by: Tres Seaver <tseaver@palladion.com>

--- packages/google-api-core/setup.py
+++ packages/google-api-core/setup.py
@@ -34,7 +34,6 @@ dependencies = [
     "google-auth >= 1.25.0, < 3.0dev",
     "requests >= 2.18.0, < 3.0.0dev",
     "setuptools >= 40.3.0",
-    'futures >= 3.2.0; python_version < "3.2"',
 ]
 extras = {
     "grpc": "grpcio >= 1.33.2, < 2.0dev",
