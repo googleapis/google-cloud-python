bd7119dce7f171c6adf83db915fdfd0306b5e18d
nicain <nicain.seattle@gmail.com>
2021-09-01 17:16:25 -0700

    chore: removing owlbot directives for conversion to main (#43)
    * chore: removing owlbot directives for conversion to main
    
    * clean up
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/google-auth-httplib2/owlbot.py
+++ packages/google-auth-httplib2/owlbot.py
@@ -12,7 +12,3 @@ paths = [".kokoro", ".github", ".flake8"]
 for p in paths:
     s.move(templated_files / p)
 
-# Remove the replacement below once https://github.com/googleapis/synthtool/pull/1188 is merged
-
-# Update googleapis/repo-automation-bots repo to main in .kokoro/*.sh files
-s.replace(".kokoro/*.sh", "repo-automation-bots/tree/master", "repo-automation-bots/tree/main")
