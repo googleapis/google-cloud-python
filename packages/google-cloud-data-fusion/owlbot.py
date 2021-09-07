6ffeeb367a2d7d9f9b6340b2a4861d47a738438b
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-09-07 11:08:57 -0400

    chore: revert owlbot main branch templates (#26)

--- packages/google-cloud-data-fusion/owlbot.py
+++ packages/google-cloud-data-fusion/owlbot.py
@@ -40,37 +40,3 @@ s.move(templated_files, excludes=[".coveragerc"]) # the microgenerator has a goo
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
 
-# ----------------------------------------------------------------------------
-# Main Branch migration
-# ----------------------------------------------------------------------------
-
-s.replace(
-  "*.rst",
-  "master",
-  "main"
-)
-
-s.replace(
-  "CONTRIBUTING.rst",
-  "kubernetes/community/blob/main",
-  "kubernetes/community/blob/master"
-)
-
-s.replace(
-  "docs/conf.py",
-  "master",
-  "main"
-)
-
-s.replace(
-  "docs/conf.py",
-  "main_doc",
-  "root_doc"
-)
-
-s.replace(
-  ".kokoro/*",
-  "master",
-  "main"
-)
-
