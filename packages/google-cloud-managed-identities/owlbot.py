238a923a63f6dd82773314a68cbd59b135cdab8a
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-09-07 11:14:12 -0400

    chore: revert owlbot main branch templates (#68)
    Reverts main branch template for OwlBot that were included for the main branch integration.

--- packages/google-cloud-managed-identities/owlbot.py
+++ packages/google-cloud-managed-identities/owlbot.py
@@ -44,31 +44,3 @@ s.move(
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
 
-# ----------------------------------------------------------------------------
-# Main Branch migration
-# ----------------------------------------------------------------------------
-
-s.replace(
-  "*.rst",
-  "master",
-  "main"
-)
-
-s.replace(
-  "*.rst",
-  "google-cloud-python/blob/main",
-  "google-cloud-python/blob/master"
-)
-
-s.replace(
-  "CONTRIBUTING.rst",
-  "kubernetes/community/blob/main",
-  "kubernetes/community/blob/master"
-)
-
-s.replace(
-  ".kokoro/*",
-  "master",
-  "main"
-)
-
