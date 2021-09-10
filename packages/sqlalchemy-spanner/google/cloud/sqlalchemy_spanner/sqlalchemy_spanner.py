ea425e1b4eb3b9d0826b447d2a7227dab8e64faa
Ilya Gurov <ilya.faer@mail.ru>
2021-09-10 13:45:19 +0300

    fix: rollback failed exception log (#106)

--- packages/sqlalchemy-spanner/google/cloud/sqlalchemy_spanner/sqlalchemy_spanner.py
+++ packages/sqlalchemy-spanner/google/cloud/sqlalchemy_spanner/sqlalchemy_spanner.py
@@ -800,13 +800,12 @@ LIMIT 1
         To prevent rollback exception, don't rollback
         committed/rolled back transactions.
         """
-        if (
-            not isinstance(dbapi_connection, spanner_dbapi.Connection)
-            and dbapi_connection.connection._transaction
-            and (
-                dbapi_connection.connection._transaction.rolled_back
-                or dbapi_connection.connection._transaction.committed
-            )
+        if not isinstance(dbapi_connection, spanner_dbapi.Connection):
+            dbapi_connection = dbapi_connection.connection
+
+        if dbapi_connection._transaction and (
+            dbapi_connection._transaction.rolled_back
+            or dbapi_connection._transaction.committed
         ):
             pass
         else:
