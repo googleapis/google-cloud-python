4b659c56dd68341c95fd4b2d38064911cbe844d6
Tres Seaver <tseaver@palladion.com>
2021-09-20 15:24:34 -0400

    fix: un-pin google-crc32c (#267)
    This reverts commit 9fa344f42a99db1af27b8ca126a2ea6b3c01d837.
    
    Co-authored-by: Andrew Gorcester <gorcester@google.com>

--- packages/google-resumable-media/setup.py
+++ packages/google-resumable-media/setup.py
@@ -24,7 +24,7 @@ with open(os.path.join(PACKAGE_ROOT, 'README.rst')) as file_obj:
 
 
 REQUIREMENTS = [
-    'google-crc32c >= 1.0, <= 1.1.2',
+    'google-crc32c >= 1.0, < 2.0dev',
 ]
 EXTRAS_REQUIRE = {
     'requests': [
