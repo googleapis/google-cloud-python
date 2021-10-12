3b0ccb8e82a812a1414a8f1a0136dbc082f8740a
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-10-12 15:14:13 -0400

    chore: revert owlbot main branch templates (#390)
    * chore: revert owlbot main branch templates
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/google-cloud-logging/owlbot.py
+++ packages/google-cloud-logging/owlbot.py
@@ -100,25 +100,3 @@ python.py_samples()
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
 
-# ----------------------------------------------------------------------------
-# Main Branch migration
-# ----------------------------------------------------------------------------
-
-s.replace(
-  "*.rst",
-  "master",
-  "main"
-)
-
-s.replace(
-  "CONTRIBUTING.rst",
-  "kubernetes/community/blob/main",
-  "kubernetes/community/blob/master"
-)
-
-s.replace(
-  ".kokoro/*",
-  "master",
-  "main"
-)
-
