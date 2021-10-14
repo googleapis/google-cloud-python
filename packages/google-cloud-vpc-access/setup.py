bc203ed413e98e85f01c9a74b4c622c4f0a279a2
Anthonios Partheniou <partheniou@google.com>
2021-10-14 19:06:28 -0400

    feat: add support for python 3.10 (#55)

--- packages/google-cloud-vpc-access/setup.py
+++ packages/google-cloud-vpc-access/setup.py
@@ -69,6 +69,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
         "Topic :: Internet",
     ],
