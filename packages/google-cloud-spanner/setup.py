6857fd1d7d92ca7c5f5a106233760ab08a42ca12
larkee <31196561+larkee@users.noreply.github.com>
2021-10-29 17:42:00 +1300

    chore: avoid dependency on bad version of proto-plus (#634)
    Co-authored-by: larkee <larkee@users.noreply.github.com>

--- packages/google-cloud-spanner/setup.py
+++ packages/google-cloud-spanner/setup.py
@@ -38,7 +38,7 @@ dependencies = [
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-cloud-core >= 1.4.1, < 3.0dev",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
-    "proto-plus >= 1.11.0",
+    "proto-plus >= 1.11.0, != 1.19.6",
     "sqlparse >= 0.3.0",
     "packaging >= 14.3",
 ]
