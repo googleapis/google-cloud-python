22dc5810b9e3cceb89ec6c73e5185e286aa907f9
Anurag Kumar <mailanu98@gmail.com>
2021-11-02 23:11:32 +0530

    chore: add classifier for python 3.9 (#132)
    added support for python 3.9
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/google-cloud-bigquery-connection/setup.py
+++ packages/google-cloud-bigquery-connection/setup.py
@@ -59,6 +59,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
