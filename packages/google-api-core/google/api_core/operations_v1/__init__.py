f6fec5044da1a3f7e068b8e1165416af4acb5c13
Ken Bandes <kbandes@comcast.net>
2021-11-24 21:17:42 -0500

    feat: add operations rest client to support long-running operations. (#311)
    * feat: add operations rest client to support long-running operations.
    
    * fix: address test coverage gaps in operations rest client.
    
    * fix: removed stray print statement.
    
    * fix: address lint, blacken, and mypy issues.
    
    * fix: address pytype, more coverage issues
    
    * fix: addressed additional pytype issues and one coverage line.
    
    * fix: renamed OperationsRestClient to AbstractOperationsClient.
    
    Co-authored-by: Kenneth Bandes <kbandes@google.com>

--- packages/google-api-core/google/api_core/operations_v1/__init__.py
+++ packages/google-api-core/google/api_core/operations_v1/__init__.py
@@ -14,7 +14,14 @@
 
 """Package for interacting with the google.longrunning.operations meta-API."""
 
+from google.api_core.operations_v1.abstract_operations_client import AbstractOperationsClient
 from google.api_core.operations_v1.operations_async_client import OperationsAsyncClient
 from google.api_core.operations_v1.operations_client import OperationsClient
+from google.api_core.operations_v1.transports.rest import OperationsRestTransport
 
-__all__ = ["OperationsAsyncClient", "OperationsClient"]
+__all__ = [
+    "AbstractOperationsClient",
+    "OperationsAsyncClient",
+    "OperationsClient",
+    "OperationsRestTransport"
+]
