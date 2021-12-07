bc8a8e46ae4c7fd2cdd35e28844add8a3dfc599e
Dov Shlachter <dovs@google.com>
2021-12-07 15:17:41 -0800

    chore: extended_operations_pb2.py is moved into api-common-protos (#1100)

--- packages/gapic-generator/setup.py
+++ packages/gapic-generator/setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=(
         "click >= 6.7",
         "google-api-core >= 2.2.0",
-        "googleapis-common-protos >= 1.53.0",
+        "googleapis-common-protos >= 1.54.0",
         "grpcio >= 1.24.3",
         "jinja2 >= 2.10",
         "protobuf >= 3.18.0",
