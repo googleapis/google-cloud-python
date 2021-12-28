858229720123611455a38059aa4da5eb9429fa9a
Vikash Singh <3116482+vi3k6i5@users.noreply.github.com>
2021-12-28 12:33:00 +0530

    fix: downgrade google-cloud-spanner to v 3.11.1 (#747)
    * chore: release 3.0.0
    
    Release-As: 3.0.0
    
    * fix: downgrade google-cloud-spanner to v 3.11.1

--- packages/django-google-spanner/setup.py
+++ packages/django-google-spanner/setup.py
@@ -17,7 +17,7 @@ description = "Bridge to enable using Django with Spanner."
 # 'Development Status :: 4 - Beta'
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
-dependencies = ["sqlparse >= 0.3.0", "google-cloud-spanner >= 3.11.1"]
+dependencies = ["sqlparse >= 0.3.0", "google-cloud-spanner == 3.11.1"]
 extras = {
     "tracing": [
         "opentelemetry-api >= 1.1.0",
