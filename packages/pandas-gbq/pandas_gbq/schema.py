1a11cc805b591fdfd166fa99e1355826b50597c5
Tim Swast <swast@google.com>
2022-01-19 09:53:19 -0600

    fix: avoid iteritems deprecation in pandas prerelease (#469)

--- packages/pandas-gbq/pandas_gbq/schema.py
+++ packages/pandas-gbq/pandas_gbq/schema.py
@@ -116,7 +116,7 @@ def generate_bq_schema(dataframe, default_type="STRING"):
     }
 
     fields = []
-    for column_name, dtype in dataframe.dtypes.iteritems():
+    for column_name, dtype in dataframe.dtypes.items():
         fields.append(
             {"name": column_name, "type": type_mapping.get(dtype.kind, default_type)}
         )
