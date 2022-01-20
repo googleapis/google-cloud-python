1fe6ff514716f48dece47696d9d4594f5d47cd16
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-01-20 13:46:18 -0700

    fix(deps): allow cachetools 5.0 for python 3.7+ (#937)
    * fix(deps): allow cachetools 5.0 for python 3.7+
    
    Changes listed https://github.com/tkem/cachetools/blob/master/CHANGELOG.rst#v500-2021-12-21
    
    * chore: remove split pin
    
    * Update setup.py
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: arithmetic1728 <58957152+arithmetic1728@users.noreply.github.com>

--- packages/google-auth/setup.py
+++ packages/google-auth/setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 
 DEPENDENCIES = (
-    "cachetools>=2.0.0,<5.0",
+    "cachetools>=2.0.0,<6.0",
     "pyasn1-modules>=0.2.1",
     # rsa==4.5 is the last version to support 2.7
     # https://github.com/sybrenstuvel/python-rsa/issues/152#issuecomment-643470233
