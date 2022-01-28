0198001e3863610369e42af749f2d5fdeb5410d0
Ilya Gurov <ilya.faer@mail.ru>
2022-01-28 11:31:21 +0000

    feat: rollback a connection returned back to pool (#193)
    * feat: drop read_only on a connection returned back to a pool
    
    * feat: rollback a connection returned back to pool
    
    Co-authored-by: Vikash Singh <3116482+vi3k6i5@users.noreply.github.com>

--- packages/sqlalchemy-spanner/google/cloud/sqlalchemy_spanner/sqlalchemy_spanner.py
+++ packages/sqlalchemy-spanner/google/cloud/sqlalchemy_spanner/sqlalchemy_spanner.py
@@ -48,6 +48,7 @@ from google.cloud.sqlalchemy_spanner._opentelemetry_tracing import trace_call
 @listens_for(Pool, "reset")
 def reset_connection(dbapi_conn, connection_record):
     """An event of returning a connection back to a pool."""
+    dbapi_conn.connection.rollback()
     if getattr(dbapi_conn.connection, "staleness", None) is not None:
         dbapi_conn.connection.staleness = None
 
