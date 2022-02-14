0798289e82255d2fa961633326c4b19166bbdb62
Anurag Kumar <mailanu98@gmail.com>
2022-02-15 04:45:01 +0530

    chore: add classifier for python 3.10 (#283)
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/proto-plus/setup.py
+++ packages/proto-plus/setup.py
@@ -50,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
