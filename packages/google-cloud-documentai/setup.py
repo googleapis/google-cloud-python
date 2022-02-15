4db5614d68bfa3a88e89e0394d39aad1c23543f4
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-02-15 09:04:16 -0700

    fix: remove libcst from setup_requires (#276)

--- packages/google-cloud-documentai/setup.py
+++ packages/google-cloud-documentai/setup.py
@@ -52,7 +52,6 @@ setuptools.setup(
         "proto-plus >= 1.10.0",
     ),
     python_requires=">=3.6",
-    setup_requires=["libcst >= 0.2.5"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
