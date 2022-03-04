69cefaf4072fc2e355388fc0f1ef158c68df26be
Anthonios Partheniou <partheniou@google.com>
2022-03-04 11:47:08 -0500

    fix(deps): require google-api-core>=1.31.5, >=2.3.2 (#82)

--- packages/google-cloud-life-sciences/setup.py
+++ packages/google-cloud-life-sciences/setup.py
@@ -29,7 +29,7 @@ dependencies = [
     # NOTE: Maintainers, please do not require google-api-core>=2.x.x
     # Until this issue is closed
     # https://github.com/googleapis/google-cloud-python/issues/10566
-    "google-api-core[grpc] >= 1.28.0, <3.0.0dev",
+    "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
     "proto-plus >= 1.15.0",
 ]
 
