e324faf515679567084846c1803ef9b5b7795341
Anthonios Partheniou <partheniou@google.com>
2022-03-07 16:50:33 -0500

    fix(deps): require google-api-core>=1.31.5, >=2.3.2 (#40)

--- packages/google-cloud-monitoring-metrics-scopes/setup.py
+++ packages/google-cloud-monitoring-metrics-scopes/setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     platforms="Posix; MacOS X; Windows",
     include_package_data=True,
     install_requires=(
-        "google-api-core[grpc] >= 1.28.0, < 3.0.0dev",
+        "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
         "proto-plus >= 1.15.0",
     ),
     python_requires=">=3.6",
