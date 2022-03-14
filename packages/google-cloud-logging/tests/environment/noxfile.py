4866430cb146a20fe9b2353668ffa80892d0706f
Drew Brown <drew@drewbrown.me>
2022-03-14 16:38:39 -0700

    chore: Update environment tests submodule (#503)

--- packages/google-cloud-logging/tests/environment/noxfile.py
+++ packages/google-cloud-logging/tests/environment/noxfile.py
@@ -69,7 +69,7 @@ def _determine_local_import_names(start_dir: str) -> List[str]:
 # We also need to specify the rules which are ignored by default:
 # ['E226', 'W504', 'E126', 'E123', 'W503', 'E24', 'E704', 'E121']
 
-DEFAULT_PYTHON_VERSION = "3.7"
+DEFAULT_PYTHON_VERSION = "3.9"
 BLACK_PATHS = ["./deployable/python"]
 BLACK_VERSION = "black==19.10b0"
 
