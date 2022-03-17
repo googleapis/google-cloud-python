f5aae63964f3581258f95faf04dd1740a885b74b
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-03-17 12:47:02 -0600

    fix: expose all names in longrunning _pb2's (#90)
    * fix: epoxse all names in longrunning _pb2's
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * chore: set version to 1.55.1b1
    
    * chore: revert changes to noxfile
    
    * chore: try again
    
    * chore: revert version #
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/googleapis-common-protos/google/longrunning/operations_grpc_pb2.py
+++ packages/googleapis-common-protos/google/longrunning/operations_grpc_pb2.py
@@ -3,4 +3,13 @@
 # all of the message and gRPC definitions.
 
 from google.longrunning.operations_proto_pb2 import *
+from google.longrunning.operations_proto_pb2 import _OPERATION
+from google.longrunning.operations_proto_pb2 import _OPERATION
+from google.longrunning.operations_proto_pb2 import _GETOPERATIONREQUEST
+from google.longrunning.operations_proto_pb2 import _LISTOPERATIONSREQUEST
+from google.longrunning.operations_proto_pb2 import _LISTOPERATIONSRESPONSE
+from google.longrunning.operations_proto_pb2 import _CANCELOPERATIONREQUEST
+from google.longrunning.operations_proto_pb2 import _DELETEOPERATIONREQUEST
+from google.longrunning.operations_proto_pb2 import _OPERATIONINFO
+from google.longrunning.operations_proto_pb2 import _OPERATIONS
 from google.longrunning.operations_pb2_grpc import *
--- packages/googleapis-common-protos/google/longrunning/operations_pb2.py
+++ packages/googleapis-common-protos/google/longrunning/operations_pb2.py
@@ -22,6 +22,21 @@ AppEngine Standard).
 try:
     from google.longrunning.operations_grpc_pb2 import *
     from google.longrunning.operations_grpc_pb2 import _OPERATION
+    from google.longrunning.operations_grpc_pb2 import _GETOPERATIONREQUEST
+    from google.longrunning.operations_grpc_pb2 import _LISTOPERATIONSREQUEST
+    from google.longrunning.operations_grpc_pb2 import _LISTOPERATIONSRESPONSE
+    from google.longrunning.operations_grpc_pb2 import _CANCELOPERATIONREQUEST
+    from google.longrunning.operations_grpc_pb2 import _DELETEOPERATIONREQUEST
+    from google.longrunning.operations_grpc_pb2 import _OPERATIONINFO
+    from google.longrunning.operations_grpc_pb2 import _OPERATIONS
 except ImportError:
     from google.longrunning.operations_proto_pb2 import *
     from google.longrunning.operations_proto_pb2 import _OPERATION
+    from google.longrunning.operations_proto_pb2 import _OPERATION
+    from google.longrunning.operations_proto_pb2 import _GETOPERATIONREQUEST
+    from google.longrunning.operations_proto_pb2 import _LISTOPERATIONSREQUEST
+    from google.longrunning.operations_proto_pb2 import _LISTOPERATIONSRESPONSE
+    from google.longrunning.operations_proto_pb2 import _CANCELOPERATIONREQUEST
+    from google.longrunning.operations_proto_pb2 import _DELETEOPERATIONREQUEST
+    from google.longrunning.operations_proto_pb2 import _OPERATIONINFO
+    from google.longrunning.operations_proto_pb2 import _OPERATIONS
