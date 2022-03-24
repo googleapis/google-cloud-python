3e28414774fa58460731df79e159e4810b5461a9
Tim Swast <swast@google.com>
2022-03-24 15:59:03 -0500

    test: add dbtime compliance tests (#90)
    * fix: address failing compliance tests in DateArray and TimeArray
    
    test: add a test session with prerelease versions of dependencies
    
    * fix min/max/median for 2D arrays
    
    * fixes except for null contains
    
    * actually use NaT as 'advertised'
    
    * fix!: use `pandas.NaT` for missing values in dbdate and dbtime dtypes
    
    This makes them consistent with other date/time dtypes, as well as internally
    consistent with the advertised `dtype.na_value`.
    
    BREAKING-CHANGE: dbdate and dbtime dtypes return NaT instead of None for missing values
    
    Release-As: 0.4.0
    
    * more progress towards compliance
    
    * address errors in TestMethods
    
    * move tests
    
    * add prerelease deps
    
    * fix: address failing tests with pandas 1.5.0
    
    test: add a test session with prerelease versions of dependencies
    
    * fix owlbot config
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * document why microsecond precision is used
    
    * use correct units
    
    * add box_func tests
    
    * typo
    
    * fix: avoid TypeError when using sorted search
    
    * add unit tests
    
    * fix: dbdate and dbtime support set item
    
    * add TestMethods
    
    * fix: allow comparison with scalar values
    
    * correct behavior for comparison with different types and shapes
    
    * use same dtype in shape comparison tests
    
    * test: add final dbdate compliance tests and sort
    
    * test: add dbtime compliance tests
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ packages/db-dtypes/tests/compliance/time/test_time_compliance.py
@@ -0,0 +1,107 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tests for extension interface compliance, inherited from pandas.
+
+See:
+https://github.com/pandas-dev/pandas/blob/main/pandas/tests/extension/decimal/test_decimal.py
+and
+https://github.com/pandas-dev/pandas/blob/main/pandas/tests/extension/test_period.py
+"""
+
+import pandas
+from pandas.tests.extension import base
+import pytest
+
+import db_dtypes
+
+
+# TODO(https://github.com/googleapis/python-db-dtypes-pandas/issues/87): Add
+# compliance tests for arithmetic operations.
+
+# TODO(https://github.com/googleapis/python-db-dtypes-pandas/issues/78): Add
+# compliance tests for reduction operations.
+
+
+class TestComparisonOps(base.BaseComparisonOpsTests):
+    pass
+
+
+class TestCasting(base.BaseCastingTests):
+    pass
+
+
+class TestConstructors(base.BaseConstructorsTests):
+    pass
+
+
+class TestDtype(base.BaseDtypeTests):
+    pass
+
+
+class TestGetitem(base.BaseGetitemTests):
+    pass
+
+
+class TestGroupby(base.BaseGroupbyTests):
+    pass
+
+
+class TestIndex(base.BaseIndexTests):
+    pass
+
+
+class TestInterface(base.BaseInterfaceTests):
+    pass
+
+
+class TestMissing(base.BaseMissingTests):
+    pass
+
+
+class TestMethods(base.BaseMethodsTests):
+    def test_combine_add(self):
+        pytest.skip("Cannot add dates.")
+
+    @pytest.mark.parametrize("dropna", [True, False])
+    def test_value_counts(self, all_data, dropna):
+        all_data = all_data[:10]
+        if dropna:
+            # Overridden from
+            # https://github.com/pandas-dev/pandas/blob/main/pandas/tests/extension/base/methods.py
+            # to avoid difference in dtypes.
+            other = db_dtypes.TimeArray(all_data[~all_data.isna()])
+        else:
+            other = all_data
+
+        result = pandas.Series(all_data).value_counts(dropna=dropna).sort_index()
+        expected = pandas.Series(other).value_counts(dropna=dropna).sort_index()
+
+        self.assert_series_equal(result, expected)
+
+
+class TestParsing(base.BaseParsingTests):
+    pass
+
+
+class TestPrinting(base.BasePrintingTests):
+    pass
+
+
+class TestReshaping(base.BaseReshapingTests):
+    pass
+
+
+class TestSetitem(base.BaseSetitemTests):
+    pass
