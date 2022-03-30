9a70b63e5ea8d3655afec856f71533a1a34464a0
Anthonios Partheniou <partheniou@google.com>
2022-03-30 11:41:44 -0400

    fix(deps): restore dependency on python-dateutil (#1187)
    * fix(deps): restore dependency on python-dateutil
    
    * fix(deps): remove redundante proto-plus
    
    Co-authored-by: Tim Swast <swast@google.com>

--- packages/google-cloud-bigquery/setup.py
+++ packages/google-cloud-bigquery/setup.py
@@ -42,8 +42,8 @@ dependencies = [
     "google-cloud-core >= 1.4.1, <3.0.0dev",
     "google-resumable-media >= 0.6.0, < 3.0dev",
     "packaging >= 14.3",
-    "proto-plus >= 1.10.0",  # For the legacy proto-based types.
     "protobuf >= 3.12.0",  # For the legacy proto-based types.
+    "python-dateutil >= 2.7.2, <3.0dev",
     "pyarrow >= 3.0.0, < 8.0dev",
     "requests >= 2.18.0, < 3.0.0dev",
 ]
