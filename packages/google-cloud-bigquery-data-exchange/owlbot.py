f809a4ba0ceb3b787a5dd762ea88c8224dbd3189
Anthonios Partheniou <partheniou@google.com>
2022-04-03 14:56:00 +0000

    chore: add license header

--- packages/google-cloud-bigquery-data-exchange/owlbot.py
+++ packages/google-cloud-bigquery-data-exchange/owlbot.py
@@ -25,32 +25,43 @@ default_version = "v1beta1"
 for library in s.get_staging_dirs(default_version):
     s.replace(
         [
-            library  / f"google/cloud/bigquery/data_exchange_{library.name}/**/*.py",
-            library  / f"tests/unit/gapic/data_exchange_{library.name}/**/*.py",
+            library / f"google/cloud/bigquery/data_exchange_{library.name}/**/*.py",
+            library / f"tests/unit/gapic/data_exchange_{library.name}/**/*.py",
         ],
         f"from google.cloud.bigquery.dataexchange.common import common_pb2",
-        f"from google.cloud.bigquery.data_exchange_{library.name} import common"
+        f"from google.cloud.bigquery.data_exchange_{library.name} import common",
     )
     s.replace(
-        library  / f"google/cloud/bigquery/data_exchange_{library.name}/**/*.py",
+        library / f"google/cloud/bigquery/data_exchange_{library.name}/**/*.py",
         "google.cloud.bigquery.dataexchange.common.common_pb2.Category",
-        "google.cloud.bigquery.data_exchange_{library.name}.common.Category",
+        f"google.cloud.bigquery.data_exchange_{library.name}.common.Category",
     )
     s.replace(
         library / f"docs/common/types.rst",
-        "google.cloud.bigquery.data_exchange.common.types"
-        "google.cloud.bigquery.data_exchange_{library.name}.common.types",
+        "google.cloud.bigquery.data_exchange.common.types",
+        f"google.cloud.bigquery.data_exchange_{library.name}.common.types",
     )
     s.replace(
         [
-            library  / f"google/cloud/bigquery/data_exchange_{library.name}/**/*.py",
-            library  / f"tests/unit/gapic/data_exchange_{library.name}/**/*.py",
+            library / f"google/cloud/bigquery/data_exchange_{library.name}/**/*.py",
+            library / f"tests/unit/gapic/data_exchange_{library.name}/**/*.py",
         ],
         "common_pb2",
-        "common"
+        "common",
     )
-    s.move(library / "google/cloud/bigquery/data_exchange/common", library / "google/cloud/bigquery/data_exchange_v1beta1/common")
-    s.move(library, excludes=["setup.py", "README.rst", "docs/index.rst", "google/cloud/bigquery/data_exchange/**"])
+    s.move(
+        library / "google/cloud/bigquery/data_exchange/common",
+        library / "google/cloud/bigquery/data_exchange_v1beta1/common",
+    )
+    s.move(
+        library,
+        excludes=[
+            "setup.py",
+            "README.rst",
+            "docs/index.rst",
+            "google/cloud/bigquery/data_exchange/**",
+        ],
+    )
 
 s.remove_staging_dirs()
 
@@ -62,7 +73,14 @@ templated_files = gcp.CommonTemplates().py_library(
     microgenerator=True,
     versions=gcp.common.detect_versions(path="./google", default_first=True),
 )
-s.move(templated_files, excludes=[".coveragerc"]) # the microgenerator has a good coveragerc file
+s.move(
+    templated_files,
+    excludes=[
+        ".coveragerc",  # the microgenerator has a good coveragerc file
+        ".github/auto-label.yaml",  # the templated file is missing a license header
+        "docs/index.rst",  # there is an additional module common which is added
+    ],
+)
 
 python.py_samples(skip_readmes=True)
 
