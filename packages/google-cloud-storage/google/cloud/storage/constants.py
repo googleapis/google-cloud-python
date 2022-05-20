d80ee2e01b1eb8c1f0e58a9766a129078c0c621d
Rebecca Peterson <44721098+rebecca-pete@users.noreply.github.com>
2022-05-20 10:54:14 -0700

    docs(samples): Update the Recovery Point Objective (RPO) sample output (#725)
    @ddelgrosso1 Related to b/217259317.
    
    Thank you for opening a Pull Request! Before submitting your PR, there are a few things you can do to make sure it goes smoothly:
    - [ ] Make sure to open an issue as a [bug/issue](https://github.com/googleapis/python-storage/issues/new/choose) before writing your code!  That way we can discuss the change, evaluate designs, and agree on the general idea
    - [ ] Ensure the tests and linter pass
    - [ ] Code coverage does not decrease (if any source code was changed)
    - [ ] Appropriate docs were updated (if necessary)
    
    Fixes #<issue_number_goes_here> 🦕

--- packages/google-cloud-storage/google/cloud/storage/constants.py
+++ packages/google-cloud-storage/google/cloud/storage/constants.py
@@ -119,13 +119,15 @@ See: https://cloud.google.com/storage/docs/public-access-prevention
 """
 
 RPO_ASYNC_TURBO = "ASYNC_TURBO"
-"""Turbo Replication RPO
+"""The recovery point objective (RPO) indicates how quickly newly written objects are asynchronously replicated to a separate geographic location.
+When the RPO value is set to ASYNC_TURBO, the turbo replication feature is enabled.
 
 See: https://cloud.google.com/storage/docs/managing-turbo-replication
 """
 
 RPO_DEFAULT = "DEFAULT"
-"""Default RPO
+"""The recovery point objective (RPO) indicates how quickly newly written objects are asynchronously replicated to a separate geographic location.
+When the RPO value is set to DEFAULT, the default replication behavior is enabled.
 
 See: https://cloud.google.com/storage/docs/managing-turbo-replication
 """
