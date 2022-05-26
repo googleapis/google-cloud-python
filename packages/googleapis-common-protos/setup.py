dd88088101d74cd266d593b94d17e5f3458998b1
Anthonios Partheniou <partheniou@google.com>
2022-05-26 15:36:36 -0400

    fix(deps): require protobuf>= 3.15.0, <4.0.0dev (#105)
    fix(deps): require grpcio >= 1.0.0, <2.0.0dev

--- packages/googleapis-common-protos/setup.py
+++ packages/googleapis-common-protos/setup.py
@@ -22,9 +22,9 @@ name = "googleapis-common-protos"
 description = "Common protobufs used in Google APIs"
 version = "1.56.1"
 release_status = "Development Status :: 5 - Production/Stable"
-dependencies = ["protobuf >= 3.15.0"]
+dependencies = ["protobuf >= 3.15.0, <4.0.0dev"]
 
-extras_require = {"grpc": ["grpcio >= 1.0.0"]}
+extras_require = {"grpc": ["grpcio >= 1.0.0, <2.0.0dev"]}
 
 package_root = os.path.abspath(os.path.dirname(__file__))
 
