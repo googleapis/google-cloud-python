551f2c4e1c7be6abc9a4e220f9b5c557793c9c35
Anthonios Partheniou <partheniou@google.com>
2022-05-26 16:26:18 -0400

    fix(deps): require protobuf>= 3.15.0, <4.0.0dev (#385)
    Also adds upper limits for extras.
    
    fix(deps): require googleapis-common-protos >= 1.56.2

--- packages/google-api-core/setup.py
+++ packages/google-api-core/setup.py
@@ -29,15 +29,15 @@ description = "Google API client core library"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "googleapis-common-protos >= 1.52.0, < 2.0dev",
-    "protobuf >= 3.12.0",
+    "googleapis-common-protos >= 1.56.2, < 2.0dev",
+    "protobuf >= 3.15.0, <4.0.0dev",
     "google-auth >= 1.25.0, < 3.0dev",
     "requests >= 2.18.0, < 3.0.0dev",
 ]
 extras = {
     "grpc": ["grpcio >= 1.33.2, < 2.0dev", "grpcio-status >= 1.33.2, < 2.0dev"],
-    "grpcgcp": "grpcio-gcp >= 0.2.2",
-    "grpcio-gcp": "grpcio-gcp >= 0.2.2",
+    "grpcgcp": "grpcio-gcp >= 0.2.2, < 1.0dev",
+    "grpcio-gcp": "grpcio-gcp >= 0.2.2, < 1.0dev",
 }
 
 
