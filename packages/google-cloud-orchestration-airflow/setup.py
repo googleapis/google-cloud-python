f1a5d81e28952b0e895b0f9315a3e233fa29fcc4
Anthonios Partheniou <partheniou@google.com>
2022-06-01 22:38:02 -0400

    fix(deps): require protobuf <4.0.0dev (#108)

--- packages/google-cloud-orchestration-airflow/setup.py
+++ packages/google-cloud-orchestration-airflow/setup.py
@@ -29,7 +29,8 @@ dependencies = [
     # Until this issue is closed
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-    "proto-plus >= 1.15.0",
+    "proto-plus >= 1.15.0, <2.0.0dev",
+    "protobuf >= 3.19.0, <4.0.0dev",
 ]
 
 package_root = os.path.abspath(os.path.dirname(__file__))
