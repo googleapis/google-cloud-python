15a761a73d011b0b2921c35f2f6dd68867edbbc0
Anthonios Partheniou <partheniou@google.com>
2022-06-01 22:30:27 -0400

    fix(deps): require protobuf <4.0.0dev (#165)

--- packages/google-area120-tables/setup.py
+++ packages/google-area120-tables/setup.py
@@ -50,7 +50,8 @@ setuptools.setup(
         # Until this issue is closed
         # https://github.com/googleapis/google-cloud-python/issues/10566
         "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-        "proto-plus >= 1.15.0",
+        "proto-plus >= 1.15.0, <2.0.0dev",
+        "protobuf >= 3.19.0, <4.0.0dev",
     ),
     python_requires=">=3.6",
     classifiers=[
