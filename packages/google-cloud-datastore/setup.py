a4618390e234a280b48cdcb6a587f81750c3fadb
Anthonios Partheniou <partheniou@google.com>
2022-06-02 13:16:12 -0400

    fix(deps): require protobuf <4.0.0dev (#318)

--- packages/google-cloud-datastore/setup.py
+++ packages/google-cloud-datastore/setup.py
@@ -37,7 +37,8 @@ dependencies = [
     # Until this issue is closed
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-cloud-core >= 1.4.0, <3.0.0dev",
-    "proto-plus >= 1.15.0",
+    "proto-plus >= 1.15.0, <2.0.0dev",
+    "protobuf >= 3.19.0, <4.0.0dev",
 ]
 extras = {"libcst": "libcst >= 0.2.5"}
 
