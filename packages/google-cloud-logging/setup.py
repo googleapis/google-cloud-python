15cc6f3123fc7bff3e806222d42637f1816f5e64
Anthonios Partheniou <partheniou@google.com>
2022-06-01 22:38:59 -0400

    fix(deps): require protobuf <4.0.0dev (#564)
    Co-authored-by: Drew Brown <drewbr@google.com>

--- packages/google-cloud-logging/setup.py
+++ packages/google-cloud-logging/setup.py
@@ -39,8 +39,9 @@ dependencies = [
     # Until this issue is closed
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-cloud-core >= 1.4.1, <3.0.0dev",
-    "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
-    "proto-plus >= 1.15.0",
+    "grpc-google-iam-v1 >=0.12.4, <1.0.0dev",
+    "proto-plus >= 1.15.0, <2.0.0dev",
+    "protobuf >= 3.19.0, <4.0.0dev",
 ]
 extras = {}
 
