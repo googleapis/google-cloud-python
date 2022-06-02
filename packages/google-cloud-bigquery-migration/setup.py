0efbe1ef1d3b7e45d332d1ee304eef1dc496c552
Anthonios Partheniou <partheniou@google.com>
2022-06-02 13:18:50 -0400

    fix(deps): require protobuf <4.0.0dev (#95)

--- packages/google-cloud-bigquery-migration/setup.py
+++ packages/google-cloud-bigquery-migration/setup.py
@@ -40,7 +40,8 @@ setuptools.setup(
     include_package_data=True,
     install_requires=(
         "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-        "proto-plus >= 1.15.0",
+        "proto-plus >= 1.15.0, <2.0.0dev",
+        "protobuf >= 3.19.0, <4.0.0dev",
     ),
     python_requires=">=3.6",
     classifiers=[
