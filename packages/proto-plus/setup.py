2c83e3857ab8a610e8d5358ea32b11442b4fd131
Anthonios Partheniou <partheniou@google.com>
2022-06-13 14:56:45 -0400

    fix(deps): allow protobuf < 5.0.0 (#324)

--- packages/proto-plus/setup.py
+++ packages/proto-plus/setup.py
@@ -36,7 +36,7 @@ setup(
     long_description=README,
     platforms="Posix; MacOS X",
     include_package_data=True,
-    install_requires=("protobuf >= 3.19.0, <4.0.0dev",),
+    install_requires=("protobuf >= 3.19.0, <5.0.0dev",),
     extras_require={
         "testing": [
             "google-api-core[grpc] >= 1.31.5",
