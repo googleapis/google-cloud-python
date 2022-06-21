af607335e48392c46613bc07c8b24a563c6f8067
Anthonios Partheniou <partheniou@google.com>
2022-06-21 13:24:31 -0400

    fix(deps): allow protobuf < 5.0.0 (#112)
    * fix(deps): allow protobuf < 5.0.0
    
    * fix: resolve import error with protobuf 4.21.1

--- packages/googleapis-common-protos/setup.py
+++ packages/googleapis-common-protos/setup.py
@@ -22,7 +22,7 @@ name = "googleapis-common-protos"
 description = "Common protobufs used in Google APIs"
 version = "1.56.2"
 release_status = "Development Status :: 5 - Production/Stable"
-dependencies = ["protobuf >= 3.15.0, <4.0.0dev"]
+dependencies = ["protobuf >= 3.15.0, <5.0.0dev"]
 
 extras_require = {"grpc": ["grpcio >= 1.0.0, <2.0.0dev"]}
 
