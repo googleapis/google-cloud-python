089044a45ac87ae5c4c020f5696236c5394e3e66
Anthonios Partheniou <partheniou@google.com>
2022-07-13 13:30:16 -0400

    fix(deps): require google-api-core>=1.32.0,>=2.8.0 (#277)
    * fix(deps): require google-api-core>=1.32.0,>=2.8.0
    
    * chore: update constraints

--- packages/google-cloud-tasks/setup.py
+++ packages/google-cloud-tasks/setup.py
@@ -24,7 +24,7 @@ description = "Cloud Tasks API API client library"
 version = "2.9.1"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 2.8.0, <3.0.0dev",
+    "google-api-core[grpc] >= 1.32.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
     "grpc-google-iam-v1 >= 0.12.4, <1.0.0dev",
     "proto-plus >= 1.15.0, <2.0.0dev",
     "protobuf >= 3.19.0, <4.0.0dev",
