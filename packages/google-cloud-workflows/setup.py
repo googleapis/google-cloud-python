b6983ce6903b2d07124eee487316bc272f3b2ca4
Anthonios Partheniou <partheniou@google.com>
2022-07-13 12:52:43 -0400

    fix(deps): require google-api-core>=1.32.0,>=2.8.0 (#198)
    * fix(deps): require google-api-core>=1.32.0,>=2.8.0
    
    * chore: update constraints

--- packages/google-cloud-workflows/setup.py
+++ packages/google-cloud-workflows/setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     platforms="Posix; MacOS X; Windows",
     include_package_data=True,
     install_requires=(
-        "google-api-core[grpc] >= 2.8.0, <3.0.0dev",
+        "google-api-core[grpc] >= 1.32.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
         "proto-plus >= 1.15.0, <2.0.0dev",
         "protobuf >= 3.19.0, <4.0.0dev",
     ),
