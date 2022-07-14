fc0127309f80db46119c4472f9d3aafc93aba4a5
Anthonios Partheniou <partheniou@google.com>
2022-07-14 17:32:35 -0400

    feat: add audience parameter (#455)
    fix(deps): require google-api-core>=1.32.0,>=2.8.0

--- packages/google-cloud-vision/google/cloud/vision_v1/services/product_search/client.py
+++ packages/google-cloud-vision/google/cloud/vision_v1/services/product_search/client.py
@@ -495,6 +495,7 @@ class ProductSearchClient(metaclass=ProductSearchClientMeta):
                 quota_project_id=client_options.quota_project_id,
                 client_info=client_info,
                 always_use_jwt_access=True,
+                api_audience=client_options.api_audience,
             )
 
     def create_product_set(
