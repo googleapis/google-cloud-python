1e8133a7c3444786941789e1f21261d157677dae
Anthonios Partheniou <partheniou@google.com>
2022-08-11 15:26:27 -0400

    fix(deps): allow protobuf < 5.0.0 (#69)
    fix(deps): require proto-plus >= 1.22.0

--- packages/google-cloud-vm-migration/setup.py
+++ packages/google-cloud-vm-migration/setup.py
@@ -26,8 +26,8 @@ url = "https://github.com/googleapis/python-vm-migration"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
     "google-api-core[grpc] >= 1.32.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
-    "proto-plus >= 1.15.0, <2.0.0dev",
-    "protobuf >= 3.19.0, <4.0.0dev",
+    "proto-plus >= 1.22.0, <2.0.0dev",
+    "protobuf >= 3.19.0, <5.0.0dev",
 ]
 
 package_root = os.path.abspath(os.path.dirname(__file__))
