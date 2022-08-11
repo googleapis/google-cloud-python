4116990fe83c93a64d38af09a53fdaf299dd74f0
Anthonios Partheniou <partheniou@google.com>
2022-08-11 14:32:12 -0400

    fix(deps): allow protobuf < 5.0.0 (#631)
    fix(deps): require proto-plus >= 1.22.0

--- packages/google-cloud-bigtable/setup.py
+++ packages/google-cloud-bigtable/setup.py
@@ -32,8 +32,8 @@ dependencies = [
     "google-api-core[grpc] >= 1.32.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
     "google-cloud-core >= 1.4.1, <3.0.0dev",
     "grpc-google-iam-v1 >= 0.12.4, <1.0.0dev",
-    "proto-plus >= 1.18.0, <2.0.0dev",
-    "protobuf >= 3.19.0, <4.0.0dev",
+    "proto-plus >= 1.22.0, <2.0.0dev",
+    "protobuf >= 3.19.0, <5.0.0dev",
 ]
 extras = {"libcst": "libcst >= 0.2.5"}
 
