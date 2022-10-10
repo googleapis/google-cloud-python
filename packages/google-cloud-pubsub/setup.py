15bd075c46672d362e706217a9316aec279dde65
Anthonios Partheniou <partheniou@google.com>
2022-10-10 16:15:40 -0400

    chore: release as 2.3.10 (#805)
    * chore: release 2.3.10
    
    Release-As: 2.3.10
    
    * mark 2.13.9 as taken

--- packages/google-cloud-pubsub/setup.py
+++ packages/google-cloud-pubsub/setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 name = "google-cloud-pubsub"
 description = "Google Cloud Pub/Sub API client library"
-version = "2.13.8"
+version = "2.13.9"
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
