fc5d57f2a22acbfebdac3377418ec2b061e2e523
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2022-10-31 15:52:06 -0400

    chore(deps): update dependencies (#261)
    * chore(deps): update dependencies
    
    * chore(deps): add comments for extra installs
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * chore(deps): update Owlbot settings
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/gcp-sphinx-docfx-yaml/owlbot.py
+++ packages/gcp-sphinx-docfx-yaml/owlbot.py
@@ -42,6 +42,7 @@ s.move(
         ".kokoro/samples/**", # no samples
         ".kokoro/test-sample*",
         ".kokoro/requirements.txt", # using custom requirements.txt file
+        ".kokoro/requirements.in", # using custom requirements.in file
         "CONTRIBUTING.rst",  # repo has a CONTRIBUTING.md
         ".github/CONTRIBUTING.md",
         ".github/PULL_REQUEST_TEMPLATE.md",
@@ -52,8 +53,3 @@ s.move(
     ],
 )
 
-s.replace(
-    ".kokoro/requirements.in",
-    r"nox",
-    "\\g<0>\ntox\nsphinx==4.5.0\nrecommonmark\ndjango==2.2\nipython\nblack==22.8.0\nparameterized==0.8.1"
-)
