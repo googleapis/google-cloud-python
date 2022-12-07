6de8028357e8a257dcbaa37fb413c2112275e280
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2022-12-07 20:13:55 +0000

    chore: added tests for page.py (#23)
    * chore: added tests for page.py
    
    * added test fixture for page.py tests
    
    * changed fixture name

--- packages/google-cloud-documentai-toolbox/tests/unit/test_page.py
+++ packages/google-cloud-documentai-toolbox/tests/unit/test_page.py
@@ -16,6 +16,14 @@
 
 
 from google.cloud.documentai_toolbox import page
+from google.cloud import documentai
+import pytest
+
+
+@pytest.fixture
+def docproto():
+    with open("tests/unit/resources/0/toolbox_invoice_test-0.json", "r") as f:
+        return documentai.Document.from_json(f.read())
 
 
 def test_table_to_csv():
@@ -82,3 +90,126 @@ def test_table_to_csv_with_empty_header_rows_and_single_body():
 Body
 """
     )
+
+
+def test_table_to_dataframe():
+    header_rows = [
+        ["This", "Is", "A", "Header", "Test"],
+        ["", "", "A", "Sub", "Header"],
+    ]
+    body_rows = [["This", "Is", "A", "Body", "Test"], ["1", "2", "3", "4", "5"]]
+    table = page.Table(
+        documentai_table=None, header_rows=header_rows, body_rows=body_rows
+    )
+    contents = table.to_dataframe()
+
+    assert len(contents.columns) == 5
+    assert len(contents.values) == 2
+
+
+def test_table_wrapper_from_documentai_table(docproto):
+    docproto_page = docproto.pages[0]
+
+    table = page._table_wrapper_from_documentai_table(
+        documentai_table=docproto_page.tables[0], text=docproto.text
+    )
+    assert len(table.body_rows) == 6
+    assert len(table.header_rows[0]) == 4
+
+
+def test_header_for_table_row_from_documentai_table_row(docproto):
+    docproto_page = docproto.pages[0]
+
+    header_row = page._table_row_from_documentai_table_row(
+        table_rows=docproto_page.tables[0].header_rows, text=docproto.text
+    )
+    assert header_row == [["Item Description", "Quantity", "Price", "Amount"]]
+
+
+def test_body_for_table_row_from_documentai_table_row(docproto):
+    docproto_page = docproto.pages[0]
+
+    body_row = page._table_row_from_documentai_table_row(
+        table_rows=docproto_page.tables[0].body_rows, text=docproto.text
+    )
+    assert body_row == [
+        ["Tool A", "500", "$1.00", "$500.00"],
+        ["Service B", "1", "$900.00", "$900.00"],
+        ["Resource C", "50", "$12.00", "$600.00"],
+        ["", "", "Subtotal", "$2000.00"],
+        ["", "", "Tax", "$140.00"],
+        ["", "", "BALANCE DUE", "$2140.00"],
+    ]
+
+
+def test_text_from_element_with_layout(docproto):
+    docproto_page = docproto.pages[0]
+
+    text = page._text_from_element_with_layout(
+        element_with_layout=docproto_page.paragraphs[0], text=docproto.text
+    )
+
+    assert text == "Invoice\n"
+
+
+def test_get_paragraphs(docproto):
+    docproto_paragraphs = docproto.pages[0].paragraphs
+
+    paragraphs = page._get_paragraphs(
+        paragraphs=docproto_paragraphs, text=docproto.text
+    )
+
+    assert len(paragraphs) == 31
+    assert paragraphs[0].text == "Invoice\n"
+
+
+def test_get_lines(docproto):
+    docproto_lines = docproto.pages[0].lines
+
+    lines = page._get_lines(lines=docproto_lines, text=docproto.text)
+
+    assert len(lines) == 37
+    assert lines[36].text == "Supplies used for Project Q.\n"
+
+
+# Class init Tests
+
+
+def test_Paragraph():
+    docai_paragraph = documentai.Document.Page.Paragraph()
+    paragraph = page.Paragraph(
+        documentai_paragraph=docai_paragraph, text="test_paragraph"
+    )
+
+    assert paragraph.text == "test_paragraph"
+
+
+def test_Line():
+    docai_line = documentai.Document.Page.Line()
+    line = page.Line(documentai_line=docai_line, text="test_line")
+
+    assert line.text == "test_line"
+
+
+def test_Table():
+    header_rows = [
+        ["This", "Is", "A", "Header", "Test"],
+        ["", "", "A", "Sub", "Header"],
+    ]
+    body_rows = [["This", "Is", "A", "Body", "Test"], ["1", "2", "3", "4", "5"]]
+    table = page.Table(
+        documentai_table=None, header_rows=header_rows, body_rows=body_rows
+    )
+
+    assert len(table.body_rows) == 2
+    assert len(table.header_rows[0]) == 5
+
+
+def test_Page(docproto):
+    docproto_page = docproto.pages[0]
+    wrapped_page = page.Page(documentai_page=docproto_page, text=docproto.text)
+
+    assert len(wrapped_page.lines) == 37
+    assert len(wrapped_page.paragraphs) == 31
+    assert wrapped_page.lines[0].text == "Invoice\n"
+    assert wrapped_page.paragraphs[30].text == "Supplies used for Project Q.\n"
