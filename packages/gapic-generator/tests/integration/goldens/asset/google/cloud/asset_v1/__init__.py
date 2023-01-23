4c097b5bc3952c0b343a8c31df501c8a7f2d7d1c
Anthonios Partheniou <partheniou@google.com>
2023-01-23 15:51:17 -0500

    fix: use gapic_version from versioned module (#1573)
    * fix: use gapic_version from versioned module
    
    * omit gapic_version.py from coverage
    
    * update golden files

--- packages/gapic-generator/tests/integration/goldens/asset/google/cloud/asset_v1/__init__.py
+++ packages/gapic-generator/tests/integration/goldens/asset/google/cloud/asset_v1/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from google.cloud.asset import gapic_version as package_version
+from google.cloud.asset_v1 import gapic_version as package_version
 
 __version__ = package_version.__version__
 
--- packages/gapic-generator/tests/integration/goldens/credentials/google/iam/credentials_v1/__init__.py
+++ packages/gapic-generator/tests/integration/goldens/credentials/google/iam/credentials_v1/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from google.iam.credentials import gapic_version as package_version
+from google.iam.credentials_v1 import gapic_version as package_version
 
 __version__ = package_version.__version__
 
