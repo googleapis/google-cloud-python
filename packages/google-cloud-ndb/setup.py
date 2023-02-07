22c434033bac4f3184ac6fafabfd97627fe5f775
Bob "Wombat" Hogg <rwhogg@google.com>
2023-02-07 11:55:39 -0500

    chore: Add Python 3.11 to Trove classifiers (#858)

--- packages/google-cloud-ndb/setup.py
+++ packages/google-cloud-ndb/setup.py
@@ -56,6 +56,7 @@ def main():
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
             "Operating System :: OS Independent",
             "Topic :: Internet",
         ],
