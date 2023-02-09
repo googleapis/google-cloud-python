42ac994c38bd7bc49526ec7568767e41ac47b242
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2023-02-09 16:41:43 -0600

    chore: add comment for snapshotted versions (#285)

--- packages/gcp-sphinx-docfx-yaml/tests/testdata/handwritten/google/cloud/storage/version.py
+++ packages/gcp-sphinx-docfx-yaml/tests/testdata/handwritten/google/cloud/storage/version.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Original snapshotted version is 2.5.0.
 __version__ = "2.0.2"
--- packages/gcp-sphinx-docfx-yaml/tests/testdata/non-cloud/pandas_gbq/version.py
+++ packages/gcp-sphinx-docfx-yaml/tests/testdata/non-cloud/pandas_gbq/version.py
@@ -2,4 +2,5 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+# Original snapshotted version is 0.19.1.
 __version__ = "2.0.2"
