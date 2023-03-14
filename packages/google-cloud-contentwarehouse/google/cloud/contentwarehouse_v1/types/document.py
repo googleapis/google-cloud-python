9fdd4a396a0fb6e8485795023bba8a6e5580cc9c
Anthonios Partheniou <partheniou@google.com>
2023-03-14 12:29:10 +0000

    Test google cloud contentwarehouse fix

--- packages/google-cloud-contentwarehouse/google/cloud/contentwarehouse_v1/types/document.py
+++ packages/google-cloud-contentwarehouse/google/cloud/contentwarehouse_v1/types/document.py
@@ -17,7 +17,7 @@ from __future__ import annotations
 
 from typing import MutableMapping, MutableSequence
 
-import google.cloud.documentai_v1.types
+from google.cloud.documentai_v1.types import document as gcd_document
 from google.protobuf import timestamp_pb2  # type: ignore
 from google.type import datetime_pb2  # type: ignore
 import proto  # type: ignore
@@ -112,7 +112,7 @@ class Document(proto.Message):
             Other document format, such as PPTX, XLXS
 
             This field is a member of `oneof`_ ``structured_content``.
-        cloud_ai_document (google.cloud.documentai_v1.types.Document):
+        cloud_ai_document (google.cloud.documentai.v1.document_pb2.Document):
             Document AI format to save the structured
             content, including OCR.
 
@@ -183,11 +183,11 @@ class Document(proto.Message):
         number=15,
         oneof="structured_content",
     )
-    cloud_ai_document: google.cloud.documentai_v1.types.Document = proto.Field(
+    cloud_ai_document: gcd_document.Document = proto.Field(
         proto.MESSAGE,
         number=4,
         oneof="structured_content",
-        message=google.cloud.documentai_v1.types.Document,
+        message=gcd_document.Document,
     )
     structured_content_uri: str = proto.Field(
         proto.STRING,
