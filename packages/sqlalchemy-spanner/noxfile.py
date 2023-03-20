92378c6ec370b66396cfdd3b69c92995ecac9fe3
Astha Mohta <35952883+asthamohta@users.noreply.github.com>
2023-03-20 11:10:25 +0530

    fix: test fix (#310)
    * chore: auto-release
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * Without changes
    
    * Update noxfile.py
    
    * changes
    
    * changes
    
    ---------
    
    Co-authored-by: surbhigarg92 <surbhigarg.92@gmail.com>

--- packages/sqlalchemy-spanner/noxfile.py
+++ packages/sqlalchemy-spanner/noxfile.py
@@ -190,9 +190,8 @@ def compliance_test_14(session):
 
     session.install("mock")
     session.install("-e", ".[tracing]")
+    session.run("pip", "install", "sqlalchemy>=1.4,<2.0", "--force-reinstall")
     session.run("python", "create_test_database.py")
-
-    session.install("sqlalchemy>=1.4")
 
     session.run(
         "py.test",
@@ -224,7 +223,7 @@ def unit(session):
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def migration_test(session):
     """Test migrations with SQLAlchemy v1.3.11+ and Alembic"""
-    session.run("pip", "install", "sqlalchemy>=1.3.11", "--force-reinstall")
+    session.run("pip", "install", "sqlalchemy>=1.3.11,<2.0", "--force-reinstall")
     _migration_test(session)
 
 
@@ -241,6 +240,11 @@ def _migration_test(session):
     import glob
     import os
     import shutil
+
+    try:
+        import sqlalchemy
+    except:
+        session.run("pip", "install", "sqlalchemy>=1.3.11,<2.0", "--force-reinstall")
 
     session.install("pytest")
     session.install("-e", ".")
@@ -307,7 +311,7 @@ def snippets(session):
         session.skip("Credentials must be set via environment variable.")
 
     session.install("pytest")
-    session.install("sqlalchemy")
+    session.install("sqlalchemy>=1.4,<2.0")
     session.install(
         "git+https://github.com/googleapis/python-spanner.git#egg=google-cloud-spanner"
     )
