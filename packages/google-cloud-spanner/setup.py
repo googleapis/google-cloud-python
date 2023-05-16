6632fcef37af578ae4565e9864fac8a4c57c2b83
Astha Mohta <35952883+asthamohta@users.noreply.github.com>
2023-05-16 12:45:54 +0530

    fix: upgrade version of sqlparse (#943)

--- packages/google-cloud-spanner/setup.py
+++ packages/google-cloud-spanner/setup.py
@@ -40,7 +40,7 @@ dependencies = [
     "google-cloud-core >= 1.4.1, < 3.0dev",
     "grpc-google-iam-v1 >= 0.12.4, <1.0.0dev",
     "proto-plus >= 1.22.0, <2.0.0dev",
-    "sqlparse >= 0.3.0",
+    "sqlparse >= 0.4.4",
     "protobuf>=3.19.5,<5.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
 ]
 extras = {
