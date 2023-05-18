b248474ac49a2c690e77363341a3116bb2a8fc59
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2023-05-18 10:52:48 -0700

    fix: filter None values from OpenTelemetry attributes (#1567)
    * fix: filter None values from OpenTelemetry attributes
    
    * moved filtering out before return value

--- packages/google-cloud-bigquery/google/cloud/bigquery/opentelemetry_tracing.py
+++ packages/google-cloud-bigquery/google/cloud/bigquery/opentelemetry_tracing.py
@@ -97,6 +97,11 @@ def _get_final_span_attributes(attributes=None, client=None, job_ref=None):
         final_attributes.update(job_attributes)
     if attributes:
         final_attributes.update(attributes)
+
+    filtered = {k: v for k, v in final_attributes.items() if v is not None}
+    final_attributes.clear()
+    final_attributes.update(filtered)
+
     return final_attributes
 
 
