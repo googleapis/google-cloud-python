761a22976626f6e53cb7f3bfeb7e87bf08bfe90e
Victor Chudnovsky <vchudnov@google.com>
2023-06-02 13:15:48 -0700

    migration: post-process
    This includes post processing for:
    google-cloud-domains google-cloud-iap

--- packages/google-cloud-domains/docs/conf.py
+++ packages/google-cloud-domains/docs/conf.py
@@ -156,7 +156,7 @@ html_theme = "alabaster"
 html_theme_options = {
     "description": "Google Cloud Client Libraries for google-cloud-domains",
     "github_user": "googleapis",
-    "github_repo": "python-domains",
+    "github_repo": "google-cloud-python",
     "github_banner": True,
     "font_family": "'Roboto', Georgia, sans",
     "head_font_family": "'Roboto', Georgia, serif",
