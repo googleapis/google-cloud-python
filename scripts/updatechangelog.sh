ad09f54211da0b57c3998d27304955e11742f983
Anthonios Partheniou <partheniou@google.com>
2023-06-23 18:59:24 +0000

    add bullet

--- scripts/updatechangelog.sh
+++ scripts/updatechangelog.sh
@@ -7,7 +7,7 @@ for package in $SCRIPT_ROOT/../packages/*/ ; do
     package_dir=$(basename $package)
     package_location=$(echo 'packages/'$package_dir)
     package_version=$(cat $SCRIPT_ROOT"/../.release-please-manifest.json" | jq -r --arg package_location "$package_location" '.[$package_location]' )
-    output+="[$package_dir==$package_version]($PACKAGES_URL/$package_dir/CHANGELOG.md)\n"    
+    output+="- [$package_dir==$package_version]($PACKAGES_URL/$package_dir/CHANGELOG.md)\n"
 done
 
 echo -e $output > $SCRIPT_ROOT/../CHANGELOG.md
