1ca3269bf9a9ad8bd7e5cc6830ea894d8ef151f6
Anthonios Partheniou <partheniou@google.com>
2023-06-23 18:52:40 +0000

    address review feedback

--- scripts/updatechangelog.sh
+++ scripts/updatechangelog.sh
@@ -5,7 +5,9 @@ PACKAGES_URL=https://github.com/googleapis/google-cloud-python/tree/main/package
 
 for package in $SCRIPT_ROOT/../packages/*/ ; do
     package_dir=$(basename $package)
-    output+="[$package_dir]("$PACKAGES_URL/$package_dir"/CHANGELOG.md)\n"    
+    package_location=$(echo 'packages/'$package_dir)
+    package_version=$(cat $SCRIPT_ROOT"/../.release-please-manifest.json" | jq -r --arg package_location "$package_location" '.[$package_location]' )
+    output+="[$package_dir==$package_version]($PACKAGES_URL/$package_dir/CHANGELOG.md)\n"    
 done
 
 echo -e $output > $SCRIPT_ROOT/../CHANGELOG.md
