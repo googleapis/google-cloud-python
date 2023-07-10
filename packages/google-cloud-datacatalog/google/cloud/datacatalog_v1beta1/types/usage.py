1c656186741efc595de5e719452308e4bae59e5a
Anthonios Partheniou <partheniou@google.com>
2023-07-10 15:31:27 +0000

    lint

--- packages/google-cloud-datacatalog/google/cloud/datacatalog_v1beta1/types/usage.py
+++ packages/google-cloud-datacatalog/google/cloud/datacatalog_v1beta1/types/usage.py
@@ -32,12 +32,12 @@ __protobuf__ = proto.module(
 class UsageStats(proto.Message):
     r"""Detailed counts on the entry's usage.
     Caveats:
-    
+
     - Only BigQuery tables have usage stats
     - The usage stats only include BigQuery query jobs
     - The usage stats might be underestimated, e.g. wildcard table
-    references are not yet counted in usage computation
-    https://cloud.google.com/bigquery/docs/querying-wildcard-tables
+      references are not yet counted in usage computation
+      https://cloud.google.com/bigquery/docs/querying-wildcard-tables
 
     Attributes:
         total_completions (float):
