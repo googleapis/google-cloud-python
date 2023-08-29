2a60dd773a8c576c8a069b76320498162ccc4d9e
Tim Swast <swast@google.com>
2023-08-29 11:40:36 -0500

    chore: add templated GitHub config files (#12)
    Pulled manually from https://github.com/googleapis/synthtool/tree/master/synthtool/gcp/templates/python_library/.github
    
    Change-Id: I7f36912dffb427af2cd388abb8109670ac162701

---
+++ packages/bigframes/owlbot.py
@@ -0,0 +1,68 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import pathlib
+
+from synthtool import gcp
+import synthtool as s
+from synthtool.languages import python
+
+REPO_ROOT = pathlib.Path(__file__).parent.absolute()
+
+common = gcp.CommonTemplates()
+
+# ----------------------------------------------------------------------------
+# Add templated files
+# ----------------------------------------------------------------------------
+
+templated_files = common.py_library(
+    unit_test_python_versions=["3.9", "3.10", "3.11"],
+    system_test_python_versions=["3.9", "3.11"],
+    cov_level=40,
+    intersphinx_dependencies={
+        "pandas": "https://pandas.pydata.org/pandas-docs/stable/",
+        "pydata-google-auth": "https://pydata-google-auth.readthedocs.io/en/latest/",
+    },
+)
+s.move(
+    templated_files,
+    excludes=[
+        # Multi-processing note isn't relevant, as pandas_gbq is responsible for
+        # creating clients, not the end user.
+        "docs/multiprocessing.rst",
+        "noxfile.py",
+        "README.rst",
+    ],
+)
+
+# ----------------------------------------------------------------------------
+# Fixup files
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+# Samples templates
+# ----------------------------------------------------------------------------
+
+python.py_samples(skip_readmes=True)
+
+# ----------------------------------------------------------------------------
+# Final cleanup
+# ----------------------------------------------------------------------------
+
+s.shell.run(["nox", "-s", "blacken"], hide_output=False)
+for noxfile in REPO_ROOT.glob("samples/**/noxfile.py"):
+    s.shell.run(["nox", "-s", "blacken"], cwd=noxfile.parent, hide_output=False)
