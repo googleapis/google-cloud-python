4ac03586b552448eda5d62e2109fd9dd18ef83d9
Anthonios Partheniou <partheniou@google.com>
2023-09-20 00:38:10 +0000

    remove whitespace

--- packages/google-ai-generativelanguage/google/ai/generativelanguage_v1beta3/services/permission_service/async_client.py
+++ packages/google-ai-generativelanguage/google/ai/generativelanguage_v1beta3/services/permission_service/async_client.py
@@ -305,11 +305,11 @@ class PermissionServiceAsyncClient:
                 role is a superset of the previous
                 role's permitted operations:
 
-                 - reader can use the resource (e.g.
+                - reader can use the resource (e.g.
                   tuned model) for inference
-                 - writer has reader's permissions and
+                - writer has reader's permissions and
                   additionally can edit and share
-                 - owner has writer's permissions and
+                - owner has writer's permissions and
                   additionally can delete
 
         """
@@ -432,11 +432,11 @@ class PermissionServiceAsyncClient:
                 role is a superset of the previous
                 role's permitted operations:
 
-                 - reader can use the resource (e.g.
+                - reader can use the resource (e.g.
                   tuned model) for inference
-                 - writer has reader's permissions and
+                - writer has reader's permissions and
                   additionally can edit and share
-                 - owner has writer's permissions and
+                - owner has writer's permissions and
                   additionally can delete
 
         """
@@ -682,11 +682,11 @@ class PermissionServiceAsyncClient:
                 role is a superset of the previous
                 role's permitted operations:
 
-                 - reader can use the resource (e.g.
+                - reader can use the resource (e.g.
                   tuned model) for inference
-                 - writer has reader's permissions and
+                - writer has reader's permissions and
                   additionally can edit and share
-                 - owner has writer's permissions and
+                - owner has writer's permissions and
                   additionally can delete
 
         """
