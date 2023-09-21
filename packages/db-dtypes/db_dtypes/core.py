f6ddbe1c67eea675e899d0936d07f95d857d3319
Lingqing Gan <lingqing.gan@gmail.com>
2023-09-21 08:40:29 -0400

    bug: fixes flake8==6.1.0 bug, unpins flake8==6.0.0 (#211)
    * bug: fixes flake8==6.1.0 bug
    
    * fix errors
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/db-dtypes/db_dtypes/core.py
+++ packages/db-dtypes/db_dtypes/core.py
@@ -90,10 +90,12 @@ class BaseDatetimeArray(
     def _cmp_method(self, other, op):
         """Compare array values, for use in OpsMixin."""
 
-        if is_scalar(other) and (pandas.isna(other) or type(other) == self.dtype.type):
+        if is_scalar(other) and (
+            pandas.isna(other) or isinstance(other, self.dtype.type)
+        ):
             other = type(self)([other])
 
-        if type(other) != type(self):
+        if type(other) is not type(self):
             return NotImplemented
 
         oshape = getattr(other, "shape", None)
