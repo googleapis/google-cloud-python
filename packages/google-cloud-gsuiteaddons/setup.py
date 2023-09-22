d3563be586c94d22d745a69ba01bd3973e3830ed
Anthonios Partheniou <partheniou@google.com>
2023-09-22 11:23:16 -0400

    chore: use standard owlbot.py (#54)

--- packages/google-cloud-gsuiteaddons/setup.py
+++ packages/google-cloud-gsuiteaddons/setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ dependencies = [
     "proto-plus >= 1.22.0, <2.0.0dev",
     "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
     "protobuf>=3.19.5,<5.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
-    "google-apps-script-type>=0.2.0, <1.0.0dev",
+    "google-apps-script-type >= 0.2.0, <1.0.0dev",
 ]
 url = "https://github.com/googleapis/python-gsuiteaddons"
 
@@ -58,9 +58,7 @@ packages = [
     if package.startswith("google")
 ]
 
-namespaces = ["google"]
-if "google.cloud" in packages:
-    namespaces.append("google.cloud")
+namespaces = ["google", "google.cloud"]
 
 setuptools.setup(
     name=name,
