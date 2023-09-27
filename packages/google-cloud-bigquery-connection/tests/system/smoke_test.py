7a381729ca6a3d1cb4dd4192c7a51f39eda9ad14
Anthonios Partheniou <partheniou@google.com>
2023-09-27 01:26:49 +0000

    lint

--- packages/google-cloud-bigquery-connection/tests/system/smoke_test.py
+++ packages/google-cloud-bigquery-connection/tests/system/smoke_test.py
@@ -30,7 +30,7 @@ def project_id():
 def test_list_connections(project_id: str, transport: str):
     client = bigquery_connection_v1.ConnectionServiceClient(transport=transport)
 
-    parent = client.common_location_path(project_id, location='us-central1')
+    parent = client.common_location_path(project_id, location="us-central1")
     client.list_connections(parent=parent)
 
     # The purpose of this smoke test is to test the communication with the API server,
