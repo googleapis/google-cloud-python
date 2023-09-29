4668285584829c0b316482d9e3e73d146208a8c4
Anthonios Partheniou <partheniou@google.com>
2023-09-29 23:20:32 +0000

    migration: post-process
    This includes post processing for:
    google-cloud-bigquery-connection google-cloud-bigquery-datatransfer google-cloud-bigquery-reservation google-cloud-container google-cloud-dataproc google-cloud-dlp google-cloud-gsuiteaddons google-cloud-os-config google-cloud-scheduler google-cloud-tasks google-cloud-texttospeech google-cloud-videointelligence

--- packages/google-cloud-videointelligence/docs/conf.py
+++ packages/google-cloud-videointelligence/docs/conf.py
@@ -156,7 +156,7 @@ html_theme = "alabaster"
 html_theme_options = {
     "description": "Google Cloud Client Libraries for google-cloud-videointelligence",
     "github_user": "googleapis",
-    "github_repo": "python-videointelligence",
+    "github_repo": "google-cloud-python",
     "github_banner": True,
     "font_family": "'Roboto', Georgia, sans",
     "head_font_family": "'Roboto', Georgia, serif",
