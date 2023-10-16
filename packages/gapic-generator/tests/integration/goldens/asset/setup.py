e3fb617fd0c94350f86c19bba8ea21734ec25dfd
Anthonios Partheniou <partheniou@google.com>
2023-10-16 16:19:48 -0400

    fix: add missing dependencies (#1804)

--- packages/gapic-generator/tests/integration/goldens/asset/setup.py
+++ packages/gapic-generator/tests/integration/goldens/asset/setup.py
@@ -40,6 +40,8 @@ dependencies = [
     "proto-plus >= 1.22.0, <2.0.0dev",
     "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
     "protobuf>=3.19.5,<5.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
+    "google-cloud-access-context-manager >= 0.1.2, <1.0.0dev",
+    "google-cloud-os-config >= 1.0.0, <2.0.0dev",
     "grpc-google-iam-v1 >= 0.12.4, <1.0.0dev",
 ]
 url = "https://github.com/googleapis/python-asset"
