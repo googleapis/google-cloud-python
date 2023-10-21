e218d38d30eb72a7198d4c926e0a0c80934575c0
Ashley Xu <139821907+ashleyxuu@users.noreply.github.com>
2023-10-21 10:33:03 -0700

    test: add sample code for use BigFrames developer guide (#118)
    * test: add sample code for use BigFrames developer guide

---
+++ packages/bigframes/samples/snippets/pandas_methods_test.py
@@ -0,0 +1,34 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def test_bigquery_dataframes_pandas_methods():
+    # [START bigquery_dataframes_pandas_methods]
+    import bigframes.pandas as bpd
+
+    # Load data from BigQuery
+    query_or_table = "bigquery-public-data.ml_datasets.penguins"
+    bq_df = bpd.read_gbq(query_or_table)
+
+    # Inspect one of the columns (or series) of the DataFrame:
+    bq_df["body_mass_g"].head(10)
+
+    # Compute the mean of this series:
+    average_body_mass = bq_df["body_mass_g"].mean()
+    print(f"average_body_mass: {average_body_mass}")
+
+    # Calculate the mean body_mass_g by species using the groupby operation:
+    bq_df["body_mass_g"].groupby(by=bq_df["species"]).mean().head()
+    # [END bigquery_dataframes_pandas_methods]
+    assert average_body_mass is not None
