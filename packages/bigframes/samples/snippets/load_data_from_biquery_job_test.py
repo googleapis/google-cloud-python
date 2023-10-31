3a6ffa093db3cec3e18dbef14d4b8886c0991a5b
Ashley Xu <139821907+ashleyxuu@users.noreply.github.com>
2023-10-31 10:54:17 -0700

    test: add code snippets for loading data from BigQuery Job (#154)
    * test: add code snippets for loading data from BigQuery Job
    
    * fix: address the comments
    
    * fix: fix the broken test
    
    * use BigQuery Client library to get the job_id
    
    * feat: Implement operator `@` for `DataFrame.dot` (#139)
    
    Thank you for opening a Pull Request! Before submitting your PR, there are a few things you can do to make sure it goes smoothly:
    - [ ] Make sure to open an issue as a [bug/issue](https://togithub.com/googleapis/python-bigquery-dataframes/issues/new/choose) before writing your code!  That way we can discuss the change, evaluate designs, and agree on the general idea
    - [ ] Ensure the tests and linter pass
    - [ ] Code coverage does not decrease (if any source code was changed)
    - [ ] Appropriate docs were updated (if necessary)
    
    Fixes b/297502513 🦕
    
    * fix: fix the comments
    
    ---------
    
    Co-authored-by: Shobhit Singh <shobs@google.com>

---
+++ packages/bigframes/samples/snippets/load_data_from_biquery_job_test.py
@@ -0,0 +1,51 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def test_bigquery_dataframes_load_data_from_bigquery_job():
+    from google.cloud import bigquery
+
+    # Construct a BigQuery client object.
+    client = bigquery.Client(project="bigframes-dev", location="us")
+
+    query = """
+        SELECT *
+        FROM `bigquery-public-data.ml_datasets.penguins`
+        LIMIT 20
+    """
+    query_job = client.query(query)
+    JOB_ID = query_job.job_id
+    your_project_id = "bigframes-dev"
+
+    # [START bigquery_dataframes_load_data_from_bigquery_job]
+    from google.cloud import bigquery
+
+    import bigframes.pandas as bpd
+
+    # Project ID inserted based on the query results selected to explore
+    project = your_project_id
+    # Location inserted based on the query results selected to explore
+    location = "us"
+    client = bigquery.Client(project=project, location=location)
+
+    # Job ID inserted based on the query results selcted to explore
+    job_id = JOB_ID
+    job = client.get_job(job_id)
+    destination = str(job.destination)
+
+    # Load data from a BigQuery table using BigFrames DataFrames:
+    bq_df = bpd.read_gbq_table(destination)
+
+    # [END bigquery_dataframes_load_data_from_bigquery_job]
+    assert bq_df is not None
