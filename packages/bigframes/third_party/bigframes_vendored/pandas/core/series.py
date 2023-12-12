b1b66dcf189b379938ba33c173ed7f602c4b856f
Ataf Fazledin Ahamed <ataf@openrefactory.com>
2023-12-13 02:32:54 +0600

    fix: replaced raise `NotImplementedError` with return `NotImplemented` (#258)
    Signed-off-by: fazledyn-or <ataf@openrefactory.com>
    Co-authored-by: Tim Swast <swast@google.com>

--- packages/bigframes/third_party/bigframes_vendored/pandas/core/series.py
+++ packages/bigframes/third_party/bigframes_vendored/pandas/core/series.py
@@ -677,13 +677,13 @@ class Series(NDFrame):  # type: ignore[misc]
         """
         Matrix multiplication using binary `@` operator in Python>=3.5.
         """
-        raise NotImplementedError(constants.ABSTRACT_METHOD_ERROR_MESSAGE)
+        return NotImplemented
 
     def __rmatmul__(self, other):
         """
         Matrix multiplication using binary `@` operator in Python>=3.5.
         """
-        raise NotImplementedError(constants.ABSTRACT_METHOD_ERROR_MESSAGE)
+        return NotImplemented
 
     def sort_values(
         self,
