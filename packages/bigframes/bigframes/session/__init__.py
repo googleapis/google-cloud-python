0726c223ec265ab1d0602ece264cdac5c1227c16
Garrett Wu <6505921+GarrettWu@users.noreply.github.com>
2023-12-12 16:56:15 -0800

    chore: filter errors explicitly caught in BF dashboard (#270)
    plx script change
    https://plx.corp.google.com/scripts2/script_68._e922fb_55ab_4c4d_9046_85fc1c4c7467
    
    Line 43 - 47: +
    
        AND (
          SELECT l.value
            FROM UNNEST(configuration.labels.label) AS l
            WHERE l.key = "error_caught"
        ) != "True"

--- packages/bigframes/bigframes/session/__init__.py
+++ packages/bigframes/bigframes/session/__init__.py
@@ -294,6 +294,7 @@ class Session(
         try:
             # Write to temp table to workaround BigQuery 10 GB query results
             # limit. See: internal issue 303057336.
+            job_config.labels["error_caught"] = "True"
             _, query_job = self._start_query(query, job_config=job_config)
             return query_job.destination, query_job
         except google.api_core.exceptions.BadRequest:
