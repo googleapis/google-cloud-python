02c9b265764b4e673e64a0aaa05278c2eff91665
Anthonios Partheniou <partheniou@google.com>
2024-01-25 12:00:03 +0000

    chore: See https://github.com/googleapis/gapic-generator-python/issues/1916

--- packages/google-analytics-admin/google/analytics/admin_v1alpha/services/analytics_admin_service/client.py
+++ packages/google-analytics-admin/google/analytics/admin_v1alpha/services/analytics_admin_service/client.py
@@ -11381,7 +11381,7 @@ class AnalyticsAdminServiceClient(metaclass=AnalyticsAdminServiceClientMeta):
                 metric's resource name.
 
                 This value should be 1-80 characters and valid
-                characters are /[a-zA-Z0-9_]/, no spaces allowed.
+                characters are `[a-zA-Z0-9_]`, no spaces allowed.
                 calculated_metric_id must be unique between all
                 calculated metrics under a property. The
                 calculated_metric_id is used when referencing this
