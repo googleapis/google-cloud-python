2c1207480a0455a70355d4064522ae6f2e3ab71d
Tim Swast <swast@google.com>
2024-01-29 16:30:16 -0600

    chore: pin pytest version (#358)
    Thank you for opening a Pull Request! Before submitting your PR, there are a few things you can do to make sure it goes smoothly:
    - [ ] Make sure to open an issue as a [bug/issue](https://togithub.com/googleapis/python-bigquery-dataframes/issues/new/choose) before writing your code!  That way we can discuss the change, evaluate designs, and agree on the general idea
    - [ ] Ensure the tests and linter pass
    - [ ] Code coverage does not decrease (if any source code was changed)
    - [ ] Appropriate docs were updated (if necessary)
    
    Fixes #<issue_number_goes_here> 🦕

--- packages/bigframes/noxfile.py
+++ packages/bigframes/noxfile.py
@@ -30,6 +30,10 @@ import nox.sessions
 
 BLACK_VERSION = "black==22.3.0"
 ISORT_VERSION = "isort==5.12.0"
+
+# pytest-retry is not yet compatible with pytest 8.x.
+# https://github.com/str0zzapreti/pytest-retry/issues/32
+PYTEST_VERSION = "pytest<8.0.0dev"
 SPHINX_VERSION = "sphinx==4.5.0"
 LINT_PATHS = ["docs", "bigframes", "tests", "third_party", "noxfile.py", "setup.py"]
 
@@ -39,7 +43,7 @@ UNIT_TEST_PYTHON_VERSIONS = ["3.9", "3.10", "3.11"]
 UNIT_TEST_STANDARD_DEPENDENCIES = [
     "mock",
     "asyncmock",
-    "pytest",
+    PYTEST_VERSION,
     "pytest-cov",
     "pytest-asyncio",
     "pytest-mock",
@@ -55,7 +59,7 @@ SYSTEM_TEST_STANDARD_DEPENDENCIES = [
     "jinja2",
     "mock",
     "openpyxl",
-    "pytest",
+    PYTEST_VERSION,
     "pytest-cov",
     "pytest-retry",
     "pytest-timeout",
