107d5eb466318a3521f91dfa47265224f3b067fb
mukund-ananthu <83691193+mukund-ananthu@users.noreply.github.com>
2024-02-08 17:47:53 -0500

    fix: add google-auth as a direct dependency (#1076)

--- packages/google-cloud-pubsub/setup.py
+++ packages/google-cloud-pubsub/setup.py
@@ -38,6 +38,7 @@ else:
 dependencies = [
     "grpcio >= 1.51.3, < 2.0dev",  # https://github.com/googleapis/python-pubsub/issues/609
     # google-api-core >= 1.34.0 is allowed in order to support google-api-core 1.x
+    "google-auth >= 2.14.1, <3.0.0dev",
     "google-api-core[grpc] >= 1.34.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,!=2.8.*,!=2.9.*,!=2.10.*",
     "proto-plus >= 1.22.0, <2.0.0dev",
     "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
