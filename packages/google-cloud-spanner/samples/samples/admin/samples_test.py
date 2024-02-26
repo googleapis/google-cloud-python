f013a213c7cca3dc51a2958aec490e83969407ff
rahul2393 <irahul@google.com>
2024-02-26 12:26:48 +0530

    docs: samples and tests for admin database APIs (#1099)
    * docs: samples and tests for admin database APIs
    
    * rebase branch with main
    
    * remove backup samples
    
    * add more database samples
    
    * remove unused import
    
    * add more samples
    
    * incorporate suggestions
    
    * fix tests
    
    * incorporate suggestions
    
    * fix tests
    
    * remove parallel run
    
    ---------
    
    Co-authored-by: Sri Harsha CH <57220027+harshachinta@users.noreply.github.com>

--- packages/google-cloud-spanner/samples/samples/admin/samples_test.py
+++ packages/google-cloud-spanner/samples/samples/admin/samples_test.py
@@ -23,6 +23,7 @@ import uuid
 
 import pytest
 from google.api_core import exceptions
+from google.cloud import spanner
 from google.cloud.spanner_admin_database_v1.types.common import DatabaseDialect
 from test_utils.retry import RetryErrors
 
@@ -127,6 +128,40 @@ def test_create_instance_explicit(spanner_client, create_instance_id):
     retry_429(instance.delete)()
 
 
+def test_create_instance_with_processing_units(capsys, lci_instance_id):
+    processing_units = 500
+    retry_429(samples.create_instance_with_processing_units)(
+        lci_instance_id,
+        processing_units,
+    )
+    out, _ = capsys.readouterr()
+    assert lci_instance_id in out
+    assert "{} processing units".format(processing_units) in out
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(lci_instance_id)
+    retry_429(instance.delete)()
+
+
+def test_create_database_explicit(sample_instance, create_database_id):
+    # Rather than re-use 'sample_database', we create a new database, to
+    # ensure that the 'create_database' snippet is tested.
+    samples.create_database(sample_instance.instance_id, create_database_id)
+    database = sample_instance.database(create_database_id)
+    database.drop()
+
+
+def test_create_database_with_encryption_config(
+    capsys, instance_id, cmek_database_id, kms_key_name
+):
+    samples.create_database_with_encryption_key(
+        instance_id, cmek_database_id, kms_key_name
+    )
+    out, _ = capsys.readouterr()
+    assert cmek_database_id in out
+    assert kms_key_name in out
+
+
+@pytest.mark.dependency(name="create_database_with_default_leader")
 def test_create_database_with_default_leader(
     capsys,
     multi_region_instance,
@@ -141,3 +176,180 @@ def test_create_database_with_default_leader(
     out, _ = capsys.readouterr()
     assert default_leader_database_id in out
     assert default_leader in out
+
+
+@pytest.mark.dependency(depends=["create_database_with_default_leader"])
+def test_update_database_with_default_leader(
+    capsys,
+    multi_region_instance,
+    multi_region_instance_id,
+    default_leader_database_id,
+    default_leader,
+):
+    retry_429 = RetryErrors(exceptions.ResourceExhausted, delay=15)
+    retry_429(samples.update_database_with_default_leader)(
+        multi_region_instance_id, default_leader_database_id, default_leader
+    )
+    out, _ = capsys.readouterr()
+    assert default_leader_database_id in out
+    assert default_leader in out
+
+
+def test_update_database(capsys, instance_id, sample_database):
+    samples.update_database(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Updated database {}.".format(sample_database.name) in out
+
+    # Cleanup
+    sample_database.enable_drop_protection = False
+    op = sample_database.update(["enable_drop_protection"])
+    op.result()
+
+
+@pytest.mark.dependency(
+    name="add_and_drop_database_roles", depends=["create_table_with_datatypes"]
+)
+def test_add_and_drop_database_roles(capsys, instance_id, sample_database):
+    samples.add_and_drop_database_roles(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Created roles new_parent and new_child and granted privileges" in out
+    assert "Revoked privileges and dropped role new_child" in out
+
+
+@pytest.mark.dependency(name="create_table_with_datatypes")
+def test_create_table_with_datatypes(capsys, instance_id, sample_database):
+    samples.create_table_with_datatypes(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Created Venues table on database" in out
+
+
+@pytest.mark.dependency(name="create_table_with_timestamp")
+def test_create_table_with_timestamp(capsys, instance_id, sample_database):
+    samples.create_table_with_timestamp(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Created Performances table on database" in out
+
+
+@pytest.mark.dependency(
+    name="add_json_column",
+    depends=["create_table_with_datatypes"],
+)
+def test_add_json_column(capsys, instance_id, sample_database):
+    samples.add_json_column(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert 'Altered table "Venues" on database ' in out
+
+
+@pytest.mark.dependency(
+    name="add_numeric_column",
+    depends=["create_table_with_datatypes"],
+)
+def test_add_numeric_column(capsys, instance_id, sample_database):
+    samples.add_numeric_column(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert 'Altered table "Venues" on database ' in out
+
+
+@pytest.mark.dependency(name="create_table_with_foreign_key_delete_cascade")
+def test_create_table_with_foreign_key_delete_cascade(
+    capsys, instance_id, sample_database
+):
+    samples.create_table_with_foreign_key_delete_cascade(
+        instance_id, sample_database.database_id
+    )
+    out, _ = capsys.readouterr()
+    assert (
+        "Created Customers and ShoppingCarts table with FKShoppingCartsCustomerId"
+        in out
+    )
+
+
+@pytest.mark.dependency(
+    name="alter_table_with_foreign_key_delete_cascade",
+    depends=["create_table_with_foreign_key_delete_cascade"],
+)
+def test_alter_table_with_foreign_key_delete_cascade(
+    capsys, instance_id, sample_database
+):
+    samples.alter_table_with_foreign_key_delete_cascade(
+        instance_id, sample_database.database_id
+    )
+    out, _ = capsys.readouterr()
+    assert "Altered ShoppingCarts table with FKShoppingCartsCustomerName" in out
+
+
+@pytest.mark.dependency(depends=["alter_table_with_foreign_key_delete_cascade"])
+def test_drop_foreign_key_contraint_delete_cascade(
+    capsys, instance_id, sample_database
+):
+    samples.drop_foreign_key_constraint_delete_cascade(
+        instance_id, sample_database.database_id
+    )
+    out, _ = capsys.readouterr()
+    assert "Altered ShoppingCarts table to drop FKShoppingCartsCustomerName" in out
+
+
+@pytest.mark.dependency(name="create_sequence")
+def test_create_sequence(capsys, instance_id, bit_reverse_sequence_database):
+    samples.create_sequence(instance_id, bit_reverse_sequence_database.database_id)
+    out, _ = capsys.readouterr()
+    assert (
+        "Created Seq sequence and Customers table, where the key column CustomerId uses the sequence as a default value on database"
+        in out
+    )
+    assert "Number of customer records inserted is 3" in out
+    assert "Inserted customer record with Customer Id:" in out
+
+
+@pytest.mark.dependency(depends=["create_sequence"])
+def test_alter_sequence(capsys, instance_id, bit_reverse_sequence_database):
+    samples.alter_sequence(instance_id, bit_reverse_sequence_database.database_id)
+    out, _ = capsys.readouterr()
+    assert (
+        "Altered Seq sequence to skip an inclusive range between 1000 and 5000000 on database"
+        in out
+    )
+    assert "Number of customer records inserted is 3" in out
+    assert "Inserted customer record with Customer Id:" in out
+
+
+@pytest.mark.dependency(depends=["alter_sequence"])
+def test_drop_sequence(capsys, instance_id, bit_reverse_sequence_database):
+    samples.drop_sequence(instance_id, bit_reverse_sequence_database.database_id)
+    out, _ = capsys.readouterr()
+    assert (
+        "Altered Customers table to drop DEFAULT from CustomerId column and dropped the Seq sequence on database"
+        in out
+    )
+
+
+@pytest.mark.dependency(name="add_column", depends=["create_table_with_datatypes"])
+def test_add_column(capsys, instance_id, sample_database):
+    samples.add_column(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Added the MarketingBudget column." in out
+
+
+@pytest.mark.dependency(
+    name="add_timestamp_column", depends=["create_table_with_datatypes"]
+)
+def test_add_timestamp_column(capsys, instance_id, sample_database):
+    samples.add_timestamp_column(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert 'Altered table "Albums" on database ' in out
+
+
+@pytest.mark.dependency(name="add_index", depends=["create_table_with_datatypes"])
+def test_add_index(capsys, instance_id, sample_database):
+    samples.add_index(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Added the AlbumsByAlbumTitle index" in out
+
+
+@pytest.mark.dependency(
+    name="add_storing_index", depends=["create_table_with_datatypes"]
+)
+def test_add_storing_index(capsys, instance_id, sample_database):
+    samples.add_storing_index(instance_id, sample_database.database_id)
+    out, _ = capsys.readouterr()
+    assert "Added the AlbumsByAlbumTitle2 index." in out
