4b9ca90bce2b49de77922d629ab1db4fc1e87f9e
Salem Jorden <115185670+SalemJorden@users.noreply.github.com>
2024-02-27 15:14:00 -0600

    docs: add a code sample for creating a kmeans model (#267)
    * k-means code sample
    
    * formatting
    
    * added test
    
    * docs: add code sampke for creating kmeans model
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * license header + region tags added
    
    * Update samples/snippets/create_kmeans_model_test.py
    
    Co-authored-by: Tim Swast <swast@google.com>
    
    * code corrections resolved
    
    * code corrections commit 1
    
    * descriptions of geospatial analysis functions
    
    * explantions revised for clarity
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Update samples/snippets/create_kmeans_model_test.py
    
    Co-authored-by: Tim Swast <swast@google.com>
    
    * code corrections
    
    * code revision
    
    * code changes
    
    * revisions
    
    * expected output previews
    
    * revisions
    
    * tests passing, expected output characters >80
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * column wrapping
    
    * reset session before running code smaples
    
    * Update samples/snippets/create_kmeans_model_test.py
    
    Co-authored-by: Tim Swast <swast@google.com>
    
    * predict function added to tutorial
    
    * replaced project_id with model_id
    
    * reformatting
    
    * reformat
    
    ---------
    
    Co-authored-by: Salem Boyland <salemb@google.com>
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Tim Swast <swast@google.com>

--- packages/bigframes/samples/snippets/conftest.py
+++ packages/bigframes/samples/snippets/conftest.py
@@ -18,6 +18,8 @@ from google.cloud import bigquery
 import pytest
 import test_utils.prefixer
 
+import bigframes.pandas as bpd
+
 prefixer = test_utils.prefixer.Prefixer(
     "python-bigquery-dataframes", "samples/snippets"
 )
@@ -43,11 +45,32 @@ def project_id(bigquery_client: bigquery.Client) -> str:
     return bigquery_client.project
 
 
+@pytest.fixture(autouse=True)
+def reset_session():
+    """An autouse fixture ensuring each sample runs in a fresh session.
+
+    This allows us to have samples that query data in different locations.
+    """
+    bpd.reset_session()
+    bpd.options.bigquery.location = None
+
+
 @pytest.fixture(scope="session")
 def dataset_id(bigquery_client: bigquery.Client, project_id: str) -> Iterator[str]:
     dataset_id = prefixer.create_prefix()
     full_dataset_id = f"{project_id}.{dataset_id}"
     dataset = bigquery.Dataset(full_dataset_id)
+    bigquery_client.create_dataset(dataset)
+    yield dataset_id
+    bigquery_client.delete_dataset(dataset, delete_contents=True, not_found_ok=True)
+
+
+@pytest.fixture(scope="session")
+def dataset_id_eu(bigquery_client: bigquery.Client, project_id: str) -> Iterator[str]:
+    dataset_id = prefixer.create_prefix()
+    full_dataset_id = f"{project_id}.{dataset_id}"
+    dataset = bigquery.Dataset(full_dataset_id)
+    dataset.location = "EU"
     bigquery_client.create_dataset(dataset)
     yield dataset_id
     bigquery_client.delete_dataset(dataset, delete_contents=True, not_found_ok=True)
@@ -62,5 +85,19 @@ def random_model_id(
     """
     random_model_id = prefixer.create_prefix()
     full_model_id = f"{project_id}.{dataset_id}.{random_model_id}"
+    yield full_model_id
+    bigquery_client.delete_model(full_model_id, not_found_ok=True)
+
+
+@pytest.fixture
+def random_model_id_eu(
+    bigquery_client: bigquery.Client, project_id: str, dataset_id_eu: str
+) -> Iterator[str]:
+    """
+    Create a new table ID each time, so random_model_id_eu can be used
+    as a target for load jobs.
+    """
+    random_model_id_eu = prefixer.create_prefix()
+    full_model_id = f"{project_id}.{dataset_id_eu}.{random_model_id_eu}"
     yield full_model_id
     bigquery_client.delete_model(full_model_id, not_found_ok=True)
