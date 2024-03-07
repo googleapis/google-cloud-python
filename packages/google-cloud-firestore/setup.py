5f64a0e396b037be2a4f4fa199361d302aaf64c7
Anthonios Partheniou <partheniou@google.com>
2024-03-07 08:14:20 -0500

    fix: add google-auth as a direct dependency (#875)
    * fix: add google-auth as a direct dependency
    
    * update warning filter for grpc

--- packages/google-cloud-firestore/setup.py
+++ packages/google-cloud-firestore/setup.py
@@ -32,6 +32,9 @@ version = version["__version__"]
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
     "google-api-core[grpc] >= 1.34.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,!=2.8.*,!=2.9.*,!=2.10.*",
+    # Exclude incompatible versions of `google-auth`
+    # See https://github.com/googleapis/google-cloud-python/issues/12364
+    "google-auth >= 2.14.1, <3.0.0dev,!=2.24.0,!=2.25.0",
     "google-cloud-core >= 1.4.1, <3.0.0dev",
     "proto-plus >= 1.22.0, <2.0.0dev",
     "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
