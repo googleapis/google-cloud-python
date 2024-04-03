bd974012b3520c61a6808dabb4a0727a6fbc5d03
Garrett Wu <6505921+GarrettWu@users.noreply.github.com>
2024-04-03 10:34:04 -0700

    refactor: remove to_pandas in ml.metrics.roc_auc_score (#560)
    * refactor: remove to_pandas in ml.metrics.roc_auc_score
    
    * fix mypy

--- packages/bigframes/bigframes/ml/metrics/_metrics.py
+++ packages/bigframes/bigframes/ml/metrics/_metrics.py
@@ -161,14 +161,10 @@ def roc_auc_score(
 
     fpr, tpr, _ = roc_curve(y_true_series, y_score_series, drop_intermediate=False)
 
-    # TODO(bmil): remove this once bigframes supports the necessary operations
-    pd_fpr = fpr.to_pandas()
-    pd_tpr = tpr.to_pandas()
-
     # Use the trapezoid rule to compute the area under the ROC curve
-    width_diff = pd_fpr.diff().iloc[1:].reset_index(drop=True)
-    height_avg = (pd_tpr.iloc[:-1] + pd_tpr.iloc[1:].reset_index(drop=True)) / 2
-    return (width_diff * height_avg).sum()
+    width_diff = fpr.diff().iloc[1:].reset_index(drop=True)
+    height_avg = (tpr.iloc[:-1] + tpr.iloc[1:].reset_index(drop=True)) / 2
+    return typing.cast(float, (width_diff * height_avg).sum())
 
 
 roc_auc_score.__doc__ = inspect.getdoc(vendored_metrics_ranking.roc_auc_score)
