b229b0e86d3e979802881504a49b3bc11a7d6ae2
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-04-26 05:01:43 -0700

    fix: increase upper limit for protobuf 5.X versions (#212)

--- packages/googleapis-common-protos/setup.py
+++ packages/googleapis-common-protos/setup.py
@@ -24,7 +24,7 @@ description = "Common protobufs used in Google APIs"
 version = "1.63.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "protobuf>=3.19.5,<5.0.0.dev0,!=3.20.0,!=3.20.1,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
+    "protobuf>=3.19.5,<6.0.0.dev0,!=3.20.0,!=3.20.1,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
 ]
 
 extras_require = {"grpc": ["grpcio >= 1.44.0, <2.0.0.dev0"]}
