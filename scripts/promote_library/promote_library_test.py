b579305d7d6ea5a947201a32fa30db63b3fcd0c1
Anthonios Partheniou <partheniou@google.com>
2024-05-27 20:07:33 +0000

    chore(python): Add github action to promote libraries to stable

---
+++ scripts/promote_library/promote_library_test.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+
+class TestPromoteLibrary(unittest.TestCase):
+
+    def test_promote_packages_to_stable(self):
+        # TODO
+        assert False
+
+    def test_protote_to_stable_in_repo_metadata(self):
+        # TODO
+        assert False
+
+    def test_is_library_major_version_zero(self):
+        # TODO
+        assert False
+
+    def test_days_since_library_launch(self):
+        # TODO
+        assert False
+
+    def test_get_stable_clients(self):
+        # TODO
+        assert False
