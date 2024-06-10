a9582cebc36c596b83e5d7fb826bd70e0cf28c3c
Anthonios Partheniou <partheniou@google.com>
2024-06-10 18:24:32 +0000

    See https://github.com/googleapis/gapic-generator-python/issues/1886

--- packages/google-cloud-securitycentermanagement/google/cloud/securitycentermanagement_v1/types/security_center_management.py
+++ packages/google-cloud-securitycentermanagement/google/cloud/securitycentermanagement_v1/types/security_center_management.py
@@ -794,10 +794,8 @@ class CreateSecurityHealthAnalyticsCustomModuleRequest(proto.Message):
             that a subsequent request to actually create the
             module could still fail because:
 
-             1. the state could have changed (e.g. IAM
-                permission lost) or
-             2. A failure occurred during creation of the
-                module. Defaults to false.
+            - The state could have changed (e.g. IAM permission lost) or
+            - A failure occurred during creation of the module. Defaults to false.
     """
 
     parent: str = proto.Field(
@@ -836,10 +834,10 @@ class UpdateSecurityHealthAnalyticsCustomModuleRequest(proto.Message):
             indicates the request is valid while an error
             response indicates the request is invalid. Note
             that a subsequent request to actually update the
-            module could still fail because 1. the state
-            could have changed (e.g. IAM permission lost) or
-            2. A failure occurred while trying to update the
-                module.
+            module could still fail because:
+
+            - The state could have changed (e.g. IAM permission lost) or
+            - A failure occurred while trying to update the module.
     """
 
     update_mask: field_mask_pb2.FieldMask = proto.Field(
@@ -879,10 +877,10 @@ class DeleteSecurityHealthAnalyticsCustomModuleRequest(proto.Message):
             indicates the request is valid while an error
             response indicates the request is invalid. Note
             that a subsequent request to actually delete the
-            module could still fail because 1. the state
-            could have changed (e.g. IAM permission lost) or
-            2. A failure occurred while trying to delete the
-                module.
+            module could still fail because:
+
+            - The state could have changed (e.g. IAM permission lost) or
+            - A failure occurred while trying to delete the module.
     """
 
     name: str = proto.Field(
@@ -1717,10 +1715,10 @@ class CreateEventThreatDetectionCustomModuleRequest(proto.Message):
             indicates the request is valid while an error
             response indicates the request is invalid. Note
             that a subsequent request to actually create the
-            module could still fail because 1. the state
-            could have changed (e.g. IAM permission lost) or
-            2. A failure occurred during creation of the
-                module.
+            module could still fail because:
+
+            - The state could have changed (e.g. IAM permission lost) or
+            - A failure occurred during creation of the module.
     """
 
     parent: str = proto.Field(
@@ -1760,10 +1758,10 @@ class UpdateEventThreatDetectionCustomModuleRequest(proto.Message):
             indicates the request is valid while an error
             response indicates the request is invalid. Note
             that a subsequent request to actually update the
-            module could still fail because 1. the state
-            could have changed (e.g. IAM permission lost) or
-            2. A failure occurred while trying to update the
-                module.
+            module could still fail because:
+
+            - The state could have changed (e.g. IAM permission lost) or
+            - A failure occurred while trying to update the module.
     """
 
     update_mask: field_mask_pb2.FieldMask = proto.Field(
@@ -1803,10 +1801,10 @@ class DeleteEventThreatDetectionCustomModuleRequest(proto.Message):
             indicates the request is valid while an error
             response indicates the request is invalid. Note
             that a subsequent request to actually delete the
-            module could still fail because 1. the state
-            could have changed (e.g. IAM permission lost) or
-            2. A failure occurred while trying to delete the
-                module.
+            module could still fail because:
+
+            - The state could have changed (e.g. IAM permission lost) or
+            - A failure occurred while trying to delete the module.
     """
 
     name: str = proto.Field(
