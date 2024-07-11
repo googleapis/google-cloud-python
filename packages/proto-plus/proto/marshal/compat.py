e735b3adf0c72df988281d6af48446300766fd3c
Anthonios Partheniou <partheniou@google.com>
2024-07-11 15:10:16 -0400

    fix: Remove check for Protobuf version (#474)
    * fix: Remove check for Protobuf version
    
    * restore unconditional behaviour
    
    * restore behaviour
    
    * restore comment
    
    * catch AttributeError

--- packages/proto-plus/proto/marshal/compat.py
+++ packages/proto-plus/proto/marshal/compat.py
@@ -19,9 +19,6 @@
 # not be included.
 
 from google.protobuf.internal import containers
-import google.protobuf
-
-PROTOBUF_VERSION = google.protobuf.__version__
 
 # Import protobuf 4.xx first and fallback to earlier version
 # if not present.
@@ -51,13 +48,12 @@ if _message:
     repeated_composite_types += (_message.RepeatedCompositeContainer,)
     repeated_scalar_types += (_message.RepeatedScalarContainer,)
 
-    # In `proto/marshal.py`, for compatibility with protobuf 5.x,
-    # we'll use `map_composite_type_names` to check whether
-    # the name of the class of a protobuf type is
-    # `MessageMapContainer`, and, if `True`, return a MapComposite.
-    # See https://github.com/protocolbuffers/protobuf/issues/16596
-    if PROTOBUF_VERSION[0:2] in ["3.", "4."]:
+    try:
         map_composite_types += (_message.MessageMapContainer,)
+    except AttributeError:
+        # The `MessageMapContainer` attribute is not available in Protobuf 5.x+
+        pass
+
 
 __all__ = (
     "repeated_composite_types",
