5ac665f42fa3aac81adf4bd1b6e925e67a1de9e1
Anthonios Partheniou <partheniou@google.com>
2024-08-13 12:58:04 -0400

    fix: Allow protobuf 5.x; require protobuf >=3.20.2 (#808)
    * fix: Allow protobuf 5.x; require protobuf >=3.20.2
    
    * update constraints
    
    ---------
    
    Co-authored-by: Chalmer Lowe <chalmer.lowe@gmail.com>

--- packages/google-cloud-bigquery-storage/setup.py
+++ packages/google-cloud-bigquery-storage/setup.py
@@ -42,7 +42,7 @@ dependencies = [
     "google-auth >= 2.14.1, <3.0.0dev",
     "proto-plus >= 1.22.0, <2.0.0dev",
     "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
-    "protobuf>=3.19.5,<5.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
+    "protobuf>=3.20.2,<6.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
 ]
 extras = {
     # 'importlib-metadata' is required for Python 3.7 compatibility
