80f3d31b97fc2af55beb683d15c183af099b74cb
Huan Chen <142538604+Genesis929@users.noreply.github.com>
2024-08-26 09:53:20 -0700

    chore: add tpch q9-13 (#927)
    * chore: add tpch q9-13
    
    * update import

---
+++ packages/bigframes/third_party/bigframes_vendored/tpch/queries/q11.py
@@ -0,0 +1,40 @@
+# Contains code from https://github.com/pola-rs/tpch/blob/main/queries/duckdb/q11.py
+
+import bigframes
+import bigframes.pandas as bpd
+
+
+def q(dataset_id: str, session: bigframes.Session):
+    supplier = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.SUPPLIER",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+    partsupp = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.PARTSUPP",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+    nation = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.NATION",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+
+    merged_df = partsupp.merge(supplier, left_on="PS_SUPPKEY", right_on="S_SUPPKEY")
+    merged_df = merged_df.merge(nation, left_on="S_NATIONKEY", right_on="N_NATIONKEY")
+
+    filtered_df = merged_df[merged_df["N_NAME"] == "GERMANY"]
+
+    filtered_df["VALUE"] = filtered_df["PS_SUPPLYCOST"] * filtered_df["PS_AVAILQTY"]
+    grouped = filtered_df.groupby("PS_PARTKEY", as_index=False).agg(
+        VALUE=bpd.NamedAgg(column="VALUE", aggfunc="sum")
+    )
+
+    grouped["VALUE"] = grouped["VALUE"].round(2)
+
+    total_value = (filtered_df["PS_SUPPLYCOST"] * filtered_df["PS_AVAILQTY"]).sum()
+    threshold = total_value * 0.0001
+
+    result_df = grouped[grouped["VALUE"] > threshold]
+
+    result_df = result_df.sort_values(by="VALUE", ascending=False)
+
+    result_df.to_gbq()
---
+++ packages/bigframes/third_party/bigframes_vendored/tpch/queries/q13.py
@@ -0,0 +1,37 @@
+# Contains code from https://github.com/pola-rs/tpch/blob/main/queries/polars/q13.py
+
+import typing
+
+import bigframes
+import bigframes.pandas as bpd
+
+
+def q(dataset_id: str, session: bigframes.Session):
+    customer = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.CUSTOMER",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+    orders = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.ORDERS",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+
+    var1 = "special"
+    var2 = "requests"
+
+    regex_pattern = f"{var1}.*{var2}"
+    orders = orders[~orders["O_COMMENT"].str.contains(regex_pattern, regex=True)]
+
+    q_final = (
+        customer.merge(orders, left_on="C_CUSTKEY", right_on="O_CUSTKEY", how="left")
+        .groupby("C_CUSTKEY", as_index=False)
+        .agg(C_COUNT=bpd.NamedAgg(column="O_ORDERKEY", aggfunc="count"))
+        .groupby("C_COUNT", as_index=False)
+        .agg("size")
+        .rename(columns={"size": "CUSTDIST"})
+    )
+    q_final = typing.cast(bpd.DataFrame, q_final).sort_values(
+        ["CUSTDIST", "C_COUNT"], ascending=[False, False]
+    )
+
+    q_final.to_gbq()
