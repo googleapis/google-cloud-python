8e15ad3fb40634c0d6e1e70fffca48745e873ca2
Huan Chen <142538604+Genesis929@users.noreply.github.com>
2024-08-29 10:31:25 -0700

    chore: add tpch q14-18 (#928)

---
+++ packages/bigframes/third_party/bigframes_vendored/tpch/queries/q14.py
@@ -0,0 +1,34 @@
+# Contains code from https://github.com/pola-rs/tpch/blob/main/queries/polars/q14.py
+
+from datetime import date
+
+import bigframes
+
+
+def q(dataset_id: str, session: bigframes.Session):
+    lineitem = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.LINEITEM",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+    part = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.PART",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+
+    var1 = date(1995, 9, 1)
+    var2 = date(1995, 10, 1)
+
+    merged = lineitem.merge(part, left_on="L_PARTKEY", right_on="P_PARTKEY")
+
+    filtered = merged[(merged["L_SHIPDATE"] >= var1) & (merged["L_SHIPDATE"] < var2)]
+
+    filtered["CONDI_REVENUE"] = (
+        filtered["L_EXTENDEDPRICE"] * (1 - filtered["L_DISCOUNT"])
+    ) * filtered["P_TYPE"].str.contains("PROMO").astype("Int64")
+
+    total_revenue = (filtered["L_EXTENDEDPRICE"] * (1 - filtered["L_DISCOUNT"])).sum()
+    promo_revenue = filtered["CONDI_REVENUE"].sum()
+
+    promo_revenue_percent = 100.00 * promo_revenue / total_revenue
+
+    _ = round(promo_revenue_percent, 2)
---
+++ packages/bigframes/third_party/bigframes_vendored/tpch/queries/q17.py
@@ -0,0 +1,40 @@
+# Contains code from https://github.com/pola-rs/tpch/blob/main/queries/polars/q17.py
+
+import bigframes
+import bigframes.pandas as bpd
+
+
+def q(dataset_id: str, session: bigframes.Session):
+    lineitem = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.LINEITEM",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+    part = session.read_gbq(
+        f"bigframes-dev-perf.{dataset_id}.PART",
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+
+    VAR1 = "Brand#23"
+    VAR2 = "MED BOX"
+
+    filtered_part = part[(part["P_BRAND"] == VAR1) & (part["P_CONTAINER"] == VAR2)]
+    q1 = bpd.merge(
+        filtered_part, lineitem, how="left", left_on="P_PARTKEY", right_on="L_PARTKEY"
+    )
+
+    grouped = (
+        q1.groupby("P_PARTKEY", as_index=False)
+        .agg(AVG_QUANTITY=bpd.NamedAgg(column="L_QUANTITY", aggfunc="mean"))
+        .rename(columns={"P_PARTKEY": "KEY"})
+    )
+    grouped["AVG_QUANTITY"] = grouped["AVG_QUANTITY"] * 0.2
+
+    q_final = bpd.merge(grouped, q1, left_on="KEY", right_on="P_PARTKEY")
+
+    q_final = q_final[q_final["L_QUANTITY"] < q_final["AVG_QUANTITY"]]
+
+    q_final = bpd.DataFrame(
+        {"AVG_YEARLY": [(q_final["L_EXTENDEDPRICE"].sum() / 7.0).round(2)]}
+    )
+
+    q_final.to_gbq()
