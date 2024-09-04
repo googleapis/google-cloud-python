fb2b94e83ca182642acdad2c0567ac6e1199f907
ohmayr <omairnaveed@ymail.com>
2024-09-04 17:16:32 -0400

    chore: optionally generate async rest and client files (#2122)

--- packages/gapic-generator/gapic/generator/generator.py
+++ packages/gapic-generator/gapic/generator/generator.py
@@ -294,9 +294,13 @@ class Generator:
                         ('transport' in template_name
                          and not self._is_desired_transport(template_name, opts))
                         or
-                        # TODO(yon-mg) - remove when rest async implementation resolved
-                        # temporarily stop async client gen while rest async is unkown
-                        ('async' in template_name and 'grpc' not in opts.transport)
+                        # TODO: Remove the following conditions once support for async rest transport is GA:
+                        # See related issue: https://github.com/googleapis/gapic-generator-python/issues/2121.
+                        ('async_client' in template_name and 'grpc' not in opts.transport and
+                         not api_schema.all_library_settings[api_schema.naming.proto_package].python_settings.experimental_features.rest_async_io_enabled)
+                        or
+                        ('rest_asyncio' in template_name and
+                         not api_schema.all_library_settings[api_schema.naming.proto_package].python_settings.experimental_features.rest_async_io_enabled)
                         or
                         ('rest_base' in template_name and 'rest' not in opts.transport)
                 ):
