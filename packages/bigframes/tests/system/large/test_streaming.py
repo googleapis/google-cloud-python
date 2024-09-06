58be875d233cc3404c983a2a22d0c3cd3bcf9ccc
Shobhit Singh <shobs@google.com>
2024-09-05 22:18:38 -0700

    test: retry streaming tests to accommodate flakiness (#956)
    * test: retry streaming tests to accommodate flakiness
    
    * reduce delay, increase retries

--- packages/bigframes/tests/system/large/test_streaming.py
+++ packages/bigframes/tests/system/large/test_streaming.py
@@ -14,10 +14,13 @@
 
 import time
 
+import pytest
+
 import bigframes
 import bigframes.streaming
 
 
+@pytest.mark.flaky(retries=3, delay=10)
 def test_streaming_df_to_bigtable(session_load: bigframes.Session):
     # launch a continuous query
     job_id_prefix = "test_streaming_"
@@ -51,6 +54,7 @@ def test_streaming_df_to_bigtable(session_load: bigframes.Session):
         query_job.cancel()
 
 
+@pytest.mark.flaky(retries=3, delay=10)
 def test_streaming_df_to_pubsub(session_load: bigframes.Session):
     # launch a continuous query
     job_id_prefix = "test_streaming_pubsub_"
