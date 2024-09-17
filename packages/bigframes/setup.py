bccde00bd13bd63b373c92f02f4408c5e1cca0e4
Chelsea Lin <124939984+chelsea-lin@users.noreply.github.com>
2024-09-17 08:39:28 -0700

    deps: limit ibis-framework version to 9.2.0 (#989)
    * chore: limit ibis version to 9.2.0
    
    * remove ibis constrains on 3.12 tests

--- packages/bigframes/setup.py
+++ packages/bigframes/setup.py
@@ -47,7 +47,7 @@ dependencies = [
     "google-cloud-iam >=2.12.1",
     "google-cloud-resource-manager >=1.10.3",
     "google-cloud-storage >=2.0.0",
-    "ibis-framework[bigquery] >=9.0.0,<=9.3.0",
+    "ibis-framework[bigquery] >=9.0.0,<=9.2.0",
     "jellyfish >=0.8.9",
     "numpy >=1.24.0",
     "pandas >=1.5.3",
