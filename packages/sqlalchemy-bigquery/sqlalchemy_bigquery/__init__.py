de4b0248193bd757743b785c536ee4366d5c44b7
Chalmer Lowe <chalmerlowe@google.com>
2024-10-02 18:49:12 -0400

    chore: adds Python 3.7/3.8 EOL pending deprecation warning (#1123)
    * Adds deprecation warning regarding Python 3.7 3.8
    
    * removes no warnings plugin and tweaks settings
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Lingqing Gan <lingqing.gan@gmail.com>

--- packages/sqlalchemy-bigquery/sqlalchemy_bigquery/__init__.py
+++ packages/sqlalchemy-bigquery/sqlalchemy_bigquery/__init__.py
@@ -20,6 +20,8 @@
 SQLAlchemy dialect for Google BigQuery
 """
 
+import warnings
+
 from .version import __version__
 
 from .base import BigQueryDialect, dialect
@@ -42,6 +44,20 @@ from ._types import (
     TIME,
     TIMESTAMP,
 )
+
+from . import _versions_helpers
+
+sys_major, sys_minor, sys_micro = _versions_helpers.extract_runtime_version()
+if sys_major == 3 and sys_minor in (7, 8):
+    warnings.warn(
+        "The python-bigquery library will stop supporting Python 3.7 "
+        "and Python 3.8 in a future major release expected in Q4 2024. "
+        f"Your Python version is {sys_major}.{sys_minor}.{sys_micro}. We "
+        "recommend that you update soon to ensure ongoing support. For "
+        "more details, see: [Google Cloud Client Libraries Supported Python Versions policy](https://cloud.google.com/python/docs/supported-python-versions)",
+        PendingDeprecationWarning,
+    )
+
 
 __all__ = [
     "__version__",
