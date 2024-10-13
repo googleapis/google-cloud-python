27cb7d7530ee1c2a0b842cd08de17799b9d84e73
Anthonios Partheniou <partheniou@google.com>
2024-10-13 19:20:11 +0000

    fix docs build in google-shopping-merchant-accounts

--- packages/google-shopping-merchant-accounts/google/shopping/merchant_accounts_v1beta/services/online_return_policy_service/client.py
+++ packages/google-shopping-merchant-accounts/google/shopping/merchant_accounts_v1beta/services/online_return_policy_service/client.py
@@ -102,9 +102,7 @@ class OnlineReturnPolicyServiceClient(metaclass=OnlineReturnPolicyServiceClientM
     ads and free listings
 
     programs. This API defines the following resource model:
-    --------------------------------------------------------
-
-    [OnlineReturnPolicy][google.shopping.merchant.accounts.v1.OnlineReturnPolicy]
+    - [OnlineReturnPolicy][google.shopping.merchant.accounts.v1.OnlineReturnPolicy]
     """
 
     @staticmethod
