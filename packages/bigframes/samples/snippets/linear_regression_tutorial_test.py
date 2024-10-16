835103ed7fd4787d4044a7cf5afa53dfc7990ea0
rey-esp <drespana@google.com>
2024-10-16 15:35:48 +0000

    docs: add ml tutorial for Evaluate the model (#1038)
    * add ml tutorial for Evaluate the model
    
    * remove alternative
    
    * remove alternative snippet
    
    * update description
    
    * avoid contractions
    
    * fix comment description
    
    * add expected results

--- packages/bigframes/samples/snippets/linear_regression_tutorial_test.py
+++ packages/bigframes/samples/snippets/linear_regression_tutorial_test.py
@@ -37,6 +37,24 @@ def test_linear_regression(random_model_id: str) -> None:
         replace=True,
     )
     # [END bigquery_dataframes_bqml_linear_regression]
+    # [START bigquery_dataframes_bqml_linear_evaluate]
+    import bigframes.pandas as bpd
+
+    # Select the model you will be evaluating. `read_gbq_model` loads model data from
+    # BigQuery, but you could also use the `model` object from the previous steps.
+    model = bpd.read_gbq_model(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model"
+    )
+
+    # Score the model with input data defined in an earlier step to compare
+    # model predictions on feature_columns to true labels in label_columns.
+    score = model.score(feature_columns, label_columns)
+    # Expected output results:
+    # index  mean_absolute_error  mean_squared_error  mean_squared_log_error  median_absolute_error  r2_score  explained_variance
+    #   0        227.012237         81838.159892            0.00507                173.080816        0.872377    0.872377
+    #   1 rows x columns
+    # [END bigquery_dataframes_bqml_linear_evaluate]
     assert feature_columns is not None
     assert label_columns is not None
     assert model is not None
+    assert score is not None
