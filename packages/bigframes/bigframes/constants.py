0a66f3f9f00f00426a9aaacba008ba507bb11135
jialuoo <jialuo@google.com>
2024-10-28 09:44:37 -0700

    add new rep locations (#1115)

--- packages/bigframes/bigframes/constants.py
+++ packages/bigframes/bigframes/constants.py
@@ -84,8 +84,17 @@ REP_ENABLED_BIGQUERY_LOCATIONS = frozenset(
         "europe-west9",
         "europe-west8",
         "me-central2",
+        "us-central1",
+        "us-central2",
+        "us-east1",
         "us-east4",
+        "us-east5",
+        "us-east7",
+        "us-south1",
         "us-west1",
+        "us-west2",
+        "us-west3",
+        "us-west4",
     }
 )
 
