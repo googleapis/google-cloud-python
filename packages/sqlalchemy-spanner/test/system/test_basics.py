d7662ab07ec0fe3acf94cbcc9bf6dd57417694a3
Knut Olav Løite <koloite@gmail.com>
2024-12-09 10:18:20 +0100

    test: add test with 'roles' table (#520)
    * test: add test with 'roles' table
    
    Add a test that verifies that a table with the same name as a
    system view can be used.
    
    * chore: fix linting

--- packages/sqlalchemy-spanner/test/system/test_basics.py
+++ packages/sqlalchemy-spanner/test/system/test_basics.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Optional
 from sqlalchemy import (
     text,
     Table,
@@ -89,6 +90,30 @@ class TestBasics(fixtures.TablesTest):
         dialect_options = index.dialect_options["spanner"]
         eq_(1, len(dialect_options["storing"]))
         eq_("alternative_name", dialect_options["storing"][0])
+
+    def test_table_name_overlapping_with_system_table(self, connection):
+        class Base(DeclarativeBase):
+            pass
+
+        class Role(Base):
+            __tablename__ = "roles"
+            id: Mapped[int] = mapped_column(Integer, primary_key=True)
+            name: Mapped[str] = mapped_column(String(100), nullable=True)
+            type: Mapped[str] = mapped_column(String(100), nullable=True)
+            description: Mapped[Optional[str]] = mapped_column(String(512))
+
+        engine = connection.engine
+        Base.metadata.create_all(engine)
+
+        with Session(engine) as session:
+            role = Role(
+                id=1,
+                name="Test",
+                type="Test",
+                description="Test",
+            )
+            session.add(role)
+            session.commit()
 
     def test_orm(self, connection):
         class Base(DeclarativeBase):
