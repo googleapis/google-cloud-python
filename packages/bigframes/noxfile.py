2e248f797bc54abe29d84d054290b38df589c5ff
Garrett Wu <6505921+GarrettWu@users.noreply.github.com>
2024-12-11 14:44:39 -0800

    docs: add Gemini 2.0 text gen sample notebook (#1211)
    * feat: add Gemini 2.0 preview text model support
    
    * update docs
    
    * docs: add Gemini 2.0 text gen sample notebook

--- packages/bigframes/noxfile.py
+++ packages/bigframes/noxfile.py
@@ -749,6 +749,7 @@ def notebook(session: nox.Session):
         # bq_dataframes_llm_code_generation creates a bucket in the sample.
         "notebooks/generative_ai/bq_dataframes_llm_code_generation.ipynb",  # Needs BUCKET_URI.
         "notebooks/generative_ai/sentiment_analysis.ipynb",  # Too slow
+        "notebooks/generative_ai/bq_dataframes_llm_gemini_2.ipynb",  # Gemini 2.0 backend hasn't ready in prod.
         # TODO(b/366290533): to protect BQML quota
         "notebooks/generative_ai/bq_dataframes_llm_claude3_museum_art.ipynb",
         "notebooks/vertex_sdk/sdk2_bigframes_pytorch.ipynb",  # Needs BUCKET_URI.
