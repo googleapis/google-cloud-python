1afc8f4fcec04f884d7341f7731039ecef557301
Anthonios Partheniou <partheniou@google.com>
2024-12-19 20:00:50 -0500

    chore: set the default python version for templates to 3.10 (#1233)
    * chore: set the default python version for templates to 3.10
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * add workaround
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/bigframes/owlbot.py
+++ packages/bigframes/owlbot.py
@@ -30,6 +30,7 @@ common = gcp.CommonTemplates()
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(
+    default_python_version="3.10",
     unit_test_python_versions=["3.9", "3.10", "3.11", "3.12"],
     system_test_python_versions=["3.9", "3.11", "3.12"],
     cov_level=35,
@@ -53,6 +54,8 @@ s.move(
         ".kokoro/build.sh",
         ".kokoro/continuous/common.cfg",
         ".kokoro/presubmit/common.cfg",
+        # Temporary workaround to update docs job to use python 3.10
+        ".github/workflows/docs.yml",
     ],
 )
 
