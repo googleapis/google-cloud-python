54fc7885722003fbe9a28bd8f9646911948cae95
rey-esp <drespana@google.com>
2025-01-08 16:56:03 -0600

    chore: rename snippet (#1275)

--- packages/bigframes/samples/snippets/create_single_timeseries_forecasting_model_test.py
+++ packages/bigframes/samples/snippets/create_single_timeseries_forecasting_model_test.py
@@ -65,7 +65,7 @@ def test_create_single_timeseries() -> None:
     model.fit(X, y)
     # [END bigquery_dataframes_single_timeseries_forecasting_model_tutorial_create]
 
-    # [START bigquery_dataframes_single_timeseries_forecasting_model_tutorial_evaluate]
+    # [START bigquery_dataframes_single_timeseries_forecasting_model_tutorial_coef]
     coef = model.coef_
     print(coef.peek())
 
@@ -73,7 +73,7 @@ def test_create_single_timeseries() -> None:
     #       ar_coefficients   ma_coefficients   intercept_or_drift
     #   0	 [0.40944762]	   [-0.81168198]	      0.0
 
-    # [END bigquery_dataframes_single_timeseries_forecasting_model_tutorial_evaluate]
+    # [END bigquery_dataframes_single_timeseries_forecasting_model_tutorial_coef]
     assert coef is not None
     assert model is not None
     assert parsed_date is not None
