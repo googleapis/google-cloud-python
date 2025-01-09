19b8dd167bf811aa4bbbed31cccd8c8dcd6196de
Anthonios Partheniou <partheniou@google.com>
2025-01-09 14:59:50 -0500

    chore: remove pb2 file generation during post processing step (#122)
    * chore: remove pb2 file generation during post processing step
    
    * revert

--- packages/grpc-google-iam-v1/owlbot.py
+++ packages/grpc-google-iam-v1/owlbot.py
@@ -11,32 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import os
-import subprocess
-import shutil
-
 import synthtool as s
 import synthtool.gcp as gcp
 from synthtool.languages import python
-from synthtool.sources import git
 
-GOOGLEAPIS_REPO = "googleapis/googleapis"
-
-# Clone googleapis
-googleapis_url = git.make_repo_clone_url(GOOGLEAPIS_REPO)
-subprocess.run(["git", "clone", googleapis_url])
-
-# This is required in order for s.copy() to work
-s._tracked_paths.add("googleapis")
-
-s.copy("googleapis/google/iam/v1/*.proto", "google/iam/v1")
-s.copy("googleapis/google/iam/v1/logging/*.proto", "google/iam/v1/logging")
-
-os.mkdir('google/type')
-os.mkdir('google/api')
-s.copy("googleapis/google/type/expr.proto", "google/type")
-s.copy("googleapis/google/api/*.proto", "google/api")
 
 # ----------------------------------------------------------------------------
 # Add templated files
@@ -62,18 +40,6 @@ s.move(templated_files / ".github", excludes=["workflows/unittest.yml"])
 s.move(templated_files / ".trampolinerc")
 
 python.py_samples(skip_readmes=True)
-
-# Generate _pb2.py files and format them
-s.shell.run(["nox", "-s", "generate_protos"], hide_output=False)
-
-# Clean up googleapis
-shutil.rmtree('googleapis')
-
-# Clean up google/api
-shutil.rmtree('google/api')
-
-# Clean up google/type
-shutil.rmtree('google/type')
 
 # Add license headers
 python.fix_pb2_headers()
