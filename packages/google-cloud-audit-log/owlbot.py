232506f53e5bd5bc79abe7accf3a541bd0b287c3
Anthonios Partheniou <partheniou@google.com>
2025-01-09 17:47:54 -0500

    chore: remove pb2 file generation during post processing step (#162)

--- packages/google-cloud-audit-log/owlbot.py
+++ packages/google-cloud-audit-log/owlbot.py
@@ -11,50 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import os
-import shutil
-import subprocess
-
 import synthtool as s
 import synthtool.gcp as gcp
 from synthtool.languages import python
-from synthtool.sources import git
-
-GOOGLEAPIS_REPO = "googleapis/googleapis"
-
-# Clean up googleapis
-shutil.rmtree("googleapis", ignore_errors=True)
-
-# Clone googleapis
-googleapis_url = git.make_repo_clone_url(GOOGLEAPIS_REPO)
-subprocess.run(["git", "clone", googleapis_url])
-
-# This is required in order for s.copy() to work
-s._tracked_paths.add("googleapis")
-
-# Create folders for dependencies of the protos that we want to compile
-common_apis = [
-    "google/api",
-    "google/iam/v1",
-    "google/rpc",
-    "google/rpc/context",
-    "google/type",
-]
-_ = [os.makedirs(dir, exist_ok=True) for dir in common_apis]
-
-# Copy dependencies of the protos that we want to compile from googleapis
-s.copy("googleapis/google/api/*.proto", "google/api")
-s.copy("googleapis/google/iam/v1/*.proto", "google/iam/v1")
-s.copy("googleapis/google/rpc/context/attribute_context.proto", "google/rpc/context")
-s.copy("googleapis/google/rpc/status.proto", "google/rpc")
-s.copy("googleapis/google/type/*.proto", "google/type")
-
-# Copy the protos that we want to compile from googleapis
-s.copy("googleapis/google/cloud/audit/*.proto", "google/cloud/audit")
-
-# Clean up googleapis
-shutil.rmtree("googleapis")
 
 # ----------------------------------------------------------------------------
 #  Add templated files
@@ -72,13 +31,6 @@ s.move(templated_files / "CODE_OF_CONDUCT.md")
 s.move(templated_files / ".github", excludes=["workflows"])
 s.move(templated_files / ".gitignore")
 s.move(templated_files / "renovate.json")
-
-# Generate _pb2.py files and format them
-s.shell.run(["nox", "-s", "generate_protos"], hide_output=False)
-
-# Clean up the folders for dependencies which are shipped via `googleapis-common-protos`
-# We should not ship them via this repository
-_ = [shutil.rmtree(dir, ignore_errors=True) for dir in common_apis]
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
 
