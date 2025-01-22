702eb8283e60cec993fab238ffc0e92a332b8a53
Rin Arakaki <rnarkkx@gmail.com>
2025-01-23 00:59:11 +0900

    feat: migrate to pyproject.toml (#496)
    * migrate to pyproject.toml
    
    * Rebase following https://github.com/googleapis/proto-plus-python/pull/502
    
    * add entries such as documentation and classifiers which exist in other projects
    
    * Use correct url for docs
    
    ---------
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/proto-plus/setup.py
+++ packages/proto-plus/setup.py
@@ -12,59 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import io
-import re
-import os
 
-from setuptools import find_namespace_packages, setup
+from setuptools import setup
 
-PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-version = None
-
-with open(os.path.join(PACKAGE_ROOT, "proto/version.py")) as fp:
-    version_candidates = re.findall(r"(?<=\")\d+.\d+.\d+(?=\")", fp.read())
-    assert len(version_candidates) == 1
-    version = version_candidates[0]
-
-with io.open(os.path.join(PACKAGE_ROOT, "README.rst")) as file_obj:
-    README = file_obj.read()
-
-setup(
-    name="proto-plus",
-    version=version,
-    license="Apache 2.0",
-    author="Google LLC",
-    author_email="googleapis-packages@google.com",
-    url="https://github.com/googleapis/proto-plus-python.git",
-    packages=find_namespace_packages(include=["proto", "proto.*"]),
-    description="Beautiful, Pythonic protocol buffers.",
-    long_description=README,
-    platforms="Posix; MacOS X",
-    include_package_data=True,
-    install_requires=("protobuf >=3.19.0, <6.0.0dev",),
-    extras_require={
-        "testing": [
-            "google-api-core >= 1.31.5",
-        ],
-    },
-    python_requires=">=3.7",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Topic :: Software Development :: Code Generators",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    zip_safe=False,
-)
+setup()
