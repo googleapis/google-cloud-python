a39856b9db5ae0ce846b8171b701d8cd81afcac8
Shenyang Cai <sycai@users.noreply.github.com>
2025-02-21 14:43:50 -0800

    fix: replace empty dict with None to avoid mutable default arguments (#1416)
    * fix: replace empty dict wit None to avoid mutable default arguments
    
    * fix format

--- packages/bigframes/bigframes/core/schema.py
+++ packages/bigframes/bigframes/core/schema.py
@@ -41,8 +41,12 @@ class ArraySchema:
     def from_bq_table(
         cls,
         table: google.cloud.bigquery.Table,
-        column_type_overrides: typing.Dict[str, bigframes.dtypes.Dtype] = {},
+        column_type_overrides: typing.Optional[
+            typing.Dict[str, bigframes.dtypes.Dtype]
+        ] = None,
     ):
+        if column_type_overrides is None:
+            column_type_overrides = {}
         items = tuple(
             SchemaItem(name, column_type_overrides.get(name, dtype))
             for name, dtype in bigframes.dtypes.bf_type_from_type_kind(
