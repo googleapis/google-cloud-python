f64712589fff9ea2e1b092808305e8c403d3449b
Anthonios Partheniou <partheniou@google.com>
2025-03-06 16:29:29 -0500

    fix: Allow protobuf 6.x (#905)
    * fix: Allow protobuf 6.x
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Delete .kokoro/docker/docs/requirements.txt
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * undo owlbot change
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Lingqing Gan <lingqing.gan@gmail.com>

--- packages/google-cloud-bigquery-storage/setup.py
+++ packages/google-cloud-bigquery-storage/setup.py
@@ -38,12 +38,12 @@ else:
     release_status = "Development Status :: 5 - Production/Stable"
 
 dependencies = [
-    "google-api-core[grpc] >= 1.34.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,!=2.8.*,!=2.9.*,!=2.10.*",
-    "google-auth >= 2.14.1, <3.0.0dev",
-    "proto-plus >= 1.22.0, <2.0.0dev",
-    "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
-    "proto-plus >= 1.25.0, <2.0.0dev; python_version>='3.13'",
-    "protobuf>=3.20.2,<6.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
+    "google-api-core[grpc] >= 1.34.0, <3.0.0,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,!=2.8.*,!=2.9.*,!=2.10.*",
+    "google-auth >= 2.14.1, <3.0.0",
+    "proto-plus >= 1.22.0, <2.0.0",
+    "proto-plus >= 1.22.2, <2.0.0; python_version>='3.11'",
+    "proto-plus >= 1.25.0, <2.0.0; python_version>='3.13'",
+    "protobuf>=3.20.2,<7.0.0,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
 ]
 extras = {
     # 'importlib-metadata' is required for Python 3.7 compatibility
