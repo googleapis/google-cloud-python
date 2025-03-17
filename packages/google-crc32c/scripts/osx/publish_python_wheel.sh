4ec7dea1d0b267737b245068b42cd8c6ded84acf
Anthonios Partheniou <partheniou@google.com>
2025-03-17 15:40:22 -0400

    build: update build scripts to prepare for release (#275)

--- packages/google-crc32c/scripts/osx/publish_python_wheel.sh
+++ packages/google-crc32c/scripts/osx/publish_python_wheel.sh
@@ -27,5 +27,5 @@ TWINE=${PYTHON_BIN}/twine
 # Disable buffering, so that the logs stream through.
 export PYTHONUNBUFFERED=1
 
-TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-1")
+TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-3")
 ${PYTHON} -m twine upload --skip-existing --username __token__ --password "${TWINE_PASSWORD}" ${REPO_ROOT}/wheels/*
