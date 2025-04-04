c14ba3886e83b5f725e103442d4ef14711f9e8e0
Nodoubtz <53144580+nodoubtz@users.noreply.github.com>
2025-04-04 09:59:50 -0400

    Add files via upload
    Signed-off-by: Nodoubtz <53144580+nodoubtz@users.noreply.github.com>

---
+++ commitlint.config (1).js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+  },
+};
