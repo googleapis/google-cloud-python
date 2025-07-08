3f3a853131e9e8da1c7698ff127c96c91daeaadc
Chalmer Lowe <chalmer.lowe@gmail.com>
2025-07-08 13:20:07 -0400

    deps: adds limits to numpy installs under 3.10 for dataproc (#370)
    * deps: adds limits to numpy installs under 3.10 for dataproc
    
    * tweaks some version numbers as a test

--- packages/db-dtypes/setup.py
+++ packages/db-dtypes/setup.py
@@ -30,7 +30,8 @@ description = "Pandas Data Types for SQL systems (BigQuery, Spanner)"
 release_status = "Development Status :: 5 - Production/Stable"
 
 dependencies = [
-    "numpy >= 1.24.0",
+    "numpy >= 1.24.0, <= 2.2.6 ; python_version == '3.10'",
+    "numpy >= 1.24.0 ; python_version != '3.10'",
     "packaging >= 24.2.0",
     "pandas >= 1.5.3",
     "pyarrow >= 13.0.0",
