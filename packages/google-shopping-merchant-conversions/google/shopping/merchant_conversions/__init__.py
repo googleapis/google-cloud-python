f8d055c21bb6edf4d7ca1a77787fa3764f93fb4b
Anthonios Partheniou <partheniou@google.com>
2025-08-13 17:01:43 -0400

    feat: update release level to stable (#14227)
    BEGIN_COMMIT_OVERRIDE
    feat: update release level to stable
    feat: set `google.shopping.merchant_conversions_v1` as the default
    import for `google.shopping.merchant_conversions`
    
    Release-As: 1.0.0
    END_COMMIT_OVERRIDE

--- packages/google-shopping-merchant-conversions/google/shopping/merchant_conversions/__init__.py
+++ packages/google-shopping-merchant-conversions/google/shopping/merchant_conversions/__init__.py
@@ -18,13 +18,13 @@ from google.shopping.merchant_conversions import gapic_version as package_versio
 __version__ = package_version.__version__
 
 
-from google.shopping.merchant_conversions_v1beta.services.conversion_sources_service.async_client import (
+from google.shopping.merchant_conversions_v1.services.conversion_sources_service.async_client import (
     ConversionSourcesServiceAsyncClient,
 )
-from google.shopping.merchant_conversions_v1beta.services.conversion_sources_service.client import (
+from google.shopping.merchant_conversions_v1.services.conversion_sources_service.client import (
     ConversionSourcesServiceClient,
 )
-from google.shopping.merchant_conversions_v1beta.types.conversionsources import (
+from google.shopping.merchant_conversions_v1.types.conversionsources import (
     AttributionSettings,
     ConversionSource,
     CreateConversionSourceRequest,
