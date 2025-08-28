4a76090ade75951719a9c39bdb1bcb8c9a4f2eae
chingor13 <chingor@google.com>
2025-08-28 21:03:23 +0000

    fix cache dir

--- .generator/run.sh
+++ .generator/run.sh
@@ -7,7 +7,7 @@ env
 if [[ -f /bazel-cache.tar.gz ]]; then
     echo "unzipping bazel cache"
     # temp_dir=$(mktmp -d)
-    tar -zxf /bazel-cache.tar.gz -C /bazel/.cache
+    tar -zxf /bazel-cache.tar.gz -C /bazel/.cache --strip-components=2
     # tar -zxf /bazel-cache.tar.gz -C "${temp_dir}" --strip-components=1
     # mv "${temp_dir}"
 fi
