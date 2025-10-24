6565faccc732ea1e0fde3657b9254c83e694ec14
Chelsea Lin <chelsealin@google.com>
2025-10-24 16:17:32 -0700

    refactor: add some numeric tests in the golden sql (#2195)

--- packages/bigframes/tests/unit/core/compile/sqlglot/expressions/test_numeric_ops.py
+++ packages/bigframes/tests/unit/core/compile/sqlglot/expressions/test_numeric_ops.py
@@ -16,6 +16,7 @@ import pandas as pd
 import pytest
 
 from bigframes import operations as ops
+import bigframes.core.expression as ex
 import bigframes.pandas as bpd
 from bigframes.testing import utils
 
@@ -218,6 +219,34 @@ def test_add_numeric(scalar_types_df: bpd.DataFrame, snapshot):
     snapshot.assert_match(bf_df.sql, "out.sql")
 
 
+def test_add_string(scalar_types_df: bpd.DataFrame, snapshot):
+    bf_df = scalar_types_df[["string_col"]]
+    sql = utils._apply_binary_op(bf_df, ops.add_op, "string_col", ex.const("a"))
+
+    snapshot.assert_match(sql, "out.sql")
+
+
+def test_add_timedelta(scalar_types_df: bpd.DataFrame, snapshot):
+    bf_df = scalar_types_df[["timestamp_col", "date_col"]]
+    timedelta = pd.Timedelta(1, unit="d")
+
+    bf_df["date_add_timedelta"] = bf_df["date_col"] + timedelta
+    bf_df["timestamp_add_timedelta"] = bf_df["timestamp_col"] + timedelta
+    bf_df["timedelta_add_date"] = timedelta + bf_df["date_col"]
+    bf_df["timedelta_add_timestamp"] = timedelta + bf_df["timestamp_col"]
+    bf_df["timedelta_add_timedelta"] = timedelta + timedelta
+
+    snapshot.assert_match(bf_df.sql, "out.sql")
+
+
+def test_add_unsupported_raises(scalar_types_df: bpd.DataFrame):
+    with pytest.raises(TypeError):
+        utils._apply_binary_op(scalar_types_df, ops.add_op, "timestamp_col", "date_col")
+
+    with pytest.raises(TypeError):
+        utils._apply_binary_op(scalar_types_df, ops.add_op, "int64_col", "string_col")
+
+
 def test_div_numeric(scalar_types_df: bpd.DataFrame, snapshot):
     bf_df = scalar_types_df[["int64_col", "bool_col", "float64_col"]]
 
@@ -279,6 +308,16 @@ def test_mul_numeric(scalar_types_df: bpd.DataFrame, snapshot):
     snapshot.assert_match(bf_df.sql, "out.sql")
 
 
+def test_mul_timedelta(scalar_types_df: bpd.DataFrame, snapshot):
+    bf_df = scalar_types_df[["timestamp_col", "int64_col", "duration_col"]]
+    bf_df["duration_col"] = bpd.to_timedelta(bf_df["duration_col"], unit="us")
+
+    bf_df["timedelta_mul_numeric"] = bf_df["duration_col"] * bf_df["int64_col"]
+    bf_df["numeric_mul_timedelta"] = bf_df["int64_col"] * bf_df["duration_col"]
+
+    snapshot.assert_match(bf_df.sql, "out.sql")
+
+
 def test_mod_numeric(scalar_types_df: bpd.DataFrame, snapshot):
     bf_df = scalar_types_df[["int64_col", "float64_col"]]
 
@@ -305,3 +344,24 @@ def test_sub_numeric(scalar_types_df: bpd.DataFrame, snapshot):
     bf_df["bool_add_int"] = bf_df["bool_col"] - bf_df["int64_col"]
 
     snapshot.assert_match(bf_df.sql, "out.sql")
+
+
+def test_sub_timedelta(scalar_types_df: bpd.DataFrame, snapshot):
+    bf_df = scalar_types_df[["timestamp_col", "duration_col", "date_col"]]
+    bf_df["duration_col"] = bpd.to_timedelta(bf_df["duration_col"], unit="us")
+
+    bf_df["date_sub_timedelta"] = bf_df["date_col"] - bf_df["duration_col"]
+    bf_df["timestamp_sub_timedelta"] = bf_df["timestamp_col"] - bf_df["duration_col"]
+    bf_df["timestamp_sub_date"] = bf_df["date_col"] - bf_df["date_col"]
+    bf_df["date_sub_timestamp"] = bf_df["timestamp_col"] - bf_df["timestamp_col"]
+    bf_df["timedelta_sub_timedelta"] = bf_df["duration_col"] - bf_df["duration_col"]
+
+    snapshot.assert_match(bf_df.sql, "out.sql")
+
+
+def test_sub_unsupported_raises(scalar_types_df: bpd.DataFrame):
+    with pytest.raises(TypeError):
+        utils._apply_binary_op(scalar_types_df, ops.sub_op, "string_col", "string_col")
+
+    with pytest.raises(TypeError):
+        utils._apply_binary_op(scalar_types_df, ops.sub_op, "int64_col", "string_col")
