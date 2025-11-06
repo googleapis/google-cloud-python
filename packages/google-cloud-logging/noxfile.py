83ab70353dc356045416907187cc1d411c013553
Lingqing Gan <lingqing.gan@gmail.com>
2025-11-05 17:37:05 -0800

    test: ignore DeprecationWarning for `credentials_file` argument and Python versions (#1058)
    * test: ignore DeprecationWarning for `credentials_file` argument
    
    * add more ignores
    
    * typo
    
    * add ignore
    
    * remove python 3.7 and 3.8 from kokoro unit test
    
    * change DEFAULT_PYTHON_VERSION to 3.10

--- packages/google-cloud-logging/noxfile.py
+++ packages/google-cloud-logging/noxfile.py
@@ -32,11 +32,9 @@ BLACK_VERSION = "black[jupyter]==23.7.0"
 ISORT_VERSION = "isort==5.11.0"
 LINT_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 
-DEFAULT_PYTHON_VERSION = "3.8"
+DEFAULT_PYTHON_VERSION = "3.10"
 
 UNIT_TEST_PYTHON_VERSIONS: List[str] = [
-    "3.7",
-    "3.8",
     "3.9",
     "3.10",
     "3.11",
