48cfa3b153539f77f594ff4bd8e1fe531da75de7
ohmayr <omairn@google.com>
2025-11-13 07:56:02 -0800

    chore(librarian): onboard to librarian (#646)
    This PR onboards `google-cloud-datastore` to the librarian system.
    
    Towards https://github.com/googleapis/librarian/issues/2458
    
    ---------
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- packages/google-cloud-datastore/google/cloud/datastore_admin_v1/gapic_version.py
+++ packages/google-cloud-datastore/google/cloud/datastore_admin_v1/gapic_version.py
@@ -1,4 +1,5 @@
-# Copyright 2022 Google LLC
+# -*- coding: utf-8 -*-
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,5 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 __version__ = "2.21.0"  # {x-release-please-version}
