40baf8b42c66b11198b2136b0bc3df186b65739b
Anthonios Partheniou <partheniou@google.com>
2025-11-17 17:45:54 +0000

    chore(librarian): fix build for google-cloud-bigquery-storage

--- .generator/cli.py
+++ .generator/cli.py
@@ -1041,9 +1041,9 @@ def _verify_library_namespace(library_id: str, repo: str, is_mono_repo: bool):
     for proto_file in library_path.rglob(proto_file):
         proto_path = str(proto_file.parent.relative_to(library_path))
         # Exclude proto paths which are not intended to be used for code generation.
-        # Generally any protos under the `samples` directory or in a
+        # Generally any protos under the `samples` or `tests` directories or in a
         # directory called `proto` are not used for code generation.
-        if proto_path.startswith("samples") or proto_path.endswith("proto"):
+        if proto_path.startswith("tests") or proto_path.startswith("samples") or proto_path.endswith("proto"):
             continue
         relevant_dirs.add(proto_file.parent)
 
