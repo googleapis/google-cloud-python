9ac4ef526a716f51285d34a7c09da82d98931213
Anthonios Partheniou <partheniou@google.com>
2025-12-17 17:04:37 +0000

    restore 3.7/3.8

--- packages/google-auth/noxfile.py
+++ packages/google-auth/noxfile.py
@@ -33,9 +33,7 @@ BLACK_PATHS = [
 ]
 
 DEFAULT_PYTHON_VERSION = "3.14"
-# TODO(https://github.com/googleapis/google-auth-library-python/issues/1787):
-# Remove or restore testing for Python 3.7/3.8
-UNIT_TEST_PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"]
+UNIT_TEST_PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14"]
 
 # Error if a python version is missing
 nox.options.error_on_missing_interpreters = True
