b7091f396bca72c24f96769299433585dd3af370
Khem Raj <raj.khem@gmail.com>
2025-12-30 15:16:31 -0800

    tests: make the TLS tests skip when pyopenssl isn't available (#1873)
    In some distributions e.g. yocto
    pyOpenSSL is a packageconfig and disabled by default, and it means pyopenssl.py module is not installed as a result.
    
    This test fails plainly when it can not find urllib3.contrib.pyopenssl instead skip the test if it does not find the module.

--- packages/google-auth/tests/transport/test__custom_tls_signer.py
+++ packages/google-auth/tests/transport/test__custom_tls_signer.py
@@ -18,12 +18,16 @@ from unittest import mock
 
 import pytest  # type: ignore
 from requests.packages.urllib3.util.ssl_ import create_urllib3_context  # type: ignore
-import urllib3.contrib.pyopenssl  # type: ignore
 
 from google.auth import exceptions
 from google.auth.transport import _custom_tls_signer
 
-urllib3.contrib.pyopenssl.inject_into_urllib3()
+urllib3_pyopenssl = pytest.importorskip(
+    "urllib3.contrib.pyopenssl",
+    reason="urllib3.contrib.pyopenssl not available in this environment",
+)
+
+urllib3_pyopenssl.inject_into_urllib3()
 
 FAKE_ENTERPRISE_CERT_FILE_PATH = "/path/to/enterprise/cert/file"
 ENTERPRISE_CERT_FILE = os.path.join(
