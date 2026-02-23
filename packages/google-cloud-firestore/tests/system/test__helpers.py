07718d7c3eaaa50404f7b354b85ba4957be0b917
Chalmer Lowe <chalmerlowe@google.com>
2026-02-23 11:59:33 -0500

    minor revision back to original name of database.

--- packages/google-cloud-firestore/tests/system/test__helpers.py
+++ packages/google-cloud-firestore/tests/system/test__helpers.py
@@ -14,7 +14,7 @@ ENTERPRISE_MODE_ERROR = "only allowed on ENTERPRISE mode"
 UNIQUE_RESOURCE_ID = unique_resource_id("-")
 EMULATOR_CREDS = EmulatorCreds()
 FIRESTORE_EMULATOR = os.environ.get(_FIRESTORE_EMULATOR_HOST) is not None
-FIRESTORE_OTHER_DB = os.environ.get("SYSTEM_TESTS_DATABASE", "system-tests-native-db")
+FIRESTORE_OTHER_DB = os.environ.get("SYSTEM_TESTS_DATABASE", "system-tests-named-db")
 FIRESTORE_ENTERPRISE_DB = os.environ.get("ENTERPRISE_DATABASE", "enterprise-db-native")
 
 # run all tests against default database, and a named database
