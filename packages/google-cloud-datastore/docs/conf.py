a0b9aee85acc82911eb7edbfa648b639eeb360b1
ohmayr <omairn@google.com>
2026-02-27 00:15:49 +0000

    add post processing for datastore with exclude patterns in conf

--- packages/google-cloud-datastore/docs/conf.py
+++ packages/google-cloud-datastore/docs/conf.py
@@ -111,6 +111,9 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = [
+    "datastore_admin_v1/**",
+    "datastore_v1/**",
+    "multiprocessing.rst",
     "_build",
     "**/.nox/**/*",
     "samples/AUTHORING_GUIDE.md",
---
+++ packages/google-cloud-datastore/docs/datastore_admin_v1/datastore_admin.rst
@@ -0,0 +1,10 @@
+DatastoreAdmin
+--------------------------------
+
+.. automodule:: google.cloud.datastore_admin_v1.services.datastore_admin
+    :members:
+    :inherited-members:
+
+.. automodule:: google.cloud.datastore_admin_v1.services.datastore_admin.pagers
+    :members:
+    :inherited-members:
