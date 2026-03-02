3704f05435fe9aa706f8fe70aa33caec34b436cd
Anthonios Partheniou <partheniou@google.com>
2026-03-02 21:38:39 +0000

    tests: See https://github.com/googleapis/gapic-generator-python/issues/2565

--- packages/google-auth/noxfile.py
+++ packages/google-auth/noxfile.py
@@ -167,16 +167,3 @@ def docs(session):
         os.path.join("docs", "_build", "html", ""),
     )
 
-
-@nox.session(python="pypy")
-def pypy(session):
-    session.install("-e", ".[testing]")
-    session.run(
-        "pytest",
-        f"--junitxml=unit_{session.python}_sponge_log.xml",
-        "--cov=google.auth",
-        "--cov=google.oauth2",
-        "--cov=tests",
-        "tests",
-        "tests_async",
-    )
