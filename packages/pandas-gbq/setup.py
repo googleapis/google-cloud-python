80734ecc89d8931ca56f3d169fa602d4400deea6
chalmer lowe <chalmerlowe@google.com>
2026-03-06 17:49:44 -0500

    updates minimum python runtime and trove classifier

--- packages/pandas-gbq/setup.py
+++ packages/pandas-gbq/setup.py
@@ -88,6 +88,7 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -102,7 +103,7 @@ setuptools.setup(
     packages=packages,
     install_requires=dependencies,
     extras_require=extras,
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     include_package_data=True,
     zip_safe=False,
 )
