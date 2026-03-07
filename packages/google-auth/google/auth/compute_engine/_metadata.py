0867d5dbd9b1a40ba11e690ebff2e6f7c8bbcc3b
Daniel Sanche <d.sanche14@gmail.com>
2026-03-06 16:40:04 -0800

    added fix

--- packages/google-auth/google/auth/compute_engine/_metadata.py
+++ packages/google-auth/google/auth/compute_engine/_metadata.py
@@ -177,7 +177,7 @@ def _prepare_request_for_mds(request, use_mtls=False) -> None:
     # Only modify the request if mTLS is enabled.
     if use_mtls:
         # Ensure the request has a session to mount the adapter to.
-        if not request.session:
+        if not hasattr(request, "session") or not request.session:
             request.session = requests.Session()
 
         adapter = _mtls.MdsMtlsAdapter()
