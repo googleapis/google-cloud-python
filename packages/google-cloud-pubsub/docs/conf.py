1ce848a4169f272a8b6207ad10cac84b7f13d519
ohmayr <omairn@google.com>
2026-03-09 11:12:29 +0000

    fix docs issue with pagers

--- packages/google-cloud-pubsub/docs/conf.py
+++ packages/google-cloud-pubsub/docs/conf.py
@@ -111,6 +111,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = [
+    "pubsub_v1/**",
     "_build",
     "**/.nox/**/*",
     "samples/AUTHORING_GUIDE.md",
