925f68430174a94ae391c84ac23291681f443d78
chalmer lowe <chalmerlowe@google.com>
2026-03-12 09:15:27 -0400

    chore: updates linting of pyarrow_to_bigquery

--- packages/pandas-gbq/pandas_gbq/schema/pyarrow_to_bigquery.py
+++ packages/pandas-gbq/pandas_gbq/schema/pyarrow_to_bigquery.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023 pandas-gbq Authors All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
- 
+
 from __future__ import annotations
 
 from typing import Optional, cast
