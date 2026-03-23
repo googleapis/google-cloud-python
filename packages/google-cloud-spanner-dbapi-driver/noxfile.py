3ceb931ff603b95626d3f3801fca76934cfd5de5
Sanjeev Bhatt <sanjeevbhatt@google.com>
2026-03-23 11:43:21 +0000

    refactor: reformat coverage report command for improved readability

--- packages/google-cloud-spanner-dbapi-driver/noxfile.py
+++ packages/google-cloud-spanner-dbapi-driver/noxfile.py
@@ -331,7 +331,9 @@ def cover(session):
     test runs (not system test runs), and then erases coverage data.
     """
     session.install("coverage", "pytest-cov")
-    session.run("coverage", "report", "--show-missing", "--fail-under=0")  # TODO: Increase this as tests are added.
+    session.run(
+        "coverage", "report", "--show-missing", "--fail-under=0"
+    )  # TODO: Increase this as tests are added.
 
     session.run("coverage", "erase")
 
