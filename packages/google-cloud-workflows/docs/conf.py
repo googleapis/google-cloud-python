dcb70499dc81adb0643011ee026ef4efa392e34c
Jon Skeet <jonskeet@google.com>
2026-03-23 08:43:56 +0000

    run librarian generate --all

--- packages/google-cloud-workflows/docs/conf.py
+++ packages/google-cloud-workflows/docs/conf.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##
-# google-cloud-workflows documentation build configuration file
+# google-cloud-workflows-executions documentation build configuration file
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -82,7 +82,7 @@ source_suffix = [".rst", ".md"]
 root_doc = "index"
 
 # General information about the project.
-project = "google-cloud-workflows"
+project = "google-cloud-workflows-executions"
 copyright = "2025, Google, LLC"
 author = "Google APIs"
 
@@ -156,7 +156,7 @@ html_theme = "alabaster"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "description": "Google Cloud Client Libraries for google-cloud-workflows",
+    "description": "Google Cloud Workflows Client Libraries for google-cloud-workflows-executions",
     "github_user": "googleapis",
     "github_repo": "google-cloud-python",
     "github_banner": True,
@@ -250,7 +250,7 @@ html_static_path = ["_static"]
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "google-cloud-workflows-doc"
+htmlhelp_basename = "google-cloud-workflows-executions-doc"
 
 # -- Options for warnings ------------------------------------------------------
 
@@ -283,8 +283,8 @@ latex_elements = {
 latex_documents = [
     (
         root_doc,
-        "google-cloud-workflows.tex",
-        "google-cloud-workflows Documentation",
+        "google-cloud-workflows-executions.tex",
+        "google-cloud-workflows-executions Documentation",
         author,
         "manual",
     )
@@ -318,8 +318,8 @@ latex_documents = [
 man_pages = [
     (
         root_doc,
-        "google-cloud-workflows",
-        "google-cloud-workflows Documentation",
+        "google-cloud-workflows-executions",
+        "google-cloud-workflows-executions Documentation",
         [author],
         1,
     )
@@ -337,11 +337,11 @@ man_pages = [
 texinfo_documents = [
     (
         root_doc,
-        "google-cloud-workflows",
-        "google-cloud-workflows Documentation",
+        "google-cloud-workflows-executions",
+        "google-cloud-workflows-executions Documentation",
         author,
-        "google-cloud-workflows",
-        "google-cloud-workflows Library",
+        "google-cloud-workflows-executions",
+        "google-cloud-workflows-executions Library",
         "APIs",
     )
 ]
