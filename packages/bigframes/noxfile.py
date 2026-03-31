ae37bd099da9298af5ff35f19980905b2bdd8166
chalmer lowe <chalmerlowe@google.com>
2026-03-31 09:35:35 -0400

    minor version change to docs session to confirm CI/CD execution

--- packages/bigframes/noxfile.py
+++ packages/bigframes/noxfile.py
@@ -514,7 +514,7 @@ def cover(session):
     session.run("coverage", "erase")
 
 
-@nox.session(python="3.10")
+@nox.session(python="3.13")
 def docs(session):
     """Build the docs for this library."""
     session.install("-e", ".[scikit-learn]")
