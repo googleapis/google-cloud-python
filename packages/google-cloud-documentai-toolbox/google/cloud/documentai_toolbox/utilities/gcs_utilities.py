20d9ae8a0dea830a9b220497b8c64f15fd523596
chalmer lowe <chalmerlowe@google.com>
2026-04-17 09:36:31 -0400

    fix(documentai-toolbox): move type ignore to correct line in gcs_utilities

--- packages/google-cloud-documentai-toolbox/google/cloud/documentai_toolbox/utilities/gcs_utilities.py
+++ packages/google-cloud-documentai-toolbox/google/cloud/documentai_toolbox/utilities/gcs_utilities.py
@@ -20,8 +20,8 @@ from typing import Dict, List, Optional, Tuple
 
 from google.api_core.gapic_v1 import client_info
 
-from google.cloud import (
-    documentai,  # type: ignore[attr-defined]
+from google.cloud import (  # type: ignore[attr-defined]
+    documentai,
     documentai_toolbox,
     storage,
 )
