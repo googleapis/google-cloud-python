6f99e0ac758212cbe6d13b183c0f9d4dedc85532
Daniel Sanche <d.sanche14@gmail.com>
2026-04-23 20:41:05 -0700

    Revert "fix(google-auth): remove deprecated rsa dependency (#16020)"
    This reverts commit e8927b9c0257912dc3eca42307bde10c505e3975.

--- packages/google-auth/google/auth/crypt/rsa.py
+++ packages/google-auth/google/auth/crypt/rsa.py
@@ -24,6 +24,7 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 
 from google.auth import _helpers
 from google.auth.crypt import _cryptography_rsa
+from google.auth.crypt import _python_rsa
 from google.auth.crypt import base
 
 RSA_KEY_MODULE_PREFIX = "rsa.key"
@@ -36,7 +37,6 @@ class RSAVerifier(base.Verifier):
         public_key (Union["rsa.key.PublicKey", cryptography.hazmat.primitives.asymmetric.rsa.RSAPublicKey]):
             The public key used to verify signatures.
     Raises:
-        ImportError: if called with an rsa.key.PublicKey, when the rsa library is not installed
         ValueError: if an unrecognized public key is provided
     """
 
@@ -45,8 +45,6 @@ class RSAVerifier(base.Verifier):
         if isinstance(public_key, RSAPublicKey):
             impl_lib = _cryptography_rsa
         elif module_str.startswith(RSA_KEY_MODULE_PREFIX):
-            from google.auth.crypt import _python_rsa
-
             impl_lib = _python_rsa
         else:
             raise ValueError(f"unrecognized public key type: {type(public_key)}")
@@ -87,7 +85,6 @@ class RSASigner(base.Signer, base.FromServiceAccountMixin):
             public key or certificate.
 
     Raises:
-        ImportError: if called with an rsa.key.PrivateKey, when the rsa library is not installed
         ValueError: if an unrecognized public key is provided
     """
 
@@ -96,8 +93,6 @@ class RSASigner(base.Signer, base.FromServiceAccountMixin):
         if isinstance(private_key, RSAPrivateKey):
             impl_lib = _cryptography_rsa
         elif module_str.startswith(RSA_KEY_MODULE_PREFIX):
-            from google.auth.crypt import _python_rsa
-
             impl_lib = _python_rsa
         else:
             raise ValueError(f"unrecognized private key type: {type(private_key)}")
