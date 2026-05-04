0df644de264a8603ff55b222607265005d9ff8a6
chalmer lowe <chalmerlowe@google.com>
2026-05-04 12:20:31 -0400

    fix(ci): remove Python 3.9 case from run_single_test.sh
    This removes the case for Python 3.9 in the unit test section, as we no longer run tests for this version.

--- ci/run_single_test.sh
+++ ci/run_single_test.sh
@@ -70,10 +70,7 @@ case ${TEST_TYPE} in
         ;;
     unit)
         case ${PY_VERSION} in
-        "3.9")
-            nox -s unit-3.9
-            retval=$?
-            ;;
+
         "3.10")
             nox -s unit-3.10
             retval=$?
