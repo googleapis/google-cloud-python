517822c94501d27e4b86676433164c11c8a20ba3
chalmer lowe <chalmerlowe@google.com>
2026-05-05 15:35:38 -0400

    fix(crc32c): Update check session to use editable install instead of wheels

--- packages/google-crc32c/noxfile.py
+++ packages/google-crc32c/noxfile.py
@@ -67,7 +67,7 @@ def build_libcrc32c(session):
 @nox.session(python=UNIT_TEST_PYTHON_VERSIONS)
 def check(session):
     session.install("pytest")
-    session.install("--no-index", f"--find-links={HERE}/wheels", "google-crc32c")
+    session.install("-e", ".")
 
     # Run py.test against the unit tests.
     session.run("py.test", "tests")
