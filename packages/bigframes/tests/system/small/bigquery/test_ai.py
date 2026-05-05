dea795ba72f04e11720b7dfafd782cd64d085d4c
Shenyang Cai <sycai@google.com>
2026-05-05 03:50:08 +0000

    fix lint

--- packages/bigframes/tests/system/small/bigquery/test_ai.py
+++ packages/bigframes/tests/system/small/bigquery/test_ai.py
@@ -359,6 +359,7 @@ def test_ai_classify_multi_model(session, bq_connection):
     df = session.from_glob_path(
         "gs://bigframes-dev-testing/a_multimodel/images/*",
         name="image",
+        connection=bq_connection,
     )
 
     result = bbq.ai.classify(df["image"], ["photo", "cartoon"])
