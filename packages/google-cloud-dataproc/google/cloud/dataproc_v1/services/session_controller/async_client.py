136a287f0564a0b744e673ba08b10b4901564646
Jon Skeet <jonskeet@google.com>
2026-05-07 14:03:19 +0000

    chore: changes in google-cloud-dataproc

--- packages/google-cloud-dataproc/google/cloud/dataproc_v1/services/session_controller/async_client.py
+++ packages/google-cloud-dataproc/google/cloud/dataproc_v1/services/session_controller/async_client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -353,11 +353,11 @@ class SessionControllerAsyncClient:
                 )
 
                 # Make the request
-                operation = client.create_session(request=request)
+                operation = await client.create_session(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -735,11 +735,11 @@ class SessionControllerAsyncClient:
                 )
 
                 # Make the request
-                operation = client.terminate_session(request=request)
+                operation = await client.terminate_session(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -863,11 +863,11 @@ class SessionControllerAsyncClient:
                 )
 
                 # Make the request
-                operation = client.delete_session(request=request)
+                operation = await client.delete_session(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
