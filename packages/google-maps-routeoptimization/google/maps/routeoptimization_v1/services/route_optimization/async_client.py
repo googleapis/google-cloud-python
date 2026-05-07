78b049649ec74b5d97d14eb7149c1771580eec5c
Jon Skeet <jonskeet@google.com>
2026-05-07 14:07:50 +0000

    chore: changes in google-maps-routeoptimization

--- packages/google-maps-routeoptimization/google/maps/routeoptimization_v1/services/route_optimization/async_client.py
+++ packages/google-maps-routeoptimization/google/maps/routeoptimization_v1/services/route_optimization/async_client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -492,11 +492,11 @@ class RouteOptimizationAsyncClient:
                 )
 
                 # Make the request
-                operation = client.batch_optimize_tours(request=request)
+                operation = await client.batch_optimize_tours(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -621,11 +621,11 @@ class RouteOptimizationAsyncClient:
                 )
 
                 # Make the request
-                operation = client.optimize_tours_long_running(request=request)
+                operation = await client.optimize_tours_long_running(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -754,11 +754,11 @@ class RouteOptimizationAsyncClient:
                 )
 
                 # Make the request
-                operation = client.optimize_tours_uri(request=request)
+                operation = await client.optimize_tours_uri(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
