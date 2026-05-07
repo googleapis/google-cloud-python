15ac752a6736e3a085c93be5801dc4b5c24a0bf1
Jon Skeet <jonskeet@google.com>
2026-05-07 14:05:53 +0000

    chore: changes in google-cloud-network-security

--- packages/google-cloud-network-security/google/cloud/network_security_v1/services/address_group_service/async_client.py
+++ packages/google-cloud-network-security/google/cloud/network_security_v1/services/address_group_service/async_client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -602,11 +602,11 @@ class AddressGroupServiceAsyncClient:
                 )
 
                 # Make the request
-                operation = client.create_address_group(request=request)
+                operation = await client.create_address_group(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -756,11 +756,11 @@ class AddressGroupServiceAsyncClient:
                 )
 
                 # Make the request
-                operation = client.update_address_group(request=request)
+                operation = await client.update_address_group(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -900,11 +900,11 @@ class AddressGroupServiceAsyncClient:
                 )
 
                 # Make the request
-                operation = client.add_address_group_items(request=request)
+                operation = await client.add_address_group_items(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -1039,11 +1039,11 @@ class AddressGroupServiceAsyncClient:
                 )
 
                 # Make the request
-                operation = client.remove_address_group_items(request=request)
+                operation = await client.remove_address_group_items(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -1178,11 +1178,11 @@ class AddressGroupServiceAsyncClient:
                 )
 
                 # Make the request
-                operation = client.clone_address_group_items(request=request)
+                operation = await client.clone_address_group_items(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -1315,11 +1315,11 @@ class AddressGroupServiceAsyncClient:
                 )
 
                 # Make the request
-                operation = client.delete_address_group(request=request)
+                operation = await client.delete_address_group(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
--- packages/google-cloud-network-security/tests/unit/gapic/network_security_v1alpha1/test_intercept.py
+++ packages/google-cloud-network-security/tests/unit/gapic/network_security_v1alpha1/test_intercept.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-    from unittest.mock import AsyncMock  # pragma: NO COVER
-except ImportError:  # pragma: NO COVER
-    import mock
-
 import json
 import math
+import os
 from collections.abc import AsyncIterable, Iterable, Mapping, Sequence
+from unittest import mock
+from unittest.mock import AsyncMock
 
 import grpc
 import pytest
@@ -1824,9 +1818,7 @@ async def test_list_intercept_endpoint_groups_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
+        async for page_ in (
             await client.list_intercept_endpoint_groups(request={})
         ).pages:
             pages.append(page_)
@@ -3830,9 +3822,7 @@ async def test_list_intercept_endpoint_group_associations_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
+        async for page_ in (
             await client.list_intercept_endpoint_group_associations(request={})
         ).pages:
             pages.append(page_)
@@ -5835,9 +5825,7 @@ async def test_list_intercept_deployment_groups_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
+        async for page_ in (
             await client.list_intercept_deployment_groups(request={})
         ).pages:
             pages.append(page_)
@@ -7836,11 +7824,7 @@ async def test_list_intercept_deployments_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
-            await client.list_intercept_deployments(request={})
-        ).pages:
+        async for page_ in (await client.list_intercept_deployments(request={})).pages:
             pages.append(page_)
         for page_, token in zip(pages, ["abc", "def", "ghi", ""]):
             assert page_.raw_page.next_page_token == token
