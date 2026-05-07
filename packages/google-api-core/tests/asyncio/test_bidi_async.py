0c736a6a8c36090664701b28458fbb484087e1e6
chalmer lowe <chalmerlowe@google.com>
2026-05-07 07:11:48 -0400

    test(api-core): remove Python < 3.10 polyfills and skips

--- packages/google-api-core/tests/asyncio/test_bidi_async.py
+++ packages/google-api-core/tests/asyncio/test_bidi_async.py
@@ -33,14 +33,6 @@ except ImportError:  # pragma: NO COVER
 from google.api_core import bidi_async
 from google.api_core import exceptions
 
-# TODO: remove this when droppping support for "Python 3.10" and below.
-if sys.version_info < (3, 10):  # type: ignore[operator]
-
-    def aiter(obj):
-        return obj.__aiter__()
-
-    async def anext(obj):
-        return await obj.__anext__()
 
 
 @pytest.mark.asyncio
@@ -185,10 +177,6 @@ class TestAsyncBidiRpc:
         callback.assert_called_once_with(mock.sentinel.future)
 
     @pytest.mark.asyncio
-    @pytest.mark.skipif(
-        sys.version_info < (3, 8),  # type: ignore[operator]
-        reason="Versions of Python below 3.8 don't provide support for assert_awaited_once",
-    )
     async def test_metadata(self):
         rpc, call = make_async_rpc()
         bidi_rpc = bidi_async.AsyncBidiRpc(rpc, metadata=mock.sentinel.A)
