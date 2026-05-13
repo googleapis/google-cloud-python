6d1da6041e7fa1034a53e030550c93a7090c7e5d
Jon Skeet <jonskeet@google.com>
2026-05-13 14:13:47 +0000

    feat: regenerate google-shopping packages (#17072)
    This Pull Request drops support for Python <=3.9 in generated packages.
    This is part of our ongoing effort to modernize the Cloud Python SDK
    libraries and remove support for End-of-Life (EOL) Python versions.
    
    ## Changes
    - Removed Python <=3.9 from testing, configuration, and constraints
    files.
    - Resolved dependency conflicts arising from dropping Python <=3.9
    support.
    - Cleaned up obsolete code related to Python <=3.9 support.
    - Updated select copyright dates
    - Updated some autogenerated headings
    - Updated some tests to resolve issues discovered during this migration.

--- packages/google-shopping-merchant-issueresolution/tests/unit/gapic/merchant_issueresolution_v1beta/test_issue_resolution_service.py
+++ packages/google-shopping-merchant-issueresolution/tests/unit/gapic/merchant_issueresolution_v1beta/test_issue_resolution_service.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-    from unittest.mock import AsyncMock  # pragma: NO COVER
-except ImportError:  # pragma: NO COVER
-    import mock
-
 import json
 import math
+import os
 from collections.abc import AsyncIterable, Iterable, Mapping, Sequence
+from unittest import mock
+from unittest.mock import AsyncMock
 
 import grpc
 import pytest
@@ -2506,7 +2500,7 @@ def test_render_account_issues_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_render_account_issues_rest_unset_required_fields():
@@ -2705,7 +2699,7 @@ def test_render_product_issues_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_render_product_issues_rest_unset_required_fields():
@@ -2894,7 +2888,7 @@ def test_trigger_action_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_trigger_action_rest_unset_required_fields():
