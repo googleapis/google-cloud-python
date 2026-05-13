44fdeaa678af742ed3a771ebe3c245b10d6e7145
Jon Skeet <jonskeet@google.com>
2026-05-13 14:08:42 +0000

    feat: regenerate google-cloud-[o-r] packages
    This Pull Request drops support for Python <=3.9 in generated
    packages. This is part of our ongoing effort to modernize the Cloud
    Python SDK libraries and remove support for End-of-Life (EOL) Python
    versions.
    
    - Removed Python <=3.9 from testing, configuration, and constraints files.
    - Resolved dependency conflicts arising from dropping Python <=3.9 support.
    - Cleaned up obsolete code related to Python <=3.9 support.
    - Updated select copyright dates
    - Updated some autogenerated headings
    - Updated some tests to resolve issues discovered during this migration.

--- packages/google-cloud-os-config/samples/generated_samples/osconfig_v1_generated_os_config_zonal_service_create_os_policy_assignment_async.py
+++ packages/google-cloud-os-config/samples/generated_samples/osconfig_v1_generated_os_config_zonal_service_create_os_policy_assignment_async.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -58,11 +58,11 @@ async def sample_create_os_policy_assignment():
     )
 
     # Make the request
-    operation = client.create_os_policy_assignment(request=request)
+    operation = await client.create_os_policy_assignment(request=request)
 
     print("Waiting for operation to complete...")
 
-    response = (await operation).result()
+    response = await operation.result()
 
     # Handle the response
     print(response)
--- packages/google-cloud-retail/tests/unit/gapic/retail_v2beta/test_serving_config_service.py
+++ packages/google-cloud-retail/tests/unit/gapic/retail_v2beta/test_serving_config_service.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-    from unittest.mock import AsyncMock  # pragma: NO COVER
-except ImportError:  # pragma: NO COVER
-    import mock
-
 import json
 import math
+import os
 from collections.abc import AsyncIterable, Iterable, Mapping, Sequence
+from unittest import mock
+from unittest.mock import AsyncMock
 
 import grpc
 import pytest
@@ -3539,11 +3533,7 @@ async def test_list_serving_configs_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
-            await client.list_serving_configs(request={})
-        ).pages:
+        async for page_ in (await client.list_serving_configs(request={})).pages:
             pages.append(page_)
         for page_, token in zip(pages, ["abc", "def", "ghi", ""]):
             assert page_.raw_page.next_page_token == token
@@ -4486,7 +4476,7 @@ def test_create_serving_config_rest_required_fields(
                 ("$alt", "json;enum-encoding=int"),
             ]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_create_serving_config_rest_unset_required_fields():
@@ -4681,7 +4671,7 @@ def test_delete_serving_config_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_delete_serving_config_rest_unset_required_fields():
@@ -4862,7 +4852,7 @@ def test_update_serving_config_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_update_serving_config_rest_unset_required_fields():
@@ -5050,7 +5040,7 @@ def test_get_serving_config_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_get_serving_config_rest_unset_required_fields():
@@ -5243,7 +5233,7 @@ def test_list_serving_configs_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_list_serving_configs_rest_unset_required_fields():
@@ -5504,7 +5494,7 @@ def test_add_control_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_add_control_rest_unset_required_fields():
@@ -5697,7 +5687,7 @@ def test_remove_control_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_remove_control_rest_unset_required_fields():
