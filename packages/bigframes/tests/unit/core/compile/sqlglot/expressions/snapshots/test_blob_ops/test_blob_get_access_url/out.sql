fbe7f75d8d8360f778a3dfddc4bf53a2c51fffc3
Shuowei Li <shuowei@google.com>
2026-05-14 03:21:26 +0000

    chore: remove orphaned blob ops snapshots

--- packages/bigframes/tests/unit/core/compile/sqlglot/expressions/snapshots/test_blob_ops/test_blob_get_access_url/out.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  `rowindex`,
-  OBJ.GET_ACCESS_URL(OBJ.MAKE_REF(`string_col`, 'my-connection'), 'r') AS `string_col`
-FROM `bigframes-dev`.`sqlglot_test`.`scalar_types` AS `bft_0`
--- packages/bigframes/tests/unit/core/compile/sqlglot/expressions/snapshots/test_blob_ops/test_blob_make_ref/out.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  `rowindex`,
-  OBJ.MAKE_REF(`string_col`, 'my-connection') AS `string_col`
-FROM `bigframes-dev`.`sqlglot_test`.`scalar_types` AS `bft_0`
