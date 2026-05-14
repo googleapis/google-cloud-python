7eaec0375716158329755b1099de3f944c9b0490
Jon Skeet <jonskeet@google.com>
2026-05-14 09:18:23 +0000

    feat: regenerate google-cloud-dialogflow
    Updates the Dialogflow API, which was previously blocked on a resource
    name conflict.
    
    This Pull Request also drops support for Python <=3.9 in generated packages.
    This is part of our ongoing effort to modernize the Cloud Python SDK
    libraries and remove support for End-of-Life (EOL) Python versions.
    
    - Removed Python <=3.9 from testing, configuration, and constraints files.
    - Resolved dependency conflicts arising from dropping Python <=3.9 support.
    - Cleaned up obsolete code related to Python <=3.9 support.
    - Updated select copyright dates
    - Updated some autogenerated headings
    - Updated some tests to resolve issues discovered during this migration.

--- packages/google-cloud-dialogflow/tests/unit/gapic/dialogflow_v2/test_answer_records.py
+++ packages/google-cloud-dialogflow/tests/unit/gapic/dialogflow_v2/test_answer_records.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-    from unittest.mock import AsyncMock  # pragma: NO COVER
-except ImportError:  # pragma: NO COVER
-    import mock
-
 import json
 import math
+import os
 from collections.abc import AsyncIterable, Iterable, Mapping, Sequence
+from unittest import mock
+from unittest.mock import AsyncMock
 
 import grpc
 import pytest
@@ -1864,11 +1858,7 @@ async def test_list_answer_records_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
-            await client.list_answer_records(request={})
-        ).pages:
+        async for page_ in (await client.list_answer_records(request={})).pages:
             pages.append(page_)
         for page_, token in zip(pages, ["abc", "def", "ghi", ""]):
             assert page_.raw_page.next_page_token == token
@@ -2342,7 +2332,7 @@ def test_list_answer_records_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_list_answer_records_rest_unset_required_fields():
@@ -2592,7 +2582,7 @@ def test_update_answer_record_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_update_answer_record_rest_unset_required_fields():
@@ -3392,6 +3382,9 @@ def test_update_answer_record_rest_call_success(request_type):
                     {
                         "tool_call": {
                             "tool": "tool_value",
+                            "ces_tool": "ces_tool_value",
+                            "ces_toolset": "ces_toolset_value",
+                            "ces_app": "ces_app_value",
                             "tool_display_name": "tool_display_name_value",
                             "tool_display_details": "tool_display_details_value",
                             "action": "action_value",
@@ -3402,6 +3395,9 @@ def test_update_answer_record_rest_call_success(request_type):
                         },
                         "tool_call_result": {
                             "tool": "tool_value",
+                            "ces_toolset": "ces_toolset_value",
+                            "ces_tool": "ces_tool_value",
+                            "ces_app": "ces_app_value",
                             "action": "action_value",
                             "error": {"message": "message_value"},
                             "raw_content": b"raw_content_blob",
@@ -4409,10 +4405,65 @@ def test_parse_answer_record_path():
     assert expected == actual
 
 
-def test_context_path():
+def test_app_path():
     project = "oyster"
-    session = "nudibranch"
-    context = "cuttlefish"
+    location = "nudibranch"
+    app = "cuttlefish"
+    expected = "projects/{project}/locations/{location}/apps/{app}".format(
+        project=project,
+        location=location,
+        app=app,
+    )
+    actual = AnswerRecordsClient.app_path(project, location, app)
+    assert expected == actual
+
+
+def test_parse_app_path():
+    expected = {
+        "project": "mussel",
+        "location": "winkle",
+        "app": "nautilus",
+    }
+    path = AnswerRecordsClient.app_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = AnswerRecordsClient.parse_app_path(path)
+    assert expected == actual
+
+
+def test_ces_tool_path():
+    project = "scallop"
+    location = "abalone"
+    app = "squid"
+    tool = "clam"
+    expected = "projects/{project}/locations/{location}/apps/{app}/tools/{tool}".format(
+        project=project,
+        location=location,
+        app=app,
+        tool=tool,
+    )
+    actual = AnswerRecordsClient.ces_tool_path(project, location, app, tool)
+    assert expected == actual
+
+
+def test_parse_ces_tool_path():
+    expected = {
+        "project": "whelk",
+        "location": "octopus",
+        "app": "oyster",
+        "tool": "nudibranch",
+    }
+    path = AnswerRecordsClient.ces_tool_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = AnswerRecordsClient.parse_ces_tool_path(path)
+    assert expected == actual
+
+
+def test_context_path():
+    project = "cuttlefish"
+    session = "mussel"
+    context = "winkle"
     expected = "projects/{project}/agent/sessions/{session}/contexts/{context}".format(
         project=project,
         session=session,
@@ -4424,9 +4475,9 @@ def test_context_path():
 
 def test_parse_context_path():
     expected = {
-        "project": "mussel",
-        "session": "winkle",
-        "context": "nautilus",
+        "project": "nautilus",
+        "session": "scallop",
+        "context": "abalone",
     }
     path = AnswerRecordsClient.context_path(**expected)
 
@@ -4436,8 +4487,8 @@ def test_parse_context_path():
 
 
 def test_intent_path():
-    project = "scallop"
-    intent = "abalone"
+    project = "squid"
+    intent = "clam"
     expected = "projects/{project}/agent/intents/{intent}".format(
         project=project,
         intent=intent,
@@ -4448,8 +4499,8 @@ def test_intent_path():
 
 def test_parse_intent_path():
     expected = {
-        "project": "squid",
-        "intent": "clam",
+        "project": "whelk",
+        "intent": "octopus",
     }
     path = AnswerRecordsClient.intent_path(**expected)
 
@@ -4459,9 +4510,9 @@ def test_parse_intent_path():
 
 
 def test_tool_path():
-    project = "whelk"
-    location = "octopus"
-    tool = "oyster"
+    project = "oyster"
+    location = "nudibranch"
+    tool = "cuttlefish"
     expected = "projects/{project}/locations/{location}/tools/{tool}".format(
         project=project,
         location=location,
@@ -4473,9 +4524,9 @@ def test_tool_path():
 
 def test_parse_tool_path():
     expected = {
-        "project": "nudibranch",
-        "location": "cuttlefish",
-        "tool": "mussel",
+        "project": "mussel",
+        "location": "winkle",
+        "tool": "nautilus",
     }
     path = AnswerRecordsClient.tool_path(**expected)
 
@@ -4484,8 +4535,39 @@ def test_parse_tool_path():
     assert expected == actual
 
 
+def test_toolset_path():
+    project = "scallop"
+    location = "abalone"
+    app = "squid"
+    toolset = "clam"
+    expected = (
+        "projects/{project}/locations/{location}/apps/{app}/toolsets/{toolset}".format(
+            project=project,
+            location=location,
+            app=app,
+            toolset=toolset,
+        )
+    )
+    actual = AnswerRecordsClient.toolset_path(project, location, app, toolset)
+    assert expected == actual
+
+
+def test_parse_toolset_path():
+    expected = {
+        "project": "whelk",
+        "location": "octopus",
+        "app": "oyster",
+        "toolset": "nudibranch",
+    }
+    path = AnswerRecordsClient.toolset_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = AnswerRecordsClient.parse_toolset_path(path)
+    assert expected == actual
+
+
 def test_common_billing_account_path():
-    billing_account = "winkle"
+    billing_account = "cuttlefish"
     expected = "billingAccounts/{billing_account}".format(
         billing_account=billing_account,
     )
@@ -4495,7 +4577,7 @@ def test_common_billing_account_path():
 
 def test_parse_common_billing_account_path():
     expected = {
-        "billing_account": "nautilus",
+        "billing_account": "mussel",
     }
     path = AnswerRecordsClient.common_billing_account_path(**expected)
 
@@ -4505,7 +4587,7 @@ def test_parse_common_billing_account_path():
 
 
 def test_common_folder_path():
-    folder = "scallop"
+    folder = "winkle"
     expected = "folders/{folder}".format(
         folder=folder,
     )
@@ -4515,7 +4597,7 @@ def test_common_folder_path():
 
 def test_parse_common_folder_path():
     expected = {
-        "folder": "abalone",
+        "folder": "nautilus",
     }
     path = AnswerRecordsClient.common_folder_path(**expected)
 
@@ -4525,7 +4607,7 @@ def test_parse_common_folder_path():
 
 
 def test_common_organization_path():
-    organization = "squid"
+    organization = "scallop"
     expected = "organizations/{organization}".format(
         organization=organization,
     )
@@ -4535,7 +4617,7 @@ def test_common_organization_path():
 
 def test_parse_common_organization_path():
     expected = {
-        "organization": "clam",
+        "organization": "abalone",
     }
     path = AnswerRecordsClient.common_organization_path(**expected)
 
@@ -4545,7 +4627,7 @@ def test_parse_common_organization_path():
 
 
 def test_common_project_path():
-    project = "whelk"
+    project = "squid"
     expected = "projects/{project}".format(
         project=project,
     )
@@ -4555,7 +4637,7 @@ def test_common_project_path():
 
 def test_parse_common_project_path():
     expected = {
-        "project": "octopus",
+        "project": "clam",
     }
     path = AnswerRecordsClient.common_project_path(**expected)
 
@@ -4565,8 +4647,8 @@ def test_parse_common_project_path():
 
 
 def test_common_location_path():
-    project = "oyster"
-    location = "nudibranch"
+    project = "whelk"
+    location = "octopus"
     expected = "projects/{project}/locations/{location}".format(
         project=project,
         location=location,
@@ -4577,8 +4659,8 @@ def test_common_location_path():
 
 def test_parse_common_location_path():
     expected = {
-        "project": "cuttlefish",
-        "location": "mussel",
+        "project": "oyster",
+        "location": "nudibranch",
     }
     path = AnswerRecordsClient.common_location_path(**expected)
 
