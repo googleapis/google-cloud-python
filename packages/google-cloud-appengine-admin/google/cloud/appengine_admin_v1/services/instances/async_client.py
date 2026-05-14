4d1a3698c852ed4a47cce73bca2fa333d6ee1419
Jon Skeet <jonskeet@google.com>
2026-05-14 08:25:55 +0000

    feat: regenerate google-cloud-a packages (#17089)
    This Pull Request drops support for Python <=3.9 in generated packages.
    This is part of our ongoing effort to modernize the Cloud Python SDK
    libraries and remove support for End-of-Life (EOL) Python versions.
    
    - Removed Python <=3.9 from testing, configuration, and constraints
    files.
    - Resolved dependency conflicts arising from dropping Python <=3.9
    support.
    - Cleaned up obsolete code related to Python <=3.9 support.
    - Updated select copyright dates
    - Updated some autogenerated headings
    - Updated some tests to resolve issues discovered during this migration.

--- packages/google-cloud-appengine-admin/google/cloud/appengine_admin_v1/services/instances/async_client.py
+++ packages/google-cloud-appengine-admin/google/cloud/appengine_admin_v1/services/instances/async_client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -524,11 +524,11 @@ class InstancesAsyncClient:
                 )
 
                 # Make the request
-                operation = client.delete_instance(request=request)
+                operation = await client.delete_instance(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
@@ -639,11 +639,11 @@ class InstancesAsyncClient:
                 )
 
                 # Make the request
-                operation = client.debug_instance(request=request)
+                operation = await client.debug_instance(request=request)
 
                 print("Waiting for operation to complete...")
 
-                response = (await operation).result()
+                response = await operation.result()
 
                 # Handle the response
                 print(response)
--- packages/google-cloud-appoptimize/tests/unit/gapic/appoptimize_v1beta/test_app_optimize.py
+++ packages/google-cloud-appoptimize/tests/unit/gapic/appoptimize_v1beta/test_app_optimize.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-    from unittest.mock import AsyncMock  # pragma: NO COVER
-except ImportError:  # pragma: NO COVER
-    import mock
-
 import json
 import math
+import os
 from collections.abc import AsyncIterable, Iterable, Mapping, Sequence
+from unittest import mock
+from unittest.mock import AsyncMock
 
 import grpc
 import pytest
@@ -2478,11 +2472,7 @@ async def test_list_reports_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
-            await client.list_reports(request={})
-        ).pages:
+        async for page_ in (await client.list_reports(request={})).pages:
             pages.append(page_)
         for page_, token in zip(pages, ["abc", "def", "ghi", ""]):
             assert page_.raw_page.next_page_token == token
@@ -3308,11 +3298,7 @@ async def test_read_report_async_pages():
             RuntimeError,
         )
         pages = []
-        # Workaround issue in python 3.9 related to code coverage by adding `# pragma: no branch`
-        # See https://github.com/googleapis/gapic-generator-python/pull/1174#issuecomment-1025132372
-        async for page_ in (  # pragma: no branch
-            await client.read_report(request={})
-        ).pages:
+        async for page_ in (await client.read_report(request={})).pages:
             pages.append(page_)
         for page_, token in zip(pages, ["abc", "def", "ghi", ""]):
             assert page_.raw_page.next_page_token == token
@@ -3443,7 +3429,7 @@ def test_create_report_rest_required_fields(
                 ("$alt", "json;enum-encoding=int"),
             ]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_create_report_rest_unset_required_fields():
@@ -3634,7 +3620,7 @@ def test_get_report_rest_required_fields(request_type=app_optimize.GetReportRequ
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_get_report_rest_unset_required_fields():
@@ -3819,7 +3805,7 @@ def test_list_reports_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_list_reports_rest_unset_required_fields():
@@ -4065,7 +4051,7 @@ def test_delete_report_rest_required_fields(
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_delete_report_rest_unset_required_fields():
@@ -4240,7 +4226,7 @@ def test_read_report_rest_required_fields(request_type=app_optimize.ReadReportRe
 
             expected_params = [("$alt", "json;enum-encoding=int")]
             actual_params = req.call_args.kwargs["params"]
-            assert expected_params == actual_params
+            assert sorted(expected_params) == sorted(actual_params)
 
 
 def test_read_report_rest_unset_required_fields():
