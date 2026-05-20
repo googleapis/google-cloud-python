fa66d9a193b0a491704ae7616e8975f16f546f9f
Tim Sweña <swast@google.com>
2026-05-20 21:35:33 +0000

    oops

--- packages/bigframes/tests/system/small/bigquery/test_array.py
+++ packages/bigframes/tests/system/small/bigquery/test_array.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import cast
+
 import numpy as np
 import pandas as pd
 import pytest
