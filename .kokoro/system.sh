2c903de2429a42f2d682cbc8eab658e27324684b
Anthonios Partheniou <partheniou@google.com>
2026-05-28 21:02:35 +0000

    tests: remove hardcoded packages in system.sh

--- .kokoro/system.sh
+++ .kokoro/system.sh
@@ -90,34 +90,8 @@ run_package_test() {
   return $res
 }
 
-packages_with_system_tests=(
-  "bigframes"
-  "google-auth"
-  "google-cloud-bigquery-storage"
-  "google-cloud-bigtable"
-  "google-cloud-compute"
-  "google-cloud-compute-v1beta"
-  "google-cloud-datastore"
-  "google-cloud-dns"
-  "google-cloud-error-reporting"
-  "google-cloud-firestore"
-  "google-cloud-logging"
-  "google-cloud-ndb"
-  "google-cloud-pubsub"
-  "google-cloud-spanner"
-  "google-cloud-storage"
-  "google-cloud-testutils"
-  "pandas-gbq"
-  "sqlalchemy-bigquery"
-  "sqlalchemy-spanner"
-)
-
 # A file for running system tests
 system_test_script="${PROJECT_ROOT}/.kokoro/system-single.sh"
-
-# Join array elements with | for the pattern match
-packages_with_system_tests_pattern=$(printf "|*%s*" "${packages_with_system_tests[@]}")
-packages_with_system_tests_pattern="${packages_with_system_tests_pattern:1}" # Remove the leading pipe
 
 # Run system tests for each package with directory packages/*/tests/system
 for path in `find 'packages' \
@@ -142,8 +116,15 @@ for path in `find 'packages' \
     "${package_path}/**/version.py"
   )
 
-  # If the package is in our "always run full system tests" list, check the whole directory
-  if [[ $package_name == @($packages_with_system_tests_pattern) ]]; then
+  # Hand-written (non-GAPIC_AUTO) packages or google-cloud-compute should check
+  # the whole directory for changes.
+  metadata="${package_path}/.repo-metadata.json"
+  library_type="UNKNOWN"
+  if [ -f "$metadata" ]; then
+    library_type=$(grep -oP '"library_type":\s*"\K[^"]+' "$metadata" || echo "UNKNOWN")
+  fi
+
+  if [[ "${library_type}" != "GAPIC_AUTO" || "${package_name}" == "google-cloud-compute"* ]]; then
     files_to_check=("${package_path}")
   fi
 
