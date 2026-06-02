8823069c1306788b3af3c5b8f1026ae3a89e1d5a
Subham Sinha <suvham@google.com>
2026-06-02 17:24:10 +0530

    style(spanner): update copyright year to 2026 for new test files
    Update copyright headers in test_dbapi_partition_query.py and test_partition_helper.py to exactly 2026, since both are brand new files created in this PR. This resolves reviewer feedback.

--- packages/google-cloud-spanner/tests/unit/spanner_dbapi/test_partition_helper.py
+++ packages/google-cloud-spanner/tests/unit/spanner_dbapi/test_partition_helper.py
@@ -1,4 +1,3 @@
-# Copyright 2024 Google LLC All rights reserved.
 # Copyright 2026 Google LLC All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
