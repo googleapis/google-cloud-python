2e0757382343e7f6febc756c05e9124e7bf10d2a
Shuowei Li <shuowei@google.com>
2026-06-03 19:40:10 +0000

    fix: gemini checks and nameless col to_frame bug

--- packages/bigframes/bigframes/ml/llm.py
+++ packages/bigframes/bigframes/ml/llm.py
@@ -522,14 +522,12 @@ class GeminiTextGenerator(base.RetriableRemotePredictor):
                 )
             )
             warnings.warn(msg)
-        if (
-            self.model_name.startswith(("gemini-2.0", "gemini-1.5"))
-            or self.model_name
-            in (
-                "gemini-2.5-flash",
-                "gemini-2.5-flash-lite",
-                "gemini-2.5-pro-preview-05-06",
-            )
+        if self.model_name.startswith(
+            ("gemini-2.0", "gemini-1.5")
+        ) or self.model_name in (
+            "gemini-2.5-flash",
+            "gemini-2.5-flash-lite",
+            "gemini-2.5-pro-preview-05-06",
         ):
             msg = exceptions.format_message(
                 _MODEL_DEPRECATE_WARNING.format(
