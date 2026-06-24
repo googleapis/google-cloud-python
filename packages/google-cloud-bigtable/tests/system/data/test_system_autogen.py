66bdec91e74746162e43539e29f7442d451aa3fd
Mattie Fu <mattiefu@google.com>
2026-06-24 17:09:34 -0400

    fix(test): add row_keys to ReadRowsQuery in test_value_bitmask_filter

--- packages/google-cloud-bigtable/tests/system/data/test_system_autogen.py
+++ packages/google-cloud-bigtable/tests/system/data/test_system_autogen.py
@@ -925,7 +925,7 @@ class TestSystem(SystemTestRunner):
 
         f = ValueBitmaskFilter(mask)
         temp_rows.add_row(b"row_key_1", value=cell_value)
-        query = ReadRowsQuery(row_filter=f)
+        query = ReadRowsQuery(row_keys=[b"row_key_1"], row_filter=f)
         row_list = target.read_rows(query)
         assert len(row_list) == bool(expect_match), (
             f"row {cell_value!r} not matched as {expect_match} with {mask!r} bitmask filter"
