3f6ba7889ca16b917ca0fee214a43b973eafe771
chalmer lowe <chalmerlowe@google.com>
2026-06-24 10:36:23 -0400

    feat(observability): remove OtelSpanEnricher from current PR

--- packages/google-api-core/google/api_core/observability/__init__.py
+++ packages/google-api-core/google/api_core/observability/__init__.py
@@ -4,19 +4,8 @@ from .options import (
     set_test_env_override,
 )
 
-try:
-    # Tell flake8 that it's okay this is unused, it's just being exposed to the package namespace.
-    from .tracing import OtelSpanEnricher  # noqa: F401
-
-    __all__ = [
-        "is_signal_enabled",
-        "set_test_env_override",
-        "clear_test_env_overrides",
-        "OtelSpanEnricher",
-    ]
-except ImportError:
-    __all__ = [
-        "is_signal_enabled",
-        "set_test_env_override",
-        "clear_test_env_overrides",
-    ]
+__all__ = [
+    "is_signal_enabled",
+    "set_test_env_override",
+    "clear_test_env_overrides",
+]
