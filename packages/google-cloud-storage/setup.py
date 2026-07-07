d1f3d344a45931d5bc028d8a2b9287ce3d4d7982
chalmer lowe <chalmerlowe@google.com>
2026-07-07 11:24:21 -0400

    chore: add 10 more packages to parallel test trigger

--- packages/google-cloud-storage/setup.py
+++ packages/google-cloud-storage/setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2026 Google LLC
+# Trigger parallel system test run
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
