8a87ead375655f7c6865aecfff11497bd0c84063
chalmer lowe <chalmerlowe@google.com>
2026-07-07 11:24:56 -0400

    chore: add 13 packages to sequential test trigger

--- packages/google-api-core/setup.py
+++ packages/google-api-core/setup.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Google LLC
+# Trigger parallel system test run
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,5 @@
 # limitations under the License.
 
 import setuptools
-
 
 setuptools.setup()
