465a6d04954c46fc0bb49b171dfc68fa9c697432
ohmayr <omairn@google.com>
2026-07-07 17:16:07 +0000

    fix: clean up setup.py bounds for Python 3.10+

--- packages/google-auth/setup.py
+++ packages/google-auth/setup.py
@@ -27,10 +27,7 @@ DEPENDENCIES = (
     *cryptography_base_require,
 )
 
-requests_extra_require = [
-    "requests >= 2.20.0, < 3.0.0; python_version < '3.10'",
-    "requests >= 2.30.0, < 3.0.0; python_version >= '3.10'",
-]
+requests_extra_require = ["requests >= 2.30.0, < 3.0.0"]
 
 aiohttp_extra_require = ["aiohttp >= 3.8.0, < 4.0.0", *requests_extra_require]
 
@@ -41,8 +38,7 @@ reauth_extra_require = ["pyu2f>=0.1.5"]
 enterprise_cert_extra_require = cryptography_base_require
 
 urllib3_extra_require = [
-    "urllib3 >= 1.21.1, < 3.0.0; python_version < '3.10'",
-    "urllib3 >= 1.26.15, < 3.0.0; python_version >= '3.10'",
+    "urllib3 >= 1.26.15, < 3.0.0",
     "packaging >= 20.0",
 ]
 
