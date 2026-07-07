54f8a72aa5a3e715228cd9ad9f52e9d8bf4a8949
chalmer lowe <chalmerlowe@google.com>
2026-07-07 11:23:55 -0400

    chore: revert sqlalchemy-bigquery trigger

--- packages/sqlalchemy-bigquery/setup.py
+++ packages/sqlalchemy-bigquery/setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # Copyright (c) 2017 The sqlalchemy-bigquery Authors
-# Trigger parallel system test run
-
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
