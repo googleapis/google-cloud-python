3dff0e4f016906c892b6d099b310ef305b33701b
Anthonios Partheniou <partheniou@google.com>
2026-07-13 22:54:23 +0000

    fix build

--- packages/google-cloud-documentai-toolbox/setup.py
+++ packages/google-cloud-documentai-toolbox/setup.py
@@ -55,6 +55,8 @@ setuptools.setup(
     install_requires=(
         "google-api-core>=2.15.0, <3.0.0",
         "pandas[performance,gcp]>=2.0.0, <3.0.0",
+        # pandas[performance] requires numba which does not have 3.15 support yet
+        "pandas[gcp]>=2.0.0, <3.0.0;python_version>='3.15'",
         "pyarrow>=15.0.0, <23.0.0",
         "tabulate>=0.9.0, <1.0.0",
         "proto-plus>=1.22.3, <2.0.0",
