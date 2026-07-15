7260cd65c6d110bce20c220bc733317c30fbda5b
Anthonios Partheniou <partheniou@google.com>
2026-07-15 15:01:52 +0000

    Run nox -s generate_sync

--- packages/google-cloud-bigtable/tests/system/data/test_system_autogen.py
+++ packages/google-cloud-bigtable/tests/system/data/test_system_autogen.py
@@ -22,14 +22,15 @@ import uuid
 import pytest
 from google.api_core import retry
 from google.api_core.exceptions import ClientError, PermissionDenied
+from google.cloud.environment_vars import BIGTABLE_EMULATOR
+from google.type import date_pb2
+
 from google.cloud.bigtable.data._cross_sync import CrossSync
 from google.cloud.bigtable.data.execute_query.metadata import SqlType
 from google.cloud.bigtable.data.read_modify_write_rules import _MAX_INCREMENT_VALUE
 from google.cloud.bigtable_v2.services.bigtable.transports.grpc import (
     _LoggingClientInterceptor as GapicInterceptor,
 )
-from google.cloud.environment_vars import BIGTABLE_EMULATOR
-from google.type import date_pb2
 
 from . import TEST_AGGREGATE_FAMILY, TEST_FAMILY, TEST_FAMILY_2, SystemTestRunner
 
