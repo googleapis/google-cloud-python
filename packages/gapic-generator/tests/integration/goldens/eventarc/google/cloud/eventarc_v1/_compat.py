1e6b56ee70b05ac3c2c8203bc819a30794ce438a
Heba Alazzeh <hebaalazzeh@google.com>
2026-07-21 18:44:22 +0000

    docs(generator): update TODO issue link to #17813 in _compat.py

--- packages/gapic-generator/tests/integration/goldens/eventarc/google/cloud/eventarc_v1/_compat.py
+++ packages/gapic-generator/tests/integration/goldens/eventarc/google/cloud/eventarc_v1/_compat.py
@@ -19,7 +19,7 @@ import uuid
 try:
     from google.api_core.gapic_v1.requests import setup_request_id  # type: ignore
 except ImportError:  # pragma: NO COVER
-    # TODO(https://github.com/googleapis/google-cloud-python/issues/17812): Remove this fallback when google-api-core >= 2.26.0 is the minimum required version.
+    # TODO(https://github.com/googleapis/google-cloud-python/issues/17813): Remove this fallback when google-api-core >= 2.26.0 is the minimum required version.
     def setup_request_id(request, field_name: str, is_proto3_optional: bool):
         """Populate a UUID4 field in the request if it is not already set.
 
