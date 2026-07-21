016d00ea3bf2f67dbedaf350a5fda74062e21f45
Heba Alazzeh <hebaalazzeh@google.com>
2026-07-21 16:18:01 +0000

    feat(api-core): centralize endpoint routing and universe domain resolution

---
+++ packages/google-api-core/tests/unit/gapic/test_client_utils.py
@@ -0,0 +1,20 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.api_core.gapic_v1 import client_utils
+from google.api_core import universe
+
+
+def test_exports():
+    assert client_utils.get_universe_domain is universe.get_universe_domain
