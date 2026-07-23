77a80a8f2e26d407d6d5d988230935664c5422c1
Heba Alazzeh <hebaalazzeh@google.com>
2026-07-23 15:17:53 +0000

    chore(generator): revert setup.py and WORKSPACE changes

--- packages/gapic-generator/setup.py
+++ packages/gapic-generator/setup.py
@@ -61,7 +61,7 @@ setuptools.setup(
     author="Google LLC",
     author_email="googleapis-packages@google.com",
     license="Apache-2.0",
-    packages=setuptools.find_namespace_packages(exclude=["docs*", "tests*"]),
+    packages=setuptools.find_namespace_packages(exclude=["docs", "tests"]),
     url=url,
     classifiers=[
         release_status,
