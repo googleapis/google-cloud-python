527dcaee5c5e14e59ae4a165c15a0c62fd929561
Anthonios Partheniou <partheniou@google.com>
2026-07-30 17:59:32 +0000

    temporarily speed up feedback

--- ci/get_package_shards.py
+++ ci/get_package_shards.py
@@ -163,7 +163,7 @@ def group_packages(packages_map):
 
     # Dynamically determine target weight to balance across max shards.
     max_shards = int(os.environ.get("MAX_SHARDS", 16))
-    target_weight = max(10, math.ceil(total_weight / max_shards))
+    target_weight = max(1, math.ceil(total_weight / max_shards))
 
     shards_list = []
     current_shard_items = []
