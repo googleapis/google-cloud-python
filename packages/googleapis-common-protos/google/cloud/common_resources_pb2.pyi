6d501b3c28a7b7f1afb6ce93258f06f8e2fc6c6a
Heba Alazzeh <hebaalazzeh@google.com>
2026-08-01 00:13:09 +0000

    chore: remove temporary build staging files

--- packages/googleapis-common-protos/google/cloud/common_resources_pb2.pyi
+++
@@ -1,5 +0,0 @@
-from google.api import resource_pb2 as _resource_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
