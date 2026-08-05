c3bdf0971c1884aac6a4690834030583e87254e1
chalmer lowe <chalmerlowe@google.com>
2026-08-05 10:21:51 -0400

    chore(ci): add trigger comment to google-cloud-testutils to fire system tests

--- packages/google-cloud-testutils/setup.py
+++ packages/google-cloud-testutils/setup.py
@@ -1,4 +1,5 @@
 # Copyright 2017 Google LLC
+# Trigger parallel system test run
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +16,7 @@
 import io
 import os
 import re
+
 import setuptools  # type: ignore
 
 version = None
