c90c0b1b2f8ef50428d268e2132e3fe20871b0b8
agrawalradhika-cell <agrawalradhika@google.com>
2026-08-06 13:46:48 -0700

    chore: Modify mock return values in test_urllib3.py
    Updated mock return values in test cases to include None for additional parameters.

--- packages/google-auth/tests/transport/test_urllib3.py
+++ packages/google-auth/tests/transport/test_urllib3.py
@@ -536,7 +536,7 @@ class TestAuthorizedHttp(object):
         with mock.patch.object(
             google.auth.transport._mtls_helper,
             "call_client_cert_callback",
-            return_value=(new_cert, new_key),
+            return_value=(new_cert, new_key, None),
         ) as mock_callback:
             # non-mTLS endpoint is used
             result = authed_http.urlopen("GET", "http://example.googleapis.com")
@@ -584,7 +584,7 @@ class TestAuthorizedHttp(object):
         with mock.patch.object(
             google.auth.transport._mtls_helper,
             "check_parameters_for_unauthorized_response",
-            return_value=(new_cert, new_key, "old_fingerprint", "new_fingerprint"),
+            return_value=(new_cert, new_key, None, "old_fingerprint", "new_fingerprint"),
         ) as mock_check_params:
             with mock.patch.object(
                 authed_http,
