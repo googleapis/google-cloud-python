787c25a5cc1e08ffff8ca94a1f78929501706499
Atharva <attharva@google.com>
2026-08-06 20:12:24 +0000

    fix(auth): add defensive dictionary checks for loaded certificate configuration JSON

--- packages/google-auth/tests/transport/test__mtls_helper.py
+++ packages/google-auth/tests/transport/test__mtls_helper.py
@@ -503,6 +503,22 @@ class TestGetWorkloadCertAndKey(object):
     @mock.patch(
         "google.auth.transport._mtls_helper._get_cert_config_path", autospec=True
     )
+    @mock.patch("os.path.exists", autospec=True)
+    def test_malformed_json_returns_error(
+        self, mock_path_exists, mock_get_cert_config_path, mock_load_json_file
+    ):
+        mock_path_exists.return_value = True
+        mock_get_cert_config_path.return_value = "/path/to/cert"
+
+        for val in [None, [], "invalid_string"]:
+            mock_load_json_file.return_value = val
+            with pytest.raises(exceptions.ClientCertError):
+                _mtls_helper._get_workload_cert_and_key("")
+
+    @mock.patch("google.auth.transport._mtls_helper._load_json_file", autospec=True)
+    @mock.patch(
+        "google.auth.transport._mtls_helper._get_cert_config_path", autospec=True
+    )
     def test_no_workload(self, mock_get_cert_config_path, mock_load_json_file):
         mock_get_cert_config_path.return_value = "/path/to/cert"
         mock_load_json_file.return_value = {"cert_configs": {}}
