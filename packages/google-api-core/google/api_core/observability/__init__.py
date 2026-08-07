01372b6920f6d42a9e6fbeae48a78a08bfbe7f9b
chalmer lowe <chalmerlowe@google.com>
2026-08-07 09:20:40 -0400

    chore(api-core): remove custom interceptor prototype

--- packages/google-api-core/google/api_core/observability/__init__.py
+++ packages/google-api-core/google/api_core/observability/__init__.py
@@ -1,9 +1,1 @@
-try:
-    # Tell flake8 that it's okay this is unused, it's just being exposed to the package namespace.
-    from .tracing import OtelUnaryClientInterceptor  # noqa: F401
-
-    __all__ = [
-        "OtelUnaryClientInterceptor",
-    ]
-except ImportError:
-    __all__ = []
+__all__ = []
