236f493684f26187150f6f435003532bea8b16ff
Daniel Sanche <d.sanche14@gmail.com>
2026-08-07 16:43:07 -0700

    fixed handler setup in metrics tests

--- packages/google-cloud-bigtable/tests/system/data/test_metrics_autogen.py
+++ packages/google-cloud-bigtable/tests/system/data/test_metrics_autogen.py
@@ -161,8 +161,9 @@ class TestMetrics(SystemTestRunner):
         error_injector.clear()
 
     @pytest.fixture(scope="session")
-    def client(self, error_injector):
+    def client(self, error_injector, handler):
         with self._make_client() as client:
+            client._metrics.handlers = [handler]
             metrics_channel = client.transport._grpc_channel._channel._channel
             client.transport._grpc_channel._channel._channel = intercept_channel(
                 metrics_channel, error_injector
@@ -176,19 +177,15 @@ class TestMetrics(SystemTestRunner):
         builder.delete_rows()
 
     @pytest.fixture(scope="session")
-    def table(self, client, table_id, instance_id, handler):
+    def table(self, client, table_id, instance_id):
         with client.get_table(instance_id, table_id) as table:
-            table.client._metrics.handlers = [handler]
             yield table
 
     @pytest.fixture(scope="session")
-    def authorized_view(
-        self, client, table_id, instance_id, authorized_view_id, handler
-    ):
+    def authorized_view(self, client, table_id, instance_id, authorized_view_id):
         with client.get_authorized_view(
             instance_id, table_id, authorized_view_id
         ) as table:
-            table.client._metrics.add_handler(handler)
             yield table
 
     def test_read_rows(self, table, temp_rows, handler, cluster_config):
