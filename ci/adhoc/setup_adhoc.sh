4b9c4e03f14ab8cb9a214d13ccdab37153d2927d
Chalmer Lowe <chalmerlowe@google.com>
2026-08-07 15:18:58 -0400

    revise log statement

--- ci/adhoc/setup_adhoc.sh
+++ ci/adhoc/setup_adhoc.sh
@@ -40,7 +40,7 @@ RESET_EPOCH=$(echo "$HEADERS" | grep -i "^x-ratelimit-reset:" | awk '{print $2}'
 if [[ -n "$RESET_EPOCH" ]]; then
     if date -d "@$RESET_EPOCH" >/dev/null 2>&1; then
         RESET_HUMAN=$(date -d "@$RESET_EPOCH" "+%Y-%m-%d %H:%M:%S UTC")
-        echo "DEBUG: X-RateLimit-Reset: $RESET_HUMAN ($RESET_EPOCH)"
+        echo "x-ratelimit-reset: $RESET_HUMAN ($RESET_EPOCH)" 
     else
         echo "DEBUG: X-RateLimit-Reset: $RESET_EPOCH"
     fi
