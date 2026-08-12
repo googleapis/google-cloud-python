9b8dfde8eb7b1128a4f4bc0848ae27fb6f536da8
Anthonios Partheniou <partheniou@google.com>
2026-08-12 20:07:25 +0000

    tests: subtle fix to compute system test

--- packages/google-cloud-compute/tests/system/test_smoke.py
+++ packages/google-cloud-compute/tests/system/test_smoke.py
@@ -80,9 +80,7 @@ class TestComputeSmoke(TestBase):
             self.client.get(instance=self.name, zone=0)
         self.assertIn(
             (
-                "0 has type int, but expected one of: bytes, unicode"
-                if PROTOBUF_VERSION[0] == "3"
-                else "('bad argument type for built-in operation',)"
+                "bad argument type for built-in operation"
             ),
             str(ex.exception.args),
         )
