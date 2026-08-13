c033f72fada3d0b3a944e16a919b37de6afaf2aa
ohmayr <omairn@google.com>
2026-08-13 18:58:42 +0000

    chore(librarian): regenerate bigtable

--- packages/google-cloud-bigtable/tests/unit/gapic/bigtable_admin_v2/test_bigtable_instance_admin.py
+++ packages/google-cloud-bigtable/tests/unit/gapic/bigtable_admin_v2/test_bigtable_instance_admin.py
@@ -1021,14 +1021,7 @@ def test_bigtable_instance_admin_client_get_mtls_endpoint_and_cert_source(client
                 config_filename = "mock_certificate_config.json"
                 config_file_content = json.dumps(config_data)
                 m = mock.mock_open(read_data=config_file_content)
-                with (
-                    mock.patch("builtins.open", m),
-                    mock.patch(
-                        "os.path.exists",
-                        side_effect=lambda path: os.path.basename(path)
-                        == config_filename,
-                    ),
-                ):
+                with mock.patch("builtins.open", m):
                     with mock.patch.dict(
                         os.environ, {"GOOGLE_API_CERTIFICATE_CONFIG": config_filename}
                     ):
@@ -1075,14 +1068,7 @@ def test_bigtable_instance_admin_client_get_mtls_endpoint_and_cert_source(client
                 config_filename = "mock_certificate_config.json"
                 config_file_content = json.dumps(config_data)
                 m = mock.mock_open(read_data=config_file_content)
-                with (
-                    mock.patch("builtins.open", m),
-                    mock.patch(
-                        "os.path.exists",
-                        side_effect=lambda path: os.path.basename(path)
-                        == config_filename,
-                    ),
-                ):
+                with mock.patch("builtins.open", m):
                     with mock.patch.dict(
                         os.environ, {"GOOGLE_API_CERTIFICATE_CONFIG": config_filename}
                     ):
