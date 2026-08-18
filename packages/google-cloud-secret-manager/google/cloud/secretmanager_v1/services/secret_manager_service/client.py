e77ec272364689ba1a5fec775b4139a14a1e3827
chalmer lowe <chalmerlowe@google.com>
2026-08-18 08:10:13 -0400

    feat(secretmanager): inject OTel interceptor explicitly in gRPC transport

--- packages/google-cloud-secret-manager/google/cloud/secretmanager_v1/services/secret_manager_service/client.py
+++ packages/google-cloud-secret-manager/google/cloud/secretmanager_v1/services/secret_manager_service/client.py
@@ -35,17 +35,16 @@ from typing import (
 )
 
 import google.protobuf
+from google.api_core import _feature_gating_helpers, gapic_v1
 from google.api_core import client_options as client_options_lib
 from google.api_core import exceptions as core_exceptions
-from google.api_core import gapic_v1
 from google.api_core import retry as retries
 from google.auth import credentials as ga_credentials  # type: ignore
 from google.auth.exceptions import MutualTLSChannelError  # type: ignore
 from google.auth.transport import mtls  # type: ignore
 from google.auth.transport.grpc import SslCredentials  # type: ignore
-from google.oauth2 import service_account  # type: ignore
-
 from google.cloud.secretmanager_v1 import gapic_version as package_version
+from google.oauth2 import service_account  # type: ignore
 
 try:
     OptionalRetry = Union[retries.Retry, gapic_v1.method._MethodDefault, None]
@@ -68,7 +67,6 @@ import google.protobuf.duration_pb2 as duration_pb2  # type: ignore
 import google.protobuf.field_mask_pb2 as field_mask_pb2  # type: ignore
 import google.protobuf.timestamp_pb2 as timestamp_pb2  # type: ignore
 from google.cloud.location import locations_pb2  # type: ignore
-
 from google.cloud.secretmanager_v1.services.secret_manager_service import pagers
 from google.cloud.secretmanager_v1.types import resources, service
 
@@ -745,18 +743,39 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
                 if isinstance(transport, str) or transport is None
                 else cast(Callable[..., SecretManagerServiceTransport], transport)
             )
+            # Resolve interceptors for gRPC
+            interceptors = None
+            if transport_init is SecretManagerServiceGrpcTransport:
+                is_tracing_enabled = _feature_gating_helpers.resolve_feature_flags(
+                    env_var="GOOGLE_CLOUD_PYTHON_TRACING_ENABLED",
+                    feature_key="tracer_provider",
+                    configuration=self._client_options,
+                )
+                if is_tracing_enabled:
+                    try:
+                        import opentelemetry.instrumentation.grpc as otel_grpc  # type: ignore[import-not-found]
+                        tracer_provider = getattr(self._client_options, "tracer_provider", None)
+                        interceptor = otel_grpc.client_interceptor(tracer_provider=tracer_provider)
+                        interceptors = [interceptor]
+                    except ImportError:
+                        pass
+
             # initialize with the provided callable or the passed in class
-            self._transport = transport_init(
-                credentials=credentials,
-                credentials_file=self._client_options.credentials_file,
-                host=self._api_endpoint,
-                scopes=self._client_options.scopes,
-                client_cert_source_for_mtls=self._client_cert_source,
-                quota_project_id=self._client_options.quota_project_id,
-                client_info=client_info,
-                always_use_jwt_access=True,
-                api_audience=self._client_options.api_audience,
-            )
+            transport_kwargs = {
+                "credentials": credentials,
+                "credentials_file": self._client_options.credentials_file,
+                "host": self._api_endpoint,
+                "scopes": self._client_options.scopes,
+                "client_cert_source_for_mtls": self._client_cert_source,
+                "quota_project_id": self._client_options.quota_project_id,
+                "client_info": client_info,
+                "always_use_jwt_access": True,
+                "api_audience": self._client_options.api_audience,
+            }
+            if interceptors is not None:
+                transport_kwargs["interceptors"] = interceptors
+
+            self._transport = transport_init(**transport_kwargs)
 
         if "async" not in str(self._transport):
             if CLIENT_LOGGING_SUPPORTED and _LOGGER.isEnabledFor(
