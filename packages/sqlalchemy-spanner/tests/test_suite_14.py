965ef97301ea7167cc8bb607f1c3e6ccb7933c07
chalmer lowe <chalmerlowe@google.com>
2026-08-20 09:41:50 -0400

    fix(sqlalchemy-spanner): use literal_round_trip fixture to avoid literal_execute TypeError

--- packages/sqlalchemy-spanner/tests/test_suite_14.py
+++ packages/sqlalchemy-spanner/tests/test_suite_14.py
@@ -1576,7 +1576,7 @@ class NumericTest(_NumericTest):
         return run
 
     @emits_warning(r".*does \*not\* support Decimal objects natively")
-    def test_render_literal_numeric(self, literal_round_trip_spanner):
+    def test_render_literal_numeric(self, literal_round_trip):
         """
         SPANNER OVERRIDE:
 
@@ -1585,14 +1585,14 @@ class NumericTest(_NumericTest):
         following insertions will fail with `Row [] already exists".
         Overriding the test to avoid the same failure.
         """
-        literal_round_trip_spanner(
+        literal_round_trip(
             Numeric(precision=8, scale=4),
             [decimal.Decimal("15.7563")],
             [decimal.Decimal("15.7563")],
         )
 
     @emits_warning(r".*does \*not\* support Decimal objects natively")
-    def test_render_literal_numeric_asfloat(self, literal_round_trip_spanner):
+    def test_render_literal_numeric_asfloat(self, literal_round_trip):
         """
         SPANNER OVERRIDE:
 
@@ -1601,13 +1601,13 @@ class NumericTest(_NumericTest):
         following insertions will fail with `Row [] already exists".
         Overriding the test to avoid the same failure.
         """
-        literal_round_trip_spanner(
+        literal_round_trip(
             Numeric(precision=8, scale=4, asdecimal=False),
             [decimal.Decimal("15.7563")],
             [15.7563],
         )
 
-    def test_render_literal_float(self, literal_round_trip_spanner):
+    def test_render_literal_float(self, literal_round_trip):
         """
         SPANNER OVERRIDE:
 
@@ -1616,7 +1616,7 @@ class NumericTest(_NumericTest):
         following insertions will fail with `Row [] already exists".
         Overriding the test to avoid the same failure.
         """
-        literal_round_trip_spanner(
+        literal_round_trip(
             Float(4),
             [decimal.Decimal("15.7563")],
             [15.7563],
