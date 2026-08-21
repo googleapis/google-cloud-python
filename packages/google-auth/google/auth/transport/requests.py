b42122e2eed2b8cea3cab6337d52379de7c9c5aa
attharva-24 <attharva@google.com>
2026-08-21 10:49:14 -0700

    Revert "fix(auth): parse hostname for mTLS and PSC endpoint certifica… (#18189)
    (#18153)
    
    This reverts commit b6423734a2470acd01a7081a4133e4373206f325.

--- packages/google-auth/google/auth/transport/requests.py
+++ packages/google-auth/google/auth/transport/requests.py
@@ -647,7 +647,13 @@ class AuthorizedSession(requests.Session):
         ):
             # Handle unauthorized permission error(401 status code)
             if response.status_code == http_client.UNAUTHORIZED:
-                use_mtls = self.is_mtls and _mtls_helper.is_mtls_endpoint(url)
+                MTLS_URL_PREFIXES = [
+                    "mtls.googleapis.com",
+                    "mtls.sandbox.googleapis.com",
+                ]
+                use_mtls = self.is_mtls and any(
+                    prefix in url for prefix in MTLS_URL_PREFIXES
+                )
                 if use_mtls:
                     (
                         call_cert_bytes,
