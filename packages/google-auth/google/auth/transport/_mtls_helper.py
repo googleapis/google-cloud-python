6687039bcd8928ce489fe60f0a04f7f8b445f5af
agrawalradhika-cell <agrawalradhika@google.com>
2026-09-06 17:50:56 -0700

    Removed the client_cert_callback parameter from the check_parameters_for_unauthorized_response function and adjusted the implementation accordingly.
    Removed the client_cert_callback parameter from the check_parameters_for_unauthorized_response function and adjusted the implementation accordingly.

--- packages/google-auth/google/auth/transport/_mtls_helper.py
+++ packages/google-auth/google/auth/transport/_mtls_helper.py
@@ -808,13 +808,11 @@ def check_use_client_cert():
     return False
 
 
-def check_parameters_for_unauthorized_response(cached_cert, client_cert_callback=None):
+def check_parameters_for_unauthorized_response(cached_cert):
     """Returns the cached and current cert fingerprint for reconfiguring mTLS.
 
     Args:
         cached_cert(bytes): The cached client certificate.
-        client_cert_callback(Optional[Callable[[], (bytes, bytes)]]):
-            The optional callback that returns the client certificate and private key bytes.
 
     Returns:
         bytes: The client callback cert bytes.
@@ -822,10 +820,7 @@ def check_parameters_for_unauthorized_response(cached_cert, client_cert_callback
         str: The base64-encoded SHA256 cached fingerprint.
         str: The base64-encoded SHA256 current cert fingerprint.
     """
-    if client_cert_callback:
-        call_cert_bytes, call_key_bytes = client_cert_callback()
-    else:
-        call_cert_bytes, call_key_bytes = call_client_cert_callback()
+    call_cert_bytes, call_key_bytes = call_client_cert_callback()
     cert_obj = _agent_identity_utils.parse_certificate(call_cert_bytes)
     current_cert_fingerprint = _agent_identity_utils.calculate_certificate_fingerprint(
         cert_obj
