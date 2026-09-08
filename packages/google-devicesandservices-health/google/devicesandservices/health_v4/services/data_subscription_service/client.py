2d3de6d32e0515695e38375e7196dddf4b2eb569
Tomo Suzuki <suztomo@gmail.com>
2026-09-08 18:55:48 +0000

    chore: update librarian version to v0.42.0

--- packages/google-devicesandservices-health/google/devicesandservices/health_v4/services/data_subscription_service/client.py
+++ packages/google-devicesandservices-health/google/devicesandservices/health_v4/services/data_subscription_service/client.py
@@ -781,10 +781,9 @@ class DataSubscriptionServiceClient(metaclass=DataSubscriptionServiceClientMeta)
             google.api_core.operation.Operation:
                 An object representing a long-running operation.
 
-                The result type for the operation will be
-                :class:`google.devicesandservices.health_v4.types.Subscriber`
-                -- Resource Messages -- A subscriber receives
-                notifications from Google Health API.
+                The result type for the operation will be :class:`google.devicesandservices.health_v4.types.Subscriber` -- Resource Messages --
+                   A subscriber receives notifications from Google
+                   Health API.
 
         """
         # Create or coerce a protobuf request object.
@@ -1076,10 +1075,9 @@ class DataSubscriptionServiceClient(metaclass=DataSubscriptionServiceClientMeta)
             google.api_core.operation.Operation:
                 An object representing a long-running operation.
 
-                The result type for the operation will be
-                :class:`google.devicesandservices.health_v4.types.Subscriber`
-                -- Resource Messages -- A subscriber receives
-                notifications from Google Health API.
+                The result type for the operation will be :class:`google.devicesandservices.health_v4.types.Subscriber` -- Resource Messages --
+                   A subscriber receives notifications from Google
+                   Health API.
 
         """
         # Create or coerce a protobuf request object.
