f05ed9e87b76da9d2d6b821be3a108db53c77940
ohmayr <omairn@google.com>
2026-09-16 00:44:25 +0000

    feat(firestore): add BSONInt32 support

--- packages/google-cloud-firestore/tests/system/test_system_async.py
+++ packages/google-cloud-firestore/tests/system/test_system_async.py
@@ -51,7 +51,12 @@ from test__helpers import (
 from google.cloud import firestore_v1 as firestore
 from google.cloud.firestore_v1.base_query import And, FieldFilter, Or
 from google.cloud.firestore_v1.base_vector_query import DistanceMeasure
-from google.cloud.firestore_v1.bson import BSONMaxKey, BSONMinKey, BSONObjectId
+from google.cloud.firestore_v1.bson import (
+    BSONInt32,
+    BSONMaxKey,
+    BSONMinKey,
+    BSONObjectId,
+)
 from google.cloud.firestore_v1.query_profile import (
     ExecutionStats,
     ExplainMetrics,
@@ -1259,6 +1264,7 @@ async def test_async_bson_document_writes(client, cleanup, database):
         "user_id": BSONObjectId("507f191e810c19729de860ea"),
         "min_key": BSONMinKey(),
         "max_key": BSONMaxKey(),
+        "int32_val": BSONInt32(42),
     }
 
     await doc_ref.set(bson_payload)
@@ -1269,6 +1275,7 @@ async def test_async_bson_document_writes(client, cleanup, database):
         "user_id": {"__oid__": "507f191e810c19729de860ea"},
         "min_key": {"__min__": None},
         "max_key": {"__max__": None},
+        "int32_val": {"__int__": 42},
     }
 
 
