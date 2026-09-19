6aae668f03406d820dbda5c689cbf2feab82fe96
Daniel Sanche <d.sanche14@gmail.com>
2026-09-18 17:47:59 -0700

    strip index in batcher exception

--- packages/google-cloud-bigtable/google/cloud/bigtable/data/_sync_autogen/mutations_batcher.py
+++ packages/google-cloud-bigtable/google/cloud/bigtable/data/_sync_autogen/mutations_batcher.py
@@ -503,6 +503,7 @@ class MutationsBatcher:
                         exc.index = None
                     exceptions.extend(exc_list)
             except FailedMutationEntryError as e:
+                e.index = None
                 exceptions.append(e)
             except Exception as e:
                 exceptions.extend(
