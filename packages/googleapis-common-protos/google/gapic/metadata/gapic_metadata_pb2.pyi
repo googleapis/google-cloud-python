2cf3bcd900e5c44d0a4b99d41fe8a32d5406ea0b
Anthonios Partheniou <partheniou@google.com>
2026-09-24 19:25:39 +0000

    nox -s format

--- packages/googleapis-common-protos/google/gapic/metadata/gapic_metadata_pb2.pyi
+++ packages/googleapis-common-protos/google/gapic/metadata/gapic_metadata_pb2.pyi
@@ -12,23 +12,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from google.protobuf.internal import containers as _containers
+from collections.abc import Iterable as _Iterable
+from collections.abc import Mapping as _Mapping
+from typing import ClassVar as _ClassVar
+from typing import Optional as _Optional
+from typing import Union as _Union
+
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from collections.abc import Iterable as _Iterable, Mapping as _Mapping
-from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+from google.protobuf.internal import containers as _containers
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class GapicMetadata(_message.Message):
-    __slots__ = ("schema", "comment", "language", "proto_package", "library_package", "services")
+    __slots__ = (
+        "schema",
+        "comment",
+        "language",
+        "proto_package",
+        "library_package",
+        "services",
+    )
     class ServicesEntry(_message.Message):
         __slots__ = ("key", "value")
         KEY_FIELD_NUMBER: _ClassVar[int]
         VALUE_FIELD_NUMBER: _ClassVar[int]
         key: str
         value: GapicMetadata.ServiceForTransport
-        def __init__(self, key: _Optional[str] = ..., value: _Optional[_Union[GapicMetadata.ServiceForTransport, _Mapping]] = ...) -> None: ...
+        def __init__(
+            self,
+            key: _Optional[str] = ...,
+            value: _Optional[_Union[GapicMetadata.ServiceForTransport, _Mapping]] = ...,
+        ) -> None: ...
+
     class ServiceForTransport(_message.Message):
         __slots__ = ("clients", "api_version")
         class ClientsEntry(_message.Message):
@@ -37,12 +53,22 @@ class GapicMetadata(_message.Message):
             VALUE_FIELD_NUMBER: _ClassVar[int]
             key: str
             value: GapicMetadata.ServiceAsClient
-            def __init__(self, key: _Optional[str] = ..., value: _Optional[_Union[GapicMetadata.ServiceAsClient, _Mapping]] = ...) -> None: ...
+            def __init__(
+                self,
+                key: _Optional[str] = ...,
+                value: _Optional[_Union[GapicMetadata.ServiceAsClient, _Mapping]] = ...,
+            ) -> None: ...
+
         CLIENTS_FIELD_NUMBER: _ClassVar[int]
         API_VERSION_FIELD_NUMBER: _ClassVar[int]
         clients: _containers.MessageMap[str, GapicMetadata.ServiceAsClient]
         api_version: str
-        def __init__(self, clients: _Optional[_Mapping[str, GapicMetadata.ServiceAsClient]] = ..., api_version: _Optional[str] = ...) -> None: ...
+        def __init__(
+            self,
+            clients: _Optional[_Mapping[str, GapicMetadata.ServiceAsClient]] = ...,
+            api_version: _Optional[str] = ...,
+        ) -> None: ...
+
     class ServiceAsClient(_message.Message):
         __slots__ = ("library_client", "rpcs")
         class RpcsEntry(_message.Message):
@@ -51,17 +77,28 @@ class GapicMetadata(_message.Message):
             VALUE_FIELD_NUMBER: _ClassVar[int]
             key: str
             value: GapicMetadata.MethodList
-            def __init__(self, key: _Optional[str] = ..., value: _Optional[_Union[GapicMetadata.MethodList, _Mapping]] = ...) -> None: ...
+            def __init__(
+                self,
+                key: _Optional[str] = ...,
+                value: _Optional[_Union[GapicMetadata.MethodList, _Mapping]] = ...,
+            ) -> None: ...
+
         LIBRARY_CLIENT_FIELD_NUMBER: _ClassVar[int]
         RPCS_FIELD_NUMBER: _ClassVar[int]
         library_client: str
         rpcs: _containers.MessageMap[str, GapicMetadata.MethodList]
-        def __init__(self, library_client: _Optional[str] = ..., rpcs: _Optional[_Mapping[str, GapicMetadata.MethodList]] = ...) -> None: ...
+        def __init__(
+            self,
+            library_client: _Optional[str] = ...,
+            rpcs: _Optional[_Mapping[str, GapicMetadata.MethodList]] = ...,
+        ) -> None: ...
+
     class MethodList(_message.Message):
         __slots__ = ("methods",)
         METHODS_FIELD_NUMBER: _ClassVar[int]
         methods: _containers.RepeatedScalarFieldContainer[str]
         def __init__(self, methods: _Optional[_Iterable[str]] = ...) -> None: ...
+
     SCHEMA_FIELD_NUMBER: _ClassVar[int]
     COMMENT_FIELD_NUMBER: _ClassVar[int]
     LANGUAGE_FIELD_NUMBER: _ClassVar[int]
@@ -74,4 +111,12 @@ class GapicMetadata(_message.Message):
     proto_package: str
     library_package: str
     services: _containers.MessageMap[str, GapicMetadata.ServiceForTransport]
-    def __init__(self, schema: _Optional[str] = ..., comment: _Optional[str] = ..., language: _Optional[str] = ..., proto_package: _Optional[str] = ..., library_package: _Optional[str] = ..., services: _Optional[_Mapping[str, GapicMetadata.ServiceForTransport]] = ...) -> None: ...
+    def __init__(
+        self,
+        schema: _Optional[str] = ...,
+        comment: _Optional[str] = ...,
+        language: _Optional[str] = ...,
+        proto_package: _Optional[str] = ...,
+        library_package: _Optional[str] = ...,
+        services: _Optional[_Mapping[str, GapicMetadata.ServiceForTransport]] = ...,
+    ) -> None: ...
--- packages/googleapis-common-protos/google/longrunning/operations_proto_pb2.pyi
+++ packages/googleapis-common-protos/google/longrunning/operations_proto_pb2.pyi
@@ -13,20 +13,24 @@
 # limitations under the License.
 
 import datetime
+from collections.abc import Iterable as _Iterable
+from collections.abc import Mapping as _Mapping
+from typing import ClassVar as _ClassVar
+from typing import Optional as _Optional
+from typing import Union as _Union
+
+from google.protobuf import any_pb2 as _any_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pb2 as _descriptor_pb2
+from google.protobuf import duration_pb2 as _duration_pb2
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import message as _message
+from google.protobuf.internal import containers as _containers
 
 from google.api import annotations_pb2 as _annotations_pb2
 from google.api import client_pb2 as _client_pb2
 from google.api import field_behavior_pb2 as _field_behavior_pb2
-from google.protobuf import any_pb2 as _any_pb2
-from google.protobuf import descriptor_pb2 as _descriptor_pb2
-from google.protobuf import duration_pb2 as _duration_pb2
-from google.protobuf import empty_pb2 as _empty_pb2
 from google.rpc import status_pb2 as _status_pb2
-from google.protobuf.internal import containers as _containers
-from google.protobuf import descriptor as _descriptor
-from google.protobuf import message as _message
-from collections.abc import Iterable as _Iterable, Mapping as _Mapping
-from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 OPERATION_INFO_FIELD_NUMBER: _ClassVar[int]
@@ -44,7 +48,14 @@ class Operation(_message.Message):
     done: bool
     error: _status_pb2.Status
     response: _any_pb2.Any
-    def __init__(self, name: _Optional[str] = ..., metadata: _Optional[_Union[_any_pb2.Any, _Mapping]] = ..., done: _Optional[bool] = ..., error: _Optional[_Union[_status_pb2.Status, _Mapping]] = ..., response: _Optional[_Union[_any_pb2.Any, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        name: _Optional[str] = ...,
+        metadata: _Optional[_Union[_any_pb2.Any, _Mapping]] = ...,
+        done: _Optional[bool] = ...,
+        error: _Optional[_Union[_status_pb2.Status, _Mapping]] = ...,
+        response: _Optional[_Union[_any_pb2.Any, _Mapping]] = ...,
+    ) -> None: ...
 
 class GetOperationRequest(_message.Message):
     __slots__ = ("name",)
@@ -64,7 +75,14 @@ class ListOperationsRequest(_message.Message):
     page_size: int
     page_token: str
     return_partial_success: bool
-    def __init__(self, name: _Optional[str] = ..., filter: _Optional[str] = ..., page_size: _Optional[int] = ..., page_token: _Optional[str] = ..., return_partial_success: _Optional[bool] = ...) -> None: ...
+    def __init__(
+        self,
+        name: _Optional[str] = ...,
+        filter: _Optional[str] = ...,
+        page_size: _Optional[int] = ...,
+        page_token: _Optional[str] = ...,
+        return_partial_success: _Optional[bool] = ...,
+    ) -> None: ...
 
 class ListOperationsResponse(_message.Message):
     __slots__ = ("operations", "next_page_token", "unreachable")
@@ -74,7 +92,12 @@ class ListOperationsResponse(_message.Message):
     operations: _containers.RepeatedCompositeFieldContainer[Operation]
     next_page_token: str
     unreachable: _containers.RepeatedScalarFieldContainer[str]
-    def __init__(self, operations: _Optional[_Iterable[_Union[Operation, _Mapping]]] = ..., next_page_token: _Optional[str] = ..., unreachable: _Optional[_Iterable[str]] = ...) -> None: ...
+    def __init__(
+        self,
+        operations: _Optional[_Iterable[_Union[Operation, _Mapping]]] = ...,
+        next_page_token: _Optional[str] = ...,
+        unreachable: _Optional[_Iterable[str]] = ...,
+    ) -> None: ...
 
 class CancelOperationRequest(_message.Message):
     __slots__ = ("name",)
@@ -94,7 +117,13 @@ class WaitOperationRequest(_message.Message):
     TIMEOUT_FIELD_NUMBER: _ClassVar[int]
     name: str
     timeout: _duration_pb2.Duration
-    def __init__(self, name: _Optional[str] = ..., timeout: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        name: _Optional[str] = ...,
+        timeout: _Optional[
+            _Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]
+        ] = ...,
+    ) -> None: ...
 
 class OperationInfo(_message.Message):
     __slots__ = ("response_type", "metadata_type")
@@ -102,4 +131,6 @@ class OperationInfo(_message.Message):
     METADATA_TYPE_FIELD_NUMBER: _ClassVar[int]
     response_type: str
     metadata_type: str
-    def __init__(self, response_type: _Optional[str] = ..., metadata_type: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self, response_type: _Optional[str] = ..., metadata_type: _Optional[str] = ...
+    ) -> None: ...
