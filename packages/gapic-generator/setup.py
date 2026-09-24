8e9acb17fae87a8b754d3059e1e5c92a982b1bfd
Anthonios Partheniou <partheniou@google.com>
2026-09-24 17:41:30 +0000

    Revert "revert"
    This reverts commit eaba74ba4c53c0838195b11d41eddde9f46a22f4.

--- packages/gapic-generator/setup.py
+++ packages/gapic-generator/setup.py
@@ -65,6 +65,7 @@ setuptools.setup(
     author_email="googleapis-packages@google.com",
     license="Apache-2.0",
     packages=setuptools.find_namespace_packages(
+        include=["gapic", "gapic.*"],
         exclude=["docs*", "tests*", "testing*", ".*", "bazel*", "rules_python_gapic*"]
     ),
     url=url,
